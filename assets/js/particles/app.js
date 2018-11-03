09c72a545ee4cde295c9c71e31d017cfc02b1677
Lee Calcote <leecalcote@gmail.com>
2018-11-03 18:16:59 +0000

    particles

--- assets/js/particles/app.js
+++ assets/js/particles/app.js
@@ -109,7 +109,7 @@ particlesJS('particles-js',
           "speed": 3
         },
         "repulse": {
-          "distance": 200
+          "distance": 100
         },
         "push": {
           "particles_nb": 4
