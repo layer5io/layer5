34335f50ae5aacfa11312d9f42a5081fb9206bfc
Tanuj22 <agarwal.5@iitj.ac.in>
2020-07-19 14:26:02 +0530

    add basic config for mdx files
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- layer5-ng/gatsby-config.js
+++ layer5-ng/gatsby-config.js
@@ -19,10 +19,23 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `posts`,
       },
     },
     `gatsby-transformer-sharp`,
---
+++ layer5-ng/src/posts/2020/2020-06-01-hello-world/index.mdx
@@ -0,0 +1,13 @@
+---
+title: Hello World - from mdx!
+date: 2019-06-01
+published: true
+---
+
+# h1 Heading
+
+My first post!!
+
+## h2 Heading
+
+### h3 Heading
