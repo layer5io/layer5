d58172b498c96b54f47064b0a38731f9d4910e56
Kush Trivedi <44091822+kushthedude@users.noreply.github.com>
2020-07-21 05:30:30 +0530

    test

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,4 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+
 
 import React from "react"
 import PropTypes from "prop-types"
