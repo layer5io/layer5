421ad42ab3842ffd21db97daec9a74bdf2e23f7c
Tanuj22 <agarwal.5@iitj.ac.in>
2020-08-03 01:50:06 +0530

    remove reduntant code
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>
    Signed-off-by: kishore007k <kishorekumarstar07@gmail.com>

--- root-wrapper.js
+++ root-wrapper.js
@@ -3,12 +3,6 @@ import { MDXProvider} from "@mdx-js/react";
 import Code from './src/components/CodeBlock'
 
 const components = {
-    h2: ({ children }) => (
-        <h2 style={{ color: 'rebeccapurple' }}>{children}</h2>
-    ),
-    'p.inlineCode': props => (
-        <code style={{ backgroundColor: 'lightgray' }} {...props} />
-    ),
     pre: ({ children: { props } }) => {
         if (props.mdxType === 'code') {
             return (
