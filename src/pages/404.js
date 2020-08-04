c2c0b24c9eb023b0966825134df74c4c60b571a1
Jashpatel1 <patel.5@iitj.ac.in>
2020-08-04 16:26:42 +0530

    added basic 404 design
    Signed-off-by: Jashpatel1 <patel.5@iitj.ac.in>

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
+import Navigation from "../sections/Navigation";
+import L404 from "../sections/404";
+import Footer from "../sections/Footer";
 
+import { GlobalStyle } from "../sections/app.style";
+import theme from "../theme/hosting/themeStyles";
 
-const NotFoundPage = () => (
-    <Layout>
-        <SEO title="404: What a mesh!" />
-        <h2>Oh, no! What a mesh.</h2>
-    </Layout>
+const NotFoundPage = () => (   
+    <ThemeProvider theme={theme}>
+        <Layout>
+            <GlobalStyle />
+            <SEO title="404: What a mesh!" />
+            <Navigation />
+            <L404 />
+            <Footer />  
+        </Layout>
+    </ThemeProvider>
 );
 
 export default NotFoundPage;
