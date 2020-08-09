d3eaa920982ad2733617b86fd963688cfc22c4aa
Tanuj22 <agarwal.5@iitj.ac.in>
2020-08-09 22:28:01 +0530

    add stufigy function
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+const slugify = function(text) {
+    return text
+        .toString()
+        .toLowerCase()
+        .replace(/\s+/g, '-')       // Replace spaces with -
+        .replace(/[^\w-]+/g, '')    // Remove all non-word chars
+        .replace(/--+/g, '-')       // Replace multiple - with single -
+        .replace(/^-+/, '')         // Trim - from start of text
+        .replace(/-+$/, '')         // Trim - from end of text
+};
+
+module.exports = { slugify };
