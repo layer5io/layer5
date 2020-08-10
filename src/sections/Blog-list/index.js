d712c87773b541039e337cc7b0fbbc93527eaaf0
Tanuj22 <agarwal.5@iitj.ac.in>
2020-08-10 20:13:34 +0530

    add blog list page for each tage filtering
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>
    Signed-off-by: kishore007k <kishorekumarstar07@gmail.com>

--- src/sections/Blog-list/index.js
+++ src/sections/Blog-list/index.js
@@ -7,10 +7,11 @@ import PageHeader from "../../reusecore/PageHeader";
 import Sidebar from "../Blog-sidebar";
 
 import blogThumb2 from "../../assets/images/blog/post/blog-post-2.png";
+import Image from "../../components/image";
 
 import { BlogPageWrapper } from "./blogList.style";
 
-const BlogList = () => {
+const BlogList = ({posts}) => {
     return (
         <BlogPageWrapper>
             <PageHeader title="Blog List" />
@@ -19,97 +20,37 @@ const BlogList = () => {
                     <Row>
                         <Col sm={12} md={8}>
                             <Row>
-                                <Col xs={12}>
-                                    <div className="post-block list">
-                                        <div className="post-thumb-block">
-                                            <img src={blogThumb2} alt="prime blog page" />
-                                            <div className="post-meta">
+                                {posts.map(({id, frontmatter, fields}) => (
+                                    <Col xs={12} key={id}>
+                                        <div className="post-block list">
+                                            <div className="post-thumb-block">
+                                                <Image {...frontmatter.thumbnail} imgStyle={{ objectFit: 'contain' }}/>
+                                                <div className="post-meta">
                                                 <span>
                                                     <em> 15 </em> SEP
                                                 </span>
+                                                </div>
                                             </div>
+                                            <h2 className="post-title">
+                                                <Link to={fields.slug}>
+                                                    {frontmatter.title}
+                                                </Link>
+                                            </h2>
+                                            <p className="post-entry">
+                                                Readable content of a page when looking at its layout. The
+                                                point of using Lorem Ipsum is that it has a more-or-less
+                                                normal distribution of letters, as opposed to using
+                                                'Content here, content here', making it look like readable
+                                                English. Many desktop publishing packages and web page
+                                                editors now use
+                                            </p>
+
+                                            <Link to={fields.slug} className="readmore-btn">
+                                                see more <IoIosArrowRoundForward />
+                                            </Link>
                                         </div>
-                                        <h2 className="post-title">
-                                            <Link to="#">
-                        It is a long established fact that
-                                            </Link>
-                                        </h2>
-                                        <p className="post-entry">
-                      Readable content of a page when looking at its layout. The
-                      point of using Lorem Ipsum is that it has a more-or-less
-                      normal distribution of letters, as opposed to using
-                      'Content here, content here', making it look like readable
-                      English. Many desktop publishing packages and web page
-                      editors now use
-                                        </p>
-
-                                        <Link to="#" className="readmore-btn">
-                      see more <IoIosArrowRoundForward />
-                                        </Link>
-                                    </div>
-                                </Col>
-                                <Col xs={12}>
-                                    <div className="post-block list text-only">
-                                        <h2 className="post-title">
-                                            <Link to="#">
-                        Friendship contrasted solicitude insipidity in
-                        introduced literature it.
-                                            </Link>
-                                        </h2>
-                                        <p className="post-entry">
-                      Readable content of a page when looking at its layout. The
-                      point of using Lorem Ipsum is that it has a more-or-less
-                      normal distribution of letters, as opposed to using
-                      'Content here, content here', making it look like readable
-                      English. Many desktop publishing packages and web page
-                      editors now use
-                                        </p>
-
-                                        <Link to="#" className="readmore-btn">
-                      see more <IoIosArrowRoundForward />
-                                        </Link>
-                                    </div>
-                                </Col>
-                                <Col xs={12}>
-                                    <div className="post-block list qutoe-post">
-                                        <h2 className="post-title">
-                                            <Link to="#">
-                        Friendship contrasted solicitude insipidity in
-                        introduced literature it. He seemed denote except as
-                        oppose do spring my.
-                                            </Link>
-                                        </h2>
-                                    </div>
-                                </Col>
-                                <Col xs={12}>
-                                    <div className="post-block list">
-                                        <div className="post-thumb-block">
-                                            <img src={blogThumb2} alt="prime blog page" />
-                                            <div className="post-meta">
-                                                <span>
-                                                    <em> 15 </em> SEP
-                                                </span>
-                                            </div>
-                                        </div>
-                                        <h2 className="post-title">
-                                            <Link to="#">
-                        It is a long established fact that
-                                            </Link>
-                                        </h2>
-                                        <p className="post-entry">
-                      Readable content of a page when looking at its layout. The
-                      point of using Lorem Ipsum is that it has a more-or-less
-                      normal distribution of letters, as opposed to using
-                      'Content here, content here', making it look like readable
-                      English. Many desktop publishing packages and web page
-                      editors now use
-                                        </p>
-
-                                        <Link to="#" className="readmore-btn">
-                      see more <IoIosArrowRoundForward />
-                                        </Link>
-                                    </div>
-                                </Col>
+                                    </Col>
+                                ))}
                             </Row>
                         </Col>
                         <Col sm={12} md={4}>
