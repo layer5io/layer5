6f2317ea9440466b12367d2e94a4a92b268b0923
Jashpatel1 <patel.5@iitj.ac.in>
2020-08-15 14:11:44 +0530

    updtaed styles
    Signed-off-by: Jashpatel1 <patel.5@iitj.ac.in>
    Signed-off-by: kishore007k <kishorekumarstar07@gmail.com>

--- src/reusecore/PageHeader/pageHeader.style.js
+++ src/reusecore/PageHeader/pageHeader.style.js
@@ -20,8 +20,10 @@ const PageHeaderWrapper = styled.div`
         
         .feature-image{
             margin-top: -100px;
+            max-width: 90%;
             height: 300px;
             width: auto;
+            object-fit: contain;
         }
         .breadcrumbs {
             display: flex;
