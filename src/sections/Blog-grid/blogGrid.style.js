72840dc1df0460d0527e384d699311ed20b2388e
Tanuj22 <agarwal.5@iitj.ac.in>
2020-08-24 16:41:04 +0530

    add button to switch between list and gird view
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- src/sections/Blog-grid/blogGrid.style.js
+++ src/sections/Blog-grid/blogGrid.style.js
@@ -6,6 +6,7 @@ export const BlogPageWrapper = styled.div`
     }
     .blog-grid-wrapper{
         padding-bottom: 60px;
+        margin-top: 100px;
     }
     .post-block{
         margin-bottom: 30px;
