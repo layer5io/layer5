d42635aa9f8e9865b4e69414f206e4ca6b1cb02c
Tanuj22 <agarwal.5@iitj.ac.in>
2020-09-05 01:54:16 +0530

    add single news page
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- src/templates/news-single.js
+++ src/templates/news-single.js
@@ -7,14 +7,14 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 import Navigation from "../sections/Navigation";
-import BlogSingle from "../sections/Blog-single";
+import NewsSingle from "../sections/News-single";
 import Footer from "../sections/Footer";
 
 import { GlobalStyle } from "../sections/app.style";
 import theme from "../theme/blog/themeStyles";
 
 export const query = graphql`
-    query BlogsBySlug($slug: String!) {
+    query NewsBySlug($slug: String!) {
         mdx(fields: { slug: { eq: $slug } }) {
             body
             frontmatter {
@@ -37,19 +37,19 @@ export const query = graphql`
     }
 `;
 
-const BlogSinglePage = ({data}) => {
+const NewsSinglePage = ({data}) => {
     return (
         <ThemeProvider theme={theme}>
             <Layout>
                 <GlobalStyle />
                 <SEO title={data.mdx.frontmatter.title} />
                 <Navigation />
-                <BlogSingle data={data}/>
+                <NewsSingle data={data}/>
                 <Footer />
             </Layout>
         </ThemeProvider>
     );
 };
 
-export default BlogSinglePage;
+export default NewsSinglePage;
 
