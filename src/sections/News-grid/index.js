80c4f00b797bfd8d571b03413bbe9d95a0f28cee
Tanuj22 <agarwal.5@iitj.ac.in>
2020-09-08 20:19:35 +0530

    seperate card component
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>
    Signed-off-by: kishore007k <kishorekumarstar07@gmail.com>

--- src/sections/News-grid/index.js
+++ src/sections/News-grid/index.js
@@ -1,11 +1,6 @@
 import React from "react";
-import { Link } from "gatsby";
-import Image from "../../components/image";
-import { IoIosArrowRoundForward } from "react-icons/io";
-import { BiLinkExternal } from "react-icons/bi";
-
 import { Container, Row, Col } from "../../reusecore/Layout";
-
+import Card from "../../components/Card";
 import PageHeader from "../../reusecore/PageHeader";
 
 import { NewsPageWrapper } from "./NewsGrid.style";
@@ -20,33 +15,7 @@ const NewsGrid = ({data}) => {
                         <Row>
                             {data.allMdx.nodes.map(({id, frontmatter, fields }) => (
                                 <Col xs={12} sm={6} lg={4} key={id}>
-                                    <div className="post-block">
-                                        <div className="post-thumb-block">
-                                            <Link className="anchor" to={fields.slug}>
-                                                <Image {...frontmatter.thumbnail} imgStyle={{ objectFit: "contain" }}/>
-                                            </Link>
-                                        </div>
-                                        <div className="post-content-block">
-                                            <h2 className="post-title">
-                                                <Link className="anchor" to={fields.slug}>
-                                                    {frontmatter.title}
-                                                </Link>
-                                            </h2>
-                                            <div className="post-meta-block">
-                                                <span>By: {frontmatter.author}</span>
-                                                <span className="divider">/</span>
-                                                <span>{frontmatter.date}</span>
-                                            </div>
-                                            <Link className="readmore-btn" to={fields.slug}>
-                                                see more <IoIosArrowRoundForward />
-                                            </Link>
-                                            {frontmatter.eurl && (
-                                                <a className="readmore-btn" href={frontmatter.eurl}>
-                                                    <BiLinkExternal />
-                                                </a>
-                                            )}
-                                        </div>
-                                    </div>
+                                    <Card frontmatter={frontmatter} fields={fields}/>
                                 </Col>
                             ))}
                         </Row>
