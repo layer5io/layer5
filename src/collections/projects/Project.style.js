b6f60630c08d93fe6c212ad952f03baba75b81d3
Jashpatel1 <patel.5@iitj.ac.in>
2020-09-10 18:59:48 +0530

    added tables
    Signed-off-by: Jashpatel1 <patel.5@iitj.ac.in>
    Signed-off-by: kishore007k <kishorekumarstar07@gmail.com>

--- src/collections/projects/Project.style.js
+++ src/collections/projects/Project.style.js
@@ -20,4 +20,43 @@ export const ProjectWrapper = styled.div`
         right: 0;
         text-align: center;
     }
+
+    .row {
+        display: flex;
+    }
+
+    .col {
+        flex: 50%;
+    }
+
+    .col4 {
+        flex: 40%;
+    }
+
+    .col6 {
+        flex: 60%;
+    }
+
+    .responsive-holder {
+        position: relative;
+        overflow: hidden;
+        width: 100%;
+        padding-top: 56.25%;
+    }
+
+    .responsive-iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+    }
+
+    @media screen and (max-width: 600px) {
+        .col {
+            flex: 100%;
+        }
+    }
 `;
