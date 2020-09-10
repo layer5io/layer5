f080634d9c99b661d6fd27d71c25dc971a627112
Jashpatel1 <patel.5@iitj.ac.in>
2020-09-10 11:44:58 +0530

    added smp
    Signed-off-by: Jashpatel1 <patel.5@iitj.ac.in>

--- src/collections/projects/Project.style.js
+++ src/collections/projects/Project.style.js
@@ -1,9 +1,22 @@
 import styled from "styled-components";
 
 export const ProjectWrapper = styled.div`
+    color:#000;
+    margin-top: 50px;
     .center{
         margin-bottom: 50px;
         text-align: center;
         color: black;
+    }
+    .image-left {
+        width: 40%;
+        float: left;
+        padding-right: 10px;
+    }
+    .half-col {
+        width: 40%;
+        height: auto;
+        right: 0;
+        text-align: center;
     }
 `;
