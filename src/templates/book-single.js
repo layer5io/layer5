8231a17eb4ab43c9ffb1dd8b198279444a9d9b32
Tanuj22 <agarwal.5@iitj.ac.in>
2020-09-12 01:48:36 +0530

    add single page for book
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

---
+++ src/templates/book-single.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { graphql } from "gatsby";
+
+import { ThemeProvider } from "styled-components";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+import Navigation from "../sections/Navigation";
+import BookSingle from "../sections/Book-single";
+import Footer from "../sections/Footer";
+
+import { GlobalStyle } from "../sections/app.style";
+import theme from "../theme/blog/themeStyles";
+
+export const query = graphql`
+    query BookBySlug($slug: String!) {
+        mdx(fields: { slug: { eq: $slug } }) {
+            body
+            frontmatter {
+                title
+                date(formatString: "YYYY MMMM Do")
+                author
+            }
+        }
+    }
+`;
+
+const BookSinglePage = ({data}) => {
+    return (
+        <ThemeProvider theme={theme}>
+            <Layout>
+                <GlobalStyle />
+                <SEO title={data.mdx.frontmatter.title} />
+                <Navigation />
+                <BookSingle data={data}/>
+                <Footer />
+            </Layout>
+        </ThemeProvider>
+    );
+};
+
+export default BookSinglePage;
+
