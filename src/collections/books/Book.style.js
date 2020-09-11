f288feb7ee675ece25d56fd7da84b441de63e8d3
Tanuj22 <agarwal.5@iitj.ac.in>
2020-09-12 02:42:24 +0530

    add second book
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>
    Signed-off-by: kishore007k <kishorekumarstar07@gmail.com>

---
+++ src/collections/books/Book.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const BookWrapper = styled.div`
+    
+    .istio-book{
+        float:right;
+        padding:20px;
+        text-align:center;
+    }
+    
+`;
