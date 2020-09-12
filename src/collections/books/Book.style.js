bb97c401c1b914d98819ff2a063beef87baa9629
Tanuj22 <agarwal.5@iitj.ac.in>
2020-09-12 21:55:04 +0530

    fix import issue
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- src/collections/books/Book.style.js
+++ src/collections/books/Book.style.js
@@ -8,4 +8,15 @@ export const BookWrapper = styled.div`
         text-align:center;
     }
     
+    .card{
+        position: relative;
+        margin: 0.5rem 0 1rem 0;
+        background-color: #fff;
+        -webkit-transition: -webkit-box-shadow .25s;
+        transition: -webkit-box-shadow .25s;
+        transition: box-shadow .25s;
+        transition: box-shadow .25s, -webkit-box-shadow .25s;
+        border-radius: 2px;
+    }
+    
 `;
