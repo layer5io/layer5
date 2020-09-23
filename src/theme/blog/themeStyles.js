a7275549ae66e3f11b09d1f2355de2b2af5270ad
Jashpatel1 <patel.5@iitj.ac.in>
2020-09-23 12:04:45 +0530

    removed pink
    Signed-off-by: Jashpatel1 <patel.5@iitj.ac.in>

--- src/theme/blog/themeStyles.js
+++ src/theme/blog/themeStyles.js
@@ -5,7 +5,7 @@ const theme = {
     white: "#ffffff",
     primaryColor: "#8A57DE",
     primaryLightColor: "#EEE6FA",  
-    secondaryColor: "#FB7B81",
+    secondaryColor: "#00B39F",
     secondaryLightColor: "#FFEBEC",
     headingColor: "#1D316C", 
     textColor: "#7A848E", 
