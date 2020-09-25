9a16c73e078735d1f9405ba76a6e449e9339d42f
Jashpatel1 <patel.5@iitj.ac.in>
2020-09-25 22:21:12 +0530

    lint fix
    Signed-off-by: Jashpatel1 <patel.5@iitj.ac.in>

--- src/sections/Contact/index.js
+++ src/sections/Contact/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Container, Row, Col } from "../../reusecore/Layout";
+import { Row, Col } from "../../reusecore/Layout";
 import SectionTitle from "../../reusecore/SectionTitle";
 import Button from "../../reusecore/Button";
 import img from "./forklift.svg";
