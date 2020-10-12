7aeff43cf64ebd28a8da96e84dcef24d841ebc8c
ashutosh1401 <ashutoshthakur_14@yahoo.com>
2020-10-12 12:52:00 +0530

    Migrated Code of Conduct page to layer5-ng
    Signed-off-by: ashutosh1401 <ashutoshthakur_14@yahoo.com>

---
+++ src/pages/terms/code-of-conduct.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import theme from "../../theme/blog/themeStyles";
+import { GlobalStyle } from "../../sections/app.style";
+
+import SEO from "../../components/seo";
+import Navigation from "../../sections/Navigation";
+import Footer from "../../sections/Footer";
+import Layout from "../../components/layout";
+import Conduct from "../../sections/Terms/index"
+
+const codeofconduct = () => (
+    <ThemeProvider theme={theme}>
+        <Layout>
+            <GlobalStyle />
+            <SEO title="Code of Conduct" />
+            <Navigation />
+            <Conduct />
+            <Footer />
+        </Layout>
+    </ThemeProvider>
+)
+
+export default codeofconduct
---
+++ src/sections/Terms/terms.style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const TermsWrapper = styled.section`
+position: relative;
+
+.conduct_heading {
+  text-align: left;
+  width: 90%;
+  font-size: 25px;
+  font-weight: 200;
+  margin-top: 30px;
+  margin-bottom: 20px;
+  h2 {
+    text-align: left;
+  }
+}
+`
+
+export default TermsWrapper
