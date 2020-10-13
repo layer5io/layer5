7cc4d1ed75e6d94fc2c498e1658d90836422c8ca
Gerald Maboshe <maboshegerald1@gmail.com>
2020-10-13 19:20:04 +0200

    added an array of service meshes
    Signed-off-by: Gerald Maboshe <maboshegerald1@gmail.com>

--- src/sections/Landscape-grid/index.js
+++ src/sections/Landscape-grid/index.js
@@ -3,9 +3,9 @@ import { Container, Row } from "../../reusecore/Layout";
 import PageHeader from "../../reusecore/PageHeader";
 import { LandscapePageWrapper } from "./LandscapeGrid.style";
 import ServiceMeshComparison from "../../assets/images/landscape/comparison-of-service-mesh-strengths.svg";
-import LandscapeGridPage from "../../pages/landscape";
+import { serviceMeshes } from '../../collections/landscape/timeline'
 
-const LandscapeGrid=({ data }) => {
+const LandscapeGrid=() => {
     return (
         <LandscapePageWrapper>
             <PageHeader title="Landscape" path="Landscape" />
@@ -15,6 +15,10 @@ const LandscapeGrid=({ data }) => {
                         <Row>
                             <h2>Comparison of Service Mesh Strengths</h2>
                             <img src={ServiceMeshComparison} width="100%" alt="Service Mesh Comparison" />
+                            {serviceMeshes.map(mesh => <div key={mesh.timeline_order}>
+                                {/* <img src={require(`../../${mesh.icon}`)} alt={mesh.name} /> */}
+                                {mesh.name}
+                            </div>)}
                         </Row>
                     </div>
                 </Container>
