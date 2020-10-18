ca8bc6706166acc395d2bedc194bb43091907df0
Tanuj Agarwal <tanujagarwal22@gmail.com>
2020-10-18 23:01:22 +0530

    Update News.style.js

--- src/collections/news/News.style.js
+++ src/collections/news/News.style.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
 export const NewsWrapper = styled.div`
-    
-    .test{
-        color: black;
+    iframe {
+    margin: 0 auto;
+    display:block;
+    @media screen and (max-width: 750px) {
+        max-width: 100% !important;
+        width: auto !important;
+        height: auto !important;
     }
-    
+}
 `;
