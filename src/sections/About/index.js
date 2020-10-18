53efd3f5a8c5412381887a27f49f0df23ae613b6
Harshita-Kanal <harshita.kgv@gmail.com>
2020-10-18 21:30:50 +0530

    Fix about page linting
    Signed-off-by: Harshita-Kanal <harshita.kgv@gmail.com>

--- src/sections/About/index.js
+++ src/sections/About/index.js
@@ -1,106 +1,106 @@
-import React from "react"
-import { Container, Row, Col } from "../../reusecore/Layout"
-import SectionTitle from "../../reusecore/SectionTitle"
-import Button from "../../reusecore/Button"
-import VintageBox from "../../reusecore/VintageBox"
-import SocialLinks from "../../components/SocialLinks"
-import particle1 from "../../assets/images/app/particle/05.png"
-import particle2 from "../../assets/images/app/particle/06.png"
-import AboutSectionWrapper from "./about.style"
+import React from "react";
+import { Container, Row, Col } from "../../reusecore/Layout";
+import SectionTitle from "../../reusecore/SectionTitle";
+import Button from "../../reusecore/Button";
+import VintageBox from "../../reusecore/VintageBox";
+import SocialLinks from "../../components/SocialLinks";
+import particle1 from "../../assets/images/app/particle/05.png";
+import particle2 from "../../assets/images/app/particle/06.png";
+import AboutSectionWrapper from "./about.style";
 
 const About = () => {
-  return (
-    <AboutSectionWrapper id="about">
-      <img className="section__particle one" src={particle1} alt="img" />
-      <img className="section__particle two" src={particle2} alt="img" />
-      <Container fullWidthSM>
-        <Row Vcenter={true}>
-          <Col xs={12} sm={6}>
-            <SectionTitle
-              className="section-title"
-              leftAlign={true}
-              UniWidth="100%"
-            >
-              {" "}
-              <h2>About us</h2>
-            </SectionTitle>
-            <div className="about-text text-one">
-              <h4>Community First</h4>
-              <p className="indent">
+    return (
+        <AboutSectionWrapper id="about">
+            <img className="section__particle one" src={particle1} alt="img" />
+            <img className="section__particle two" src={particle2} alt="img" />
+            <Container fullWidthSM>
+                <Row Vcenter={true}>
+                    <Col xs={12} sm={6}>
+                        <SectionTitle
+                            className="section-title"
+                            leftAlign={true}
+                            UniWidth="100%"
+                        >
+                            {" "}
+                            <h2>About us</h2>
+                        </SectionTitle>
+                        <div className="about-text text-one">
+                            <h4>Community First</h4>
+                            <p className="indent">
                 The Layer5 community represents the largest collection of
                 service mesh projects and their maintainers in the world.
-              </p>
-            </div>
-            <div className="about-text text-two">
-              <h4>Open Source First</h4>
-              <p className="indent">
+                            </p>
+                        </div>
+                        <div className="about-text text-two">
+                            <h4>Open Source First</h4>
+                            <p className="indent">
                 Our projects establish industry standards and enable service
                 developers, owners, and operators with repeatable patterns and
                 best practices for managing all aspects of distributed services.
                 Our shared commitment to the open source spirit push the Layer5
                 community and its projects forward.
-              </p>
-            </div>
-            <div className="about-text text-two">
-              <h4>
-                <a href="http://slack.layer5.io">Collaborate</a>
-              </h4>
-              <p>
-                <a className="community" href="http://slack.layer5.io">
+                            </p>
+                        </div>
+                        <div className="about-text text-two">
+                            <h4>
+                                <a href="http://slack.layer5.io">Collaborate</a>
+                            </h4>
+                            <p>
+                                <a className="community" href="http://slack.layer5.io">
                   Join the community
-                </a>{" "}
+                                </a>{" "}
                 and collaborate on our projects.
-              </p>
-              <SocialLinks />
-            </div>
-          </Col>
+                            </p>
+                            <SocialLinks />
+                        </div>
+                    </Col>
 
-          <Col xs={12} sm={6}>
-            <div>
-              <Row>
-                <Col xs={12} sm={6}>
-                  <h4>Locations</h4>
-                  <div className="section">
-                    <h4>USA</h4>
-                    <p>
+                    <Col xs={12} sm={6}>
+                        <div>
+                            <Row>
+                                <Col xs={12} sm={6}>
+                                    <h4>Locations</h4>
+                                    <div className="section">
+                                        <h4>USA</h4>
+                                        <p>
                       Layer5, Inc.
-                      <br />
+                                            <br />
                       Corporate Headquarters
-                      <br />
-                      <a href="https://goo.gl/maps/3oeuqrsMtHPQSTmQ8">
+                                            <br />
+                                            <a href="https://goo.gl/maps/3oeuqrsMtHPQSTmQ8">
                         701 Brazos Street, Suite 1600
-                        <br />
+                                                <br />
                         Austin, TX 78701
-                      </a>
-                    </p>
-                  </div>
-                </Col>
+                                            </a>
+                                        </p>
+                                    </div>
+                                </Col>
 
-                <Col xs={12} sm={6}>
-                  <br />
-                  <h4>Scotland, UK</h4>
-                  <p>
+                                <Col xs={12} sm={6}>
+                                    <br />
+                                    <h4>Scotland, UK</h4>
+                                    <p>
                     Layer5, Inc.
-                    <br />
+                                        <br />
                     Engineering
-                    <br />
-                    <a href="https://goo.gl/maps/1nF7vNmVq5fm2GLS6">
+                                        <br />
+                                        <a href="https://goo.gl/maps/1nF7vNmVq5fm2GLS6">
                       3 Queen Street Edinburgh, EH2 1JE
-                    </a>
-                  </p>
-                </Col>
-              </Row>
-              <VintageBox right={false} vintageTwo={true} position="relative">
-                <Button className="about-button">
-                  <a href="mailto:hello@layer5.io">Contact Us</a>
-                </Button>
-              </VintageBox>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </AboutSectionWrapper>
-  )
-}
+                                        </a>
+                                    </p>
+                                </Col>
+                            </Row>
+                            <VintageBox right={false} vintageTwo={true} position="relative">
+                                <Button className="about-button">
+                                    <a href="mailto:hello@layer5.io">Contact Us</a>
+                                </Button>
+                            </VintageBox>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </AboutSectionWrapper>
+    );
+};
 
-export default About
+export default About;
