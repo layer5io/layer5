32b89c3d5ea6ad59b492f81d3d74a5d5c5264eb7
Tanuj22 <agarwal.5@iitj.ac.in>
2020-11-03 22:25:42 +0530

    add new pager
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- src/components/pager.js
+++ src/components/pager.js
@@ -1,51 +1,72 @@
 import React from "react";
 import { Link } from "gatsby";
 import styled from "styled-components";
+import ReactPaginate from "react-paginate";
 import { IoIosArrowRoundForward, IoIosArrowRoundBack } from "react-icons/io";
 
 export const PagerWrapper = styled.div`
-    display: flex; 
-    justify-content: space-between;
-    
-    .old-post{
-        justify-self: flex-end;
-    }
-    h4{
-        display: flex;
-        align-items: center;
-        svg{
-           margin-left: 0px;
-           font-size: 32px;
-           transition: all 0.3s linear;
-       } 
-        &:hover{
-             color: ${props => props.theme.secondaryColor};
-             svg{
-                margin-left: 3px;
-                transform: scale(1.2);
-           }
+     
+    .pagination {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      list-style: none;
+      padding-left: 0;
+      cursor: pointer;
+      a {
+          padding: 10px;
+          border-radius: 5px;
+          border: 1px solid ${props => props.theme.secondaryColor};
+          color: ${props => props.theme.secondaryColor};
+      }
+      .next, .previous {
+         a{
+            display: flex;
+            align-items: center;
+            svg{
+               margin-left: 0px;
+               font-size: 20px;
+           } 
         }
+      }
+    }
+    
+    .pagination__link {
+      font-weight: bold;
+    }
+    
+    .pagination__link--active a {
+      color: #fff;
+      background: ${props => props.theme.secondaryColor};
+    }
+    
+    .pagination__link--disabled a {
+      color: rgb(198, 197, 202);
+      border: 1px solid rgb(198, 197, 202);
     }
 `;
 
 const Pager = ({ pageContext, isListView }) => {
-    const { previousPagePath, nextPagePath } = pageContext;
+    const { numberOfPages, previousPagePath, nextPagePath } = pageContext;
+
+    const handlePageClick = ({ selected: selectedPage }) =>{
+        console.log(pageContext);
+    };
+
+
     return (
         <PagerWrapper>
-            <div>
-                {previousPagePath && (
-                    <Link to={previousPagePath} state={{ isListView }}>
-                        <h4><IoIosArrowRoundBack />Newer Posts</h4>
-                    </Link>
-                )}
-            </div>
-            <div className="old-post">
-                {nextPagePath && (
-                    <Link to={nextPagePath} state={{ isListView }}>
-                        <h4>Older Posts<IoIosArrowRoundForward/></h4>
-                    </Link>
-                )}
-            </div>
+            <ReactPaginate
+                previousLabel={<><IoIosArrowRoundBack/> Previous</>}
+                nextLabel={<>Next <IoIosArrowRoundForward/></>}
+                pageCount={numberOfPages}
+                onPageChange={handlePageClick}
+                containerClassName="pagination"
+                previousLinkClassName="pagination__link"
+                nextLinkClassName="pagination__link"
+                disabledClassName="pagination__link--disabled"
+                activeClassName="pagination__link--active"
+            />
         </PagerWrapper>
     );
 };
