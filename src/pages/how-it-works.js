40897d6741b920fc793794ebc51df09b82b66fd0
leecalcote <leecalcote@gmail.com>
2020-11-02 21:06:36 -0600

    logos
    Signed-off-by: leecalcote <leecalcote@gmail.com>

--- src/pages/how-it-works.js
+++ src/pages/how-it-works.js
@@ -21,13 +21,15 @@ const Howitworks = () => (
             <Container>
                 <br></br>
                 <Works
-                    title="Secure access to hosts and services"
-                    description="Traditional approaches like SSH bastion hosts or VPNs require distributing and managing credentials, configuring network controls like firewalls, and exposing the private network. Boundary provides a secure way to access hosts and critical systems without having to manage credentials or expose your network, and is entirely open source."
+                    title="Meshery, the service mesh management plane"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo a diam sollicitudin tempor id. Purus faucibus ornare suspendisse sed. Odio ut sem nulla pharetra diam sit amet. Faucibus scelerisque eleifend donec pretium vulputate. Eget duis at tellus at urna. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Bibendum enim facilisis gravida neque convallis a cras semper auctor. Felis donec et odio pellentesque diam volutpat commodo sed egestas. Id volutpat lacus laoreet non curabitur gravida arcu ac tortor. Nunc sed augue lacus viverra vitae congue eu consequat. Semper eget duis at tellus at urna condimentum. Cursus metus aliquam eleifend mi in nulla posuere. Quam adipiscing vitae proin sagittis. Eget sit amet tellus cras adipiscing enim eu turpis.
+
+                    Semper auctor neque vitae tempus quam pellentesque nec nam aliquam. At quis risus sed vulputate odio ut enim blandit. Eleifend quam adipiscing vitae proin sagittis. Dui sapien eget mi proin sed libero. Velit dignissim sodales ut eu sem integer. Aliquam faucibus purus in massa tempor nec feugiat. Pellentesque dignissim enim sit amet venenatis urna cursus eget nunc. Malesuada fames ac turpis egestas. Rhoncus est pellentesque elit ullamcorper dignissim cras. Faucibus et molestie ac feugiat sed lectus. Vel pharetra vel turpis nunc eget lorem. Quam lacus suspendisse faucibus interdum posuere lorem. In vitae turpis massa sed elementum. Vitae congue eu consequat ac felis donec et."
                     features={[
                         {
-                            title: "Authenticate",
+                            title: "Service Mesh Management",
                             description:
-                  "Authenticate with any trusted identity provider you are already using. No need to distribute new credentials and manage them.",
+                  "Iaculis eu non diam phasellus vestibulum. Quis vel eros donec ac odio. Cursus mattis molestie a iaculis at erat pellentesque adipiscing. Elit ut aliquam purus sit amet luctus venenatis lectus. Ornare suspendisse sed nisi lacus. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. Eu ultrices vitae auctor eu augue ut lectus. Massa sed elementum tempus egestas sed. Vel eros donec ac odio tempor orci dapibus. Tellus id interdum velit laoreet id. Turpis egestas sed tempus urna et pharetra pharetra massa. Gravida rutrum quisque non tellus orci ac auctor augue. Neque viverra justo nec ultrices dui sapien eget mi proin.",
                             logos: [
                                 {
                                     alt: "GitHub",
@@ -60,44 +62,56 @@ const Howitworks = () => (
                             ],
                         },
                         {
-                            title: "Authorize",
+                            title: "Service Meshes",
                             description:
-                  "Authorize access based on logical roles and services, instead of physical IP addresses. Manage dynamic infrastructure and integrate service registries so hosts and service catalogs are kept up-to-date.",
+                  "Eget nunc lobortis mattis aliquam faucibus purus in. Purus sit amet volutpat consequat mauris nunc. Dignissim suspendisse in est ante in nibh. Dictumst vestibulum rhoncus est pellentesque. At in tellus integer feugiat scelerisque varius morbi enim. Quis enim lobortis scelerisque fermentum. Amet purus gravida quis blandit turpis. Eget est lorem ipsum dolor sit amet consectetur adipiscing. Eget mi proin sed libero enim sed faucibus turpis in. Aliquet bibendum enim facilisis gravida neque convallis a cras semper. Arcu vitae elementum curabitur vitae. In hac habitasse platea dictumst quisque sagittis purus sit amet. Viverra aliquet eget sit amet tellus. Pharetra pharetra massa massa ultricies mi quis. Faucibus purus in massa tempor nec.",
                             logos: [
                                 {
                                     alt: "Consul",
-                                    url: require("../images/How-It-works/aws.svg"),
+                                    url: require("../images/how-it-works/consul.svg"),
                                 },
                                 {
-                                    alt: "AWS",
-                                    url: require("../images/How-It-works/aws.svg"),
+                                    alt: "Istio",
+                                    url: require("../images/how-it-works/istio.svg"),
                                 },
                                 {
-                                    alt: "Microsoft Azure",
-                                    url: require("../images/How-It-works/aws.svg"),
+                                    alt: "Linkerd",
+                                    url: require("../images/how-it-works/linkerd.svg"),
                                 },
                                 {
-                                    alt: "Terraform",
-                                    url: require("../images/How-It-works/aws.svg"),
+                                    alt: "Octarine",
+                                    url: require("../images/how-it-works/octarine.svg"),
                                 },
                                 {
-                                    alt: "Google Cloud Platform",
-                                    url: require("../images/How-It-works/aws.svg"),
+                                    alt: "Network Service Mesh",
+                                    url: require("../images/how-it-works/nsm.svg"),
                                 },
                                 {
-                                    alt: "Kubernetes",
-                                    url: require("../images/How-It-works/aws.svg"),
+                                    alt: "Traefik Mesh",
+                                    url: require("../images/how-it-works/maesh.svg"),
                                 },
                                 {
-                                    url: require("../images/How-It-works/aws.svg"),
-                                    alt: "More integrations",
+                                    alt: "Citrix Service Mesh",
+                                    url: require("../images/how-it-works/citrix.svg"),
+                                },
+                                {
+                                    alt: "NGINX Service Mesh",
+                                    url: require("../images/how-it-works/nginx-sm.svg"),
+                                },
+                                {
+                                    alt: "VMware Service Mesh",
+                                    url: require("../images/how-it-works/tanzu.svg"),
+                                },
+                                {
+                                    alt: "Open Service Mesh",
+                                    url: require("../images/how-it-works/osm.svg"),
                                 },
                             ],
                         },
                         {
-                            title: "Access",
+                            title: "Managing Workload Business Logic",
                             description:
-                  "Automate credential injection to securely access services and hosts with HashiCorp Vault. Reduce risk of leaking credentials with dynamic secrets and just-in-time credentials.",
+                  "Vitae justo eget magna fermentum iaculis. Tincidunt dui ut ornare lectus sit amet est. Aliquam eleifend mi in nulla. Tincidunt lobortis feugiat vivamus at. Habitant morbi tristique senectus et netus et malesuada fames ac. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae. Vulputate ut pharetra sit amet aliquam. Faucibus in ornare quam viverra. Eget velit aliquet sagittis id. Risus at ultrices mi tempus imperdiet. Fusce id velit ut tortor pretium viverra suspendisse potenti nullam. Habitant morbi tristique senectus et netus et. Nunc eget lorem dolor sed viverra ipsum. Dolor sit amet consectetur adipiscing elit pellentesque habitant. Convallis tellus id interdum velit laoreet id donec. At tempor commodo ullamcorper a.",
                         },
                     ]}/>
             </Container>
