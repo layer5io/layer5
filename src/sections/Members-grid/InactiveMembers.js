49ec3086a53eb2ab9bd94cfe93f90d7a26d98e29
Lee Calcote <leecalcote@gmail.com>
2020-11-06 14:44:47 -0600

    Update src/sections/Members-grid/InactiveMembers.js

--- src/sections/Members-grid/InactiveMembers.js
+++ src/sections/Members-grid/InactiveMembers.js
@@ -18,7 +18,7 @@ const InactiveMembers = props =>{
                             twitter
                             status
                             meshmate
-                            Linkedin
+                            linkedin
                             image_path{
                                 childImageSharp{
                                     fluid(maxWidth: 200){
