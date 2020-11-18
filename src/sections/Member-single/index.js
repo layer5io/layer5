3a142b04deb359a52df3a6a3430a726968054734
Tanuj22 <agarwal.5@iitj.ac.in>
2020-11-18 18:59:37 +0530

    refactor member page
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- src/sections/Member-single/index.js
+++ src/sections/Member-single/index.js
@@ -1,17 +1,83 @@
 import React from "react";
+import {Link} from "gatsby";
 import { MemberSingleWrapper } from "./memberSingle.style";
-import ProfileCard from "../../components/Profile-Page";
+import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 import {Col, Container, Row} from "../../reusecore/Layout";
+import Image from "../../components/image";
+import MeshMateIconWhite from "../../svgs/meshmate-icon.svg";
+import MeshMateIcon from "../../svgs/meshmate-icon.svg";
 
 const MemberSingle = ({frontmatter}) =>{
+    const {name, position,  github, twitter, image_path, meshmate, linkedin, location, skills, technologies, bio} = frontmatter;
     return(
         <MemberSingleWrapper>
             <Container>
-                <Row>
-                    <Col xs={12} sm={12} lg={12} >
-                        <ProfileCard frontmatter={frontmatter}/>
-                    </Col>
-                </Row>
+                <div>
+                    <Row>
+                        <Col xs={12} sm={6} lg={6} >
+                            <Image className="profile-image" {...image_path} imgStyle={{ objectFit: "contain" }} alt={name}/>
+                        </Col>
+                        <Col xs={12} sm={6} lg={6} >
+                            <header>
+                                <h1>{name}</h1>
+                                { position && (
+                                    <h2>{position}</h2>
+                                )}
+                                {
+                                    location &&
+                                    <h4>Location: {location}</h4>
+                                }
+
+                                {
+                                    bio &&
+                                    <h6>{bio}</h6>
+                                }
+                                {
+                                    skills &&
+                                    <h4>skills: {skills}</h4>
+                                }
+                                {
+                                    technologies &&
+                                    <h4>technologies: {technologies}</h4>
+                                }
+                            </header>
+                            <div className="social-bg">
+                                <ul className="profile-social-links">
+                                    { github && (
+                                        <li>
+                                            <a href={`https://github.com/${github}`}>
+                                                <FaGithub className="github" size={32}/>
+                                            </a>
+                                        </li>
+                                    )}
+                                    { twitter && (
+                                        <li>
+                                            <a href={`https://twitter.com/${twitter}`}>
+                                                <FaTwitter className="twitter" size={32}/>
+                                            </a>
+                                        </li>
+                                    )}
+                                    { linkedin && (
+                                        <li>
+                                            <a href={`https://Linkedin.com/in/${linkedin}`}>
+                                                <FaLinkedin className="linkedin" size={32}/>
+                                            </a>
+                                        </li>
+                                    )}
+                                    { meshmate && (
+                                        <li>
+                                            <Link to={"/community/meshmates"}>
+                                                <MeshMateIconWhite className="meshmate-white" size={32}/>
+                                                <MeshMateIcon className="meshmate-color" size={32}/>
+                                            </Link>
+                                        </li>
+                                    )}
+                                </ul>
+                            </div>
+
+                        </Col>
+                    </Row>
+                </div>
             </Container>
         </MemberSingleWrapper>
     );
--- src/sections/Member-single/memberSingle.style.js
+++ src/sections/Member-single/memberSingle.style.js
@@ -1,36 +1,129 @@
 import styled from "styled-components";
 
-
 export const MemberSingleWrapper = styled.div`
     padding: 250px 0 180px 0;
-    
-    .profile-card{
-        width: 700px;
+    margin-bottom: 25px;
+    ul{
+        list-style: none;
+    }
+    .profile-image{
+        border-radius: 5px;
+        transition: 0.9s;
+    }
+    .profile-image:hover{
+        transition: 0.9s;
+        border-radius: 5px 50px 5px 50px;
+    }
+        header {
+            display: block;
+            position: relative;
+            text-align: center;
+            padding: 30px 0 20px;
+            z-index: 1;
+            overflow: hidden;
+            
+            .gatsby-image-wrapper {
+                display: block;
+                margin: auto;
+                border-radius: 100%;
+                overflow: hidden;
+                width: 150px;
+                height: 150px;
+                box-shadow: 0 1px 0 rgba(0,0,0,.1),0 1px 2px rgba(0,0,0,.1);
+            }
+            
+            h1 {
+                font-weight: 300;
+                font-size: 30px;
+                color: black;
+                letter-spacing: -2px;
+                margin-top: 15px;
+                padding: 0;
+                text-align: left;
+            }
+            
+            h2 {
+                font-weight: 400;
+                font-size: 20px;
+                color: black;
+                letter-spacing: .5px;
+                margin-top: 10px;
+                padding: 0;
+                text-align: left;
+            }
+            h6{
+                font-weight: 400;
+                font-size: 11px;
+                color: black;
+                letter-spacing: .5px;
+                margin-top: 10px;
+                padding: 0;
+                text-align: left;
+            }
+            h4{
+                font-weight: 400;
+                font-size: 14px;
+                color: black;
+                letter-spacing: .5px;
+                margin-top: 10px;
+                padding: 0;
+                text-align: left;
+            }
+        }
         
-        @media only screen and (max-width: 754px) {
-            width: 500px;
-        }
-        @media only screen and (max-width: 512px) {
-            width: 300px;
-        }
-   
-        background: #00B39F;
-        &:hover{
-            background: #00B39F;
-        }
-        header{
-            .gatsby-image-wrapper{
-                height: 500px;
-                width: 500px;
-                @media only screen and (max-width: 754px) {
-                    width: 300px;
-                    height: 300px;
-                }
-                @media only screen and (max-width: 512px) {
-                    width: 150px;
-                    height: 150px;
+        .profile-social-links {
+            display: flex;
+            justify-content: left;
+            width: 90%;
+            padding-left: 0px !important;
+            
+            li {
+                a{
+                    display: block;
+                    text-align: left;
+                    padding: 10px;
+                    border-radius: 100%;
+                    -webkit-transition: box-shadow 0.2s;
+                    -moz-transition: box-shadow 0.2s;
+                    -o-transition: box-shadow 0.2s;
+                    transition: box-shadow 0.2s;
+                    transition: 0.3s;
+                    
+                    
+                    &:hover{
+                        box-shadow: 0 1px 1.5px 0 rgba(0,0,0,.12),0 1px 1px 0 rgba(0,0,0,.24);
+                        background: #00b4a0;
+                        
+                        .github{
+                            color: #211F1F;
+                        }
+                        .linkedin{
+                            color: #0E76A8;
+                        }
+                        .twitter{
+                            color: #1DA1F2;
+                        }
+                        .meshmate-color{
+                            display: block;
+                        }
+                        .meshmate-white{
+                            display: none;
+                        }
+                    }
+                    
+                    &:active{
+                        box-shadow: 0 4px 5px 0 rgba(0,0,0,.14),0 1px 10px 0 rgba(0,0,0,.12),0 2px 4px -1px rgba(0,0,0,.2);
+                    }
+                    
+                    svg{
+                        color: #00d4ab;
+                        display: block;
+                        width: 32px;
+                    }
+                    .meshmate-color{
+                        display: none;
+                    }
                 }
             }
         }
-    }
 `;
