94309be99ba84528d4aba98534042475ac14b012
Tanuj22 <agarwal.5@iitj.ac.in>
2020-11-22 22:59:15 +0530

    meshmate badge
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- src/components/Profile-card/index.js
+++ src/components/Profile-card/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "gatsby";
 import { ProfileCardWrapper } from "./ProfileCard.style";
-import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 import MeshMateIcon from "../../assets/images/meshmate/meshmate-icon.svg";
 import MeshMateIconWhite from "../../assets/images/meshmate/meshmate-icon-white.svg";
 import Image from "../image";
 
 const ProfileCard = (props) =>{
-    const {name, position,  github, twitter, status, image_path, meshmate, linkedin} = props.frontmatter;
+    const {name, status, image_path, meshmate } = props.frontmatter;
     const links = props.cardlink;
     return(
         <ProfileCardWrapper status={status}>
@@ -15,43 +14,14 @@ const ProfileCard = (props) =>{
                 <Link to={links}>
                     <header>
                         <Image {...image_path} imgStyle={{ objectFit: "contain" }} alt={name}/>
-                        <h1>{name}</h1>
-                        { position && (
-                            <h2>{position}</h2>
+                        { meshmate && (
+                            <Link className="meshmate" to="/community/meshmates">
+                                <img src={MeshMateIcon}  alt="meshmate-color-icon" />
+                            </Link>
                         )}
+                        <h1>{name}</h1>
                     </header>
                 </Link>
-                <ul className="profile-social-links">
-                    { github && (
-                        <li>
-                            <a href={`https://github.com/${github}`}>
-                                <FaGithub className="github" size={32}/>
-                            </a>
-                        </li>
-                    )}
-                    { twitter && (
-                        <li>
-                            <a href={`https://twitter.com/${twitter}`}>
-                                <FaTwitter className="twitter" size={32}/>
-                            </a>
-                        </li>
-                    )}
-                    { linkedin && (
-                        <li>
-                            <a href={`https://Linkedin.com/in/${linkedin}`}>
-                                <FaLinkedin className="linkedin" size={32}/>
-                            </a>
-                        </li>
-                    )}
-                    { meshmate && (
-                        <li>
-                            <Link to="/community/meshmates">
-                                <img src={MeshMateIconWhite} className="meshmate-white" alt="meshmate-white-icon" />
-                                <img src={MeshMateIcon} className="meshmate-color" alt="meshmate-color-icon" />
-                            </Link>
-                        </li>
-                    )}
-                </ul>
             </div>
         </ProfileCardWrapper>
     );
