a4e38af26917e2d722e6afb3e32c5e3dbe7bf1ae
Tanuj22 <agarwal.5@iitj.ac.in>
2020-11-24 14:42:27 +0530

    link fixed
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- src/sections/Meshmates-grid/index.js
+++ src/sections/Meshmates-grid/index.js
@@ -60,9 +60,7 @@ const MeshmatesGrid=() => {
                         <Row>
                             {data.allMdx.nodes.map(({ id, frontmatter, fields }) => (
                                 <Col xs={12} sm={6} lg={4} key={id}>
-                                    <Link to={fields.slug}>
-                                        <ProfileCard frontmatter={frontmatter} />
-                                    </Link>
+                                    <ProfileCard frontmatter={frontmatter} cardlink = {fields.slug}/>
                                 </Col>
                             ))}
                         </Row>
