8b55c9fa5a8f7f4774d649b2559df81035de5d97
vineethvanga18 <reddy.8@iitj.ac.in>
2020-11-28 01:32:02 +0530

    stacked up menus
    Signed-off-by: vineethvanga18 <reddy.8@iitj.ac.in>

--- src/sections/Navigation/utility/ScrollspyMenu.js
+++ src/sections/Navigation/utility/ScrollspyMenu.js
@@ -19,52 +19,50 @@ const ScrollspyMenu = ({ menuItems, ...props }) => {
     return (
         <ul className={addAllClasses.join(" ")}>
             {menuItems.map((menu, index) => (
-                <React.Fragment key={index}>
-                    <li
-                        key={index}
-                        className={
-                            menu.subItems !== undefined ? "nav-item has-dropdown" : "nav-item"
-                        }
-                        onMouseOver={() => handleMouseHover(index)}
-                    >
-                        <AnchorLink to={menu.path}>{menu.name}</AnchorLink>
-                    </li>
-                    <div className="fakediv">
-                        {active.subItems !== undefined && (
-                            <ul className="dropdown">
-                                {active.name}
-                                <div className="nav-grid">
-                                    <div className="hr">
-                                        {active.subItems.map((subItem, i) => (
-                                            <li key={i}>
-                                                <Link to={subItem.path} partiallyActive={true} className={subItem.sepLine && "sub-item"}>
-                                                    {subItem.name}
-                                                </Link>
-                                            </li>
-                                        ))}
-                                        <div className="action-item">
-                                            <Link to={active.actionLink}>
-                                                <span>{active.actionName}</span>
-                                                <img src={Icon} alt="appion app" />
-                                            </Link>
-                                        </div>
-                                    </div>
-                                    <div className="nav-display">
-                                        <div className="nav-div">
-                                            <a href={active.div1.path}><img src={active.div1.src} className="nav-img" /></a>
-                                            <div> {active.div1.descr} </div>
-                                        </div>
-                                        <div className="nav-div">
-                                            <a href={active.div2.path}><img src={active.div2.src} className="nav-img" /></a>
-                                            <div> {active.div2.descr} </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </ul>
-                        )}
-                    </div>
-                </React.Fragment>
+                <li
+                    key={index}
+                    className={
+                        menu.subItems !== undefined ? "nav-item has-dropdown" : "nav-item"
+                    }
+                    onMouseOver={() => handleMouseHover(index)}
+                >
+                    <AnchorLink to={menu.path}>{menu.name}</AnchorLink>
+                </li>
             ))}
+            <div className="fakediv">
+                {active.subItems !== undefined && (
+                    <ul className="dropdown">
+                        {active.name}
+                        <div className="nav-grid">
+                            <div className="hr">
+                                {active.subItems.map((subItem, i) => (
+                                    <li key={i}>
+                                        <Link to={subItem.path} partiallyActive={true} className={subItem.sepLine && "sub-item"}>
+                                            {subItem.name}
+                                        </Link>
+                                    </li>
+                                ))}
+                                <div className="action-item">
+                                    <Link to={active.actionLink}>
+                                        <span>{active.actionName}</span>
+                                        <img src={Icon} alt="appion app" />
+                                    </Link>
+                                </div>
+                            </div>
+                            <div className="nav-display">
+                                <div className="nav-div">
+                                    <a href={active.div1.path}><img src={active.div1.src} className="nav-img" /></a>
+                                    <div> {active.div1.descr} </div>
+                                </div>
+                                <div className="nav-div">
+                                    <a href={active.div2.path}><img src={active.div2.src} className="nav-img" /></a>
+                                    <div> {active.div2.descr} </div>
+                                </div>
+                            </div>
+                        </div>
+                    </ul>
+                )}
+            </div>
         </ul>
     );
 };
