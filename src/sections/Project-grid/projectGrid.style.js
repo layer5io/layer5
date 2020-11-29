4643ef48bb36ae9b574a4f8ccfd9790f8558e5c7
Nikhil-Ladha <nikhilladha1999@gmail.com>
2020-11-29 14:19:35 +0530

    Updated the projects grid and contact cards
    Signed-off-by: Nikhil-Ladha <nikhilladha1999@gmail.com>

--- src/sections/Project-grid/projectGrid.style.js
+++ src/sections/Project-grid/projectGrid.style.js
@@ -22,15 +22,17 @@ padding: 100px 0 110px 0;
         border-radius: 4%;
         position: relative;
         text-align: center;
+        transition: 0.6s;
         &:hover{
             background-color: #00b39f;
-            box-shadow: 0px 0px 10px #FFBD2E;
             img {
                 filter: brightness(0) invert(1);
             }
             h1 {
                 color: white;
             }
+            transition: 0.4s;
+            transform: scale(1.05);
         }
         h1{
             margin-bottom: 20px;
