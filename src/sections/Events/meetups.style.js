ee6ed49fbaa0afe91792ad087496f9df2de6bc03
kishore007k <kishorekumarstar07@gmail.com>
2020-12-01 20:40:03 +0530

    Added: Pagination and styling
    Signed-off-by: kishore007k <kishorekumarstar07@gmail.com>

--- src/sections/Events/meetups.style.js
+++ src/sections/Events/meetups.style.js
@@ -19,6 +19,7 @@ export const MeetupStyle = styled.div`
         border-bottom: 1px solid #a0aaaa;
         margin-top: 50px;
 
+
         button{
             outline: none;
             border: none;
@@ -27,13 +28,47 @@ export const MeetupStyle = styled.div`
             transition: 0.5s ease-in-out all;
             height: 100%;
             font-size: 20px;
-            color: #00b39f;
+            color: #00d3a9;
             font-weight: 300;
             background: white;
+            filter: grayscale(0.7);
             cursor: pointer;
+            margin-right: 40px;
+            transition: 0.5s ease-in all;
+            
+            &:hover{
+                background-color: rgba(0,0,0,0.02);
+            }
         }
         .active{
             border-bottom: 2px solid lightseagreen;
+            filter: grayscale(0);
+        }
+    }
+
+    .prevAndNextBtns{
+        display: block;
+        width: 100%;
+        height: auto;
+        margin: 40px auto;
+    }
+
+    @media (max-width: 560px){
+        .filterBtns{
+            button{
+                margin-right: 10px;
+                font-size: 18px;
+            }
+        }
+    }
+
+    @media (max-width: 420px){
+        .filterBtns{
+            button{
+                margin-right: 0px;
+                font-size: 18px;
+                padding: 10px;
+            }
         }
     }
 `;
