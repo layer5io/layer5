2f03ff579d77bdd40103f46c181ed15a148a5131
Tanuj22 <agarwal.5@iitj.ac.in>
2020-12-14 14:28:36 +0530

    comment out breadcrumbs
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- src/sections/Blog-list/index.js
+++ src/sections/Blog-list/index.js
@@ -23,7 +23,7 @@ const BlogList = ({
     const { totalCount, nodes } = data.allMdx;
     const header = tag
         ? `${totalCount} blog${totalCount === 1 ? "" : "s"} tagged with "${tag}"`
-        : category ? `${totalCount} blog${totalCount === 1 ? "" : "s"} tagged with "${category}"`: "Blogs";
+        : category ? `${totalCount} blog${totalCount === 1 ? "" : "s"} categorised with "${category}"`: "Blogs";
 
     return (
         <BlogPageWrapper>
