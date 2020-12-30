c3b8cb466467d3adb7afa454df0e80222188553a
Jashpatel1 <patel.5@iitj.ac.in>
2020-12-30 20:59:02 +0530

    updated SMP section
    Signed-off-by: Jashpatel1 <patel.5@iitj.ac.in>

--- src/sections/Brand/Brand-components/smp.js
+++ src/sections/Brand/Brand-components/smp.js
@@ -4,6 +4,7 @@ import { Row, Col } from "../../../reusecore/Layout";
 import Button from "../../../reusecore/Button";
 import ColorBox from "../../../components/ColorBox";
 
+import { FiDownloadCloud } from "react-icons/fi";
 import SmpDark from "../../../assets/images/smp/icon/smp-dark.png";
 import SmpLight from "../../../assets/images/smp/icon/smp-light.png";
 import SmpWhite from "../../../assets/images/smp/icon/smp-white.png";
@@ -32,27 +33,30 @@ const SMPWrap = styled.section`
 const SMPBrand = () => {
     return (
         <SMPWrap>
-            <Row className="post-content">
-                <Col xs={12} sm={6} lg={6} xl={6}>
+            <div className="post-content">
+                <Row>
                     <h1 className="layerH3">
                         <strong>Service Mesh Performance</strong>
                     </h1>
-                </Col>
-                <Col xs={12} sm={6} lg={6} xl={6}>
-                    <Button primary className="l5-dark-yellow" title="DOWNLOAD LOGO ASSETS" url="https://layer5.io/assets/resources/brand/smp-brand-kit.zip" external="true" />
-                </Col>
-                <Col xs={12} sm={12} lg={12} xl={12}>
-                    <Row>
-                        <p>
-              The Service Mesh Performance mark includes SMP name & logo and any
-              word, phrase, image, or other designation that identifies the
-              source or origin of any Layer5 projects. PLease don't modify the
-              marks or use them in a confusing way, including suggesting
-              sponsorship or endorsement by Layer5 with another brand(including
-              your own)
-                        </p>
-                    </Row>
-                </Col>
+                    <Button primary className="l5-dark-yellow" title="DOWNLOAD LOGO ASSETS" url="https://layer5.io/assets/resources/brand/smp-brand-kit.zip" external="true">
+                        <FiDownloadCloud size={21} className="icon-left" />
+                    </Button>
+                </Row>
+                <Row>
+                    <p>
+            The Service Mesh Performance mark includes SMP name & logo and any
+            word, phrase, image, or other designation that identifies the
+            source or origin of any Layer5 projects. PLease don't modify the
+            marks or use them in a confusing way, including suggesting
+            sponsorship or endorsement by Layer5 with another brand(including
+            your own)
+                    </p>
+                </Row>
+                <Row>
+                    <h1 className="layerH3 in">
+                            Logos
+                    </h1>
+                </Row>
                 <Row className="ImgDiv">
                     <Col xs={12} sm={4} lg={4} xl={4}>
                         <a href="#">
@@ -74,24 +78,40 @@ const SMPBrand = () => {
                         </div>
                     </Col>
                 </Row>
-                <Row>
-                    <Col xs={12} sm={6} lg={6} xl={6}>
-                        <h3 className="layerH3">
-                            <strong>Colors</strong>
-                        </h3>
+                <Row className="ImgDiv">
+                    <Col xs={12} sm={4} lg={4} xl={4}>
+                        <a href="#">
+                            <img src={SmPDarkTextSide} />
+                        </a>
                     </Col>
-                    <Col xs={12} sm={12} lg={12} xl={12}>
-                        <p>
-              The SMP color palette consists of gray, green and colors and
-              additional shades. The SMP logo should be white or monochrome when
-              using project colors as the background.
-                        </p>
+                    <Col xs={12} sm={4} lg={4} xl={4}>
+                        <div className="SmpWhite">
+                            <a href="#">
+                                <img src={SmPWhiteTextSide} />
+                            </a>
+                        </div>
+                    </Col>
+                    <Col xs={12} sm={4} lg={4} xl={4}>
+                        <div className="SmpWhite">
+                            <a href="#">
+                                <img src={SmPLightTextSide} />
+                            </a>
+                        </div>
                     </Col>
                 </Row>
                 <Row>
-                    <Col xs={12} sm={12} lg={12} xl={12}>
-                        <h4 className="layerH3">Web</h4>
-                    </Col>
+                    <h1 className="layerH3 in">
+                            Colors
+                    </h1>
+                </Row>
+                <Row>
+                    <p>
+            The SMP color palette consists of gray, green and colors and
+            additional shades. The SMP logo should be white or monochrome when
+            using project colors as the background.
+                    </p>
+                </Row>
+                <Row>
                     <div className="colors">
                         <Row>
                             <ColorBox R="R: 0" G="G: 211" B="B: 169" colorCode="#00d3a9" />
@@ -101,32 +121,8 @@ const SMPBrand = () => {
                             <ColorBox R="R: 60" G="G: 73" B="B: 78" colorCode="#3C494E" />
                         </Row>
                     </div>
-                    <Col xs={12} sm={12} lg={12} xl={12}>
-                        <p className="layerH3">Logos to use online:</p>
-                    </Col>
-                    <Row className="ImgDiv">
-                        <Col xs={12} sm={4} lg={4} xl={4}>
-                            <a href="#">
-                                <img src={SmPDarkTextSide} />
-                            </a>
-                        </Col>
-                        <Col xs={12} sm={4} lg={4} xl={4}>
-                            <div className="SmpWhite">
-                                <a href="#">
-                                    <img src={SmPWhiteTextSide} />
-                                </a>
-                            </div>
-                        </Col>
-                        <Col xs={12} sm={4} lg={4} xl={4}>
-                            <div className="SmpWhite">
-                                <a href="#">
-                                    <img src={SmPLightTextSide} />
-                                </a>
-                            </div>
-                        </Col>
-                    </Row>
                 </Row>
-            </Row>
+            </div>
         </SMPWrap>
     );
 };
