0c41475061a10781b0c97b89c343d82b7aaf587e
Tanuj22 <agarwal.5@iitj.ac.in>
2021-01-04 22:07:05 +0530

    fix height of platforms
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- src/sections/Meshery-platforms/MesheryPlatforms.style.js
+++ src/sections/Meshery-platforms/MesheryPlatforms.style.js
@@ -1,31 +1,31 @@
 import styled from "styled-components";
 
 export const MesheryPlatformsWrapper = styled.div`
-   
+   margin-top: 5rem;
   .background-wrapper {
         position: absolute;
         width: 100%;
-        height: 100%;
+        height: 52rem;
         overflow: hidden;
         transform: skewY(10deg);
-        transform-origin: 0 center;
+        transform-origin: 50% center;
         background-color: ${props => props.theme.secondaryColor};
         z-index: -1;
     }
   
-  .content{
+  .content{ 
       text-align: center;
       h2 {
         color: ${props => props.theme.white};
         font-weight: 700;
       }
       .step-1 {
-          padding-top: 18.5rem;
+          padding-top: 8rem;
           @media (max-width: 62rem) {
-            padding-top: 10rem;
+            padding-top: 6rem;
           }
            @media (max-width: 32rem) {
-            padding-top: 6rem;
+            padding-top: 4rem;
           }
       }
       .supported-platforms{
