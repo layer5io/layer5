8c3c876426e552326493e9b312d8870da4cf7dc0
leecalcote <leecalcote@gmail.com>
2021-01-22 16:35:09 -0600

    update
    Signed-off-by: leecalcote <leecalcote@gmail.com>

--- src/reusecore/Blockquote/index.js
+++ src/reusecore/Blockquote/index.js
@@ -1,9 +1,6 @@
 import React from "react"; 
 import PropTypes from "prop-types";
 import BlockquoteStyle from "./blockquote.style";
-import {Link} from "gatsby"; 
-
-
 
 const Blockquote = ({
   className,
@@ -16,37 +13,35 @@ const Blockquote = ({
     addClasses.push(className);
   }
 
-  const initalBlockquote = 
-    <BlockquoteStyle 
-      className={addClasses.join(" ")}{...props} >
-      {children}
-      {props.quote}
-    </BlockquoteStyle>;
-
+  // const initalBlockquote = 
+  //   <BlockquoteStyle 
+  //     className={addClasses.join(" ")}{...props} >
+  //     {children}
+  //     {props.quote}
+  //   </BlockquoteStyle>;
 
   return (
     <React.Fragment>
       {
         <div className="blockquote-wrapper">
-        <div className="blockquote">
-        <h1>
-         Intuitive design happens when <span>current knowledge</span> is the same as the <span>target knowledge.</span>
-         </h1>
-        <h4>Jared Spool<br /><em>Web Site UsabilityA Designer Guide</em></h4>
-      </div>
-    </div>      }
+          <div className="blockquote">
+            <h1 className="blockquote">
+            Intuitive design happens when <span>current knowledge</span> is the same as the <span>target knowledge.</span>
+            </h1>
+            <h4 className="blockquote">Jared Spool<br /><em>Web Site UsabilityA Designer Guide</em></h4>
+          </div>
+        </div>      
+      }
     </React.Fragment>
   );
 };
 
 Blockquote.propTypes = {
   as: PropTypes.oneOf(["Blockquote", "blockquote",]), //--
-  upperCase: PropTypes.bool,
   quote: PropTypes.string,
-  url: PropTypes.string,
+  person: PropTypes.string,
   className: PropTypes.string,
-  external: PropTypes.bool,
-  icon: PropTypes.string,
+  title: PropTypes.string,
 };
 
 export default Blockquote;
