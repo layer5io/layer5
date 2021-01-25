6cd18c47f3acb9c8a1c24557d8f8691ac6cdf19d
Tanuj22 <agarwal.5@iitj.ac.in>
2021-01-25 18:27:07 +0530

    update single members page design
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- src/sections/Community/Member-single/memberSingle.style.js
+++ src/sections/Community/Member-single/memberSingle.style.js
@@ -1,8 +1,31 @@
 import styled from "styled-components";
 
 export const MemberSingleWrapper = styled.div`
-    padding: 120px 0 50px 0;
-    margin-bottom: 25px;
+    padding: 7.5rem 0 3.125rem 0;
+    margin-bottom: 1.5625rem;
+    .backBtn {
+		font-weight: 600;
+		margin-bottom: 1rem;
+		@media screen and (max-width: 62rem) {
+            display: none;
+	    }
+		a{
+			display: flex;
+		    color: ${props => props.theme.primaryColor};
+		    &:hover{
+		        color: ${props => props.theme.linkColor};
+			}
+			
+			h4 {
+				line-height: 1.75rem;
+				margin-left: 0.5rem;
+			}
+			svg {
+				font-size: 1.75rem;
+			}
+		}
+	}
+	
     ul{
         list-style: none;
     }
@@ -18,68 +41,57 @@ export const MemberSingleWrapper = styled.div`
         z-index: 1;
         overflow: hidden;
 
-        .h4{
+        .text{
             font-weight: 400;
-            font-size: 15px;
-            color: black;
+            font-size: 1rem;
             letter-spacing: .5px;
-            margin-top: 10px;
+            margin-top: 0.625rem;
             padding: 0;
             text-align: left;
         }
         .bio{
-            margin-top: 20px;
+            margin-top: 1.25rem;
         }
         .location {
-            margin-top: 0px;
+            margin-top: 0;
         }
         
         .bold{
             font-weight: 600;
         }
     }
-    .profile-image:hover{
-        transition: 0.9s;
-        border-radius: 5px 50px 5px 50px;
-    }
+    .profile-image{
+        &:hover{
+            transition: 0.9s;
+            border-radius: 5px 50px 5px 50px;
+        }
+       }
         .header {
-            font-family: 'Open Sans', sans-serif;
             display: block;
             position: relative;
             text-align: center;
-            padding: 30px 0 20px;
+            padding: 1.875rem 0 1.25rem;
             z-index: 1;
             overflow: hidden;
             
-            .gatsby-image-wrapper {
-                display: block;
-                margin: auto;
-                border-radius: 100%;
-                overflow: hidden;
-                width: 150px;
-                height: 150px;
-                box-shadow: 0 1px 0 rgba(0,0,0,.1),0 1px 2px rgba(0,0,0,.1);
-            }
-            
-            .h1 {
-                font-family: 'Open Sans', sans-serif;
+            .name {
                 font-weight: 600;
                 font-size: 40px;
                 line-height: 1;
                 color: black;
                 letter-spacing: -2px;
-                margin-top: 15px;
+                margin-top: 0.9375rem;
                 padding: 0;
                 text-align: left;
             }
             
-            .h2 {
+            .position {
                 font-weight: 400;
                 font-size: 22px;
                 font-style: italic;
                 color: ${props => props.theme.menuColor};
                 letter-spacing: .5px;
-                margin-top: 15px;
+                margin-top: 0.9375rem;
                 padding-top: 1px;
                 padding-bottom: 3px;
                 border-bottom: 3px solid ${props => props.theme.secondaryColor};
@@ -92,7 +104,7 @@ export const MemberSingleWrapper = styled.div`
             display: flex;
             justify-content: left;
             width: 90%;
-            padding-left: 0px !important;
+            padding-left: 0 !important;
             
             li {
                 a{
@@ -106,7 +118,6 @@ export const MemberSingleWrapper = styled.div`
                     -o-transition: box-shadow 0.2s;
                     transition: box-shadow 0.2s;
                     transition: 0.3s;
-                    
                     
                     &:hover{
                         box-shadow: 0 1px 1.5px 0 rgba(0,0,0,.12),0 1px 1px 0 rgba(0,0,0,.24);
@@ -130,15 +141,22 @@ export const MemberSingleWrapper = styled.div`
                     svg{
                         color: greyscale;
                         display: block;
-                        width: 32px;
+                        width: 2rem;
                     }
                     img {
                         display: block;
-                        width: 32px;
-                        height: 32px;
+                        width: 2rem;
+                        height: 2rem;
                         max-width: none;
                     }
                 }
             }
+        }
+        .start-contributing{
+          margin-top: 2.5rem;
+          text-align: center;
+          p{
+            font-weight: 700;
+          }
         }
 `;
