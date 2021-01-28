febae5ac871900f3dc40fb0119a59e8d5c6fe3ab
Jashpatel1 <patel.5@iitj.ac.in>
2021-01-28 13:20:59 +0530

    added metaImage
    Signed-off-by: Jashpatel1 <patel.5@iitj.ac.in>

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
 
-function SEO({ description, lang, meta, title }) {
+function SEO({ description, lang, meta, title, image }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -19,6 +19,7 @@ function SEO({ description, lang, meta, title }) {
             title
             description
             author
+            defaultImage
           }
         }
       }
@@ -26,6 +27,7 @@ function SEO({ description, lang, meta, title }) {
   );
 
   const metaDescription = description || site.siteMetadata.description;
+  const metaImage = `${site.siteMetadata.siteUrl}${image ? image : site.siteMetadata.defaultImage}`;
 
   return (
     <Helmet
@@ -66,6 +68,10 @@ function SEO({ description, lang, meta, title }) {
         {
           name: "twitter:description",
           content: metaDescription,
+        },
+        {
+          name: "image",
+          content: metaImage,
         },
       ].concat(meta)}
     />
