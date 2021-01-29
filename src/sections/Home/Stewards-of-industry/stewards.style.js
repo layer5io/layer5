cdb881e786dbafc79cce9fc62173340ce6dce7fa
Bart Kalanski <bkalanski@gmail.com>
2021-01-29 19:30:08 +0000

    Refactored component and added test
    Signed-off-by: Bart Kalanski <bkalanski@gmail.com>

--- src/sections/Home/Stewards-of-industry/stewards.style.js
+++ src/sections/Home/Stewards-of-industry/stewards.style.js
@@ -3,61 +3,57 @@ import styled from "styled-components";
 import backgroundImg from "./images/stewards.png";
 
 const BannerSectionWrapper = styled.section`
-  .wrapper {
+  .section {
     background-image: url(${backgroundImg});
+    background-size: cover;
     height: 75vh;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-
   }
-  .header, .content {
+  .section-header,
+  .section-content {
     text-align: center;
     padding: 5vh 20vw 2.5vh 20vw;
-
   }
-  .header{
+  .section-header {
     font-weight: 700;
     font-size: 50px;
     line-height: 50px;
     color: white;
-
+    padding: 5vh 20vw 2.5vh 20vw;
   }
-  .content {
+  .section-content {
     font-family: Open Sans;
     font-weight: 400;
     font-size: 18px;
     line-height: 23.4px;
     color: white;
+    padding: 0 20vw 2.5vh 20vw;
   }
-  .button {
-      display: block;
-      margin: 0 auto;
-      width: 200px;
-      height: 54px;
-      background: #EBC017;
-      border-radius: 5px;
-      border: none;
-      font-family: Open Sans;
-      font-weight: 600;
-      font-sizze: 17px;
-      line-height: 17px;
-      cursor: pointer;
-      &:hover{
-        background: #EBC045;
-      }
+  .section-button {
+    display: block;
+    margin: 0 auto;
+    width: 200px;
+    height: 54px;
+    background: #ebc017;
+    border-radius: 5px;
+    border: none;
+    font-family: Open Sans;
+    font-weight: 600;
+    font-size: 17px;
+    line-height: 17px;
+    cursor: pointer;
+    &:hover {
+      background: #ebc045;
+    }
   }
-
-  @media only screen and (max-width: 1600px) {
-  }
-  @media only screen and (max-width: 1440px) {
-  }
-  @media only screen and (max-width: 1200px) {
-  }
-  @media only screen and (max-width: 912px) {
-  }
-  @media only screen and (max-width: 576px) {
+  @media only screen and (max-width: 500px) {
+    .section-header,
+    .section-content {
+      text-align: left;
+    }
   }
 `;
 
