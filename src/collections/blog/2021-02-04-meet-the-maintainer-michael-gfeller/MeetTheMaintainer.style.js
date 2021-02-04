e6db613eb807927e4cb0ce068239a22909622baf
leecalcote <leecalcote@gmail.com>
2021-02-04 01:18:04 -0600

    draft meet the maintainer
    Signed-off-by: leecalcote <leecalcote@gmail.com>

---
+++ src/collections/blog/2021-02-04-meet-the-maintainer-michael-gfeller/MeetTheMaintainer.style.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+export const MeetTheMaintainer = styled.div`
+    
+    div.intro {
+        padding-left: 3rem;
+        padding-right: 3rem;
+        font-style: italic;
+        font-size: .8rem;
+    }
+
+    div.interviewer {
+        margin-top: 1rem;
+        text-align:left;
+        span {
+            font-weight: 600;
+        }
+    }
+    div.interviewee {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        color: #222;
+        font-style: italic;
+        text-align:left;
+        border-left: 2px green solid;
+        padding-left: 2rem;
+        span {
+            font-weight: 600;
+        }
+    }
+`;
