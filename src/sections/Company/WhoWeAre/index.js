cba50b936f46079fcb3f9cf8e8d54bbd130f04f3
leecalcote <leecalcote@gmail.com>
2021-02-13 17:07:40 -0600

    whoweare
    Signed-off-by: leecalcote <leecalcote@gmail.com>

--- src/sections/Company/WhoWeAre/index.js
+++ src/sections/Company/WhoWeAre/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "../../../reusecore/Layout";
 import SectionTitle from "../../../reusecore/SectionTitle";
-import Button from "../../../reusecore/Button";
-import VintageBox from "../../../reusecore/VintageBox";
 import WhoWeAreSectionWrapper from "./whoweare.style.js";
 import checkCircle from "./images/check-circle_lightgreen.svg";
 
@@ -11,9 +9,9 @@ import checkCircle from "./images/check-circle_lightgreen.svg";
 const WhoWeAre = () => {
   return (
     <WhoWeAreSectionWrapper id="whoweare">
-      <Container fullWidthSM className="whoweare">
+      <Container fullWidthSM>
         <Row Vcenter={true}>
-          <Col xs={12} sm={12}>
+          <Col xs={12} sm={12} lg={12}>
             <SectionTitle
               className="section-title"
               leftAlign={false}
@@ -23,16 +21,6 @@ const WhoWeAre = () => {
               <h1>from around the globe</h1>
 
               <h3>Join the community and collaborate on our projects</h3>
-              <VintageBox left={true} vintageTwo={true} >
-                <Button
-                  primary
-                  className="whoweare-button"
-                  url="/community"
-                  title="Start Contributing"
-                  external={false}
-                />
-              </VintageBox>
-              <img src={checkCircle} className="check-circle" />
             </SectionTitle>
           </Col>
         </Row>
@@ -43,6 +31,80 @@ const WhoWeAre = () => {
           </Col>
         </Row>
       </Container >
+
+      <Row className="row">
+        <Col xs={3} sm={3} md={3} lg={3} className="col">
+          <div className="item">
+            <img src={checkCircle} alt="CNCF" />
+            <h4>Docker Captains</h4>
+          </div>
+        </Col>
+        <Col xs={3} sm={3} md={3} lg={3} className="col">
+          <div className="item">
+            <img src={checkCircle} alt="Envoy" />
+          </div>
+        </Col>
+        <Col xs={3} sm={3} md={3} lg={3} className="col">
+          <div className="item">
+            <img src={checkCircle} alt="Linkerd" />
+            <h4>Cloud Native Ambassadors</h4>
+          </div>
+        </Col>
+
+        <Col xs={3} sm={3} md={3} lg={3} className="col">
+          <div className="item">
+            <img src={checkCircle} alt="Meshery" />
+            <h4>CNCF TOC Contributors</h4>
+          </div>
+        </Col>
+        <Col xs={3} sm={3} md={3} lg={3} className="col">
+          <div className="item">
+            <img src={checkCircle} alt="OAM" />
+            <h4>Service Mesh Interface Maintainers</h4>
+          </div>
+        </Col>
+      </Row>
+      <Row className="row">
+        <Col xs={3} sm={3} md={3} lg={2} className="col">
+          <div className="item">
+            <img src={checkCircle} alt="Istio" />
+            <h4>Istio</h4>
+          </div>
+        </Col>
+        <Col xs={3} sm={3} md={3} lg={2} className="col">
+          <div className="item">
+            <img src={checkCircle} alt="OSM" />
+          </div>
+        </Col>
+        <Col xs={3} sm={3} md={3} lg={2} className="col">
+          <div className="item">
+            <img src={checkCircle} alt="SMP" />
+            <h4>Service Mesh Performance Maintainers</h4>
+          </div>
+        </Col>
+        <Col xs={3} sm={3} md={3} lg={2} className="col">
+          <div className="item">
+            <img src={checkCircle} alt="ImageHub" />
+            <h4>Image Hub</h4>
+          </div>
+        </Col>
+        <Col xs={3} sm={3} md={3} lg={2} className="col">
+          <div className="item">
+            <img src={checkCircle} alt="SMI" />
+          </div>
+        </Col>
+        <Col xs={3} sm={3} md={3} lg={2} className="col">
+          <div className="item">
+            <img src={checkCircle} alt="Traefik" />
+            <h4>Traefik Mesh</h4>
+          </div>
+        </Col>
+      </Row>
+
+
+
+
+
     </WhoWeAreSectionWrapper >
   );
 };
--- src/sections/Company/WhoWeAre/whoweare.style.js
+++ src/sections/Company/WhoWeAre/whoweare.style.js
@@ -3,165 +3,68 @@ import collabMap from "./images/layer5-collaboration-map.svg";
 
 const AboutSectionWrapper = styled.section`
 
-   
     background: linear-gradient(255.21deg, #477E96 20.89%, #01B29F 36.99%, #00B39F 59.78%, #3C494F 90.08%);
     mix-blend-mode: darken; 
-    
-    height: 55rem;
     color: white;
-    text-align: centers;
-    .check-circle {
-      fill: ${props => props.theme.caribbeanGreenColor};
-      stroke: red;
+    text-align: center;
+    margin: auto;
+ 
+    h1 {
+      color: white;
     }
-  .whoweare-title {
-    
     h2 {
       display: block;
       font-weight: 400;     
+      margin-top: 4rem;
       margin-bottom: 1rem;
       line-height: 1rem;
-    }
-    
-  }
-  
-  .section-title {
-    h1 {color: white;}
-    h2 {
       color: white;
-      margin-top: 4rem;
-      margin-bottom: 0rem;
     }
     h3 {
         color: white;
         margin-bottom: 1.7vw;
       }
-  }
+
   .section-bottom {
     margin-bottom: 5rem;
   }
 
-  @media only screen and (max-width: 1400px) {
-    .about-thumb {
-      transform: translateX(0px);
-    }
-    .section__particle {
-      &.one {
-        top: 9%;
-      }
-      &.two {
-        bottom: -92px;
-        left: -38px;
-      }
-    }
-  }
-  @media screen and (max-width: 1200px) {
-    .section-title {
-      h2 {
-        margin-bottom: 1rem;
-      }
-      h1{
-        
-        margin-bottom: -4rem;
-      }
+  .row {
+		justify-content: center;
+		margin: 1rem;
+		overflow: hidden;
+	}
 
-      img {
-        margin-bottom: -5rem;
-      }
-    }
-  }
-  @media only screen and (max-width: 1024px) {
-    .section-title {
-      h2 {
-        margin-bottom: 0rem;
-      }
-      h1{
-        
-        margin-bottom: -4rem;
-      }
+	.col {
+		flex: 0 0 25%;
+		max-width: 20%;
+	}
 
-      img {
-        margin-bottom: -6rem;
-      }
-    }
-    .section__particle {
-      &.one {
-        top: 0%;
-      }
-    }
-  }
-  @media only screen and (max-width: 912px) {
-    .section-title {
-      h2 {
-        margin-top: 2rem;
-      }
-      h1{
-        margin-bottom: -9rem;
-      }
+	.item {
+		height: 7rem;
+		background: ${props => props.theme.tertiaryColor};
+		display: flex;
+		border-radius: 1.5rem;
+		justify-content: left;
+		margin: auto;
+		padding: 1rem;
 
-      img {
-        margin-bottom: -6rem;
-      }
-    }
-    .about-text.text-two {
-      margin-bottom: 10px;
-    }
-  }
-  @media screen and (max-width: 911px) and (min-width: 569px) {
-    
-    .section-bottom {
-      h2 {
-        margin-bottom: -8rem;
-      }
-    }
-  }
-  @media only screen and (max-width: 760px) {
-    
-    padding: 60px 0 60px 0;
-    .about-thumb {
-      margin-bottom: 70px;
-    }
-  }
-  @media only screen and (max-width: 568px) {
-    .section-title,
-    .about-content-wrap {
-      text-align: center;
-    }
-    .section-title {
+		h4 {
+      padding-left: .9rem;
+			font-weight: 500;
+			text-align: left;
+      color: white;
+      margin: auto;
+		}
 
-      h2 {
-        margin-bottom: 0rem;
-      }
-      h1{
-        
-        margin-bottom: 0rem;
-      }
-      img {
-        padding:0px;
-        margin: 0px;
-        height:auto;
-      }
-    }
-  }
-  @media only screen and (max-width: 480px) {
-    padding: 40px 0 60px 0;
-    .section-title {
-    h2 {
-      margin-bottom: 0rem;
-      font-size: 1.4rem;
-    }
-    h1{
-      font-size: 1.6rem;
-      margin-bottom: 0rem;
-    }
-  }
+		@media screen and (max-width: 900px) {
+			height: 6.5rem;
+		}
+		@media screen and (max-width: 480px) {
+			height: 5rem;
+		}
+	}
 
-  .community {
-    color: blue;
-  }
-  .about-button {
-    height: 40px;
-    margin-top: 20px;
-  }
 `;
+
 export default AboutSectionWrapper;
