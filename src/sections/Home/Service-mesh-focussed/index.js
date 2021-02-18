3efd68683c111f989c6022aa64feb7925cc4435e
leecalcote <leecalcote@gmail.com>
2021-02-17 18:38:21 -0600

    refactor section
    Signed-off-by: leecalcote <leecalcote@gmail.com>

--- src/sections/Home/Service-mesh-focussed/index.js
+++ src/sections/Home/Service-mesh-focussed/index.js
@@ -5,52 +5,37 @@ import MeshFocusWrapper from "./service-mesh-focussed.style";
 import ServiceMeshFocusedImage from "./The-Enterprise-path-to-service-mesh-Architectures-2nd-Edition.png";
 import BlockQuoteImage from "../../../assets/images/blockquote/quote-left.svg";
 import Button from "../../../reusecore/Button";
-import {FaArrowRight} from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
 
 const ServiceMeshFocused = () => {
 
   return (
     <MeshFocusWrapper>
-            <Container>
-
-      <Row>
-        <Col>asdf</Col>
-        </Row>
-        <Row>
-        <Col>asdf</Col>
-        <Col>asdf</Col>
-      </Row>
-      <div className="main">
-        <Row className="head-cont-2">
+      <Container>
+        <Row className="heading">
           <Col>
-          <h5>Service mesh focused.</h5>
-          {/* <h1>Service mesh focused.</h1> */}
-          <h1>Service mesh neutral</h1>
-          <h4>Dedicated to empowering you with any service mesh.</h4>
+            <h5>Service mesh focused.</h5>
+            {/* <h1>Service mesh focused.</h1> */}
+            <h1>Service mesh neutral</h1>
+            <h4>Dedicated to empowering you with any service mesh.</h4>
           </Col>
         </Row>
-        <Row className="card-cont-1">
-          <Col className="img-col">
-            <img src={ServiceMeshFocusedImage} alt="The-Enterprise-Path-to-Service-Mesh-Architectures" />
+        <Row Vcenter >
+          <Col xs={12} sm={6} lg={6}>
+            <img src={ServiceMeshFocusedImage} className="book" alt="The-Enterprise-Path-to-Service-Mesh-Architectures" />
           </Col>
-          <Col className="content-col">
-            <img src={BlockQuoteImage} alt="Quote-left"/>
-            <p className="content">
-              
+          <Col xs={12} sm={6} lg={6}>
+            <img src={BlockQuoteImage} className="quote-icon" alt="Quote from book" />
+            <p className="quote">
               "Diverse microservices patterns and technologies, together with the requirements of given microservice applications,provide myriad opportunities for service mesh differentiation and specialization - including meshes native to specific cloud platforms. This will lead to a world where many           enterprises use multiple service mesh products, whether separately or together."
-              
             </p>
             <Button url="/learn/service-mesh-books/the-enterprise-path-to-service-mesh-architectures-2nd-edition">
-              Read Story
-              <FaArrowRight className="icon-right"/>
+              Read Book
+              <FaArrowRight className="icon-right" />
             </Button>
-
           </Col>
-
         </Row>
-      </div>
       </Container>
-
     </MeshFocusWrapper>
   );
 };
--- src/sections/Home/Service-mesh-focussed/service-mesh-focussed.style.js
+++ src/sections/Home/Service-mesh-focussed/service-mesh-focussed.style.js
@@ -1,106 +1,50 @@
 import styled from "styled-components";
 
 const MeshFocusWrapper = styled.div`
-   .main {
     display:flex;
     flex-direction:column;
     justify-content:center;
     align-items:center;
     align-content:center;
-    margin: 4rem;
-   } 
+    margin: 2rem; 
     
-    .head-cont-2{
-        text-align:center;
-        padding:2rem 1rem;
+    .heading {
+        text-align: center;
+        margin: 1rem;
     }
-    
-    .card-cont-1{
-        display:flex;
-        width:60%;
-     
-        background-color: ${props => props.theme.secondaryLightColor}
+
+    .quote-icon{
+            width: 10vw;
+            opacity: .1;
+            min-width: 60px;
+            top: 4rem;
+            margin-bottom: -4rem;
+            
     }
-    .img-col img{
+    .quote {
+        margin-bottom: 4rem;
+    }
+    .book {
         object-fit: contain;
         margin: auto;
+        padding: 4rem;
     }
-    .content-col
-    {
-        width:68%;
-        padding:4rem;
-
-        img {
-            width:5rem;
-        }
-
-        p.content
-        {
-            padding: 2rem 0rem;
-            font-weight: normal;
-            font-style: oblique;
-        }
-        Button {
-            background:none;
-            color:${props => props.theme.secondaryColor};
-            font-size:1.2rem;
-            border-bottom:3px solid ${props => props.theme.secondaryColor};
-            padding:0px;
-            min-width:unset;
-            border-radius:0px;
-        }
-        
-        Button:hover {
-            background-color:${props => props.theme.secondaryColor};
-            color:black;
-            
-        }
-
+    p {
+        font-style: italic;
     }
-    @media only screen and (max-width:1400px){
-        .card-cont-1{
-            width:80%;
+    @media only screen and (max-width:992px){
+        .book {
+            padding: 1rem;
         }
     }
-    @media only screen and (max-width:1050px){
-
-        .card-cont-1{
-            width:93%;
-            margin:2rem;;
-            flex-direction:column;
-            justify-content:center;align-items:center;
-            .img-col{
-                width:unset;
-            }
-            .content-col
-            {
-                width:100%;
-                padding:1rem;
-            }
+    @media only screen and (max-width:786px){
+        .book {
+            padding: 0rem;
         }
-        
     }
     @media only screen and (max-width:500px){
 
-    .card-cont-1{
-        width:93%;
-        margin:2rem;
-        flex-direction:column;
-        justify-content:center;
-        align-items:center;
-        .img-col{
-            width:unset;
-        }
-        .content-col
-        {
-            width:100%;
-            padding:1rem;
-            img{
-               width:3rem;
-                height:2.5rem;
-            }
-        }
-}
+    }
 
 }
 `;
