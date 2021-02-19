c2380a042eed4cbf622af5f254c2f8ba3d77b719
leecalcote <leecalcote@gmail.com>
2021-02-19 06:38:25 +0000

    Deploying to site from @ layer5io/layer5@a41a6d904926abf0c64eef3c649ad92b2872d598 🚀

--- src/sections/Company/News-grid/NewsGrid.style.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-export const NewsPageWrapper = styled.div`
-
-    .news-grid-wrapper{
-        margin-bottom: 5rem;
-    }
-`;
--- src/sections/Learn/Book-single/index.test.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import BookSinglePage from './index';
-it('Book-single renders without crashing', () => {
-  shallow(<BookSinglePage />);
-});
