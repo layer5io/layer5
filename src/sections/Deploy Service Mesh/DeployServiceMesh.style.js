9199fe7300c5b95996fc702f740d281fb3ab6d7a
vinayaksh42 <vinayaksh42@gmail.com>
2021-02-25 22:14:53 +0530

    Axios | deploy-service-mesh improvements
    Signed-off-by: vinayaksh42 <vinayaksh42@gmail.com>

--- src/sections/Deploy Service Mesh/DeployServiceMesh.style.js
+++ src/sections/Deploy Service Mesh/DeployServiceMesh.style.js
@@ -1,18 +1,23 @@
 import styled from "styled-components";
 import bookCover from "../../assets/images/learn/book-cover.png";
 
-const DeployServiceMeshWrapper = styled.div`     
-    scroll-behavior: smooth; 
+const DeployServiceMeshWrapper = styled.div`   
+    html {
+        scroll-behavior: smooth;
+    }
+    h3.sub-heading {
+        margin-top: 2rem;
+        color: ${props => props.theme.white};
+    }
     #hero{
         background-color: #EBC017;
-        height: 246px;
+        height: 35rem;
         text-align: center;
-        padding: 50px 0px 10px 0px;
+        padding: 200px 0px 10px 0px;
         p{
             padding-top: 20px; 
         }
     }
-
     #info{
         padding: 70px 0px 70px 0px;
         h3{
@@ -62,12 +67,12 @@ const DeployServiceMeshWrapper = styled.div`
         }
         #arrow-box{
             
-            padding: 20px 40px 20px 40px;
+            padding: 0px 40px 20px 40px;
             display: flex;
             .arrow-left {
                 // width: 90%;
                 height: 0;
-                padding-top: 2.3em;
+                padding-top: 1.3em;
                 padding-bottom: 25%;
                 padding-left:20px;
                 position: relative;
@@ -78,11 +83,10 @@ const DeployServiceMeshWrapper = styled.div`
                 display: block;
                 width: 0; 
                 height: 0; 
-                border-top: 20px solid transparent;
-                border-bottom: 20px solid transparent; 
-                border-right: 60em solid #00B39F;
+                border-top: 4em solid transparent;
+                border-bottom: 4em solid transparent; 
+                border-right: 6em solid #00B39F;
             }
-
             .arrow-right {
                 margin-left: auto;
                 // width: 10%;
@@ -98,8 +102,8 @@ const DeployServiceMeshWrapper = styled.div`
                 display: block;
                 width: 0; 
                 height: 0; 
-                border-top: 2em solid transparent;
-                border-bottom: 2em solid transparent;
+                border-top: 4em solid transparent;
+                border-bottom: 4em solid transparent;
                 border-left: 8em solid #00B39F;
             } 
             @media only screen and (min-width: 1000px) {
@@ -163,8 +167,14 @@ const DeployServiceMeshWrapper = styled.div`
             }
         }
     }
-
     #form{
+        h3{
+            font-weight: 700;
+        }
+        #submit-btn{
+            text-align: center;
+            padding:  20px 0px 100px 0px;
+         }
         #text{
             color: black;
             display: flex; 
@@ -177,7 +187,6 @@ const DeployServiceMeshWrapper = styled.div`
                 margin-left: auto;
             }
         }
-
         #slider{
             display: flex;
             padding: 40px
@@ -194,8 +203,38 @@ const DeployServiceMeshWrapper = styled.div`
     }
     .slidecontainer {
         width: 100%;
+        position: absolute;
+        margin: 0 auto;
       }
       
+      @media screen and (-webkit-min-device-pixel-ratio:0) {
+        input[type='range'] {
+            -webkit-appearance: none;
+            background-color: #D2CECC;
+            -webkit-appearance: none;
+            -moz-apperance: none;
+            border-radius: 6px;
+            height: 6px;  
+        }
+        .bubble {
+            background: red;
+            color: white;
+            padding: 4px 12px;
+            position: absolute;
+            border-radius: 4px;
+            left: 50%;
+            transform: translateX(-50%);
+            font-size: 5px;
+          }
+          .bubble::after {
+            content: "";
+            position: absolute;
+            width: 2px;
+            height: 2px;
+            background: red;
+            top: -1px;
+            left: 50%;
+          }
       .slider {
         -webkit-appearance: none;
         width: 100%;
@@ -206,6 +245,8 @@ const DeployServiceMeshWrapper = styled.div`
         opacity: 0.7;
         -webkit-transition: .2s;
         transition: opacity .2s;
+        // overflow: hidden;
+        cursor: pointer;
       }
       
       .slider:hover {
@@ -219,6 +260,7 @@ const DeployServiceMeshWrapper = styled.div`
         height: 26px;
         border-radius: 50%;
         border:5px solid #00B39F;
+        // box-shadow: -100vw 0 0 100vw dodgerblue;
         background: #FFFFFF;
         cursor: pointer;
       }
@@ -230,9 +272,22 @@ const DeployServiceMeshWrapper = styled.div`
         background: #4CAF50;
         cursor: pointer;
       }
+      #additional{
+          position: relative;
+      }
       #submit{
           text-align: center;
           padding: 150px 0px 200px 0px;
+          background-color: rgba(228, 231, 237, 0.8);
+          position:absolute;
+          left: 0;
+        right: 0;
+        margin: auto
+          border-radius: 25px;
+          z-index: 3000;
+        width: 100%;
+        height: 90%;
+        
           h2{
               font-weight: 700;
               padding: 0px 0px 20px 0px;
@@ -247,14 +302,20 @@ const DeployServiceMeshWrapper = styled.div`
               width: 60px;
           }
       }
-
+      
       #bot{
+          /*
+        switch this section to React Row, Col
+        */
+        display: flex;
           #green{
               background: #00B39F;
-              width: 100%;
+              
               min-height: 400px;
+              width: 60%;
               #text{
                   color: white;
+                  
                   padding: 120px 70px 120px 150px;
                   #cover{
                       border: 2px solid white;
@@ -270,26 +331,70 @@ const DeployServiceMeshWrapper = styled.div`
                   }
               }
           }
+          /*
+        switch this section to React Row, Col
+        */
           #book{
-
-            background-image: url(${bookCover});
+            /* background-image: url(${bookCover}); */
             background-repeat: no-repeat;
-            background-attachment: fixed;
-            background-position: right;
+            /* background-attachment: fixed; */
+            background-position: center;
+            /* min-height: 800px; */
+            vertical-align: center;
+            background-size: contain;
             
-            @media only screen and (min-width: 1600px) {
-                background-size: 55em 2000px;
-            }
-            @media only screen and (max-width: 1400px) {
-                background-size: 50em 2000px;
-            }
-            @media only screen and (max-width: 1200px) {
-                background-size: 45em 2000px;
-            }
-            @media only screen and (max-width: 1000px) {
-                background-size: 40em 2000px;
+            #spacer{
+                minwidth: 850px;
+                vertical-align: center;
             }
           }
+      }
+      .tooltip {
+        position: relative;
+        display: inline-block;
+      }
+      
+      .tooltip .tooltiptext {
+        visibility: hidden;
+        
+        background-color: black;
+        color: #fff;
+        text-align: center;
+        border-radius: 6px;
+        padding: 5px 0;
+        font-size: 1rem;
+        
+        /* Position the tooltip */
+        position: absolute;
+        z-index: 1;
+        bottom: 100%;
+        left: 50%;
+        padding: 1rem;
+        margin-left: -140px;
+        width: 12.5rem;
+        font-size: .9125rem;
+        line-height: 1.25rem;
+        text-align: center;
+        width: 300px;
+        color: ${props => props.theme.white};
+        background: ${props => props.theme.tertiaryColor};
+        z-index: 3000;
+        border-bottom-right-radius: 0.8rem;
+        border-bottom-left-radius: 0.8rem;
+        transition: all 0.5s ease;
+        p {
+            margin: 0;
+        }
+        &:hover {
+            cursor: default;
+            .tooltiptext {
+                transform: translateY(-0.5rem);
+            }
+        }
+      }
+      
+      .tooltip:hover .tooltiptext {
+        visibility: visible;
       }
 `;
 
