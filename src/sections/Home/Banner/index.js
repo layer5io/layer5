e75f283eec560efeedf9ae8cf52787e3eef7d08d
vineethvanga18 <reddy.8@iitj.ac.in>
2021-03-02 01:55:06 +0530

    updates on banner
    Signed-off-by: vineethvanga18 <reddy.8@iitj.ac.in>

--- src/sections/Home/Banner/index.js
+++ src/sections/Home/Banner/index.js
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from "react";
 import BannerDefault from "../Banner-default";
-// import BannerAlt from "../Banner-alt"; 
+// import BannerAlt from "../Banner-alt";
 
 const Banners = [
   <BannerDefault key={0} />,
-  // <BannerAlt key={1}/> 
+  // <BannerAlt key={1} /> 
 ];
 
 const Banner = () => {
-  const [Banner, setBanner] = useState(0);
+  let initialValue;
+  try {
+    initialValue = sessionStorage.getItem("banner") || 0;
+  } catch (e) {
+    initialValue = 0;
+  }
+  const [Banner, setBanner] = useState(initialValue);
   useEffect(() => {
     if (sessionStorage.getItem("banner")) {
       sessionStorage.setItem("banner", (1 + Number(Banner)) % Banners.length);
     } else {
-      sessionStorage.setItem("banner", 0);
-    }
-  });
-  useEffect(() => {
-    if (Banner != sessionStorage.getItem("banner")) {
-      setBanner(sessionStorage.getItem("banner"));
+      sessionStorage.setItem("banner", 1);
     }
   });
   return (Banners[Banner]);
