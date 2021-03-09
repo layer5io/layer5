a01c6fde49b035b1571af9701427f9a6631de523
Tanuj22 <agarwal.5@iitj.ac.in>
2021-03-10 03:40:17 +0530

    fix link
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- src/sections/Home/Projects-home/index.js
+++ src/sections/Home/Projects-home/index.js
@@ -28,15 +28,12 @@ const Projects = () => {
                 Operate any service mesh <span>with confidence</span>.
               </h2>
             </SectionTitle>
-            {/* <p className="project-text">
-                        Layer5 projects establish industry standards and enable service developers, owners, and operators with repeatable patterns and best practices for managing all aspects of distributed services.
-            </p> */}
           </div>
         </Row>
         <Row>
           <div className="project__block__wrap">
             <Col sm={12} md={6} lg={3}>
-              <Link className="project-card" to="/landscape">
+              <Link className="project-card" to="/service-mesh-landscape">
                 <div className="project__block__inner">
                   <img src={projectImage1} alt="appion project" />
                   <h5>Landscape</h5>
@@ -45,7 +42,7 @@ const Projects = () => {
               </Link>
             </Col>
             <Col sm={12} md={6} lg={3}>
-              <Link className="project-card" to="/meshery">
+              <Link className="project-card" to="/service-mesh-management/meshery">
                 <div className="project__block__inner">
                   <img src={projectImage3} alt="appion project" />
                   <h5>Meshery</h5>
