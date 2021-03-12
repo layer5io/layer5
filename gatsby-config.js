559506bd3b15d1208d10e9a665ebe25c71ad6d4f
Nikhil-Ladha <nikhilladha1999@gmail.com>
2021-03-12 21:49:40 +0530

    Add RSS feed capability
    Signed-off-by: Nikhil-Ladha <nikhilladha1999@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,53 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-plugin-feed",
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allPosts }}) => {
+              return allPosts.nodes.map( node => {
+                return Object.assign({}, node.frontmatter, {
+                  // description: node.excerpt,
+                  // date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  // custom_elements: [{ "content:encoded": node.html }],
+                });
+              });
+            },
+            query: `
+              {
+                allPosts: allMdx(
+                  filter: { frontmatter: { published: { eq: true } } }
+                ) {
+                  nodes {
+                    fields {
+                      collection
+                      slug
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+          },
+        ],
+      },
+    },
+    {
       resolve: "gatsby-plugin-styled-components",
       options: {
         minify: false,
