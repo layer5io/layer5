facc963c41a1a37969ac633d7d1e15e0528be648
chinmaym07 <b418020@iiit-bh.ac.in>
2021-03-14 19:02:41 +0530

    Refactored Partner's Page
    Signed-off-by: chinmaym07 <b418020@iiit-bh.ac.in>

--- src/sections/Partners/partner.style.js
+++ src/sections/Partners/partner.style.js
@@ -4,25 +4,34 @@ const PartnerWrapper = styled.section`
     position: relative;
     min-height: 100%;
     top: 0px;
-    .partnerHeader {
-        padding: 6rem 0;
-        background: ${props => props.theme.tertiaryColor};
-        text-align: center;
-        h1{
-            color: ${props => props.theme.white};
-        }
-        p{
-            margin: 1.5rem auto 4rem;
-            max-width: 60%;
+    .partner-container{
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        background-color: ${props => props.theme.darkJungleGreenColor};
+        color: white;
+        padding: 4rem 0;
+        .partner-container-head{
+            padding:2rem;
             text-align: center;
-            color: ${props => props.theme.white};
+            h1{
+                color:white;
+            }
+            
+        }
+        .partner-container-body{
+            width:60%;
+            display: flex;
+            justify-content: center;
+            padding:2rem;
         }
     }
+    
     .container {
         position: relative;
         text-align: justify;
-        margin: 0 auto;
-        margin-bottom: 40px;
+        margin: 0 2rem;
         max-width: 1280px;
         z-index: 1;
         padding: 1em;
@@ -52,14 +61,21 @@ const PartnerWrapper = styled.section`
         font-size: 2.75rem;
     }
     h2 {
-        font-weight: 300;
+        font-weight: 600;
+        margin: 1rem 0rem;
     }
     .img1 {
-        max-height: 160px;
-        max-width: 130px;
+        max-height: 256px;
+        width: 256px;
         position: relative;
         margin: auto;
-        text-align: center;
+        
+        object-fit: cover;
+        paddingTop: 1.0625em 
+    }
+
+    .custom-col{
+        display: flex;
     }
     .para {
         text-align: center;
@@ -117,24 +133,141 @@ const PartnerWrapper = styled.section`
         float: center;
     }
     .btn-section {
-        
         display: flex;
-        
         text-align: center;
         margin: auto;
         vertical-align: middle;
-
     }
-    .partner-button-row {
-        justify-content: center;
-    }
-    .partner-button {
+    .partner-button-cont{
         text-align: center;
+        padding:2rem;
+        margin:1rem;
+        .partner-btn-cont-head{
+            padding: 2rem;
+        }
+        .partner-btn-cont-body{
+            padding: 1rem;
+        }
     }
-
-    @media only screen and (max-width: 760px) {
-        .img1 {
-            display:none;
+    .partner-button{
+        margin: auto 0;
+    }
+    .custom-btn{
+        margin:1rem 0;
+    }
+    .academic-container{
+        display: flex;
+        justify-content: center;
+        padding: 2rem;
+        margin: 1rem;
+        .acad-container-body{
+            width:78%;
+            text-align: center;
+        }
+    }
+    .partner-btn{
+        border-radius: 50%;
+        display: flex;
+        height: 140px;
+        flex-direction: column;
+        p{
+            margin:0;   
+            color:white;
+        }
+    }
+    .cont-row-reverse{
+        flex-direction: row-reverse;
+    }
+    .cont-row{
+        flex-direction: row;
+    }
+    @media only screen and (max-width: 1500px) {
+        .partner-container{
+            .partner-container-body{
+                width:70%;
+            }
+        }
+    }
+    @media only screen and (max-width: 1100px) {
+        .partner-container{
+            .partner-container-body{
+                width:90%;
+            }
+        }
+        .academic-container{
+            .acad-container-body{
+                width:90%;
+            }
+        }
+        .container{
+            margin: 0;
+        }
+        .partner-btn{
+            height: 130px;
+        }
+    }
+    @media only screen and (max-width: 768px) {
+        .partner-container{
+            .partner-container-body{
+                width:100%;
+                padding:1rem;
+            }
+            .partner-button{
+                display: none;
+            }
+        }
+        .academic-container{
+            padding:0rem;
+            .acad-container-body{
+                width:100%;        
+            }
+        }   
+        .cont-row-reverse{
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+        .cont-row{
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+        .container{
+            padding:1rem 0;
+            margin:1rem 0;
+        }
+        .custom-col{
+            flex:0 0 30%;
+            max-width:30%;
+        }
+        .partner-button-cont{
+            
+            padding:2rem 0;
+            margin:1rem 0;
+            .partner-btn-cont-head{
+                padding: 2rem 0;
+            }
+            .partner-btn-cont-body{
+                padding: 1rem 0;
+            }
+        }
+    }
+    @media only screen and (max-width:576px){
+        .custom-col{
+            flex: 0 0 35%;
+            max-width: 35%;
+        }
+    }
+    @media only screen and (max-width:476px){
+        .custom-col{
+            flex: 0 0 40%;
+            max-width: 40%;
+        }
+    }
+    @media only screen and (max-width:376px){
+        .custom-col{
+            flex: 0 0 50%;
+            max-width: 50%;
         }
     }
 `;
