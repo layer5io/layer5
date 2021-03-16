85f474ff485270c6d1c64ab4445ad170e13527b3
chinmaym07 <b418020@iiit-bh.ac.in>
2021-03-17 02:24:46 +0530

    Added Learn More Part & smooth transition of the accordion
    Signed-off-by: chinmaym07 <b418020@iiit-bh.ac.in>

--- src/sections/Partners/index.js
+++ src/sections/Partners/index.js
@@ -5,20 +5,21 @@ import {partners} from "./partners";
 import PartnerWrapper from "./partner.style";
 import { BsArrowDown } from "react-icons/bs";
 import { BsArrowUp } from "react-icons/bs";
+import Partner_Icon from "../../assets/images/contact/partner_white.svg";
 
 const Partner = () => {
   let academic_partners = partners.filter(partner => partner.type === "academic");
   let technology_partners = partners.filter(partner => partner.type === "technology");
-  
+  let len_acad_part = academic_partners.length;
   const [isOpen, setIsOpen] = useState(false);
   const [ID, setID] = useState(-1);
 
-
   const toggleActive = (id) => {
+    
     if(isOpen){
       if(ID === id){
         setIsOpen(false);
-        setID("");
+        setID(-1);
       } else{
         setIsOpen(false);
         setID(id);
@@ -27,7 +28,7 @@ const Partner = () => {
     } else{
       setID(id);
       setIsOpen(true);
-    }
+    } 
   };
 
   return (
@@ -42,11 +43,7 @@ const Partner = () => {
             <p>Layer5 is dedicated to partnering with leading educational institutions, technology vendors, solution providers and industry experts to drive the adoption of cloud native technologies focused on the evolution of how developers write modern applications, how operators run infrastructure and how product owners manage service offerings.</p>      
           </div>
           <div className="partner-button">
-            <Button primary className="partner-btn" url="mailto:partners@layer5.io?subject=Partner Program" external={true}>
-              <p>Become</p>
-              <p>A</p>
-              <p>Partner</p>
-            </Button>
+            <Button primary className="partner-btn" url="mailto:partners@layer5.io?subject=Partner Program" external={true} icon={Partner_Icon} title="Become A Partner" />
           </div>
         </div>
       </div>
@@ -59,64 +56,129 @@ const Partner = () => {
         </div>
         {
           academic_partners.map((partner,index) => (
-            <Row key={index} className={ID === index ? "active cont-row" : (index) % 2 === 1 ? "cont-row-reverse" : "cont-row"} >
-              <Col xs={5} sm={3} lg={2} className={ID === index? "custom-col-active":"custom-col"}>
-                <div className="img1">
-                  <a href={partner.imageRoute} target="_blank" rel="noopener noreferrer">
-                    <img  src={partner.imageLink} title="Click to know More about our partner" alt={partner.name} />
-                  </a>
-                </div>
-              </Col>
-              <Col xs={12} sm={9} lg={10} className={ID === index? "custom-col-active-desc":""}>
-                <div className="container">
-                  <h2>{partner.name}</h2>
-                  <div>
-                    {partner.desc}
+            <div key={index}>
+              <Row  className={(index) % 2 === 1?"cont-row-reverse":"cont-row"} >
+                <Col xs={5} sm={3} lg={2} className="custom-col">
+                  <div className="img1">
+                    <a href={partner.imageRoute} target="_blank" rel="noopener noreferrer">
+                      <img  src={partner.imageLink} title="Click to know More about our partner" alt={partner.name} />
+                    </a>
                   </div>
-                  {
-                    isOpen && ID === index ?<div className="moreInfo-div">
-                      {partner.moreInfo}
-                    </div>:null
-                  }
-                  {
-                    isOpen && ID === index? <button onClick={() => toggleActive(index)} className="readmeBtn"> Read Less <BsArrowUp className="icon" size={30} /></button> :
-                      <button onClick={() => toggleActive(index)} className="readmeBtn readmreBtn"> Read More <BsArrowDown className="icon" size={30} /></button> 
-                  }
-                </div>
-              </Col>
-            </Row>
+                </Col>
+                <Col xs={12} sm={9} lg={10}>
+                  <div className="container">
+                    <h2>{partner.name}</h2>
+                    <div>
+                      {partner.desc}
+                    </div>
+                    {
+                      isOpen && ID === index? <button onClick={() => toggleActive(index)} className="readmeBtn"> Read Less <BsArrowUp className="icon" size={30} /></button> :
+                        <button onClick={() => toggleActive(index)} className="readmeBtn readmreBtn"> Read More <BsArrowDown className="icon" size={30} /></button> 
+                    }
+                  </div>
+                </Col>
+              </Row>
+              {
+                index % 2 == 1 ? <Row className={isOpen && ID === index ? "row-elem moreInfo-div-active cont-row-reverse": "row-elem moreInfo-div cont-row-reverse"}>
+                  <Col xs={5} sm={3} lg={4} className="custom-col">
+                    <div className="img1">
+                      <a href={partner.imageRoute} target="_blank" rel="noopener noreferrer">
+                        <img  src={partner.imageLink} title="Click to know More about our partner" alt={partner.name} />
+                      </a>
+                    </div>
+                  </Col>
+                  <Col xs={12} sm={9} lg={8}>
+                    <div className={isOpen&& ID === index ? "container container-active":"container"}>
+                      <h2>{partner.name}</h2>
+                      <div>
+                        {partner.moreInfo}
+                      </div>
+                    </div>
+                  </Col>
+                </Row> : <Row className={isOpen && ID === index ? "row-elem moreInfo-div-active cont-row": "row-elem moreInfo-div cont-row"}>
+                  <Col xs={5} sm={3} lg={4} className="custom-col">
+                    <div className="img1">
+                      <a href={partner.imageRoute} target="_blank" rel="noopener noreferrer">
+                        <img  src={partner.imageLink} title="Click to know More about our partner" alt={partner.name} />
+                      </a>
+                    </div>
+                  </Col>
+                  <Col xs={12} sm={9} lg={8}>
+                    <div className={isOpen&& ID === index ? "container container-active":"container"}>
+                      <h2>{partner.name}</h2>
+                      <div>
+                        {partner.moreInfo}
+                      </div>
+                    </div>
+                  </Col>
+                </Row>
+              }
+            </div>
           ))
         }
         <h1 className="heading">Our Technology Partners</h1>
         {
           technology_partners.map((partner,index) => (
-            <Row key={index+1} className={ID === index ? "active cont-row" :(academic_partners.length % 2 == 0 && index % 2 == 1) || (academic_partners.length % 2 == 1 && index % 2 == 0)?"cont-row-reverse":"cont-row"}>
-              <Col xs={5} sm={3} lg={2} className={ID === index? "custom-col-active":"custom-col"}>
-                <div className="img1">
-                  <a href={partner.imageRoute} target="_blank" rel="noopener noreferrer">
-                    <img src={partner.imageLink} title="Click to know More about our partner" alt={partner.name} />
-                  </a>
-                </div>
-              </Col>
-              <Col xs={12} sm={9} lg={10} className={ID === index? "custom-col-active-desc":""}>
-                <div className="container">
-                  <h2>{partner.name}</h2>
-                  <div>
-                    {partner.desc}
+            <div key={index+len_acad_part}>
+              <Row  className={((len_acad_part+ index) % 2 == 1)?"cont-row-reverse":"cont-row"}>
+                <Col xs={5} sm={3} lg={2} className="custom-col">
+                  <div className="img1">
+                    <a href={partner.imageRoute} target="_blank" rel="noopener noreferrer">
+                      <img src={partner.imageLink} title="Click to know More about our partner" alt={partner.name} />
+                    </a>
                   </div>
-                  {
-                    isOpen ?<div className="moreInfo-div">
-                      {partner.moreInfo}
-                    </div>:null
-                  }
-                  {
-                    isOpen && ID === index? <button onClick={() => toggleActive(index)} className="readmeBtn"> Read Less <BsArrowUp className="icon" size={30} /></button> :
-                      <button onClick={() => toggleActive(index)} className="readmeBtn readmreBtn"> Read More <BsArrowDown className="icon" size={30} /></button> 
-                  }
-                  
-                </div>
-              </Col>
-            </Row>
+                </Col>
+                <Col xs={12} sm={9} lg={10}>
+                  <div className="container">
+                    <h2>{partner.name}</h2>
+                    <div>
+                      {partner.desc}
+                    </div>
+                    {
+                      isOpen && ID === index+len_acad_part? <button onClick={() => toggleActive(index+len_acad_part)} className="readmeBtn"> Read Less <BsArrowUp className="icon" size={30} /></button> :
+                        <button onClick={() => toggleActive(index+len_acad_part)} className="readmeBtn readmreBtn"> Read More <BsArrowDown className="icon" size={30} /></button> 
+                    }
+                  </div>
+                </Col>
+              </Row>
+              {
+                (len_acad_part+ index) % 2 == 1?<Row className={isOpen && ID === index + len_acad_part? "row-elem moreInfo-div-active cont-row-reverse": "row-elem moreInfo-div cont-row-reverse"}>
+                  <Col xs={5} sm={3} lg={4} className="custom-col">
+                    <div className="img1">
+                      <a href={partner.imageRoute} target="_blank" rel="noopener noreferrer">
+                        <img  src={partner.imageLink} title="Click to know More about our partner" alt={partner.name} />
+                      </a>
+                    </div>
+                  </Col>
+                  <Col xs={12} sm={9} lg={8}>
+                    <div className={isOpen&& ID === index+len_acad_part ? "container container-active":"container"}>
+                      <h2>{partner.name}</h2>
+                      <div>
+                        {partner.moreInfo}
+                        {partner.desc}
+                      </div>
+                    </div>
+                  </Col>
+                </Row> : <Row  className={isOpen && ID === index + len_acad_part ? "row-elem moreInfo-div-active cont-row": "row-elem moreInfo-div cont-row"}>
+                  <Col xs={5} sm={3} lg={4} className="custom-col">
+                    <div className="img1">
+                      <a href={partner.imageRoute} target="_blank" rel="noopener noreferrer">
+                        <img  src={partner.imageLink} title="Click to know More about our partner" alt={partner.name} />
+                      </a>
+                    </div>
+                  </Col>
+                  <Col xs={12} sm={9} lg={8}>
+                    <div className={isOpen&& ID === index+len_acad_part ? "container container-active":"container"}>
+                      <h2>{partner.name}</h2>
+                      <div>
+                        {partner.moreInfo}
+                        {partner.desc}
+                      </div>
+                    </div>
+                  </Col>
+                </Row>
+              }
+            </div>
           ))
         }
         <div className='partner-button-cont'>
@@ -128,7 +190,7 @@ const Partner = () => {
           </div>
         </div>
       </Container>
-    </PartnerWrapper >
+    </PartnerWrapper>
   );
 };
 
