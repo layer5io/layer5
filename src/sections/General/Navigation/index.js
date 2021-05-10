f139637b236201616b5ea3ce846f90bb7d8a1d11
chinmaym07 <b418020@iiit-bh.ac.in>
2021-05-10 22:00:15 +0530

    Oops DCO
    Signed-off-by: chinmaym07 <b418020@iiit-bh.ac.in>

--- src/sections/General/Navigation/index.js
+++ src/sections/General/Navigation/index.js
@@ -8,7 +8,6 @@ import { Container } from "../../../reusecore/Layout";
 import layer5_logo from "../../../assets/images/app/layer5.svg";
 import smp_dark_text from "../../../assets/images/service-mesh-performance/stacked/smp-dark-text.svg";
 import meshery from "../../../assets/images/meshery/icon-only/meshery-logo-light.svg";
-
 import Data from "./utility/menu-items.js";
 import ScrollspyMenu from "./utility/ScrollspyMenu.js";
 
