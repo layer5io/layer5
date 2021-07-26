d2b341f2ad038788b5eb82c081eff25f25ba5e10
Nikhil-Ladha <nikhilladha1999@gmail.com>
2021-07-27 01:25:20 +0530

    Learn: Updated the design of Learning path and Pre-req cards
    Signed-off-by: Nikhil-Ladha <nikhilladha1999@gmail.com>

--- src/components/Learn-Components/Setup-Pre-Requisites/setup-pre-req.style.js
+++ src/components/Learn-Components/Setup-Pre-Requisites/setup-pre-req.style.js
@@ -13,16 +13,29 @@ const SetupPreReqWrapper = styled.div`
         a {
             color: black;
             margin: 1rem;
-            
         }
         .card {
             padding: 2rem;
             background-color: #eee;
             max-width: 350px;
             min-height: 200px;
-            h2 {
-                font-size: 1.6rem;
-                margin: 1rem 0;
+            .card-header {
+                display: flex;
+                flex: auto;
+                img {
+                    height: 3rem;
+                    align-self: center;
+                    flex: 0 0 25%;
+                }
+                svg {
+                    font-size: 4.15rem;
+                    align-self: center;
+                    flex: 0 0 25%;
+                }
+                h2 {
+                    font-size: 1.75rem;
+                    margin: 1rem 0;
+                }
             }
         }
     }
@@ -40,6 +53,16 @@ const SetupPreReqWrapper = styled.div`
             .card{
                 margin: 0;
                 max-width: none;
+                .card-header {
+                    img {
+                        height: 2rem;
+                        flex: 0 0 10%;
+                    }
+                    svg {
+                        font-size: 2.15rem;
+                        flex: 0 0 10%;
+                    }
+                }
             }
             a{
                 margin: 1rem 0rem;
