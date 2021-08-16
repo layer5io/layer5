eaa6e66e33072d1924bb9e553c6e3dffcb2c34f5
Jassi10000 <jaskeerat10000@gmail.com>
2021-08-16 18:03:58 +0530

    Updated the meeting minutes with correct link of docs of Layer5  NewComers Meeting
    Signed-off-by: Jassi10000 <jaskeerat10000@gmail.com>

--- src/sections/Community/Handbook/repository.js
+++ src/sections/Community/Handbook/repository.js
@@ -46,13 +46,13 @@ const Repository= () => {
 
           <h2>Frontend Projects</h2>
           <p>
-          <li><a href="https://layer5.io/">Layer.io</a></li> 
-          <li><a href="https://meshery.io">Meshery.io</a></li>
-          <li><a href="http://docs.meshery.io">Meshery Documentation</a></li>
-          <li><a href="https://smp-spec.io">Service Mesh Performance</a></li>
-          <li><a href="https://github.com/layer5io/meshery/tree/master/ui">Meshery UI</a></li>
-          <li><a href="https://docs.google.com/document/d/1lHfMo4iIx2WXFZIspfHyxTsPR1T63_2IV5NUkgxoo0w/edit#"> GetNighthawk</a></li>
-          <li><a href="https://drive.google.com/a/layer5.io/open?id=1AMi5UTUwLRFeQRQj7YuRQgMG-4IXWie24z4R88Eoazs">MeshSync UI (MeshMap)</a></li>
+            <li><a href="https://layer5.io/">Layer.io</a></li> 
+            <li><a href="https://meshery.io">Meshery.io</a></li>
+            <li><a href="http://docs.meshery.io">Meshery Documentation</a></li>
+            <li><a href="https://smp-spec.io">Service Mesh Performance</a></li>
+            <li><a href="https://github.com/layer5io/meshery/tree/master/ui">Meshery UI</a></li>
+            <li><a href="https://docs.google.com/document/d/1lHfMo4iIx2WXFZIspfHyxTsPR1T63_2IV5NUkgxoo0w/edit#"> GetNighthawk</a></li>
+            <li><a href="https://drive.google.com/a/layer5.io/open?id=1AMi5UTUwLRFeQRQj7YuRQgMG-4IXWie24z4R88Eoazs">MeshSync UI (MeshMap)</a></li>
           </p>
 
           <h2>Backend Projects</h2>
@@ -66,27 +66,27 @@ const Repository= () => {
           <h3><span className="number">2</span>Adapter Dedicated Repos</h3>
           <Row>
             <Col lg={6} md={6} sm={12}>
-          <p>
-            <li><a href="https://github.com/layer5io/meshery-istio">Meshery-istio</a></li>
-            <li><a href="https://github.com/layer5io/meshery-linkerd">Meshery-linkerd</a></li>
-            <li><a href="https://github.com/layer5io/layer5-istio-adapter">Layer5-istio-adapter</a></li>
-            <li><a href="https://github.com/layer5io/meshery-nsm">Meshery-nsm</a></li> 
-            <li><a href="https://github.com/layer5io/meshery-octarine">Meshery-octarine</a></li>
-            <li><a href="https://github.com/layer5io/meshery-traefik-mesh">Meshery-traefik-mesh</a></li>
-            <li><a href="https://github.com/layer5io/meshery-kuma">Meshery-kuma</a></li>
-          </p>
-          </Col>
-          <Col lg={6} md={6} sm={12}>
-          <p>
-            <li><a href="https://github.com/layer5io/meshery-consul">Meshery-consul</a></li>
-            <li><a href="https://github.com/layer5io/meshery-cpx">Meshery-cpx</a></li> 
-            <li><a href="https://github.com/layer5io/meshery-adapter-template">Meshery-adapter-template</a></li> 
-            <li><a href="https://github.com/layer5io/meshery-tanzu-sm">Meshery-tanzu-sm</a></li> 
-            <li><a href="https://github.com/layer5io/meshery-nginx-sm">Meshery-Nginx-sm</a></li>  
-            <li><a href="https://github.com/layer5io/meshkit">Meshkit</a></li> 
-            <li><a href="https://github.com/layer5io/meshery-adapter-library">Meshery-adapter-library</a></li> 
-          </p>
-          </Col>
+              <p>
+                <li><a href="https://github.com/layer5io/meshery-istio">Meshery-istio</a></li>
+                <li><a href="https://github.com/layer5io/meshery-linkerd">Meshery-linkerd</a></li>
+                <li><a href="https://github.com/layer5io/layer5-istio-adapter">Layer5-istio-adapter</a></li>
+                <li><a href="https://github.com/layer5io/meshery-nsm">Meshery-nsm</a></li> 
+                <li><a href="https://github.com/layer5io/meshery-octarine">Meshery-octarine</a></li>
+                <li><a href="https://github.com/layer5io/meshery-traefik-mesh">Meshery-traefik-mesh</a></li>
+                <li><a href="https://github.com/layer5io/meshery-kuma">Meshery-kuma</a></li>
+              </p>
+            </Col>
+            <Col lg={6} md={6} sm={12}>
+              <p>
+                <li><a href="https://github.com/layer5io/meshery-consul">Meshery-consul</a></li>
+                <li><a href="https://github.com/layer5io/meshery-cpx">Meshery-cpx</a></li> 
+                <li><a href="https://github.com/layer5io/meshery-adapter-template">Meshery-adapter-template</a></li> 
+                <li><a href="https://github.com/layer5io/meshery-tanzu-sm">Meshery-tanzu-sm</a></li> 
+                <li><a href="https://github.com/layer5io/meshery-nginx-sm">Meshery-Nginx-sm</a></li>  
+                <li><a href="https://github.com/layer5io/meshkit">Meshkit</a></li> 
+                <li><a href="https://github.com/layer5io/meshery-adapter-library">Meshery-adapter-library</a></li> 
+              </p>
+            </Col>
           </Row>
           <h3><span className="number">3</span>Installation Dedicated Repos </h3>
           <p>
