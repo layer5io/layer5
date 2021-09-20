5697147634285acdb34aa8b6902fc33cbeabc2f3
Goodness Chris-Ugari <goodnesschrisugari@yahoo.com>
2021-09-20 12:40:54 +0100

    Added padding and fixed image links
    Signed-off-by: Goodness Chris-Ugari <goodnesschrisugari@yahoo.com>

--- src/sections/Community/Handbook/community.js
+++ src/sections/Community/Handbook/community.js
@@ -3,19 +3,26 @@ import { Container, Row, Col } from "../../../reusecore/Layout";
 import { HandbookWrapper } from "./Handbook.style";
 import TOC from "../../../components/handbook-navigation/index";
 import {Link} from "gatsby";
-import CGB from "./images/community-green.svg";
-import PL from "./images/patterns-logo.png";
-import LG from "./images/landscape-green.png";
-import LIH from "./images/layer5-image-hub.svg";
-import MLL from "./images/meshery-logo-light.svg";
-import MOD from "./images/meshery-operator-dark.svg";
-import SDT from "./images/smp-dark-text.svg";
-import GNL from "./images/getnighthawk-logo.png";
+import Point from "./images/bullet.svg";
+import CommunityLogo from "../../../assets/images/community/community-green.svg";
+import PatternsLogo from "./images/patterns-logo.png";
+import LandscapeGreen from "../../../assets/images/landscape/layer5_landscape_green.svg";
+import ImageHubLogo from "../../../assets/images/image-hub/layer5-image-hub.svg";
+import MesheryLogo from "../../../assets/images/meshery/icon-only/meshery-logo-light.svg";
+import MesheryOperator from "../../../assets/images/meshery-operator/meshery-operator-dark.svg";
+import ServiceMeshPerformance from "../../../assets/images/service-mesh-performance/stacked/smp-dark-text.svg";
+import GetNightHawkLogo from "../../../assets/images/getnighthawk/icon-only/SVG/getnighthawk-logo.svg";
 
 const badgeStyle = {
   height: "25px",
   width: "25px",
   marginRight: "5px"
+};
+const badgeListStyle = {
+  listStyleType: "none"
+};
+const paddingBottom18 = {
+  paddingBottom: "18px"
 };
 
 const CommunityGuide= () => {
@@ -28,44 +35,53 @@ const CommunityGuide= () => {
       <div className="page-section">
         <Container>
           <div className="content">
-            <h2 id="communityGuidelines">Community Guidelines</h2>
+            <h2 id="communityGuidelines" style={paddingBottom18}>Community Guidelines</h2>
             <p>
               Welcome to the Layer5 community! We’re happy to have you here and handhold you for your first contribution. Here are some tips to help you get started:
             </p>
             <p>
+              <img className="logo" src={Point} />
               <strong>For Easy Identification -</strong> When setting up your slack workspace, your display name should be the same as your GitHub username for easy identification,
               engage freely, and in whatever form you will: as a listener, user, contributor, maintainer, ambassador, or bystander. Involvement in any way is warmly received.
             </p>
             <p>
+              <img className="logo" src={Point} />
               <strong>Familiarize with the community -</strong> As you get to familiarize yourself with the community, know that there are multiple initiatives for you to explore.
               Some initiatives have working groups that meet at different times each week, while others use the weekly community meeting as their place of discussion. Details of each can be found on the <a href="https://bit.ly/2SbrRhe">community calendar</a>.
               Everyone is welcome to join the calls and engage with other community members over Slack, the mailing list, and so on.
             </p>
             <p>
+              <img className="logo" src={Point} />
               <strong>Familiarize with the projects -</strong> The following is a sample of the many active, ongoing community projects. One way of doing so is to review the Layer5 Repository Overview section of this handbook to get a better understanding of each project.
               This document will help you to understand the projects based on their technology domain(s). Be sure to star the community’s <a href="https://github.com/layer5io">repositories</a> on GitHub!
             </p>
             <p>
+              <img className="logo" src={Point} />
               <strong>Be friendly and open-minded -</strong> We encourage you to be welcoming to new collaborators and those just getting started. Other contributors might not have the same degree
               of experience or background as you, but that doesn't mean they can't provide useful ideas.
             </p>
             <p>
+              <img className="logo" src={Point} />
               <strong>Be honest -</strong> “A half-truth is a whole lie.” Being truthful allows you to reach a better agreement. As a result, be open and honest about who you are, what you do, and how you want to accomplish it.
             </p>
             <p>
+              <img className="logo" src={Point} />
               <strong>Be respectful and inclusive -</strong> We are a diverse group of people with diverse backgrounds and opinions. We expect everyone to be civil and professional in their activities. Disrespectful behavior is not tolerated.
               Do not publish anything that would be considered insulting, abusive, or hate speech by a reasonable person.
             </p>
             <p>
+              <img className="logo" src={Point} />
               <strong>Collaborate and Contribute -</strong> Members are expected to attend community and workgroup meetings, find ways to help, check up on our Github page regularly etc. Within the community and outside, we encourage teamwork.
             </p>
             <p>
+              <img className="logo" src={Point} />
               <strong>Resources -</strong> Be sure to access the resources in the <a href="https://drive.google.com/drive/u/0/folders/0ABH8aabN4WAKUk9PVA">community drive</a> and sign-up on the <a href="https://layer5.io/subscribe">community mailer</a>.
               Ask for a copy of <a href="https://layer5.io/books/the-enterprise-path-to-service-mesh-architectures">The Enterprise Path to Service Mesh Architectures</a>.
             </p>
             <p>
+              <img className="logo" src={Point} />
               <strong>We do not allow:</strong>
-              <ul>
+              <ul >
                 <li>Threats of violence</li>
                 <li>Sexually obscene content</li>
                 <li>Bullying</li>
@@ -76,12 +92,12 @@ const CommunityGuide= () => {
               </ul>
             </p>
 
-            <h3>Community Manager</h3>
+            <h3 style={paddingBottom18}>Community Manager</h3>
             <p>
               A Community Manager is a  person who has an innate drive to contribute to the community's prosperity. A community manager serves as a link between the organisation and its community, overcoming obstacles as they arise—or even before they arise!—by
               collaborating with other departments and the community to find solutions that benefit the entire community.
             </p>
-            <h3>Meshmates</h3>
+            <h3 style={paddingBottom18}>Meshmates</h3>
             <p>
               Layer5 MeshMates are committed to helping community members be successful contributors. MeshMates aid in identifying areas of projects to engage within, working groups to join, and helping community members grow in their open-source and cloud-native knowledge.
               By connecting one-on-one, MeshMates will share tips on how to have the best community experience possible.
@@ -91,34 +107,34 @@ const CommunityGuide= () => {
               MeshMates are Layer5 ambassadors (not employees) and their commitment to helping others and sharing their expertise has a huge impact on the Layer5 community – don’t hesitate to reach out to them!
             </p>
 
-            <h3>Contributors</h3>
+            <h3 style={paddingBottom18}>Contributors</h3>
             <p>
               As you contribute to the Layer5 community, do follow the <a href="https://github.com/layer5io/layer5/blob/master/CONTRIBUTING.md">contributing guidelines</a> to make sure you are on the right track. For information about the community, please refer to the <Link to="/community/handbook/community/#communityGuidelines">community guidelines</Link>.
               Always remember to reach out to a <Link to="/community/meshmates">MeshMate</Link> when in doubt or in need of a helping hand.
             </p>
 
-            <h3>Badges</h3>
+            <h3 style={paddingBottom18}>Badges</h3>
             <p>
             As you continue your contributions to this community, you stand a chance to receive a badge in acknowledgement of your engagement within and/or contribution to the representative project or (sub-)community. A variety of badges exist so that community members and their efforts may be affiliated with a particular project or community initiative.
             </p>
-            <ul>
-              <li><img src={CGB} style={badgeStyle}/>Community</li>
-              <li><img src={LG} style={badgeStyle}/>Landscape</li>
-              <li><img src={LIH} style={badgeStyle}/>Image Hub</li>
-              <li><img src={MLL} style={badgeStyle}/>Meshery</li>
-              <li><img src={MOD} style={badgeStyle}/>Meshery Operator</li>
-              <li><img src={SDT} style={badgeStyle}/>Service Mesh Performance</li>
-              <li><img src={GNL} style={badgeStyle}/>GetNighthawk</li>
-              <li><img src={PL} style={badgeStyle}/>Patterns</li>
+            <ul style={badgeListStyle}>
+              <li><img src={CommunityLogo} style={badgeStyle}/>Community</li>
+              <li><img src={LandscapeGreen} style={badgeStyle}/>Landscape</li>
+              <li><img src={ImageHubLogo} style={badgeStyle}/>Image Hub</li>
+              <li><img src={MesheryLogo} style={badgeStyle}/>Meshery</li>
+              <li><img src={MesheryOperator} style={badgeStyle}/>Meshery Operator</li>
+              <li><img src={ServiceMeshPerformance} style={badgeStyle}/>Service Mesh Performance</li>
+              <li><img src={GetNightHawkLogo} style={badgeStyle}/>GetNighthawk</li>
+              <li><img src={PatternsLogo} style={badgeStyle}/>Patterns</li>
             </ul>
 
-            <h2>Path to Leadership</h2>
-            <h3>What does leadership mean to the Layer5 community?</h3>
+            <h2 style={paddingBottom18}>Path to Leadership</h2>
+            <h3 style={paddingBottom18}>What does leadership mean to the Layer5 community?</h3>
             <p>
               A leader is someone who can contribute to the Layer5 Community's growth by being accountable, participating in decision-making, and feeling responsible.
             </p>
-            <h3>What does it take to be a leader?</h3>
-            <h4><strong>First, you have to be a MeshMate</strong></h4>
+            <h3 style={paddingBottom18}>What does it take to be a leader?</h3>
+            <h4 style={paddingBottom18}><strong>First, you have to be a MeshMate</strong></h4>
             <p>
               Roles/Responsibilities
               <ul>
@@ -128,7 +144,7 @@ const CommunityGuide= () => {
                 <li>Facilitate newcomers call</li>
               </ul>
             </p>
-            <h4><strong>Maintainer</strong></h4>
+            <h4 style={paddingBottom18}><strong>Maintainer</strong></h4>
             <p>
               Roles/Responsibilities
               <ul>
@@ -155,7 +171,7 @@ const CommunityGuide= () => {
             <input type="checkbox" name="maintainer-checklist-7" value="Understands the workflow of the Issues and Pull Requests"/>Understands the workflow of the Issues and Pull Requests<br/>
             <br/>
 
-            <h4><strong>Community Manager</strong></h4>
+            <h4 style={paddingBottom18}><strong>Community Manager</strong></h4>
             <p>
               Roles/Responsibilities
               <ul>
