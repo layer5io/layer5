67dd658f3f7468d9ce474b999eefc4bca7c039a2
shubham-tyagi <raj.tyagi2000@gmail.com>
2021-10-29 00:07:50 +0530

    update project icons : they are now responsive
    Signed-off-by: shubham-tyagi <raj.tyagi2000@gmail.com>

--- src/sections/Community/Handbook/projects.js
+++ src/sections/Community/Handbook/projects.js
@@ -25,22 +25,17 @@ const Maintainer = () => {
             <p>
               <h3>
                 <a href="https://layer5.io/">
-                  <img
-                    margin-left="10%"
-                    width="3.5%"
-                    align="left"
-                    src={layer5icon}
-                  />
-                  &nbsp; Layer5{" "}
+                  <img className="h-icon" src={layer5icon} alt="Layer5" />
+									&nbsp; Layer5{" "}
                 </a>
               </h3>
             </p>
             <p>
-              Its service mesh community represents the largest collection of
-              service Mesh projects.Emerging projects like GetNighthawk{" "}
-              <img width="2.5%" src={hawkIcon} /> , community with{" "}
-              <img width="2.5%" src={meshmateicon} /> MeshMates, catch-all org,
-              “home base”. <br />
+							Its service mesh community represents the largest collection of
+							service Mesh projects.Emerging projects like GetNighthawk{" "}
+              <img className="p-icon" src={hawkIcon} /> , community with{" "}
+              <img className="p-icon" src={meshmateicon} /> MeshMates, catch-all
+							org, “home base”. <br />
               <ul>
                 <li>
                   <h4>
@@ -48,76 +43,70 @@ const Maintainer = () => {
                   </h4>{" "}
                 </li>
                 <p>
-                  Layer5 is the official website of the Layer5 community showing
-                  an overview of the Layer5 projects. The different service mesh
-                  landscapes, resources to learn about service mesh, and
-                  communities. <br />
+									Layer5 is the official website of the Layer5 community showing
+									an overview of the Layer5 projects. The different service mesh
+									landscapes, resources to learn about service mesh, and
+									communities. <br />
                 </p>
                 <li>
                   <h4>
                     <a href="https://layer5.io/projects/image-hub">
-                      Image-Hub :
+											Image-Hub :
                     </a>
                   </h4>{" "}
                 </li>
                 <p>
-                  Image Hub is a sample application written to run on Consul for
-                  exploring WebAssembly modules used as Envoy filters are
-                  written in Rust. These modules can be used to implement
-                  multi-tenancy or to implement per-user rate-limiting in your
-                  application's endpoints. This application was first
-                  demonstrated at DockerCon 2020. <br />
+									Image Hub is a sample application written to run on Consul for
+									exploring WebAssembly modules used as Envoy filters are
+									written in Rust. These modules can be used to implement
+									multi-tenancy or to implement per-user rate-limiting in your
+									application's endpoints. This application was first
+									demonstrated at DockerCon 2020. <br />
                 </p>
                 <li>
                   <h4>
                     <a href="https://layer5.io/projects/getnighthawk">
-                      GetNightHawk :
+											GetNightHawk :
                     </a>
                   </h4>{" "}
                 </li>
                 <p>
-                  This Is a Layer 7 (HTTP/HTTPS/HTTP2) performance
-                  characterization tool. Nighthawk is Envoy’s load generator and
-                  is written in C++. Meshery integrates Nighthawk as one of
-                  (currently) three choices of load generators for
-                  characterizing and managing the performance of service meshes
-                  and their workloads. <br />
+									This Is a Layer 7 (HTTP/HTTPS/HTTP2) performance
+									characterization tool. Nighthawk is Envoy’s load generator and
+									is written in C++. Meshery integrates Nighthawk as one of
+									(currently) three choices of load generators for
+									characterizing and managing the performance of service meshes
+									and their workloads. <br />
                 </p>
               </ul>
             </p>
             <p>
               <h3>
                 <a href="https://layer5.io/service-mesh-management/meshery">
-                  <img
-                    margin="10%"
-                    width="3%"
-                    align="left"
-                    src={meshery}
-                    alt="Meshery"
-                  />
-                  &nbsp; Meshery
+                  <img className="h-icon" src={meshery} alt="Meshery" />
+									&nbsp; Meshery
                 </a>
               </h3>
             </p>
             <p>
-              Meshery and its components Meshery Operator{" "}
-              <img width="2%" src={mesheryoperatoricon} /> and MeshSync{" "}
-              <img width="2%" src={mesherysyncicon} />
+							Meshery and its components Meshery Operator{" "}
+              <img className="p-icon" src={mesheryoperatoricon} /> and MeshSync{" "}
+              <img className="p-icon" src={mesherysyncicon} />
               <ul>
                 <li>
                   <h4>
                     <a href="https://layer5.io/service-mesh-management/meshery">
-                      Meshery
+											Meshery
                     </a>
                   </h4>{" "}
                 </li>
                 <p>
-                  It is a multi-service mesh management plane offering lifecycle
-                  management of more types of service meshes than any other tool
-                  available today. Meshery facilitates adopting, configuring,
-                  operating, and managing the performance of different service
-                  meshes and incorporates the collection and display of metrics
-                  from applications running on top of any service mesh.
+									It is a multi-service mesh management plane offering lifecycle
+									management of more types of service meshes than any other tool
+									available today. Meshery facilitates adopting, configuring,
+									operating, and managing the performance of different service
+									meshes and incorporates the collection and display of metrics
+									from applications running on top of any service mesh.
                   <br />{" "}
                 </p>
                 <li>
@@ -126,57 +115,56 @@ const Maintainer = () => {
                   </h4>{" "}
                 </li>
                 <p>
-                  Website for Meshery, the multi-mesh manager. <br />
+									Website for Meshery, the multi-mesh manager. <br />
                 </p>
                 <li>
                   <h4>
                     <a href="https://layer5.io/service-mesh-management/meshery/meshery-operator">
-                      Meshery-operator :
+											Meshery-operator :
                     </a>
                   </h4>{" "}
                 </li>
                 <p>
-                  Meshery Operator is the multi-service mesh operator and
-                  implementation of MeshSync. <br />
+									Meshery Operator is the multi-service mesh operator and
+									implementation of MeshSync. <br />
                 </p>
               </ul>
             </p>
             <h3>
               <a href="https://smp-spec.io/">
                 <img
-                  className="channels-img"
-                  align="left"
+                  className="h-icon"
+                  alt="service mesh performance"
                   src={servicemeshperformance}
                 />
-                &nbsp; Service Mesh Performance{" "}
+								&nbsp; Service Mesh Performance{" "}
               </a>
             </h3>
             <p>
               <a href="https://layer5.io/projects/service-mesh-performance">
-                The Service Mesh Performance (SMP)
+								The Service Mesh Performance (SMP)
               </a>{" "}
-              is a vendor-neutral specification for capturing details of
-              environment and infrastructure details, service mesh and it’s
-              configuration, service/application details, and bundling of
-              statistical analysis of the result. <br />
+							is a vendor-neutral specification for capturing details of
+							environment and infrastructure details, service mesh and it’s
+							configuration, service/application details, and bundling of
+							statistical analysis of the result. <br />
             </p>
 
             <p>
               <h3>
                 <a href="https://github.com/service-mesh-patterns/service-mesh-patterns">
                   <img
-                    margin="10%"
-                    width="3%"
-                    align="left"
+                    className="h-icon"
+                    alt="service mesh patterns"
                     src="https://user-images.githubusercontent.com/85789734/134711787-5bf0aeaa-008c-4e2c-a81e-e05f3dcfb54c.png"
                   />{" "}
-                  &nbsp; Service Mesh Patterns{" "}
+									&nbsp; Service Mesh Patterns{" "}
                 </a>
               </h3>
             </p>
             <p>
-              A collection of curated patterns of service mesh use cases
-              compatible with Meshery. <br />
+							A collection of curated patterns of service mesh use cases
+							compatible with Meshery. <br />
             </p>
           </div>
         </Container>
