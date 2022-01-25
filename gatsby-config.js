fbef0d94466cebd3610e64643219609263bdbbf8
Lee Calcote <leecalcote@gmail.com>
2022-01-24 20:16:25 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -191,7 +191,7 @@ module.exports = {
               }
             `,
             output: "/resources/feed.xml",
-            title: "Layer5",
+            title: "Layer5 Blog",
           },
           {
             serialize: ({ query: { site, allPosts } }) => {
