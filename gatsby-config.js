54f7806805c192eee52e8ec5188588d17be7b68f
Sunny Kumar <sunny514173@gmail.com>
2022-01-29 14:44:58 +0530

    add rss feed to events
    Signed-off-by: Sunny Kumar <sunny514173@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -241,6 +241,53 @@ module.exports = {
             output: "/blog/feed.xml",
             title: "Layer5 Blogs"
           },
+          {
+            serialize: ({ query: { site, allPosts }}) => {
+              return allPosts.nodes.map( node => {
+                return Object.assign({}, node.frontmatter, {
+                  title: node.frontmatter.title,
+                  author: node.frontmatter.author,
+                  description: node.frontmatter.description,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  enclosure: node.frontmatter.thumbnail && {
+                    url: site.siteMetadata.siteUrl + node.frontmatter.thumbnail.publicURL,
+                  },
+                  custom_elements: [{ "content:encoded": node.html }],
+                });
+              });
+            },
+            query: `
+              {
+                allPosts: allMdx(
+                  sort: { fields: [frontmatter___date], order: DESC }
+                  filter: { fields: { collection: { in: ["events"] } }, frontmatter: { published: { eq: true }, } }
+                  limit: 20
+                ) {
+                  nodes {
+                    body
+                    html
+                    frontmatter {
+                      title
+                      author
+                      description
+                      date(formatString: "MMM DD YYYY")
+                      thumbnail {
+                        publicURL
+                      }
+                    }
+                    fields {
+                      collection
+                      slug
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/events/feed.xml",
+            title: "Layer5"
+          },
         ],
       },
     },
