00329d9b15e4a5bef396e81089e6fb8c6b3fb6c2
Abhijay Jain <Abhijay007j@gmail.com>
2022-02-10 20:48:25 +0530

    Chore[Site Performance] : reverted SVGs images
    Signed-off-by: Abhijay Jain <Abhijay007j@gmail.com>

--- src/sections/Home/Proud-maintainers/index.js
+++ src/sections/Home/Proud-maintainers/index.js
@@ -1,56 +1,51 @@
 import React from "react";
 import { Container, Row, Col } from "../../../reusecore/Layout";
 import SectionTitle from "../../../reusecore/SectionTitle";
-
-const Envoy = "../../../assets/images/service-mesh-icons/envoy/horizontal/color/envoy-horizontal-color.svg";
-const Linkerd = "../../../assets/images/service-mesh-icons/linkerd/horizontal/color/linkerd-horizontal-color.svg";
-const Traefik = "../../../assets/images/service-mesh-icons/traefik.svg";
-const Istio = "../../../assets/images/service-mesh-icons/istio.svg";
-const OSM = "../../../assets/images/service-mesh-icons/open-service-mesh/stacked/color/openservicemesh-stacked-color.svg";
-const SMI = "../../../assets/images/service-mesh-icons/service-mesh-interface/horizontal-stackedtext/color/servicemeshinterface-horizontal-stackedtext-color.svg";
-const SMP = "../../../assets/images/service-mesh-performance/horizontal/smp-dark-text-side.svg";
-const CNCF = "../../../assets/images/partners/cncf/horizontal/color/cncf-color.svg";
-const Meshery = "../../../assets/images/meshery/full-logo/meshery-logo-light-text-side.svg";
-const ImageHub = "../../../assets/images/image-hub/layer5-image-hub.svg";
-const OAM = "../../../assets/images/partners/oam.svg";
-
+import Envoy from "../../../assets/images/service-mesh-icons/envoy/horizontal/color/envoy-horizontal-color.svg";
+import Linkerd from "../../../assets/images/service-mesh-icons/linkerd/horizontal/color/linkerd-horizontal-color.svg";
+import Traefik from "../../../assets/images/service-mesh-icons/traefik.svg";
+import Istio from "../../../assets/images/service-mesh-icons/istio.svg";
+import OSM from "../../../assets/images/service-mesh-icons/open-service-mesh/stacked/color/openservicemesh-stacked-color.svg";
+import SMI from "../../../assets/images/service-mesh-icons/service-mesh-interface/horizontal-stackedtext/color/servicemeshinterface-horizontal-stackedtext-color.svg";
+import SMP from "../../../assets/images/service-mesh-performance/horizontal/smp-dark-text-side.svg";
+import CNCF from "../../../assets/images/partners/cncf/horizontal/color/cncf-color.svg";
+import Meshery from "../../../assets/images/meshery/full-logo/meshery-logo-light-text-side.svg";
+import ImageHub from "../../../assets/images/image-hub/layer5-image-hub.svg";
+import OAM from "../../../assets/images/partners/oam.svg";
 import ProjectItemWrapper from "./proudMaintainers.style";
-import { StaticImage } from "gatsby-plugin-image";
 
 const ProudMaintainers = () => {
   return (
     <ProjectItemWrapper>
       <Container>
         <Row className="project-title">
-          <SectionTitle
-            UniWidth="75%"
-          >
+          <SectionTitle UniWidth="75%">
             <h4>Proudly maintaining and contributing</h4>
             <h2>
               <span>Open Source and Community First</span>.
             </h2>
           </SectionTitle>
           <p className="project-text">
-            We are the world’s largest collection of service mesh practitioners and
-            maintainers of leading open source projects
-            We are open source contributors and maintainers.
+            We are the world’s largest collection of service mesh practitioners
+            and maintainers of leading open source projects We are open source
+            contributors and maintainers.
           </p>
         </Row>
       </Container>
       <Row className="row">
         <Col xs={3} sm={3} md={3} lg={3} className="proj_row_1_col">
           <div className="proj_icon_1">
-            <StaticImage src={CNCF} alt="CNCF" objectFit="contain"/>
+            <img src={CNCF} alt="CNCF" />
           </div>
         </Col>
         <Col xs={3} sm={3} md={3} lg={3} className="proj_row_1_col">
           <div className="proj_icon_1">
-            <StaticImage  src={Envoy} alt="Envoy" objectFit="contain" />
+            <img src={Envoy} alt="Envoy" />
           </div>
         </Col>
         <Col xs={3} sm={3} md={3} lg={3} className="proj_row_1_col">
           <div className="proj_icon_1">
-            <StaticImage  src={Linkerd} alt="Linkerd" objectFit="contain"/>
+            <img src={Linkerd} alt="Linkerd" />
           </div>
         </Col>
         {/* <Col xs={3} sm={3} md={3} lg={2}>
@@ -61,46 +56,46 @@ const ProudMaintainers = () => {
         </Col> */}
         <Col xs={3} sm={3} md={3} lg={3} className="proj_row_1_col">
           <div className="proj_icon_1">
-            <StaticImage  src={Meshery} alt="Meshery" objectFit="contain"/>
+            <img src={Meshery} alt="Meshery" />
           </div>
         </Col>
         <Col xs={3} sm={3} md={3} lg={3} className="proj_row_1_col">
           <div className="proj_icon_1">
-            <StaticImage  src={OAM} alt="OAM" objectFit="contain" />
+            <img src={OAM} alt="OAM" />
           </div>
         </Col>
       </Row>
       <Row className="row row_2">
         <Col xs={3} sm={3} md={3} lg={2} className="proj_row_2_col">
           <div className="proj_icon_2">
-            <StaticImage  src={Istio} alt="Istio" objectFit="contain"/>
+            <img src={Istio} alt="Istio" />
             <h4>Istio</h4>
           </div>
         </Col>
         <Col xs={3} sm={3} md={3} lg={2} className="proj_row_2_col">
           <div className="proj_icon_1">
-            <StaticImage src={OSM} alt="OSM" objectFit="contain" />
+            <img src={OSM} alt="OSM" />
           </div>
         </Col>
         <Col xs={3} sm={3} md={3} lg={2} className="proj_row_2_col">
           <div className="proj_icon_1">
-            <StaticImage src={SMP} alt="SMP" objectFit="contain" />
+            <img src={SMP} alt="SMP" />
           </div>
         </Col>
         <Col xs={3} sm={3} md={3} lg={2} className="proj_row_2_col">
           <div className="proj_icon_2">
-            <StaticImage  src={ImageHub} alt="ImageHub" objectFit="contain" />
+            <img src={ImageHub} alt="ImageHub" />
             <h4>Image Hub</h4>
           </div>
         </Col>
         <Col xs={3} sm={3} md={3} lg={2} className="proj_row_2_col">
           <div className="proj_icon_1">
-            <StaticImage  src={SMI} alt="SMI" objectFit="contain"/>
+            <img src={SMI} alt="SMI" />
           </div>
         </Col>
         <Col xs={3} sm={3} md={3} lg={2} className="proj_row_2_col">
           <div className="proj_icon_2">
-            <StaticImage  src={Traefik} alt="Traefik" objectFit="contain" />
+            <img src={Traefik} alt="Traefik" />
             <h4>Traefik Mesh</h4>
           </div>
         </Col>
