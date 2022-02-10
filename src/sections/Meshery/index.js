166797016ecc28b8aec45a1eaa5e811d091fd981
leecalcote <leecalcote@gmail.com>
2022-02-10 14:25:26 -0600

    720p
    Signed-off-by: leecalcote <leecalcote@gmail.com>

--- src/sections/Meshery/index.js
+++ src/sections/Meshery/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import {ThemeProvider} from "styled-components";
-import Layout from "../../components/layout"; 
+import { ThemeProvider } from "styled-components";
+import Layout from "../../components/layout";
 
 import { Container, Row, Col } from "../../reusecore/Layout";
 import Button from "../../reusecore/Button";
@@ -11,7 +11,7 @@ import { GiClockwork } from "@react-icons/all-files/gi/GiClockwork";
 import FeaturesTable from "./Features-Col";
 
 import mesheryFullStack from "./images/meshery-logo-light-text-tag.png";
-import mesheryDemo from "../../assets/video/meshery-full-perf.mp4";
+import mesheryDemo from "../../assets/video/meshery/meshery-full-perf-720p.mp4";
 
 
 import MesheryWrapper from "./meshery.style";
@@ -21,30 +21,30 @@ import Features from "./Meshery-features";
 
 const MesheryPage = () => {
   return (
-    <ThemeProvider theme={theme}> 
+    <ThemeProvider theme={theme}>
       <Layout>
         <MesheryWrapper>
-          <Container> 
+          <Container>
             <div className="info">
               <Row className="description">
                 <Col className="desc-text" lg={6} md={6} sm={8} xs={8}>
                   <h1 className="heading-1"> the service mesh</h1>
                   <h1 className="heading-2"> <span className="heading-2">management plane</span></h1>
                   <p className="desc-p">
-                    Meshery is the service mesh management plane. <br/>
-                    Adopt and operate any service mesh with confidence using Meshery's management features. 
+                    Meshery is the service mesh management plane. <br />
+                    Adopt and operate any service mesh with confidence using Meshery's management features.
                   </p>
                   <Button primary className="banner-btn" title="How Meshery Works" url="./operating-service-meshes">
                     <GiClockwork size={21} className="button-icon" />
-                  </Button> 
+                  </Button>
                   <Button secondary className="banner-btn" title="Run Meshery" url="./getting-started">
                     <FiDownloadCloud size={21} className="button-icon" />
                   </Button>
                 </Col>
-                <Col lg={6} md={6} sm={4} xs={4}className="meshery-hero-img">
-                <video autoPlay muted loop controls className="meshery-video">
-                   <source src={mesheryDemo} type="video/mp4" />
-                </video>
+                <Col lg={6} md={6} sm={4} xs={4} className="meshery-hero-img">
+                  <video autoPlay muted loop controls className="meshery-video">
+                    <source src={mesheryDemo} type="video/mp4" />
+                  </video>
                   {/* <img className="meshery-sup-img" src={mesheryFullStack} alt="Meshery the multi-mesh manager" /> */}
                 </Col>
               </Row>
