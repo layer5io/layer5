e3ffef49c515c0b974c8f9eee8117f4678b2a9e2
subhradwip01 <subhradwip01@gmail.com>
2022-02-12 10:22:49 +0530

    Calender loading animation modified
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/pages/community/calendar.js
+++ src/pages/community/calendar.js
@@ -9,21 +9,60 @@ import Layout from "../../components/layout";
 import Loadable from "react-loadable";
 import Loader from "./Loader.style";
 import LoadingIcon from "../../assets/images/LoadingIcon";
-
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import { Container } from "../../reusecore/Layout";
+import CalendarStyleWrapper from "../../sections/Community/Calendar/calendar.style";
+import PageHeader from "../../reusecore/PageHeader";
 
 const LoadableCalendar = Loadable({
   loader: () => import("../../sections/Community/Calendar"),
   loading() {
-    return <Loader><LoadingIcon/></Loader>;
-  }
+    return (
+      <Loader>
+        <div className="loading-modal">
+          <LoadingIcon />
+        </div>
+        <div className="calender">
+          <CalendarStyleWrapper>
+            <PageHeader
+              title="Community Calendar"
+              path="Community > Calendar"
+              subtitle="Join any or all of the weekly meetings"
+            />
+            <Container>
+              <div className="calendar-wrapper">
+                <div className="calendar-grid">
+                  <FullCalendar
+                    plugins={[dayGridPlugin]}
+                    initialView="dayGridMonth"
+                    contentHeight={700}
+                    headerToolbar={{
+                      left: "prev,next today",
+                      center: "title",
+                      right: "addToCalendar",
+                    }}
+                  />
+                </div>
+              </div>
+            </Container>
+          </CalendarStyleWrapper>
+        </div>
+      </Loader>
+    );
+  },
 });
+
 
 const CalendarPage = () => {
   return (
     <ThemeProvider theme={theme}>
       <Layout>
         <GlobalStyle />
-        <SEO title="Service Mesh Events Calendar" description="Join any or all of the Layer5 community meetings." />
+        <SEO
+          title="Service Mesh Events Calendar"
+          description="Join any or all of the Layer5 community meetings."
+        />
         <Navigation />
         <LoadableCalendar />
         <Footer />
