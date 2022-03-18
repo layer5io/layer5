3de523695576c7505dec937d375b8ebeae6c3009
Sayan Chowdhury <98570396+sayan011@users.noreply.github.com>
2022-03-18 18:59:57 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@
  *
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
-/*eslint linebreak-style: ["error", "windows"]*/
+
 import React from "react";
 import PropTypes from "prop-types";
 import {Helmet} from "react-helmet";
