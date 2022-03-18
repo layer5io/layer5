4b65d4c6da8a33d688e48bec0c5e4b278a17dd15
Sayan Chowdhury <98570396+sayan011@users.noreply.github.com>
2022-03-18 19:01:19 +0530

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import {Helmet} from "react-helmet";
 import { useLocation } from "@reach/router";
 import { useStaticQuery, graphql } from "gatsby";
-/*eslint linebreak-style: ["error", "windows"]*/
+
 const SEO = ({ canonical, description, image, lang, meta, schemaMarkup, title }) => {
   const { pathname } = useLocation();
   const { site } = useStaticQuery(
