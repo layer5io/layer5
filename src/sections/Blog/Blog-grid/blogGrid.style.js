a1701aa9867c06bf04aae34cf635be1f8f34ffa7
Ekene Nwobodo <nwobodoe71@gmail.com>
2022-03-26 18:40:59 +0100

    Add search feature to blog page
    Signed-off-by: Ekene Nwobodo <nwobodoe71@gmail.com>

--- src/sections/Blog/Blog-grid/blogGrid.style.js
+++ src/sections/Blog/Blog-grid/blogGrid.style.js
@@ -1,19 +1,56 @@
 import styled from "styled-components";
+// export const BlogPageWrapper = styled.div`
+
+//     .blog-grid-wrapper{
+//         margin: 5rem auto;
+//     }
+
+//     @media only screen and (max-width: 992px) {
+//         .blog-page-wrapper{
+//             margin-bottom: 5rem;
+//         }
+//     }
+
+//     @media only screen and (max-width: 575px) {
+//         .blog-grid-wrapper{
+//             margin: 0 auto 1rem;
+//         }
+//     }
+// `;
+
 export const BlogPageWrapper = styled.div`
-
-    .blog-grid-wrapper{
-        margin: 5rem auto;
+  @media only screen and (max-width: 992px) {
+    .blog-page-wrapper {
+      margin-bottom: 5rem;
     }
+  }
+`;
 
-    @media only screen and (max-width: 992px) {
-        .blog-page-wrapper{
-            margin-bottom: 5rem;
-        }
+export const SearchBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  .header {
+    display: flex;
+    justify-content: space-between;
+  }
+  input {
+    padding: 0.5rem;
+    border-radius: 20px;
+    background: #f5f7fa;
+    border: 2px solid #f5f7fa;
+    font-size: 1rem;
+    padding: 0.7rem 2rem;
+    ::placeholder {
+      color: #8ba3cb;
+      font-size: 1rem;
     }
-
-    @media only screen and (max-width: 575px) {
-        .blog-grid-wrapper{
-            margin: 0 auto 1rem;
-        }
+    :focus {
+      outline: none;
+      box-shadow: 0 0 4px #ccc;
     }
+    :hover {
+      box-shadow: 0 0 4px #ccc;
+    }
+  }
 `;
--- src/sections/Blog/Blog-grid/index.js
+++ src/sections/Blog/Blog-grid/index.js
@@ -1,43 +1,40 @@
-import React,{useState} from "react";
-import BlogViewToolTip from "../../../components/blog-view-tooltip";
-import Card from "../../../components/Card";
-import Pager from "../../../components/pager";
+import React, { useState } from "react";
 import { Container, Row, Col } from "../../../reusecore/Layout";
-
 import PageHeader from "../../../reusecore/PageHeader";
 import Sidebar from "../Blog-sidebar";
 import RssFeedIcon from "../../../assets/images/socialIcons/rss-sign.svg";
+import { BlogPageWrapper} from "./blogGrid.style";
+import Search from "./js-search";
 
-import { BlogPageWrapper } from "./blogGrid.style";
-
-import Paginate from "../paginate";
-
-const BlogGrid = ({data, isListView, setListView, setGridView, pageContext}) => {
+const BlogGrid = ({
+  data,
+  isListView,
+  setListView,
+  setGridView,
+  pageContext,
+}) => {
   return (
     <BlogPageWrapper>
-      <PageHeader title="Blog" path="Blog" img={RssFeedIcon} feedlink="/blog/feed.xml"/>
+      <PageHeader
+        title="Blog"
+        path="Blog"
+        img={RssFeedIcon}
+        feedlink="/blog/feed.xml"
+      />
       <div className="blog-page-wrapper">
         <Container>
           <Row>
             <Col xs={12} lg={8}>
-              <BlogViewToolTip isListView={isListView} setListView={setListView}
-                setGridView ={setGridView}
+              <Search
+                data1={data.allMdx.nodes}
+                setGridView={setGridView}
+                setListView={setListView}
+                pageContext={pageContext}
+                isListView={isListView}
               />
-              <div className="blog-grid-wrapper">
-                <Row>
-                  {data.allMdx.nodes.map(({id, frontmatter, fields }) => (
-                    <Col key={id} xs={12} sm={6} >
-                      <Card frontmatter={frontmatter} fields={fields}/>
-                    </Col>
-                  ))}
-                  <Col>
-                    <Paginate pageContext={pageContext} isListView={isListView}/>
-                  </Col>
-                </Row>
-              </div>
             </Col>
             <Col xs={12} lg={4}>
-              <Sidebar pageContext={pageContext}/>
+              <Sidebar pageContext={pageContext} />
             </Col>
           </Row>
         </Container>
