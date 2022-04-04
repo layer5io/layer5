ddfff48687df32a83540cd9e07e8ae033e153872
Debopriya Bhattacharjee <debopriyabhattacharjee@Debopriyas-MacBook-Air.local>
2022-04-04 19:09:08 +0530

    docker meshery page
    Signed-off-by: Debopriya Bhattacharjee <debopriyabhattacharjee@Debopriyas-MacBook-Air.local>

---
+++ src/pages/meshery-docker-beta-program.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Navigation from "../sections/General/Navigation";
+import DockerMesheryPage from "../sections/Docker-Meshery";
+import Footer from "../sections/General/Footer";
+import { GlobalStyle } from "../sections/app.style";
+import theme from "../theme/app/themeStyles";
+
+
+const MesheryDocker = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <GlobalStyle />
+        <SEO title="Meshery Docker Beta Program"
+          description="The Docker Desktop Extension for Meshery extends Docker Desktop’s position as the cloud native developer’s go-to Kubernetes environment with easy access to the next layer of cloud native infrastructure: service meshes."
+          image="/images/meshery-logo-dark-text.png" />
+        <Navigation />
+        <DockerMesheryPage />
+        <Footer />
+      </Layout>
+    </ThemeProvider>
+  );
+};
+export default MesheryDocker;
