65609282f7f3a7cde0aa1b5be3fc7a8b65f8a689
Michellebuchiokonicha <michelleokonicha@gmail.com>
2022-04-10 16:28:49 +0100

    layer5-about-community-first-image
    Signed-off-by: Michellebuchiokonicha <michelleokonicha@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -62,7 +62,7 @@ module.exports = {
     ],
     "linebreak-style": [
       "error",
-      "unix"
+      "windows"
     ],
     "quotes": [
       "error",
@@ -74,5 +74,8 @@ module.exports = {
     ],
     "strict": 0
   }
+//   rules: {
+//     'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
+// }
 };
 
