c6ed403d8e542cb7d436d47ec76f46dd09bec4d7
Lee Calcote <leecalcote@gmail.com>
2022-05-16 15:17:44 -0500

    Apply suggestions from code review

--- src/sections/Company/Brand/Brand-components/meshmark.js
+++ src/sections/Company/Brand/Brand-components/meshmark.js
@@ -40,11 +40,11 @@ const MeshMarkBrand = () => {
           </Col>
           <Col>
             <p>
-              The MeshMark includes MeshMark name & logo and any
+              The MeshMark mark includes MeshMark name & logo and any
               word, phrase, image, or other designation that identifies the
               source or origin of any Layer5 projects. PLease don't modify the
               marks or use them in a confusing way, including suggesting
-              sponsorship or endorsement by Layer5 with another brand(including
+              sponsorship or endorsement by Layer5 with another brand (including
               your own)
             </p>
           </Col>
