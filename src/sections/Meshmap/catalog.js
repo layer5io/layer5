c2150b8dce3c042daaf96e5c9587409b5fa8cbec
Debopriya Bhattacharjee <debopriyabhattacharjee@Debopriyas-MacBook-Air.local>
2022-05-27 14:57:59 +0530

    blur fix
    Signed-off-by: Debopriya Bhattacharjee <debopriyabhattacharjee@Debopriyas-MacBook-Air.local>

--- src/sections/Meshmap/catalog.js
+++ src/sections/Meshmap/catalog.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+
 import Multicluster from "../../assets/images/meshmap/catalog-card-images/multi-cluster.svg";
 import Retries from "../../assets/images/meshmap/catalog-card-images/retries.svg";
 import Node_agent from "../../assets/images/meshmap/catalog-card-images/node-agent.svg";
@@ -30,29 +31,14 @@ h2{
 }
 #carousel{
 	width: 100%;
-	height: 150%;
+	height: 100%;
 	position: absolute;
 	transform-style: preserve-3d;
-	animation: rotation 30s infinite linear;
+	animation: swirl 40s steps(10000, end) infinite; 
 }
 #carousel:hover{
 	animation-play-state: paused;
 }
-#carousel figure{
-	display: block;
-	position: absolute;
-	overflow: hidden;
-}
-#carousel figure:nth-child(1){transform: rotateY(0deg) translateZ(300px);}
-#carousel figure:nth-child(2) { transform: rotateY(36deg) translateZ(300px);}
-#carousel figure:nth-child(3) { transform: rotateY(72deg) translateZ(300px);}
-#carousel figure:nth-child(4) { transform: rotateY(108deg) translateZ(300px);}
-#carousel figure:nth-child(5) { transform: rotateY(144deg) translateZ(300px);}
-#carousel figure:nth-child(6) { transform: rotateY(180deg) translateZ(300px);}
-#carousel figure:nth-child(7) { transform: rotateY(216deg) translateZ(300px);}
-#carousel figure:nth-child(8) { transform: rotateY(252deg) translateZ(300px);}
-#carousel figure:nth-child(9) { transform: rotateY(288deg) translateZ(300px);}
-#carousel figure:nth-child(10) { transform: rotateY(324deg) translateZ(300px);}
 
 #carousel{
 img{
@@ -63,16 +49,53 @@ img:hover{
   transform: scale(1.1,1.1);
 }
 }
+  
+  .slide {
+	position: absolute;
+	top: 10px;
+	left: 10px;
+  }
+  
+  .slide.one {
+	transform: rotateY(0deg) translateZ(382px);
+  }
+  .slide.two {
+	transform: rotateY(36deg) translateZ(382px);
+  }
+  .slide.three {
+	transform: rotateY(72deg) translateZ(382px);
+  }
+  .slide.four {
+	transform: rotateY(108deg) translateZ(382px);
+  }
+  .slide.five {
+	transform: rotateY(144deg) translateZ(382px);
+  }
+  .slide.six {
+	transform: rotateY(180deg) translateZ(382px);
+  }
+  .slide.seven {
+	transform: rotateY(216deg) translateZ(382px);
+  }
+  .slide.eight {
+	transform: rotateY(252deg) translateZ(382px);
+  }
+  .slide.nine {
+	transform: rotateY(288deg) translateZ(382px);
+  }
+  .slide.ten {
+	transform: rotateY(324deg) translateZ(382px);
+  }
 
-@keyframes rotation{
-	from{
-		transform: rotateY(0deg);
+  @keyframes swirl {   
+	from {
+	  transform: rotateY(-360deg);
 	}
-	to{
-		transform: rotateY(360deg);
+	to {
+	  transform: rotateY(0deg);
 	}
-}
-
+  } 
+  
 `;
 
 const Catalog = () => {
@@ -82,20 +105,40 @@ const Catalog = () => {
         <h2>Choose Your Content</h2>
         <img src={Options} alt="" />
 
-        <div className="container">
-          <div id="carousel">
-            <figure><img src={Multicluster} alt="Multi-cluster" /></figure>
-            <figure><img src={Node_agent} alt="Node Agent" /></figure>
-            <figure><img src={Retries} alt="Retries" /></figure>
-            <figure><img src={Correlate} alt="Correlate Event" /></figure>
-            <figure><img src={Retry_deadline} alt="Retry Deadline" /></figure>
-            <figure><img src={Singleton} alt="" /></figure>
-            <figure><img src={Pre_provision} alt="Pre Provision" /></figure>
-            <figure><img src={Denial} alt="L7 Denial" /></figure>
-            <figure><img src={Circuit} alt="Circuit Breaker" /></figure>
-            <figure><img src={Http_metrics} alt="HTTP Metrics" /></figure>
-          </div>
-        </div>
+   	 <div className="container">
+    	 <div id="carousel">
+		 <div className="slide one">
+      	   <img src={Multicluster} />
+      	 </div>
+      	 <div className="slide two">
+      	   <img src={Node_agent} />
+      	 </div>
+      	 <div className="slide three">
+      	   <img src={Retries} />
+      	 </div>
+      	 <div className="slide four">
+      	   <img src={Correlate} />
+      	 </div>
+   		 <div className="slide five">
+   		   <img src={Retry_deadline} />
+   		 </div>
+   		 <div className="slide six">
+   		   <img src={Singleton} />
+   		 </div>
+   		 <div className="slide seven">
+   		   <img src={Pre_provision} />
+   		 </div>
+   		 <div className="slide eight">
+   		   <img src={Denial} />
+   		 </div>
+   		 <div className="slide nine">
+   		   <img src={Circuit} />
+   		 </div>
+            <div className="slide ten">
+   		   <img src={Http_metrics} />
+    	</div>
+   		</div>
+  	</div>
       </Container>
     </CatalogWrapper>
   );
