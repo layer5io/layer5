19de4f1c3cae0abe458b6e2e7f00929f60b4f57c
Harshit Dandriyal <56961488+Harshit-Dandriyal@users.noreply.github.com>
2022-06-08 00:44:35 +0530

    Added Coookies for banner
    Signed-off-by: Harshit Dandriyal <56961488+Harshit-Dandriyal@users.noreply.github.com>

--- src/sections/Home/Banner/index.js
+++ src/sections/Home/Banner/index.js
@@ -2,14 +2,20 @@ import React, { useEffect, useState } from "react";
 import Banner3 from "../Banner-3";
 import Banner2 from "../Banner-2";
 import Banner1 from "../Banner-1";
-
+import { useCookies } from "react-cookie";
 const BannersList = [
   <Banner1 key={0} />,
   <Banner2 key={1} />,
   <Banner3 key={2} />
 ];
+const RotationalBanner =  () => {
+  const [cookies, setCookie] = useCookies(["user"]);
+  const handle = (num) => {
+    setCookie("bannerNumber", num, { path: "/" });
+  };
 
-const RotationalBanner = () => {
+  let loadingbanner=0;
+  loadingbanner=parseInt(cookies.bannerNumber);
   let initialValue;
   try {
     initialValue = sessionStorage.getItem("banner") || 0;
@@ -26,10 +32,11 @@ const RotationalBanner = () => {
     }
     setMounted(true);
   }, []);
-
+  
   return (
     <>
-      {mounted ? BannersList[Number(Banner)] : <Banner1 />}
+      {console.log(loadingbanner)}
+      {mounted ? <> <div onLoad={handle(Number(Banner))}>{BannersList[Number(Banner)]} </div></> : BannersList[loadingbanner] }
     </>
   );
 };
