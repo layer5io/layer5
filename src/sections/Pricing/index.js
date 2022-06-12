6f76f01a446f74f5c929f9b33e03f1b7e58e2c87
Debopriya Bhattacharjee <debopriyabhattacharjee@Debopriyas-MacBook-Air.local>
2022-06-13 03:01:13 +0530

    edit
    Signed-off-by: Debopriya Bhattacharjee <debopriyabhattacharjee@Debopriyas-MacBook-Air.local>

--- src/sections/Pricing/index.js
+++ src/sections/Pricing/index.js
@@ -16,10 +16,10 @@ const Pricing = () => {
 
       <div className="headers">
         <h1 className="header-heading">Layer5 Pricing</h1>
-        <svg className="header-svg" aria-hidden="true" role="presentation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-          {/* <polygon fill="white" points="0,100 100,0 100,100"/>
-      <polygon fill="rgba(0,179,159,0.2)" points="50,50 100,0 100,100"/> */}
-        </svg>
+        {/* <svg className="header-svg" aria-hidden="true" role="presentation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+          <polygon fill="white" points="0,100 100,0 100,100"/>
+      <polygon fill="rgba(0,179,159,0.2)" points="50,50 100,0 100,100"/>
+        </svg> */}
       </div>
 
       <div className="wrapper">
