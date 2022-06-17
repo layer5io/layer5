2128437d46650885a7d4672c3525d94e494845e3
leecalcote <leecalcote@gmail.com>
2022-06-17 11:47:12 -0500

    descriptions
    Signed-off-by: leecalcote <leecalcote@gmail.com>

--- src/sections/Pricing/data.js
+++ src/sections/Pricing/data.js
@@ -4,10 +4,10 @@ import { GiCheckMark } from "@react-icons/all-files/gi/GiCheckMark";
 export const details = [
   { id: 0, category: "Lifecycle", description: "aLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" },
   { id: 1, category: "Chaos", description: "bLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" },
-  { id: 2, category: "Performance", description: "cLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" },
+  { id: 2, category: "Performance", description: "Baseline and track your service mesh performance from release to release" },
   { id: 3, category: "Management", description: "dLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" },
-  { id: 4, category: "Account", description: "eLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" },
-  { id: 5, category: "Feature1", description: "fLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" },
+  { id: 4, category: "Account", description: "Own and control the user accounts of your enterprise members through your identity provider (IdP)" },
+  { id: 5, category: "Dry-run", description: "Test and verify configuration changes in a separate environment" },
   { id: 6, category: "Feature2", description: "gLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" },
   { id: 7, category: "Feature3", description: "hLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt" },
   { id: 8, category: "Feature4", description: null },
@@ -29,7 +29,7 @@ export const options = [
       { id: 2, category: "Performance", description: "", value: "Comparative Testing" },
       { id: 3, category: "Management", description: "", value: "MeshMark" },
       { id: 4, category: "Account", description: "", value: "Single Team" },
-      { id: 5, category: "Feature1", description: "", value: <GiCheckMark className="icon" /> },
+      { id: 5, category: "Dry-run", description: "", value: <GiCheckMark className="icon" /> },
       { id: 6, category: "Feature2", description: "", value: <GiCheckMark className="icon" /> },
       { id: 7, category: "Feature3", description: "", value: <GiCheckMark className="icon" /> },
       { id: 8, category: "Feature4", description: "", value: <GiCheckMark className="icon" /> }]
@@ -48,7 +48,7 @@ export const options = [
       { id: 2, category: "Performance", description: "", value: "Continuous Optimizer" },
       { id: 3, category: "Management", description: "", value: "Automated Canary" },
       { id: 4, category: "Account", description: "", value: "Single Organization" },
-      { id: 5, category: "Feature1", description: "", value: <GiCheckMark className="icon" /> },
+      { id: 5, category: "Dry-run", description: "Test and verify configuration changes in a separate environment", value: <GiCheckMark className="icon" /> },
       { id: 6, category: "Feature2", description: "", value: <GiCheckMark className="icon" /> },
       { id: 7, category: "Feature3", description: "", value: <GiCheckMark className="icon" /> },
       { id: 8, category: "Feature4", description: "", value: <GiCheckMark className="icon" /> }]
@@ -64,9 +64,9 @@ export const options = [
     subdata: [
       { id: 0, category: "Lifecycle Management", feature: "", description: "", value: "" },
       { id: 1, category: "Chaos", description: "", value: "Blog" },
-      { id: 2, category: "Performance", description: "", value: "News" },
+      { id: 2, category: "Performance", description: "", value: "Automatically suggest optimal resiliency settings for a given service." },
       { id: 3, category: "Management", description: "", value: "Workshop" },
-      { id: 4, category: "Account", description: "", value: "Own and control the user accounts of your enterprise members through your identity provider (IdP)." },
+      { id: 4, category: "Account", description: "Own and control the user accounts of your enterprise members through your identity provider (IdP)", value: "Multiple Organizations" },
       { id: 5, category: "Audit Log", description: "Quickly review the actions performed by members of your organization.", value: <GiCheckMark className="icon" /> },
       { id: 6, category: "LDAP", description: "Access Meshery Cloud using your existing accounts and centrally manage access.", value: <GiCheckMark className="icon" /> },
       { id: 7, category: "SAML", description: "", value: <GiCheckMark className="icon" /> },
