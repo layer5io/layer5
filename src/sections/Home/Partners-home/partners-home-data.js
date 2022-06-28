15a4ad6221e6a36589b88f37130dc1a0d7177a7b
Santosh Kaluskar <dtshbl@gmail.com>
2022-06-29 02:46:39 +0530

    Add VMware as partner and link it to logo on homepage
    Signed-off-by: Santosh Kaluskar <dtshbl@gmail.com>

--- src/sections/Home/Partners-home/partners-home-data.js
+++ src/sections/Home/Partners-home/partners-home-data.js
@@ -9,60 +9,66 @@ import rackspace from "../../../assets/images/partners/rackspace_black.svg";
 import intel from "../../../assets/images/partners/intel-grey.svg";
 import vmware from "../../../assets/images/partners/vmware.png";
 
-
 export const partners = [
   {
     name: "Redhat",
     imageLink: redhat,
     imageRoute: "/partners#redhat",
-    innerDivStyle: "partner__block__inner horizontal"
-  }, {
+    innerDivStyle: "partner__block__inner horizontal",
+  },
+  {
     name: "Intel",
     imageLink: intel,
     imageRoute: "/partners#intel",
-    innerDivStyle: "partner__block__inner"
-
-  }, {
+    innerDivStyle: "partner__block__inner",
+  },
+  {
     name: "University of Texas at Austin partnership with Layer5",
     imageLink: utaustin,
     imageRoute: "/partners#utaustin",
-    innerDivStyle: "partner__block__inner horizontal"
-  }, {
+    innerDivStyle: "partner__block__inner horizontal",
+  },
+  {
     name: "Citrix",
     imageLink: citrix,
     imageRoute: "/partners#Citrix",
-    innerDivStyle: "partner__block__inner"
-  }, {
+    innerDivStyle: "partner__block__inner",
+  },
+  {
     name: "Hashicorp partnership with Layer5",
     imageLink: hashicorp,
-    imageRoute: "/company/news/layer5-and-hashicorp-launch-service-mesh-partnership",
-    innerDivStyle: "partner__block__inner"
-  }, {
+    imageRoute:
+			"/company/news/layer5-and-hashicorp-launch-service-mesh-partnership",
+    innerDivStyle: "partner__block__inner",
+  },
+  {
     name: "Hewlett-Packard Enterprise",
     imageLink: hpe,
     imageRoute: "/partners#hpe",
-    innerDivStyle: "partner__block__inner"
-  }, {
+    innerDivStyle: "partner__block__inner",
+  },
+  {
     name: "National Institute of Technology Karnataka",
     imageLink: nitk,
     imageRoute: "/partners#nitk",
-    innerDivStyle: "partner__block__inner"
-  }, {
+    innerDivStyle: "partner__block__inner",
+  },
+  {
     name: "Rackspace Technology",
     imageLink: rackspace,
     imageRoute: "/partners#rackspace",
-    innerDivStyle: "partner__block__inner"
-
-  }, {
+    innerDivStyle: "partner__block__inner",
+  },
+  {
     name: "UEM",
     imageLink: uem,
     imageRoute: "/partners#uem",
-    innerDivStyle: "partner__block__inner"
-  }, {
-    name:"VMware",
+    innerDivStyle: "partner__block__inner",
+  },
+  {
+    name: "VMware",
     imageLink: vmware,
-    imageRoute:"",
-    innerDivStyle:"partner__block__inner"
-    
-  }
-];  
+    imageRoute: "/partners#VMware",
+    innerDivStyle: "partner__block__inner",
+  },
+];
