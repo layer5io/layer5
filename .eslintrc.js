cb2e724d62d2d980d56199027aac45686be350b7
Nikhil-Ladha <nikhilladha1999@gmail.com>
2022-06-29 16:13:51 +0530

    Update eslint config to improve lint
    Signed-off-by: Nikhil-Ladha <nikhilladha1999@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,78 +1,88 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "node": true,
-    "amd": true
+  "env" : {
+    "browser" : true,
+    "es6" : true,
+    "node" : true,
+    "amd" : true
   },
-  "settings": {
-    "react": {
-      "version": "require('./package.json').dependencies.react"
+  "settings" : {
+    "react" : {
+      "version" : "require('./package.json').dependencies.react"
     }
   },
-  "extends": [
+  "extends" : [
     "eslint:recommended",
     "plugin:react/recommended"
   ],
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  "parser" : "@babel/eslint-parser",
+  "parserOptions" : {
+    "ecmaFeatures" : {
+      "jsx" : true
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    "ecmaVersion" : 2018,
+    "sourceType" : "module"
   },
-  "plugins": [
+  "plugins" : [
     "react"
   ],
-  "rules": {
-    "arrow-spacing": [
+  "rules" : {
+    "array-bracket-spacing" : ["error", "never"],
+    "comma-style" : ["error"],
+    "arrow-spacing" : [
       "error",
       {
-        "after": true,
-        "before": true
+        "after" : true,
+        "before" : true
       }
     ],
-    "block-scoped-var": "error",
-    "block-spacing": "error",
-    "brace-style": [
+    "block-scoped-var" : "error",
+    "block-spacing" : "error",
+    "brace-style" : [
       "error",
       "1tbs"
     ],
-    "no-unused-vars": [
+    "jsx-quotes" : ["error", "prefer-double"],
+    "keyword-spacing" : "error",
+    "key-spacing" : ["error", {
+      "beforeColon" : true,
+      "afterColon" : true,
+    }],
+    "no-unused-vars" : [
       "warn",
       {
-        "varsIgnorePattern": "React"
+        "varsIgnorePattern" : "React"
       }
     ],
-    "react/display-name": [0],
-    "react/prop-types": [0],
-    "react/no-unescaped-entities": [0],
-    "react/jsx-no-duplicate-props": [0],
-    "indent": [
+    "no-trailing-spaces" : "error",
+    "object-curly-spacing" : ["error", "always"],
+    "react/display-name" : 0,
+    "react/prop-types" : 0,
+    "react/no-unescaped-entities" : [0],
+    "react/jsx-no-duplicate-props" : [0],
+    "indent" : [
       "error", 2, {
-        "FunctionExpression": {"parameters": "first"},
-        "FunctionDeclaration": {"parameters": "first"},
-        "MemberExpression": 1,
-        "SwitchCase": 1,
-        "outerIIFEBody": 0,
-        "VariableDeclarator": { "var": 2, "let": 2, "const": 3 },
-        ignoredNodes: ["TemplateLiteral"]
+        "FunctionExpression" : { "parameters" : "first" },
+        "FunctionDeclaration" : { "parameters" : "first" },
+        "MemberExpression" : 1,
+        "SwitchCase" : 1,
+        "outerIIFEBody" : 0,
+        "VariableDeclarator" : { "var" : 2, "let" : 2, "const" : 3 },
+        ignoredNodes : ["TemplateLiteral"]
       }
     ],
-    "linebreak-style": [
+    "linebreak-style" : [
       "error",
       "unix"
     ],
-    "quotes": [
+    "quotes" : [
       "error",
       "double"
     ],
-    "semi": [
+    "semi" : [
       "error",
       "always"
     ],
-    "strict": 0
+    "strict" : 0
   }
 };
 
