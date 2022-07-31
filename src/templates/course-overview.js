2aa18036361463c3745c5a0cca54c3b7bbc37318
Nishant Mittal <mittalnishant14@outlook.com>
2022-07-31 12:51:14 +0530

    automatic lint by eslint
    Signed-off-by: Nishant Mittal <mittalnishant14@outlook.com>

--- src/templates/course-overview.js
+++ src/templates/course-overview.js
@@ -86,7 +86,7 @@ export const query = graphql`
       }
   }
 `;
-const CourseOverviewTemplate = ({ data}) => {
+const CourseOverviewTemplate = ({ data }) => {
   return (
     <ThemeProvider theme={theme}>
       <Layout>
