21ad379c8c80aaf714dcf3d675f7ce7b02cec8d6
bensonOSei <kbenson643@gmail.com>
2022-08-10 18:22:20 +0000

    Footer Hyperlink Update

--- src/sections/General/Footer/index.js
+++ src/sections/General/Footer/index.js
@@ -121,7 +121,7 @@ const Footer = () => {
                   </li>
                   <li>
                     <Link className="category-link" to="/careers/programs">
-                      Internships
+                      Internships Programs
                     </Link>
                   </li>
                   <li>
