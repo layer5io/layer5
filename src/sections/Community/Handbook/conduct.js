ccded40fe839b9bf5b9e0c545e6682e51f652619
Kamal Nayan <geniusamansingh@gmail.com>
2022-08-12 05:44:40 +0000

    resolving conduct.js w/signoff
    Signed-off-by: Kamal Nayan <geniusamansingh@gmail.com>

--- src/sections/Community/Handbook/conduct.js
+++ src/sections/Community/Handbook/conduct.js
@@ -6,8 +6,7 @@ import TocPagination from "../../../components/handbook-navigation/TocPagination
 import { StaticImage } from "gatsby-plugin-image";
 
 
-const CodeofConduct= () => {
-  
+const CodeofConduct = () => {
   const Point = "./images/bullet.svg";
   return (
     <HandbookWrapper>
