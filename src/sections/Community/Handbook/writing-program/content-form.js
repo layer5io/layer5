703adda4a4a58e9cf3a480e1696bf351ef783866
sourav-sharma7 <souravsharma1321@gmail.com>
2022-08-22 08:46:02 +0530

    Fixed FAQ section issue#3079
    Signed-off-by: sourav-sharma7 <souravsharma1321@gmail.com>

--- src/sections/Community/Handbook/writing-program/content-form.js
+++ src/sections/Community/Handbook/writing-program/content-form.js
@@ -32,7 +32,7 @@ const ContentForm = () => {
 
   return (
     <ContentFormWrapper>
-            {
+      {
         stepNumber === 0 &&
       <div className="form-data">
         <Formik
@@ -78,73 +78,73 @@ const ContentForm = () => {
         >
           <Form className="form" method="post">
 
-        <label htmlFor="email" className="form-name">Email Address <span className="required-sign">*</span></label>
-        <Field type="text" className="text-field" id="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required />
+            <label htmlFor="email" className="form-name">Email Address <span className="required-sign">*</span></label>
+            <Field type="text" className="text-field" id="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required />
 
-        <label htmlFor="name" className="form-name">Name <span className="required-sign">*</span></label>
-        <Field type="text" className="text-field" id="name" name="name" required />
-         
-        <label id="slack-member" className="form-name">Are you a member of the Layer5 Slack Community?<span className="required-sign">*</span></label>
-        {validateSlack && <p className="validation">Please fill in this field</p>}
-        <div role="group" aria-labelledby="slack-member">
-        <label>
-              <Field type="radio" name="slack" value="yes" />
+            <label htmlFor="name" className="form-name">Name <span className="required-sign">*</span></label>
+            <Field type="text" className="text-field" id="name" name="name" required />
+
+            <label id="slack-member" className="form-name">Are you a member of the Layer5 Slack Community?<span className="required-sign">*</span></label>
+            {validateSlack && <p className="validation">Please fill in this field</p>}
+            <div role="group" aria-labelledby="slack-member">
+              <label>
+                <Field type="radio" name="slack" value="yes" />
               Yes
-        </label>
-        <label>
-              <Field type="radio" name="slack" value="no" />
+              </label>
+              <label>
+                <Field type="radio" name="slack" value="no" />
               No
-        </label>
-        </div>
+              </label>
+            </div>
 
-        <label htmlFor="slackName" className="form-name">If yes, please mention your username on Slack. If no, you are welcome to join - http://slack.layer5.io/</label>
-        <Field type="text" className="text-field" id="slackName" name="slackName" maxLength="32" />
+            <label htmlFor="slackName" className="form-name">If yes, please mention your username on Slack. If no, you are welcome to join - http://slack.layer5.io/</label>
+            <Field type="text" className="text-field" id="slackName" name="slackName" maxLength="32" />
 
-        <label id="content-type" className="form-name">Choose the type of content<span className="required-sign">*</span></label>
-        {validateContent && <p className="validation">Please provide your choice of content.</p>}
-        <div role="group" aria-labelledby="content-type">
-        <label>
-              <Field type="radio" name="content" value="blog" />
+            <label id="content-type" className="form-name">Choose the type of content<span className="required-sign">*</span></label>
+            {validateContent && <p className="validation">Please provide your choice of content.</p>}
+            <div role="group" aria-labelledby="content-type">
+              <label>
+                <Field type="radio" name="content" value="blog" />
               Blog Post
-        </label>
-        <label>
-              <Field type="radio" name="content" value="article" />
+              </label>
+              <label>
+                <Field type="radio" name="content" value="article" />
               Article
-        </label>
-        <label>
-              <Field type="radio" name="content" value="tutorial" />
+              </label>
+              <label>
+                <Field type="radio" name="content" value="tutorial" />
               Tutorial
-        </label>
-        <label>
-              <Field type="radio" name="content" value="workshop" />
+              </label>
+              <label>
+                <Field type="radio" name="content" value="workshop" />
               Workshop
-        </label>
-        <label>
-              <Field type="radio" name="content" value="event" />
+              </label>
+              <label>
+                <Field type="radio" name="content" value="event" />
               Event
-        </label>
-        <label>
-              <Field type="radio" name="content" value="other" />
+              </label>
+              <label>
+                <Field type="radio" name="content" value="other" />
               Other
-        </label>
-        </div>
+              </label>
+            </div>
 
-        <label htmlFor="title" className="form-name">Title</label>
-        <Field type="text" className="text-field" id="title" name="title" />
+            <label htmlFor="title" className="form-name">Title</label>
+            <Field type="text" className="text-field" id="title" name="title" />
 
-        <label htmlFor="details" className="form-name">Details</label>
-        <Field type="text" className="text-field" id="details" name="details" />
+            <label htmlFor="details" className="form-name">Details</label>
+            <Field type="text" className="text-field" id="details" name="details" />
 
-        <label htmlFor="comments" className="form-name">Comments</label>
-        <Field type="text" className="text-field" id="comments" name="comment" />
+            <label htmlFor="comments" className="form-name">Comments</label>
+            <Field type="text" className="text-field" id="comments" name="comment" />
 
-              <div className="form-submit">
+            <div className="form-submit">
               <Button secondary className="btn" title="Submit" />
-              </div>
+            </div>
           </Form>
         </Formik>
       </div>
-    }
+      }
       {
         stepNumber === 1 &&
                     <ThankYou />
@@ -155,12 +155,12 @@ const ContentForm = () => {
 
 const ThankYou = () => {
   return (
-      <div className="thank-you-box">
-        <h2>Thank you for your interest in the Layer5 Writing Program!</h2>
-        <p>You will soon receive detailed guidance from our content team. We'll get back to you as soon as we can.</p>
-        <p>In the meantime, please visit our <a href="https://discuss.layer5.io">community forum</a> or join us in our <a href="https://slack.layer5.io">community Slack</a>.</p>
-        <h3 className="white">- Team <img src={layer5_img} alt="Layer5" width="125" /></h3>
-      </div>
+    <div className="thank-you-box">
+      <h2>Thank you for your interest in the Layer5 Writing Program!</h2>
+      <p>You will soon receive detailed guidance from our content team. We'll get back to you as soon as we can.</p>
+      <p>In the meantime, please visit our <a href="https://discuss.layer5.io">community forum</a> or join us in our <a href="https://slack.layer5.io">community Slack</a>.</p>
+      <h3 className="white">- Team <img src={layer5_img} alt="Layer5" width="125" /></h3>
+    </div>
   );
 };
 
