27dce39965a71f510e23d94e874a46090bf178af
Anant Vijay <anantvijay3@gmail.com>
2022-10-01 15:43:02 +0530

    update css
    Signed-off-by: Anant Vijay <anantvijay3@gmail.com>

--- src/sections/Community/Handbook/mentorships.js
+++ src/sections/Community/Handbook/mentorships.js
@@ -26,20 +26,18 @@ const ConductWrapper = styled.div`
           }
       }
       img{
-          padding-top:5rem;
           height: auto;
           width: 23rem;
           display: block;
           margin-left: auto;
           margin-right: auto;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
       }
       Button{
           margin: 1.25rem 0px;
           color: white;
+      }
+      #col2 {
+        align-self: center
       }
   }
   @media only screen and (max-width: 750px){
