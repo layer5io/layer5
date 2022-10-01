f70acfd7945a674c140b01234493f083e82d9876
Kamal Nayan <geniusamansingh@gmail.com>
2022-10-01 15:05:57 +0000

    updating gatsby config w/signoff
    Signed-off-by: Kamal Nayan <geniusamansingh@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -516,6 +516,34 @@ module.exports = {
         policy: [{ userAgent: "*", allow: "/" }],
       }
     },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              height: 400, // Optional: Overrides optional.ratio
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+              loadingStrategy: "lazy", //Optional: Enable support for lazy-load offscreen iframes. Default is disabled.
+              urlOverrides: [
+                {
+                  id: "youtube",
+                  embedURL: videoId =>
+                    `https://www.youtube-nocookie.com/embed/${videoId}`,
+                },
+              ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
+              containerClass: "embedVideo-container", //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
+              iframeId: false, //Optional: if true, iframe's id will be set to what is provided after 'video:' (YouTube IFrame player API requires iframe id)
+            },
+            // "gatsby-remark-responsive-iframe", //Optional: Must be loaded after gatsby-remark-embed-video
+          },
+        ],
+      },
+    },
     "gatsby-plugin-meta-redirect", // make sure this is always the last one
   ],
 
