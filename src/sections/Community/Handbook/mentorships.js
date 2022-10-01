f234adf1a079edb420979680a82998956ed963bf
Anant Vijay <anantvijay3@gmail.com>
2022-10-01 13:31:31 +0530

    center image
    Signed-off-by: Anant Vijay <anantvijay3@gmail.com>

--- src/sections/Community/Handbook/mentorships.js
+++ src/sections/Community/Handbook/mentorships.js
@@ -32,6 +32,10 @@ const ConductWrapper = styled.div`
           display: block;
           margin-left: auto;
           margin-right: auto;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
       }
       Button{
           margin: 1.25rem 0px;
