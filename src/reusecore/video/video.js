d560b67da6bd8b71e706a71a9e5da2c95be77cc6
Kamal Nayan <geniusamansingh@gmail.com>
2022-10-01 15:05:05 +0000

    making reuasable component w/signoff
    Signed-off-by: Kamal Nayan <geniusamansingh@gmail.com>

---
+++ src/reusecore/video/video.js
@@ -0,0 +1,57 @@
+const config = {
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  graphqlTypegen: true,
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              height: 400, // Optional: Overrides optional.ratio
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+              loadingStrategy: "lazy", //Optional: Enable support for lazy-load offscreen iframes. Default is disabled.
+              urlOverrides: [
+                {
+                  id: "youtube",
+                  embedURL: videoId =>
+                    `https://www.youtube-nocookie.com/embed/${videoId}`
+                }
+              ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
+              containerClass: "embedVideo-container", //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
+              iframeId: false, //Optional: if true, iframe's id will be set to what is provided after 'video:' (YouTube IFrame player API requires iframe id)
+              sandbox: "allow-same-origin allow-scripts allow-presentation" // Optional: iframe sandbox options - Default: undefined
+            }
+          },
+          "gatsby-remark-responsive-iframe" //Optional: Must be loaded after gatsby-remark-embed-video
+        ]
+      }
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png"
+      }
+    },
+
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/"
+      },
+      __key: "pages"
+    }
+  ]
+}
+
+export default config
