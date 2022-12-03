db2ee9665a8c9eade7d12906fcdcfb94148561d1
suhail34 <suhailkna@gmail.com>
2022-12-03 18:55:54 +0530

    Converting Stack of filter to carousel
    Signed-off-by: suhail34 <suhailkna@gmail.com>

--- src/sections/Meshery/Meshery-integrations/IntegrationsGrid.js
+++ src/sections/Meshery/Meshery-integrations/IntegrationsGrid.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
 import { HoneycombGrid } from "./Integration.style";
 import { ResponsiveHoneycomb, Hexagon } from "react-honeycomb";
-import Button from "../../../reusecore/Button";
+// import Button from "../../../reusecore/Button";
 import useDataList from "../../../utils/usedataList";
 import SearchBox from "../../../reusecore/Search";
 import EmptyResources from "../../Resources/Resources-error/emptyStateTemplate";
-import { Location } from "@reach/router";
+// import { Location } from "@reach/router";
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const IntegrationsGrid = ({ category, theme, count }) => {
   const data = useStaticQuery(graphql`
@@ -206,7 +209,7 @@ const IntegrationsGrid = ({ category, theme, count }) => {
         setHideFilter={setHideFilter}
         classnames={["integration-search"]}
       />
-      <section className="category">
+      <Slider slidesToShow={3} slidesToScroll={3} autoplay={true} dots={true} autoplaySpeed={3000} infinite={true} className="category">
         {!hideFilter &&
           categoryNameList.map((item) => {
             return (
@@ -219,7 +222,7 @@ const IntegrationsGrid = ({ category, theme, count }) => {
               </p>
             );
           })}
-      </section>
+      </Slider>
 
       {searchQuery.length > 0 && queryResults.length < 1 ? (
         <EmptyResources
--- src/sections/app.style.js
+++ src/sections/app.style.js
@@ -267,6 +267,15 @@ section{
   }
 }
 
+.slick-prev::before, .slick-next::before{
+  color:black !important;
+}
+
+.slick-slide {
+  width: auto !important;
+}
+
+/* Modal CSS Ends */
 `;
 
 export { GlobalStyle };
