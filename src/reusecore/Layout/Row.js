edc05c58c6b5a0e94bd52a24c29fc06b7dafa0e4
Idise Praise <Idisepraise@gmail.com>
2022-12-27 23:35:37 +0100

    fixed image alignment bug
    Signed-off-by: Idise Praise <Idisepraise@gmail.com>

--- src/reusecore/Layout/Row.js
+++ src/reusecore/Layout/Row.js
@@ -4,7 +4,6 @@ import styled, { css } from "styled-components";
 const RowWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  align-items: baseline;
   margin-left: -15px;
   margin-right: -15px;
   ${(props) =>
