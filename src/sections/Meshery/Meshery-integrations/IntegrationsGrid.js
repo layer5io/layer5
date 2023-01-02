cb8294c8bec8bfca0fb270cd3aaa06a8a7b21b8a
Gaganpreet Kaur Kalsi <gagansinghkalsi4126@gmail.com>
2023-01-03 01:08:17 +0530

    fix: initial array empty
    Signed-off-by: Gaganpreet Kaur Kalsi <gagansinghkalsi4126@gmail.com>

--- src/sections/Meshery/Meshery-integrations/IntegrationsGrid.js
+++ src/sections/Meshery/Meshery-integrations/IntegrationsGrid.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
 import { HoneycombGrid } from "./Integration.style";
 import useDataList from "../../../utils/usedataList";
@@ -51,13 +51,12 @@ const IntegrationsGrid = ({ category, theme, count }) => {
     ["frontmatter", "title"],
     ["frontmatter", "title"]
   );
-  const [activeIntegrationList, setIntegrationList] = useState(
-    data.allMdx.nodes
-  );
+  const [activeIntegrationList, setIntegrationList] = useState([]);
   const [hideFilter, setHideFilter] = useState(false);
+  const allIntegrations = useRef(data.allMdx.nodes);
 
   // fetch all the category names from activeIntegrationList and remove the duplicate category names
-  const categoryNames = activeIntegrationList.reduce(
+  const categoryNames = allIntegrations.current.reduce(
     (initCategory, integration) => {
       if (!initCategory.includes(integration.frontmatter.category)) {
         initCategory.push(integration.frontmatter.category);
@@ -68,7 +67,7 @@ const IntegrationsGrid = ({ category, theme, count }) => {
   );
 
   const categoryCount = (categoryName) => {
-    return activeIntegrationList.reduce((count, integration) => {
+    return allIntegrations.current.reduce((count, integration) => {
       if (integration.frontmatter.category === categoryName) {
         count += 1;
       }
@@ -81,7 +80,7 @@ const IntegrationsGrid = ({ category, theme, count }) => {
       id: -1,
       name: "All",
       isSelected: false,
-      count: activeIntegrationList.length,
+      count: allIntegrations.current.length,
     },
     ...categoryNames.map((categoryName) => {
       return {
