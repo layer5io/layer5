cbc8724e23b2441fbaef6adbe1ba91cd189042c5
Yash sharma <71271069+Yashsharma1911@users.noreply.github.com>
2023-01-22 14:08:44 +0530

    Fixed border radius of project_container
    Changed border-radius of project__card from % unit to rem unit to get a correct curved border in display resolution
    
    Signed-off-by: Yash sharma <71271069+Yashsharma1911@users.noreply.github.com>

--- src/sections/Projects/Project-grid/projectGrid.style.js
+++ src/sections/Projects/Project-grid/projectGrid.style.js
@@ -51,7 +51,7 @@ export const ProjectWrapper = styled.div`
     .project__card{
         box-shadow: 0px 0px 10px ${props => props.theme.DarkTheme ? "#00d3a9" : "lightgrey"};
         background-color: ${props => props.theme.DarkTheme ? "#212121" : "#FFFFFF"};
-        border-radius: 11px;
+        border-radius: 0.5rem;
         display: flex;
         justify-content: center;
         align-items: center;
