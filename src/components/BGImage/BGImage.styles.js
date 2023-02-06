a6df39b5b3a28f258df9416314a0851c178b4c1a
Idise Praise <Idisepraise@gmail.com>
2023-02-06 23:10:33 +0100

    bug: fixed too much whitespace in banner4
    Signed-off-by: Idise Praise <Idisepraise@gmail.com>

--- src/components/BGImage/BGImage.styles.js
+++ src/components/BGImage/BGImage.styles.js
@@ -28,7 +28,7 @@ export const Content = styled.div`
   height: 100%;
   width: 100%;
   z-index: 2;
-  @media (max-width: 768px){
-    top: 40%;
+  @media (max-width: 992px){
+    top: 35%;
   } 
 `;
