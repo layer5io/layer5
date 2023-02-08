d97dd9eadf443b006e436bee4981a892a21c611e
leecalcote <leecalcote@gmail.com>
2023-02-07 23:54:57 -0600

    unsubscribe
    Signed-off-by: leecalcote <leecalcote@gmail.com>

---
+++ src/components/ContactCard-unsubscribe/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Row, Col } from "../../reusecore/Layout";
+import SectionTitle from "../../reusecore/SectionTitle";
+import Button from "../../reusecore/Button";
+// import { MdPermContactCalendar } from "@react-icons/all-files/md/MdPermContactCalendar";
+import { MdEmail } from "@react-icons/all-files/md/MdEmail";
+import ContactWrapper from "./contact.style";
+import { StaticImage } from "gatsby-plugin-image";
+
+const service_mesh_img = "../../assets/images/service-mesh-icons/service-mesh.svg";
+
+const ContactCard = () => {
+  return (
+    <ContactWrapper id="contact">
+      <div className="parentcard">
+        <form id="mc-unsubscribe-form" action="https://layer5.us15.list-manage.com/unsubscribe/post" method="POST">
+          <SectionTitle className="section-title" UniWidth="100%">
+            <h3><span>Unsubscribe</span></h3>
+          </SectionTitle>
+          <Row>
+            <Col md={3}><StaticImage src={service_mesh_img} alt="Service Mesh Icon" /></Col>
+            <Col md={9}>
+              <Row>
+                <Col className="nopad" md={1} xs={1}><MdEmail className="contact-icon" size={30}></MdEmail></Col>
+                <Col md={11} xs={11}><input className="inputrow" type="email" placeholder="Email Address" name="EMAIL" id="email-address" required /></Col>
+              </Row>
+              <Button title="Unsubscribe" id="mc-embedded-subscribe" />
+            </Col>
+          </Row>
+        </form>
+      </div>
+    </ContactWrapper>
+  );
+};
+
+export default ContactCard;
