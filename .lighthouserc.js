af02a5cdbeb357b24213fde3f8eb508ea58898d6
ayush_gitk <ayushsharmaa101@gmail.com>
2023-02-11 19:42:35 +0530

    add statistdir
    Signed-off-by: ayush_gitk <ayushsharmaa101@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,8 +1,8 @@
 module.exports = {
     "ci": {
       "collect": {
-        "startServerCommand": "npx http-server -p 35341",
-        "url" : " http://localhost:35341/404.html" ,
+        "staticDistDir": "./public",
+        "url" : " http://localhost/404.html" ,
       },
       "assert": {
         "preset": "lighthouse:no-pwa",
