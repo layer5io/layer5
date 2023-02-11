49a34e6201d9d68d6864e9790ea2a59dc00d9fde
ayush_gitk <ayushsharmaa101@gmail.com>
2023-02-11 19:00:47 +0530

    add npx server
    Signed-off-by: ayush_gitk <ayushsharmaa101@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "ci": {
       "collect": {
-        "url" : " http://localhost/404.html" ,
+        "startServerCommand": "npx http-server -p 35341",
+        "url" : " http://localhost:35341/404.html" ,
       },
       "assert": {
         "preset": "lighthouse:no-pwa",
