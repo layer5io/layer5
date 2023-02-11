5cfaf485dcc842d95e5d56a40fab4f54289531c2
ayush_gitk <ayushsharmaa101@gmail.com>
2023-02-11 22:27:42 +0530

    add only 4 sites in --url
    Signed-off-by: ayush_gitk <ayushsharmaa101@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
     "ci": {
       "collect": {
         "staticDistDir": "./public",
-        "url" : " http://localhost/404.html" ,
+        "url" : ["http://localhost/404.html" ,"http://localhost/index.html","http://localhost/404/index.html","http://localhost/about/index.html"],
       },
       "assert": {
         "preset": "lighthouse:no-pwa",
