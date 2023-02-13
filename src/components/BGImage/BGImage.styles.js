e1df3971f83088a0d3f26bbd601d67ec07d102ec
Deb Chatterjee <38078961+ShuvamChatterjee28@users.noreply.github.com>
2023-02-13 11:28:25 +0530

    Reverted back
    Signed-off-by: Deb Chatterjee <38078961+ShuvamChatterjee28@users.noreply.github.com>

--- src/components/BGImage/BGImage.styles.js
+++ src/components/BGImage/BGImage.styles.js
@@ -13,23 +13,21 @@ export const FakeBGImg = styled(GatsbyImage)`
   top: 0;
   left: 0;
   width: 100%;
-  max-height:32rem;
-  
+  max-height: 32rem;
   z-index: 0;
   border-radius: 10px;
-  opacity: 1;
+  opacity:1;
   @media (min-width: 768px){
-    opaacity:0;
-    display: none !important;
+    opacity:0;
   } 
 `;
 
 export const Content = styled.div`
-  position: relative;
+  position: absolute;
   top:0;
   height: 100%;
   width: 100%;
-  z-index: 0;
+  z-index: 2;
   @media (max-width: 768px){
     top: 40%;
   } 
