f57139efa5a78952fbbd93aba7d34ba8f64f60a4
Lee Calcote <lee.calcote@layer5.io>
2023-02-17 08:46:30 -0600

    update - needs work
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- src/sections/Meshmap/FeaturesSection/Design/DesignerFeatures_diagram.js
+++ src/sections/Meshmap/FeaturesSection/Design/DesignerFeatures_diagram.js
@@ -1,7 +1,10 @@
 import React from "react";
 import DiagramStyles from "../../diagram/diagram.style";
-import Design1 from "../../images/design1.svg";
-import Design2 from "../../images/design2.svg";
+
+import roleBind1 from "./images/role-binding-1.svg";
+import roleBind2 from "./images/role-binding-2.svg";
+import roleBind3 from "./images/role-binding-3.svg";
+import roleBind4 from "./images/role-binding-4.svg";
 import { useInView } from "react-intersection-observer";
 
 const DesignerFeaturesDiagram = ({ activeExampleIndex }) => {
@@ -9,8 +12,10 @@ const DesignerFeaturesDiagram = ({ activeExampleIndex }) => {
   return (
     <DiagramStyles>
       <div className="root">
-        <img id="design-image1" ref={ref} className={inView ? "show" : "render"} src={Design1} alt="" />
-        <img id="design-image2" className={(activeExampleIndex >= 1) ? "show" : "render"} src={Design2} alt="" />
+        <img id="design-image1" ref={ref} className={inView ? "show" : "render"} src={roleBind1} alt="" />
+        <img id="design-image2" className={(activeExampleIndex >= 1) ? "show" : "render"} src={roleBind2} alt="" />
+        <img id="design-image2" className={(activeExampleIndex >= 2) ? "show" : "render"} src={roleBind3} alt="" />
+        <img id="design-image2" className={(activeExampleIndex >= 3) ? "show" : "render"} src={roleBind4} alt="" />
       </div>
     </DiagramStyles>
   );
