0b3ab06c6b8739456ae3d4792d3dccfb408c31a7
Pranav Singh <pranavsingh02@hotmail.com>
2023-02-21 01:31:44 +0530

    Update index.js

--- src/components/CommonForm/index.js
+++ src/components/CommonForm/index.js
@@ -45,7 +45,7 @@ const CommonForm = ({ form, title,account_desc, submit_title, submit_body }) => 
 
   useEffect(() => {
     if (submit) {
-      axios.post("https://hook.us1.make.com/r5qgpjel5tlhtyndcgjvkrdkoc65417y", {
+      axios.post("https://hook.us1.make.com/nt0s4oiuntfomjsvvrom4x85sx3leg8h", {
         memberFormOne,
       });
     }
