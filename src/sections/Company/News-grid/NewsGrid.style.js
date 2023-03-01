13e9137c63f709ec3d020b1949a61b7192555d8f
shravanikaware <shravanikaware12@gmail.com>
2023-03-01 23:17:03 +0530

    fixed margin issue in mobile screen on press-release buttons
    Signed-off-by: shravanikaware <shravanikaware12@gmail.com>

--- src/sections/Company/News-grid/NewsGrid.style.js
+++ src/sections/Company/News-grid/NewsGrid.style.js
@@ -13,6 +13,8 @@ export const NewsPageWrapper = styled.div`
         width: 100%;
         justify-content: start;
         margin-bottom: 3rem;
+        flex-wrap: wrap;
+        justify-content: start;
         @media only screen and (max-width:990px){
                 
             justify-content: center;
@@ -20,7 +22,7 @@ export const NewsPageWrapper = styled.div`
     }
     }
     .press-release-button{
-        margin-left: 3rem;
+        margin-left: 1.2rem;
         background : rgb(240, 240, 240);
         border-radius: 5px;
         color: black;
@@ -36,6 +38,17 @@ export const NewsPageWrapper = styled.div`
             box-shadow: 0 2px 10px ${props => props.theme.DarkTheme ? "rgba(0, 179, 159, 1.0)" : "rgba(0, 0, 0, 0.4)"};
             }
     }
+    @media only screen and (max-width:450px) and (min-width: 0px){ 
+        .coverage-button,  .press-release-button {
+            margin: 1rem 0;
+            }  
+        } 
+        
+        @media only screen and (max-width:990px){
+           .coverage-button, .press-release-button{
+                margin: 1rem calc(3% / 2);
+           } 
+        }
     
     .filter-buttons {
         display: flex;
@@ -45,7 +58,7 @@ export const NewsPageWrapper = styled.div`
     }
     }
     .search-box-container {
-        width: 35%;
+        width: 0 calc(3% / 2);
     }
     
     .post-content-block {
@@ -70,6 +83,7 @@ export const NewsPageWrapper = styled.div`
                     flex:0 0 100%;
                     max-width:100%;
                     margin-bottom: 1.8rem;
+                    justify-content: center;
                 
             }
         }
