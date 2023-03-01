4a56b2cab9fd03e742cc02df70ba9b7a91aa2769
Ishaan Dwivedi <ishaan.dwivedi99@gmail.com>
2023-03-01 13:54:05 +0530

    fixed margin issue in mobile screen on coverage and press-release buttons in news page

--- src/sections/Company/News-grid/NewsGrid.style.js
+++ src/sections/Company/News-grid/NewsGrid.style.js
@@ -13,20 +13,23 @@ export const NewsPageWrapper = styled.div`
         width: 100%;
         justify-content: start;
         margin-bottom: 3rem;
+        flex-wrap: wrap;
+        justify-content: start;
         @media only screen and (max-width:990px){
-                
             justify-content: center;
-        
-    }
+        }
+      
     }
     .press-release-button{
-        margin-left: 3rem;
+        // when in desktop screen size, the press-release btn is sperated by 1rem space
+        margin-left: 1rem;
         background : rgb(240, 240, 240);
         border-radius: 5px;
         color: black;
         &:hover{
         box-shadow: 0 2px 10px ${props => props.theme.DarkTheme ? "rgba(0, 179, 159, 1.0)" : "rgba(0, 0, 0, 0.5)"};
         }
+      
     }   
     .coverage-button {
         background : rgb(240, 240, 240);
@@ -36,16 +39,30 @@ export const NewsPageWrapper = styled.div`
             box-shadow: 0 2px 10px ${props => props.theme.DarkTheme ? "rgba(0, 179, 159, 1.0)" : "rgba(0, 0, 0, 0.4)"};
             }
     }
+
+
+    // Media Query on coverage and press-release buttons for small screen devices 
+        
+        @media only screen and (max-width:450px) and (min-width: 0px){ 
+        .coverage-button,  .press-release-button {
+            margin: 1rem 0;
+            }  
+        } 
+        
+        @media only screen and (max-width:990px){
+           .coverage-button, .press-release-button{
+                margin: 1rem calc(3% / 2);
+           } 
+        }
     
     .filter-buttons {
         display: flex;
         @media only screen and (max-width:990px){
             display:block;
-        
-    }
+        }
     }
     .search-box-container {
-        width: 35%;
+        width: 0 calc(3% / 2);
     }
     
     .post-content-block {
@@ -66,10 +83,10 @@ export const NewsPageWrapper = styled.div`
         .searchBox{
             flex:0 0 50%;
             @media only screen and (max-width:990px){
-                
                     flex:0 0 100%;
                     max-width:100%;
                     margin-bottom: 1.8rem;
+                    justify-content: center;
                 
             }
         }
