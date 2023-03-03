c929f12798eb842c0998bcef7453b2f114ed31d9
Shivam Sood <shivamsood634@gmail.com>
2023-03-04 01:09:43 +0530

    added collaborate route
    Signed-off-by: Shivam Sood <shivamsood634@gmail.com>

--- src/pages/cloud-native-management/meshmap/collaborate.js
+++ src/pages/cloud-native-management/meshmap/collaborate.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
 
-import Layout from "../../components/layout";
+import Layout from "../../../components/layout";
 
-import SEO from "../../components/seo";
-import Navigation from "../../sections/General/Navigation";
-import Footer from "../../sections/General/Footer";
-import { GlobalStyle } from "../../sections/app.style";
-import { darktheme } from "../../theme/app/themeStyles";
-import lighttheme from "../../theme/app/themeStyles";
-import Meshmap from "../../sections/Meshmap/index";
+import SEO from "../../../components/seo";
+import Navigation from "../../../sections/General/Navigation";
+import Footer from "../../../sections/General/Footer";
+import { GlobalStyle } from "../../../sections/app.style";
+import { darktheme } from "../../../theme/app/themeStyles";
+import lighttheme from "../../../theme/app/themeStyles";
+import Meshmap from "../../../sections/Meshmap/index";
 
 const MeshmapPage = () => {
   const [theme, setTheme] = useState();
