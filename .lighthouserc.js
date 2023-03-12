1c617b332e4b3acf8bd3d0733c6d823609198f7d
ayush_gitk <ayushsharmaa101@gmail.com>
2023-03-12 19:03:44 +0530

    add more page & disable aria-hidden-focus
    Signed-off-by: ayush_gitk <ayushsharmaa101@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -7,12 +7,15 @@ module.exports = {
                  "http://localhost/404/index.html",
                  "http://localhost/about/index.html" ,
                  "http://localhost/cloud-native-management/meshery/index.html",
-                 "http://localhost/cloud-native-management/meshmap/index.html"],
+                 "http://localhost/cloud-native-management/meshmap/index.html",
+                 "http://localhost/learn/index.html",
+                 "http://localhost/community/index.html"],
       },
       "assert": {
         "preset": "lighthouse:no-pwa",
         "assertions": {
           "csp-xss": "off",
+          "aria-hidden-focus": "off"
         }
       }
     }
