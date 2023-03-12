7ae70d14915ce0e1154cc3327a4983242125d6c8
ayush_gitk <ayushsharmaa101@gmail.com>
2023-03-12 17:13:18 +0530

    add 2 more pages
    Signed-off-by: ayush_gitk <ayushsharmaa101@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,7 +2,12 @@ module.exports = {
     "ci": {
       "collect": {
         "staticDistDir": "./public",
-        "url" : ["http://localhost/404.html" ,"http://localhost/index.html","http://localhost/404/index.html","http://localhost/about/index.html"],
+        "url" : ["http://localhost/404.html",
+                 "http://localhost/index.html",
+                 "http://localhost/404/index.html",
+                 "http://localhost/about/index.html" ,
+                 "http://localhost/cloud-native-management/mesher/index.html",
+                 "http://localhost/cloud-native-management/mesher/index.html"],
       },
       "assert": {
         "preset": "lighthouse:no-pwa",
