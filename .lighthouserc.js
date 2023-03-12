83c830b9bad8c1092a9430e08b9f39fd51916621
ayush_gitk <ayushsharmaa101@gmail.com>
2023-03-12 17:33:21 +0530

    fix1
    Signed-off-by: ayush_gitk <ayushsharmaa101@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -6,8 +6,8 @@ module.exports = {
                  "http://localhost/index.html",
                  "http://localhost/404/index.html",
                  "http://localhost/about/index.html" ,
-                 "http://localhost/cloud-native-management/mesher/index.html",
-                 "http://localhost/cloud-native-management/mesher/index.html"],
+                 "http://localhost/cloud-native-management/meshery/index.html",
+                 "http://localhost/cloud-native-management/meshmap/index.html"],
       },
       "assert": {
         "preset": "lighthouse:no-pwa",
