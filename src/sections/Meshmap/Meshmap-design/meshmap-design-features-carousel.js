b2442fb068929221eb17d5c87fc7aea0965a7745
avikt <avikumrawat18@gmail.com>
2023-03-14 15:42:25 +0530

    add floating animation to cards
    Signed-off-by: avikt <avikumrawat18@gmail.com>

--- src/sections/Meshmap/Meshmap-design/meshmap-design-features-carousel.js
+++ src/sections/Meshmap/Meshmap-design/meshmap-design-features-carousel.js
@@ -50,6 +50,23 @@ const DivWrapper = styled.div`
     flex: 1;
     // right: 20px;
     top: -50px;
+
+    animation-name: floating;
+    animation-duration: 3s;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in-out;
+
+    @keyframes floating {
+      0% {
+        transform: translate(0, 0px);
+      }
+      50% {
+        transform: translate(0, 15px);
+       }
+      100% {
+        transform: translate(0, -0px);
+      }
+    }
   }
 `;
 
