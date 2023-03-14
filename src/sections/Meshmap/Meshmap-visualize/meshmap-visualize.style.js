0765dc3667afde73142eef098b9ee8f975d4ab50
Shivam Sood <shivamsood634@gmail.com>
2023-03-15 02:09:22 +0530

    added visualize page
    Signed-off-by: Shivam Sood <shivamsood634@gmail.com>

---
+++ src/sections/Meshmap/Meshmap-visualize/meshmap-visualize.style.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+const MeshmapVisualizeWrapper = styled.div`
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  :div:first-child {
+    align-items: center;
+  }
+
+  .banner-background {
+    margin: 1rem;
+
+    .description{
+      margin-top: 3rem;
+
+        .desc-text {
+          padding: 0 1rem 0 1rem;
+          h1{
+              font-weight: 600;
+          }
+          .heading-1 {
+              font-weight: 500;
+          }
+          .heading-2 {
+              font-weight: 700;
+              color: ${props => props.theme.secondaryColor};
+          }
+          .desc-p {
+
+              margin: 1.875rem 0 2.5rem 0;
+          }
+        }
+    }
+  }
+
+`;
+
+export default MeshmapVisualizeWrapper;
