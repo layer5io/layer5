debf651e23316aa66997ae2d4f2db6547ab66c6d
Randy Lau <randychilau@gmail.com>
2023-03-16 15:21:52 -0700

    Add useStyledDarkMode hook to access toggle, etc
    Signed-off-by: Randy Lau <randychilau@gmail.com>

---
+++ src/theme/app/useStyledDarkMode.js
@@ -0,0 +1,16 @@
+//hook to provide access to toggle functions and more from the ThemeManagerContext via useContext
+
+import { useContext } from "react";
+import { ThemeManagerContext } from "./ThemeManager";
+
+export const useStyledDarkMode = () => {
+  const { isDark, changeThemeSetting, toggleDark, themeSetting } = useContext(
+    ThemeManagerContext);
+
+  return {
+    isDark,
+    changeThemeSetting,
+    toggleDark,
+    themeSetting,
+  };
+};
