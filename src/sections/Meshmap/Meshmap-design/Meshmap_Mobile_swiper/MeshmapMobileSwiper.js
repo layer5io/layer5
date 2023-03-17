6617bfb8401e4d823376de9278908b82ae4afd31
shravanikaware <shravanikaware12@gmail.com>
2023-03-18 01:08:43 +0530

    swiper improved
    Signed-off-by: shravanikaware <shravanikaware12@gmail.com>

--- src/sections/Meshmap/Meshmap-design/Meshmap_Mobile_swiper/MeshmapMobileSwiper.js
+++ src/sections/Meshmap/Meshmap-design/Meshmap_Mobile_swiper/MeshmapMobileSwiper.js
@@ -1,3 +1,7 @@
+ /* eslint-disable */
+
+
+
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import styled from "styled-components";
@@ -5,50 +9,66 @@ import "swiper/css";
 import "swiper/css/effect-cards";
 import { EffectCards, Pagination, Navigation, Autoplay } from "swiper";
 import "./cardStyle.css";
+import img from "./images/BlankSwiper.png";
 
 const CONTENT = [
   {
     title: "Design your deployments the way you want",
     description:
       "Drag-and-drop your cloud native infrastructure using a pallete of thousands of versioned Kubernetes components. Say goodbye to YAML configurations.",
+    img: img,
+    readMoreLink: "https://google.com",
   },
   {
     title: "Manage your applications visually and intuitively",
     description:
       "Play with powerful features including context-aware designs and namespace configurations to easily manage all services.",
+    img: img,
+    readMoreLink: "https://google.com",
   },
   {
     title: "Build the bridge",
     description:
       "Go from developer handoffs to ongoing conversations with a shared language between design and development.",
+    img: img,
+    readMoreLink: "https://google.com",
   },
   {
     title: "Stop finger-pointing",
     description:
       "Make infrastructure and application management less about finger-pointing and more about collaborating.",
+    img: img,
+    readMoreLink: "https://google.com",
   },
 ];
 
-const Container = styled.div`
-  @media (min-width: 769px) {
-    display: none;
-  }
 
-  width: 45vw;
+const Container = styled.div`
+@media (min-width: 769px) {
+  display: none;
+}
+// @media (max-width:625px){
+//   width:50vw;
+// }
+@media (max-width:470px){
+  width:70vw;
+}
+@media (max-width:560px){
+  width:62vw;
+}
+
+  border-radius:20px;
+  width: 57vw;
   // height:100vh;
 
   .swiper {
     width: 100%;
     height: 100%;
+   
   }
 
   h1 {
     text-align: center;
-  }
-
-  img {
-    object-fit: contain;
-    // image-position:cover;
   }
 
   h2 {
@@ -60,11 +80,13 @@ const Container = styled.div`
   }
 `;
 
-const Card = ({ title, description }) => {
+const Card = ({ title, description, img , readMoreLink }) => {
   return (
     <div className="card">
       <h2>{title}</h2>
       <p>{description}</p>
+      <img src={img} alt="" />
+      <a href={readMoreLink}>Read More</a>
     </div>
   );
 };
@@ -78,14 +100,14 @@ export const MeshmapMobileSwiper = () => {
           Designer and Visualizer live side-by-side, so all design work, from ideation to operation, can be found in one
           place.
         </h6>
-        <Swiper
+      <Swiper
           autoplay={{
             delay: 2000,
             disableOnInteraction: false,
           }}
-          pagination={{
-            type: "fraction",
-          }}
+          // pagination={{
+          //   type: "fraction",
+          // }}
           scrollbar={{
             draggable: true,
           }}
@@ -95,9 +117,9 @@ export const MeshmapMobileSwiper = () => {
           navigation={false}
           className="mySwiper"
         >
-          {CONTENT.map(({ title, description }) => (
+          {CONTENT.map(({ title, description, img, readMoreLink }) => (
             <SwiperSlide key={title}>
-              <Card title={title} description={description} />
+              <Card title={title} description={description} img={img} readMoreLink={readMoreLink}/>
             </SwiperSlide>
           ))}
         </Swiper>
@@ -105,3 +127,4 @@ export const MeshmapMobileSwiper = () => {
     </>
   );
 };
+/* eslint-disable-line */ 
