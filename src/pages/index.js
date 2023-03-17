4cff4415c41362124823b36a833b65dddebefc4f
Randy Lau <randychilau@gmail.com>
2023-03-16 17:47:08 -0700

    removed Navigation, themeProvider and all related
    Signed-off-by: Randy Lau <randychilau@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Partners from "../sections/Home/Partners-home";
