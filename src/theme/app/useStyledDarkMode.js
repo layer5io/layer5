f32724403aefbb55244f54b152c755d0ff10a1ed
Randy Lau <randychilau@gmail.com>
2023-03-16 17:43:18 -0700

    added comments
    Signed-off-by: Randy Lau <randychilau@gmail.com>

--- src/theme/app/useStyledDarkMode.js
+++ src/theme/app/useStyledDarkMode.js
@@ -4,8 +4,7 @@ import { useContext } from "react";
 import { ThemeManagerContext } from "./ThemeManager";
 
 export const useStyledDarkMode = () => {
-  const { isDark, changeThemeSetting, toggleDark, themeSetting } = useContext(
-    ThemeManagerContext);
+  const { isDark, changeThemeSetting, toggleDark, themeSetting } = useContext(ThemeManagerContext);
 
   return {
     isDark,
