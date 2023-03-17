75b422b9930ab75c8b295eabc14f6a2db265b80e
Randy Lau <randychilau@gmail.com>
2023-03-16 17:40:16 -0700

    added GlobalStyle and Navigation
    Signed-off-by: Randy Lau <randychilau@gmail.com>

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,14 +5,20 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
+// added GlobalStyle in Layout as recommended: https://www.gatsbyjs.com/docs/how-to/styling/global-css/
+
 import React from "react";
 import PropTypes from "prop-types";
 import ScrollToTopBtn from "./Scrolltotop-button";
+import { GlobalStyle } from "../sections/app.style";
+import Navigation from "../sections/General/Navigation";
 
 const Layout = ({ children }) => {
 
   return (
     <>
+      <GlobalStyle />
+      <Navigation/>
       {children}
       <ScrollToTopBtn />
     </>
