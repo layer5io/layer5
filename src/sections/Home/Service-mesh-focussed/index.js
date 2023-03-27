84f136b7d875e01067df5c0478f7e7fcb4320a57
Shivam Sood <shivamsood634@gmail.com>
2023-03-27 16:05:20 +0530

    added second book
    Signed-off-by: Shivam Sood <shivamsood634@gmail.com>

--- src/sections/Home/Service-mesh-focussed/index.js
+++ src/sections/Home/Service-mesh-focussed/index.js
@@ -6,6 +6,7 @@ import Button from "../../../reusecore/Button";
 import { FaArrowRight } from "@react-icons/all-files/fa/FaArrowRight";
 import { StaticImage } from "gatsby-plugin-image";
 const ServiceMeshFocusedImage = "../../../assets/images/homePage-images/The-Enterprise-path-to-service-mesh-Architectures-2nd-Edition.png";
+const ServiceMeshPatternsImage = "../../../assets/images/homePage-images/service-mesh-patterns.png";
 
 const ServiceMeshFocused = () => {
 
@@ -14,9 +15,9 @@ const ServiceMeshFocused = () => {
       <Container>
         <Row className="heading">
           <Col>
-            <p className="epic">Cloud native focused.</p>
+            {/* <p className="epic">Cloud native focused.</p> */}
             {/* <h1>Service mesh focused.</h1> */}
-            <h1>Service mesh neutral</h1>
+            <h1>Cloud native focused</h1>
             <p className="epic2"> Dedicated to empowering you to expect more from your infrastructure.</p>
           </Col>
         </Row>
@@ -32,6 +33,20 @@ const ServiceMeshFocused = () => {
             <Button className="book_btn" primary url="/learn/service-mesh-books/the-enterprise-path-to-service-mesh-architectures-2nd-edition" title="Read Book">
               <FaArrowRight className="icon-right" />
             </Button>
+          </Col>
+        </Row>
+        <Row Vcenter >
+          <Col xs={12} sm={6} lg={6}>
+            <img src={BlockQuoteImage} className="quote-icon" alt="Quote from book" />
+            <p className="quote">
+              "A service mesh is a layer in your infrastructure that facilitates communication between services… and so much more. Its value is enormous, and the value you derive from one is very much related to what role you play in the design, implementation, and operations of your cloud native applications and infrastructure."
+            </p>
+            <Button className="book_btn" primary url="/learn/service-mesh-books/service-mesh-patterns" title="Read Book">
+              <FaArrowRight className="icon-right" />
+            </Button>
+          </Col>
+          <Col xs={12} sm={6} lg={6} className="book-col" >
+            <StaticImage src={ServiceMeshPatternsImage} className="book" alt="The-Enterprise-Path-to-Service-Mesh-Architectures" />
           </Col>
         </Row>
       </Container>
