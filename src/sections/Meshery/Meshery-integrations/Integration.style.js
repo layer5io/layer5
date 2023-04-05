c242fba0250bd3017f49321ae373291fe5de90b7
AkashS20 <akashsingh.mail2@gmail.com>
2023-04-05 23:49:18 +0530

    Fixed inconsistent button behavior on hover
    Signed-off-by: AkashS20 <akashsingh.mail2@gmail.com>

--- src/sections/Meshery/Meshery-integrations/Integration.style.js
+++ src/sections/Meshery/Meshery-integrations/Integration.style.js
@@ -158,6 +158,11 @@ export const IntegrationsWrapper = styled.div`
   max-width: 75rem;
   margin: 5.1875rem auto;
   text-align: center;
+  .seeAll-btn {
+    &:hover {
+      color: white;
+    }
+  }
 `;
 
 export const IntegrationCard = styled.div`
