c4132583bf7c53cf17a688f289a3f6f5723295f2
ayushthe1 <ayushsharmaa101@gmail.com>
2023-04-07 18:26:24 +0530

    update config file
    Signed-off-by: ayushthe1 <ayushsharmaa101@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,27 +1,11 @@
+const urls = require('./urls.json');
+
 module.exports = {
     "ci": {
       "collect": {
         "staticDistDir": "./public",
-        "url" : ["http://localhost/404.html",
-                 "http://localhost/index.html",
-                 "http://localhost/404/index.html",
-                 "http://localhost/about/index.html" ,
-                 "http://localhost/cloud-native-management/meshery/index.html",
-                 "http://localhost/cloud-native-management/meshmap/index.html",
-
-                 "http://localhost/learn/learning-paths/index.html",
-                 "http://localhost/learn/service-mesh-books/index.html",
-                 "http://localhost/learn/service-mesh-workshops/index.html",
-                 "http://localhost/learn/index.html",
-
-                 "http://localhost/blog/index.html",
-                 "http://localhost/resources/index.html",
-                 "http://localhost/careers/index.html",
-
-                 "http://localhost/community/members/index.html",
-                 "http://localhost/community/meshmates/index.html",
-                 "http://localhost/community/events/index.html",
-                 "http://localhost/community/newcomers/index.html",],
+        "numberOfRuns": 1,
+        "url": urls
       },
       "assert": {
         "preset": "lighthouse:no-pwa",
