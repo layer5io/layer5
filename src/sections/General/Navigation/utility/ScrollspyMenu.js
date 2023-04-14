c1f7797679f15a46cc9a944c8740e86e3056034a
melsonic <madhurjyakalita2017@gmail.com>
2023-04-14 10:39:26 +0530

    removed unwanted formatting
    Signed-off-by: melsonic <madhurjyakalita2017@gmail.com>

--- src/sections/General/Navigation/utility/ScrollspyMenu.js
+++ src/sections/General/Navigation/utility/ScrollspyMenu.js
@@ -18,14 +18,14 @@ import EmptyDark from "../../../../sections/Meshmap/Meshmap-collaborate/images/b
 import { useInView } from "react-intersection-observer";
 
 const ScrollspyMenu = ({ menuItems, theme, ...props }) => {
-  const { blogData, className } = props;
+  const { blogData,className } = props;
 
   const addAllClasses = className ? [className] : [""];
-  const [activeState, setActiveState] = useState(null);
+  const [activeState,setActiveState] = useState(null);
 
   const [hover, setHover] = useState(false);
 
-  const [isWrapVisible, setIsWrapperVisible] = useState(false);
+  const [isWrapVisible,setIsWrapperVisible] = useState(false);
   const [imageInView, setimageInView] = useState(false);
 
   const handleMouseOver = (index) => {
@@ -43,30 +43,24 @@ const ScrollspyMenu = ({ menuItems, theme, ...props }) => {
   const [transitionRef, inView] = useInView({ threshold: 0.7 });
 
   useEffect(() => {
-    if (inView && !imageInView) {
+    if (inView && !imageInView){
       setimageInView(true);
-    } else if (imageInView && !inView) {
+    } else if (imageInView && !inView){
       setimageInView(false);
     }
-  }, [inView, imageInView]);
+  },[inView,imageInView]);
 
   return (
-    <ul
-      className={addAllClasses.join(" ")}
-      onMouseEnter={wrapDisplay}
-      onMouseLeave={wrapNone}
-    >
+    <ul className={addAllClasses.join(" ")} onMouseEnter={wrapDisplay} onMouseLeave={wrapNone}>
       {menuItems.map((menu, index) => (
         <li
           key={index}
-          className={menu.subItems ? "nav-item has-dropdown" : "nav-item"}
+          className={
+            menu.subItems ? "nav-item has-dropdown" : "nav-item"
+          }
           onMouseOver={() => handleMouseOver(index)}
         >
-          <Link
-            to={menu.path}
-            className="menu-link"
-            activeClassName="nav-link-active"
-          >
+          <Link to={menu.path} className="menu-link" activeClassName="nav-link-active">
             <span>
               {menu.name}
             </span>
@@ -78,9 +72,7 @@ const ScrollspyMenu = ({ menuItems, theme, ...props }) => {
           <ul className="dropdown" style={{ zIndex: "101" }}>
             <div className="nav-grid">
               <div
-                className="hr"
-                onMouseOver={() => setHover(true)}
-                onMouseOut={() => setHover(false)}
+                className="hr" onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)}
               >
                 <Link
                   className="section"
@@ -91,166 +83,71 @@ const ScrollspyMenu = ({ menuItems, theme, ...props }) => {
                 </Link>
                 {activeState.subItems.map((subItem, i) => (
                   <li key={i}>
-                    {subItem.name === "Forum"
-                      ? (
-                        <a
-                          href={subItem.path}
-                          target="_blank"
-                          className="sub-item"
-                          rel="noreferrer"
-                        >
-                          {subItem.name}
-                        </a>
-                      )
-                      : (
-                        <div>
-                          {hover
-                            ? (
-                              <Link
-                                to={subItem.path}
-                                partiallyActive={true}
-                                className={subItem.sepLine && "sub-item"}
-                                activeClassName="nav-link-active"
-                              >
-                                {subItem.name}
-                              </Link>
-                            )
-                            : (
-                              <Link
-                                to={subItem.path}
-                                partiallyActive={true}
-                                className={subItem.sepLine && "sub-item"}
-                                activeClassName="nav-link-active"
-                                style={(activeState.name === "Projects" &&
-                                    !subItem.sepLine)
-                                  ? { display: "none" }
-                                  : { display: "block" }}
-                              >
-                                {subItem.name}
-                              </Link>
-                            )}
-                        </div>
-                      )}
+                    {subItem.name === "Forum" ?
+                      <a href={subItem.path} target="_blank" className="sub-item" rel="noreferrer">
+                        {subItem.name}
+                      </a>
+                      : <div>
+                        {hover
+                          ? <Link to={subItem.path} partiallyActive={true} className={subItem.sepLine && "sub-item"} activeClassName="nav-link-active"
+                          >
+                            {subItem.name}
+                          </Link>
+                          : <Link to={subItem.path} partiallyActive={true} className={subItem.sepLine && "sub-item"} activeClassName="nav-link-active" style={(activeState.name === "Projects" && !subItem.sepLine) ? { display: "none" } : { display: "block" }}
+                          >
+                            {subItem.name}
+                          </Link>
+                        }
+                      </div>
+                    }
                   </li>
                 ))}
                 <div className="action-items">
                   {activeState.actionItems.map((actionItem, i) => (
-                    actionItem.actionName === "Join the discussion"
-                      ? (
-                        <a
-                          href={actionItem.actionLink}
-                          target="_blank"
-                          className="action-link"
-                          rel="noreferrer"
-                        >
-                          <span className="readmore-btn">
-                            {actionItem.actionName} <IoIosArrowRoundForward />
-                          </span>
-                        </a>
+                    (actionItem.actionName === "Join the discussion" ?
+                      <a href={actionItem.actionLink} target="_blank" className="action-link" rel="noreferrer">
+                        <span className="readmore-btn">
+                          {actionItem.actionName} <IoIosArrowRoundForward />
+                        </span>
+                      </a>
+                      : (<Link key={i} to={actionItem.actionLink} partiallyActive={true} className="action-link">
+                        <span className="readmore-btn">
+                          {actionItem.actionName} <IoIosArrowRoundForward />
+                        </span>
+                      </Link>
                       )
-                      : (
-                        <Link
-                          key={i}
-                          to={actionItem.actionLink}
-                          partiallyActive={true}
-                          className="action-link"
-                        >
-                          <span className="readmore-btn">
-                            {actionItem.actionName} <IoIosArrowRoundForward />
-                          </span>
-                        </Link>
-                      )
+                    )
                   ))}
                 </div>
               </div>
-              {blogData[activeState.name].nodes.length !== 0
-                ? (
-                  <div className="nav-display">
-                    <Card
-                      frontmatter={blogData[activeState.name].nodes[0]
-                        .frontmatter}
-                      fields={blogData[activeState.name].nodes[0].fields}
-                    />
-                    <Card
-                      frontmatter={blogData[activeState.name].nodes[1]
-                        .frontmatter}
-                      fields={blogData[activeState.name].nodes[1].fields}
-                    />
-                  </div>
-                )
-                : (
-                  <>
-                    <Link to="/cloud-native-management/meshmap">
-                      <div className="single-card">
-                        <div
-                          className="transition-container"
-                          ref={transitionRef}
-                        >
-                          <img
-                            className="canvas"
-                            src={theme == "dark" ? EmptyDark : EmptyLight}
-                            alt=""
-                          />
-                          <img
-                            className="service-interface"
-                            src={theme == "dark"
-                              ? ServiceIntefaceImageDark
-                              : ServiceIntefaceImage}
-                            alt=""
-                          />
-                          <img
-                            className={imageInView
-                              ? "ingress-gateway-transition ingress-gateway"
-                              : "ingress-gateway"}
-                            src={theme == "dark"
-                              ? IngressGatewayImageDark
-                              : IngressGatewayImage}
-                            alt=""
-                          />
-                          <img
-                            className={imageInView
-                              ? "kubernetes-transition kubernetes"
-                              : "kubernetes"}
-                            src={theme == "dark"
-                              ? KubernetesImageDark
-                              : KubernetesImage}
-                            alt=""
-                          />
-                          <img
-                            className={imageInView
-                              ? "pod-transition pod"
-                              : "pod"}
-                            src={theme == "dark" ? PodImageDark : PodImage}
-                            alt=""
-                          />
-                          <img
-                            className={imageInView
-                              ? "prometheus-transition prometheus"
-                              : "prometheus"}
-                            src={theme == "dark"
-                              ? PrometheusImageDark
-                              : PrometheusImage}
-                            alt=""
-                          />
-                          <img
-                            className={imageInView
-                              ? "supporting-arrows-transition supporting-arrows"
-                              : "supporting-arrows"}
-                            src={SupportingArrows}
-                            alt=""
-                          />
-                        </div>
-                        <div className="card-text">
-                          <p>MeshMap</p>
-                          <h6>Stop finger-pointing and start collaborating.</h6>
-                        </div>
+              {blogData[activeState.name].nodes.length !== 0 ? (
+                <div className="nav-display">
+                  <Card frontmatter={blogData[activeState.name].nodes[0].frontmatter} fields={blogData[activeState.name].nodes[0].fields} />
+                  <Card frontmatter={blogData[activeState.name].nodes[1].frontmatter} fields={blogData[activeState.name].nodes[1].fields} />
+                </div>) : (
+                <>
+                  <Link to="/cloud-native-management/meshmap">
+                    <div className="single-card">
+                      <div className="transition-container" ref={transitionRef}>
+                        <img className="canvas" src={theme == "dark" ? EmptyDark : EmptyLight} alt="" />
+                        <img className="service-interface" src={theme == "dark" ? ServiceIntefaceImageDark : ServiceIntefaceImage} alt="" />
+                        <img className={imageInView ? "ingress-gateway-transition ingress-gateway" : "ingress-gateway"} src={theme == "dark" ? IngressGatewayImageDark : IngressGatewayImage} alt="" />
+                        <img className={imageInView ? "kubernetes-transition kubernetes" : "kubernetes"} src={theme == "dark" ? KubernetesImageDark : KubernetesImage} alt="" />
+                        <img className={imageInView ? "pod-transition pod" : "pod"} src={theme == "dark" ? PodImageDark : PodImage} alt="" />
+                        <img className={imageInView ? "prometheus-transition prometheus" : "prometheus"} src={theme == "dark" ? PrometheusImageDark : PrometheusImage} alt="" />
+                        <img className={imageInView ? "supporting-arrows-transition supporting-arrows" : "supporting-arrows"} src={SupportingArrows} alt="" />
                       </div>
-                    </Link>
-                  </>
-                )}
+                      <div className="card-text">
+                        <p>MeshMap</p>
+                        <h6>Stop finger-pointing and start collaborating.</h6>
+                      </div>
+                    </div>
+                  </Link>
+                </>)
+              }
             </div>
           </ul>
-          {isWrapVisible && <div className="wrap" style={{ zIndex: "100" }} />}
+          {isWrapVisible && <div className="wrap"  style={{ zIndex: "100" }} />}
         </React.Fragment>
       )}
     </ul>
