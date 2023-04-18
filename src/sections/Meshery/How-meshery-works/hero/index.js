1f09eb4a83faab792cc25b62bb1417d3a21ce98c
Randy Lau <randychilau@gmail.com>
2023-04-18 00:43:03 -0700

    center HeroImage on operating-service-meshes page
    Signed-off-by: Randy Lau <randychilau@gmail.com>

--- src/sections/Meshery/How-meshery-works/hero/index.js
+++ src/sections/Meshery/How-meshery-works/hero/index.js
@@ -40,7 +40,9 @@ const HowMesheryWorksHeroWrapper = styled.div`
         }
 
         padding: 2rem 2rem;
-        margin: auto;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
         @media only screen and (max-width: 992px){
             margin:auto;
             padding: 1rem 0rem 0rem 0rem;
