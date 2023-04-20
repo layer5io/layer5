37f6c86d43a1fca70ed05221dcc2c4938e848bea
Akash Singh <akashsingh.mail2@gmail.com>
2023-04-20 11:05:08 +0530

    fix changes
    Signed-off-by: Akash Singh <akashsingh.mail2@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,12 +24,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-purgecss",
       options: {
-        printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        // tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+        printRejected: false,
       }
     },
     "gatsby-plugin-sitemap",
