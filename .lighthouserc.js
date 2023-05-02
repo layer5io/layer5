8fe17c6c0afbcf9dc75af2088be7fd887b0da80f
Randy Lau <randychilau@gmail.com>
2023-05-02 15:02:05 -0700

    revert lighthouse provide reporting with links
    Signed-off-by: Randy Lau <randychilau@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,32 +1,34 @@
 module.exports = {
-  "ci": {
-    "collect": {
-      "staticDistDir": "./public",
+    "ci": {
+      "collect": {
+        "staticDistDir": "./public",
         "url" : ["http://localhost/404.html",
-               "http://localhost/index.html",
-               "http://localhost/404/index.html",
-               "http://localhost/company/about.html" ,
-               "http://localhost/cloud-native-management/meshery.html",
-               "http://localhost/cloud-native-management/meshmap.html",
-               "http://localhost/learn/learning-paths.html",
-               "http://localhost/learn/service-mesh-books.html",
-               "http://localhost/learn/service-mesh-workshops.html",
-               "http://localhost/learn.html",
-               "http://localhost/blog.html",
-               "http://localhost/resources.html",
-               "http://localhost/careers.html",
-               "http://localhost/community/members.html",
-               "http://localhost/community/meshmates.html",
-               "http://localhost/community/events.html",
-               "http://localhost/community/newcomers.html",],
-    },
-    "assert": {
-      "assertions": {
-        "categories:performance": "warn",
-        "categories:accessibility": "warn",
-        "categories:best-practices": "warn",
-        "categories:seo": "warn",
-      }
-    },
-  }
-};
+                 "http://localhost/index.html",
+                 "http://localhost/404/index.html",
+                 "http://localhost/company/about.html" ,
+                 "http://localhost/cloud-native-management/meshery.html",
+                 "http://localhost/cloud-native-management/meshmap.html",
+                 "http://localhost/learn/learning-paths.html",
+                 "http://localhost/learn/service-mesh-books.html",
+                 "http://localhost/learn/service-mesh-workshops.html",
+                 "http://localhost/learn.html",
+                 "http://localhost/blog.html",
+                 "http://localhost/resources.html",
+                 "http://localhost/careers.html",
+                 "http://localhost/community/members.html",
+                 "http://localhost/community/meshmates.html",
+                 "http://localhost/community/events.html",
+                 "http://localhost/community/newcomers.html",],
+      },
+      "assert": {
+        "preset": "lighthouse:no-pwa",
+        "assertions": {
+          "csp-xss": "off",
+          "aria-hidden-focus": "off"
+        }
+      },
+      "upload": {
+        "target": "temporary-public-storage",
+      },
+    }
+  };
