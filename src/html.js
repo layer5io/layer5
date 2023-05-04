69389febde7a7c6c731a1252dd55becbe3018356
ayushthe1 <ayushsharmaa101@gmail.com>
2023-05-04 13:03:01 +0530

    migrate to gtm
    Signed-off-by: ayushthe1 <ayushsharmaa101@gmail.com>

--- src/html.js
+++ src/html.js
@@ -1,10 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const gtmScript = `
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-PS26QB9');`;
+
 export default function HTML(props) {
   return (
     <html lang="en" {...props.htmlAttributes}>
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: gtmScript,
+          }}
+        />
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
@@ -14,6 +26,12 @@ export default function HTML(props) {
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PS26QB9"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         <script dangerouslySetInnerHTML={{
           __html:
             `(function() {
