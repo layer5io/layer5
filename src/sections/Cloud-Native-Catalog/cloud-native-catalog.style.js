90b430d99faca8262450c524221886c076b15d51
Senali <senalidilumika@gmail.com>
2023-05-05 02:19:53 +0530

    Catalog header diagram
    Signed-off-by: Senali <senalidilumika@gmail.com>

--- src/sections/Cloud-Native-Catalog/cloud-native-catalog.style.js
+++ src/sections/Cloud-Native-Catalog/cloud-native-catalog.style.js
@@ -23,33 +23,206 @@ export const CloudNativeCatalogWrapper = styled.div`
       margin-bottom: 20px;
     }
     .header__title_description {
-      color: #1E2117;
+      color: #1e2117;
       font-weight: 400;
       font-size: 1.438rem;
       line-height: 2rem;
       padding-right: 80px;
       margin-bottom: 30px;
     }
-    .catalogIcon {
-      height: 12rem;
-      width: auto;
-    }
-    .col {
+    .catalog {
       display: flex;
-    }
-    .catalog-image-col {
       justify-content: center;
+      align-items: center;
+      flex-wrap: wrap;
+      .calalog__card {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #1e2117;
+        border: 1px solid #00b39f;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        border-radius: 10px;
+        .calalog__card__title {
+          text-align: center;
+          color: #ffffff;
+        }
+        .catalog__card__icon {
+          height: 3.5rem;
+          width: 3.5rem;
+        }
+      }
+      .calalog__card--main {
+        padding: 40px 20px;
+        z-index: 1;
+        max-width: 260px;
+        .calalog__card__title {
+          font-weight: 500;
+          font-size: 35px;
+          line-height: 42px;
+        }
+      }
+      .calalog__card--child {
+        padding: 25px 30px;
+        position: absolute;
+        .calalog__card__title {
+          font-weight: 400;
+          font-size: 27px;
+          line-height: 32px;
+        }
+      }
+      .calalog__card--programs {
+        top: 10px;
+        left: 20px;
+        z-index: 2;
+        .calalog__card__title {
+          margin-right: 5px;
+        }
+        .catalog__card__icon {
+          margin-left: 5px;
+        }
+      }
+      .calalog__card--policies {
+        bottom: 20px;
+        right: 20px;
+        z-index: 2;
+        .calalog__card__title {
+          margin-right: 5px;
+        }
+        .catalog__card__icon {
+          margin-left: 5px;
+        }
+      }
+      .calalog__card--patterns {
+        bottom: 20px;
+        left: 20px;
+        z-index: 0;
+        padding: 25px 65px;
+        .calalog__card__title {
+          margin-left: 5px;
+        }
+        .catalog__card__icon {
+          margin-right: 5px;
+        }
+      }
+      .calalog__card--webAssembly {
+        top: 0px;
+        right: -20px;
+        z-index: 0;
+        .calalog__card__title {
+          margin-left: 5px;
+        }
+        .catalog__card__icon {
+          margin-right: 5px;
+        }
+      }
     }
 
-    @media screen and (max-width: 768px) {
-      .catalogIcon {
-        height: 5rem;
-        width: auto;
+    @media screen and (max-width: 1199px) {
+      .catalog {
+        .calalog__card--main {
+          padding: 35px 20px;
+          .calalog__card__title {
+            font-size: 30px;
+            line-height: 37px;
+          }
+        }
+        .calalog__card--child {
+          padding: 15px 30px;
+          .calalog__card__title {
+            font-size: 22px;
+            line-height: 27px;
+          }
+        }
+        .calalog__card--programs {
+          left: -50px;
+          top: 60px;
+        }
+        .calalog__card--policies {
+          bottom: 50px;
+        }
+        .calalog__card--patterns {
+          bottom: 50px;
+          left: -10px;
+        }
+        .calalog__card--webAssembly {
+          right: 0px;
+          top: 35px;
+        }
       }
-      img {
-        margin: auto;
-        height: 50%;
-        width: 50%;
+    }
+
+    @media screen and (max-width: 992px) {
+      .row {
+        flex-direction: column;
+        min-height: 61.125rem;
+        justify-content: space-around;
+      }
+      .catalog {
+        .calalog__card--programs {
+          left: 100px;
+          top: -60px;
+        }
+        .calalog__card--policies {
+          bottom: -50px;
+          right: 80px;
+        }
+        .calalog__card--patterns {
+          bottom: -60px;
+          left: 50px;
+          padding-left: 50px;
+          padding-right: 50px;
+        }
+        .calalog__card--webAssembly {
+          right: 80px;
+          top: -80px;
+        }
+      }
+    }
+
+    @media screen and (max-width: 767px) {
+      min-height: unset;
+      padding: 50px 0;
+      .header__detail {
+        display: flex;
+        flex-direction: column;
+      }
+      .header__title {
+        padding-right: 0;
+        text-align: center;
+        margin-bottom: 10px;
+      }
+      .header__title_description {
+        padding-right: 0;
+        text-align: center;
+        margin-bottom: 20px;
+      }
+      .header__button_section {
+        display: flex;
+        justify-content: center;
+      }
+      .catalog {
+        margin: 50px 0 0 0;
+        justify-content: unset;
+        .calalog__card--main {
+          display: none;
+        }
+        .calalog__card--child {
+          position: relative;
+          top: 0px;
+          right: 0;
+          left: 0;
+          margin-bottom: 10px;
+          padding: 10px 30px;
+        }
+        .calalog__card--patterns,
+        .calalog__card--programs {
+          right: 0;
+        }
+        .calalog__card--policies,
+        .calalog__card--webAssembly {
+          right: 0;
+        }
       }
     }
   }
--- src/sections/Cloud-Native-Catalog/header.js
+++ src/sections/Cloud-Native-Catalog/header.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { CloudNativeCatalogWrapper } from "./cloud-native-catalog.style";
 import { Row, Col, Container } from "../../reusecore/Layout";
-import catalog from "../../assets/images/catalog/catalog.svg";
+import patterns from "../../assets/images/catalog/patterns.svg";
+import policies from "../../assets/images/catalog/policies.svg";
+import programs from "../../assets/images/catalog/programs.svg";
+import webAssembly from "../../assets/images/catalog/webassembly.svg";
 import Button from "../../reusecore/Button";
 
 const Header = () => {
@@ -10,19 +13,37 @@ const Header = () => {
       <div className="cloud-native-catalog header">
         <Container>
           <Row className="row">
-            <Col className="col header__detail" md={6} sm={12}>
+            <Col className="col header__detail" lg={6} md={12}>
               <h1 className="header__title">Cloud Native Catalog</h1>
               <p className="header__title_description">
                 Discover top-quality cloud native services for your
                 infrastructure with the Cloud Native Catalog. Browse now and
                 upgrade your cloud native operations!
               </p>
-              <di>
+              <di className="header__button_section">
                 <Button primary title="Create Catalog" url=""></Button>
               </di>
             </Col>
-            <Col className="col catalog-image-col" md={6} sm={12}>
-              <img src={catalog} alt="Catalog" className="catalogIcon" />
+            <Col className="col catalog" lg={6} md={12}>
+              <div className="calalog__card calalog__card--child calalog__card--programs">
+                <span className="calalog__card__title">Programs</span>
+                <img src={programs} alt="Programs" className="catalog__card__icon" />
+              </div>
+              <div className="calalog__card calalog__card--child calalog__card--webAssembly">
+                <img src={webAssembly} alt="WebAssembly" className="catalog__card__icon" />
+                <span className="calalog__card__title">WebAssembly</span>
+              </div>
+              <div className="calalog__card calalog__card--main">
+                <span className="calalog__card__title">Cloud Native Catalog</span>
+              </div>
+              <div className="calalog__card calalog__card--child calalog__card--patterns">
+                <img src={patterns} alt="Patterns" className="catalog__card__icon" />
+                <span className="calalog__card__title">Patterns</span>
+              </div>
+              <div className="calalog__card calalog__card--child calalog__card--policies">
+                <span className="calalog__card__title">Policies</span>
+                <img src={policies} alt="Policies" className="catalog__card__icon" />
+              </div>
             </Col>
           </Row>
         </Container>
