9232b7ec35b22ae3a63e478bb20c94d10f72cf0c
Senali <senalidilumika@gmail.com>
2023-05-06 03:26:31 +0530

    New catalog and reviews section
    Signed-off-by: Senali <senalidilumika@gmail.com>

--- src/sections/Cloud-Native-Catalog/index.js
+++ src/sections/Cloud-Native-Catalog/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { CloudNativeCatalogWrapper } from "./cloud-native-catalog.style";
-import Catalog from "./catalog";
 import Patterns from "./patterns";
 import Header from "./header";
+import NewCatalog from "./new-catalog";
+import Reviews from "../Pricing/review-slider";
 
 const CloudNativeCatalogPage = () => {
   return (
     <CloudNativeCatalogWrapper>
       <Header />
-      <Catalog />
       <Patterns />
+      <Reviews />
+      <NewCatalog />
     </CloudNativeCatalogWrapper>
   );
 };
---
+++ src/sections/Cloud-Native-Catalog/new-catalog.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import { Container } from "../../reusecore/Layout";
+import Button from "../../reusecore/Button";
+
+const NewCatalogWrapper = styled.div`
+  min-height: 500px;
+	background-color: #F0F0F0;
+  .container {
+    min-height: 500px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  .new-catalog__title {
+    text-align: center;
+		color: #1E2117;
+		font-weight: 600;
+		font-size: 3.125rem;
+		line-height: 3.813rem;
+		padding: 0 200px 20px 200px;
+  }
+  .new-catalog__button_section {
+    display: flex;
+    justify-content: center;
+		padding-top: 20px;
+  }
+	@media screen and (max-width: 992px) {
+		.new-catalog__title {
+		padding: 0 0 10px 0;
+  }
+	.new-catalog__button_section {
+		padding-top: 10px;
+  }
+	}
+	@media screen and (max-width: 767px) {
+		.new-catalog__title {
+		font-size: 2rem;
+		line-height: 2.5rem;
+  }
+	}
+`;
+
+const NewCatalog = () => {
+  return (
+    <NewCatalogWrapper className="new-catalog">
+      <Container className="container">
+        <p className="new-catalog__title">
+          CREATE A CATALOG AND SHARE WITH YOUR FRIENDS
+        </p>
+        <di className="new-catalog__button_section">
+          <Button primary title="Create Catalog" url=""></Button>
+        </di>
+      </Container>
+    </NewCatalogWrapper>
+  );
+};
+
+export default NewCatalog;
