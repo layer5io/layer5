3c1cc1c4dde77611b6c3341833666f500a083331
ayushthe1 <ayushsharmaa101@gmail.com>
2023-05-21 16:42:03 +0530

    fix 6
    Signed-off-by: ayushthe1 <ayushsharmaa101@gmail.com>

--- src/html.js
+++ src/html.js
@@ -1,12 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const gtmScript = `
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-PS26QB9');`;
 
 export default function HTML(props) {
   return (
@@ -26,12 +20,6 @@ export default function HTML(props) {
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PS26QB9"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          }}
-        />
         <script dangerouslySetInnerHTML={{
           __html:
             `(function() {
