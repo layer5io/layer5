7c190e45ae5aa226a7006b979313e5fdfae94913
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2023-05-23 15:40:07 +0530

    move-docs-badge-above-imageHub-and-revise-suggested-texts
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

--- src/sections/Community/Handbook/recognition.js
+++ src/sections/Community/Handbook/recognition.js
@@ -95,60 +95,59 @@ const RecognitionPage = () => {
             <ul style={badgeListStyle}>
               <li>
                 <img src={CommunityLogo} style={badgeStyle} />
-                <b>Community</b> - awarded community members who repeatedly engage in welcoming, encouraging, and supporting other Layer5 community members. Community members who earn this badge occasionally graduate to undertaking the Community Manager role.
+                <b>Community</b> - awarded to the community members who repeatedly engage in welcoming, encouraging, and supporting other Layer5 community members. Community members who earn this badge occasionally graduate to undertaking the Community Manager role.
               </li>
               <li>
                 <img src={DockerExtension} style={badgeStyle} />
-                <b>Docker Extension</b> - awarded community members who make consistent and impactful contributions to the Docker Extension of meshery project in recognition and appreciation of their efforts.
+                <b>Docker Extension</b> - awarded to the community members who make consistent and impactful contributions to the Docker Extension of meshery project in recognition and appreciation of their efforts.
+              </li>
+              <li>
+                <img src={DocsLogo} style={badgeStyle} />
+                <b>Docs</b> - awarded to the community members who make consistent and impactful contributions to the <a href="https://docs.meshery.io/">Meshery docs</a> in recognition and appreciation of their efforts.
               </li>
               <li>
                 <img src={ImageHubLogo} style={badgeStyle} />
-                <b>Image Hub</b> - awarded community members who make consistent and impactful contributions to the Image Hub project in recognition and appreciation of their efforts.
-              </li>
-
-              <li>
-                <img src={DocsLogo} style={badgeStyle} />
-                <b>Docs</b> - awarded community members who make consistent and impactful contributions to <a href="https://docs.meshery.io/">Meshery docs</a> in recognition and appreciation of their efforts.
+                <b>Image Hub</b> - awarded to the community members who make consistent and impactful contributions to the Image Hub project in recognition and appreciation of their efforts.
               </li>
               <li>
                 <img src={LandscapeGreen} style={badgeStyle} />
-                <b>Landscape</b> - awarded community members who make consistent and impactful contributions to the layer5.io website.
+                <b>Landscape</b> - awarded to the community members who make consistent and impactful contributions to the layer5.io website.
               </li>
               <li>
                 <img src={MesheryLogo} style={badgeStyle} />
-                <b>Meshery</b> - awarded community members who make consistent and impactful contributions to the Meshery project. Community members who earn this badge occasionally become a project maintainer.
+                <b>Meshery</b> - awarded to the community members who make consistent and impactful contributions to the Meshery project. Community members who earn this badge occasionally become a project maintainer.
               </li>
               <li>
                 <img src={MesheryOperator} style={badgeStyle} />
-                <b>Meshery Operator</b> - awarded community members who make consistent and impactful contributions to Meshery Operator of the Meshery project in recognition and appreciation of their efforts.
+                <b>Meshery Operator</b> - awarded to the community members who make consistent and impactful contributions to Meshery Operator of the Meshery project in recognition and appreciation of their efforts.
               </li>
               <li>
                 <img src={MesheryCatalogLogo} style={badgeStyle} />
-                <b>Meshery Catalog</b> - awarded community members who make consistent and impactful contributions to the <a href="https://meshery.io/catalog">Meshery Catalog</a> of Meshery project in recognition and appreciation of their efforts.
+                <b>Meshery Catalog</b> - awarded to the community members who make consistent and impactful contributions to the <a href="https://meshery.io/catalog">Meshery Catalog</a> of Meshery project in recognition and appreciation of their efforts.
               </li>
               <li>
                 <img src={meshmapLogo} style={badgeStyle} />
-                <b>MeshMap</b> - awarded community members who make consistent and impactful contributions to the <Link to="/cloud-native-management/meshmap">MeshMap</Link> project in recognition and appreciation of their efforts. Community members who earn this badge occasionally become a project maintainer.
+                <b>MeshMap</b> - awarded to the community members who make consistent and impactful contributions to the <Link to="/cloud-native-management/meshmap">MeshMap</Link> project in recognition and appreciation of their efforts. Community members who earn this badge occasionally become a project maintainer.
               </li>
               <li>
                 <img src={NightHawkLogo} style={badgeStyle} />
-                <b>Nighthawk</b> - awarded community members who make consistent and impactful contributions to the NightHawk project in recognition and appreciation of their efforts. Community members who earn this badge occasionally become a project maintainer.
+                <b>Nighthawk</b> - awarded to the community members who make consistent and impactful contributions to the NightHawk project in recognition and appreciation of their efforts. Community members who earn this badge occasionally become a project maintainer.
               </li>
               <li>
                 <img src={PatternsLogo} style={badgeStyle} />
-                <b>Patterns</b> - awarded community members who make consistent and impactful contributions to the <Link to="/learn/service-mesh-books/service-mesh-patterns">Service Mesh Patterns</Link> project in recognition and appreciation of their efforts.
+                <b>Patterns</b> - awarded to the community members who make consistent and impactful contributions to the <Link to="/learn/service-mesh-books/service-mesh-patterns">Service Mesh Patterns</Link> project in recognition and appreciation of their efforts.
               </li>
               <li>
                 <img src={ServiceMeshPerformance} style={badgeStyle} />
-                <b>Service Mesh Performance</b> - awarded community members who make consistent and impactful contributions to the Service Mesh Performance project. Community members who earn this badge occasionally become a project maintainer.
+                <b>Service Mesh Performance</b> - awarded to the community members who make consistent and impactful contributions to the Service Mesh Performance project. Community members who earn this badge occasionally become a project maintainer.
               </li>
               <li>
                 <img src={uiuxrLogo} style={badgeStyle} />
-                <b>UI/UX</b> - awarded community members who create or improve designs for visual aspects or user flow for any of the websites, flyers, promotions, Meshery UI, and so on in recognition and appreciation of their efforts.
+                <b>UI/UX</b> - awarded to the community members who create or improve designs for visual aspects or user flow for any of the websites, flyers, promotions, Meshery UI, and so on in recognition and appreciation of their efforts.
               </li>
               <li>
                 <img src={writersLogo} style={badgeStyle} />
-                <b>Writer's Program</b> - awarded community members who make with two or more published writings whether in article, blog post, project documentation or other form in recognition and appreciation of their efforts.
+                <b>Writer's Program</b> - awarded to the community members who make with two or more published writings whether in article, blog post, project documentation or other form in recognition and appreciation of their efforts.
               </li>
             </ul>
             <a id="SocialMedia">
