81fe18e196d65cd5d1a8b0fd1965f52723152a3c
Lee Calcote <lee.calcote@layer5.io>
2023-05-26 23:32:03 -0500

    spacing
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- src/sections/gitops/index.js
+++ src/sections/gitops/index.js
@@ -8,29 +8,29 @@ import GitOpsWrapper from "./meshery.style.js";
 
 
 const GitOpsPage = () => {
-    return (
-        <GitOpsWrapper>
-            <Container>
-                <div>
-                    <Row>
-                        <Col lg={6} md={6}>
-                            <img src={Cone} />
-                            <img src={MeshMap} className="meshmap-logo" />
-                        </Col>
-                        <Col lg={6} md={6}>
-                            MeshMap creates a visual map of your codebase that stays up to date with every PR. Visualize your whole codebase, see generated insights, and share a single, accurate source of truth to help quickly onboard new contributors.
+  return (
+    <GitOpsWrapper>
+      <Container>
+        <div>
+          <Row>
+            <Col lg={6} md={6}>
+              <img src={Cone} />
+              <img src={MeshMap} className="meshmap-logo" />
+            </Col>
+            <Col lg={6} md={6}>
+                MeshMap creates a visual map of your codebase that stays up to date with every PR. Visualize your whole codebase, see generated insights, and share a single, accurate source of truth to help quickly onboard new contributors.
 
-                            MeshMap uses GitHub to generate dependencies between files and update your map to reflect changes in your codebase.
+                MeshMap uses GitHub to generate dependencies between files and update your map to reflect changes in your codebase.
 
-                            We do not store your code, your code is only stored by GitHub. We analyze your code while it's on GitHub's servers using a GitHub Action.
+                We do not store your code, your code is only stored by GitHub. We analyze your code while it's on GitHub's servers using a GitHub Action.
 
-                            See our docs for more information on why we request these permissions.
-                        </Col>
-                    </Row>
-                </div>
-            </Container>
-        </GitOpsWrapper>
-    );
+                See our docs for more information on why we request these permissions.
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </GitOpsWrapper>
+  );
 };
 
 export default GitOpsPage;
