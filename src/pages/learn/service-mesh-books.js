d16d1d670b7d637334e9230db4f116830c6fa52e
8rxn <rajxryn@gmail.com>
2023-06-02 02:57:42 +0530

    Fix service mesh books prop error
    Signed-off-by: 8rxn <rajxryn@gmail.com>

--- src/pages/learn/service-mesh-books.js
+++ src/pages/learn/service-mesh-books.js
@@ -5,19 +5,23 @@ import BooksPage from "../../sections/Learn/Books-grid";
 import LearnServiceMeshCTA from "../../sections/Learn/Learn-Service-Mesh-CTA";
 import Footer from "../../sections/General/Footer";
 
-const BooksGridPage = ({ hide_path }) => {
+const BooksGridPage = ({ hide_path, location }) => {
   return (
     <>
       <BooksPage hide_path={hide_path} />
       <LearnServiceMeshCTA />
       <Footer location={location} />
-
     </>
   );
 };
 export default BooksGridPage;
 export const Head = () => {
-  return  <SEO title="Service Mesh Books" description="Learn how to service mesh with books written by Layer5 authors.
+  return (
+    <SEO
+      title="Service Mesh Books"
+      description="Learn how to service mesh with books written by Layer5 authors.
   Layer5 is the makers of Meshery and service mesh standards.
-  We are the largest collection of service mesh projects and their maintainers in the world." />;
+  We are the largest collection of service mesh projects and their maintainers in the world."
+    />
+  );
 };
