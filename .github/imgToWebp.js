4968b1028c9b17ac2efdf386386249f2863302ce
ayushthe1 <ayushsharmaa101@gmail.com>
2023-06-05 15:06:04 +0530

    add script files
    Signed-off-by: ayushthe1 <ayushsharmaa101@gmail.com>

---
+++ .github/imgToWebp.js
@@ -0,0 +1,31 @@
+// Convert the images to .webp format
+const glob = require('glob');
+const path = require('path');
+const sharp = require('sharp');
+const fs = require('fs');
+
+// const files = glob.sync('./src/assets/images/*/.{png,jpg}', { nodir: true });
+const files = glob.sync('../src/**/*.{png,jpg}'
+, { nodir: true });
+console.log(files.size);
+a=0
+files.forEach(async (file) => {
+  const outputDir = path.dirname(file);
+  const outputFileName = path.basename(file, path.extname(file)) + '.webp';
+  const outputFile = `${outputDir}/${outputFileName}`;
+  a++
+//   try {
+//     await sharp(file)
+//       .toFormat('webp')
+//       .toFile(outputFile);
+
+//     console.log(`Image converted to webp: ${file} -> ${outputFile}`);
+
+//     fs.unlinkSync(file); // Delete the original file
+//     console.log(`Original image deleted: ${file}`);
+//   } catch (error) {
+//     console.error(`Error converting image: ${file}`);
+//     console.error(error);
+//   }
+});
+console.log(a)
---
+++ .github/updateImagePath.js
@@ -0,0 +1,44 @@
+// --- Update the image path in files
+
+const fs = require('fs');
+const path = require('path');
+
+// Define the project directory path
+const projectDirectory = '../src';
+
+// Function to recursively process files in a directory
+function processFiles(directory) {
+  const files = fs.readdirSync(directory);
+
+  files.forEach((file) => {
+    const filePath = path.join(directory, file);
+    const stats = fs.statSync(filePath);
+
+    if (stats.isDirectory()) {
+      processFiles(filePath); // Recurse into subdirectory
+    } else if (stats.isFile() && (file.endsWith('.js') || file.endsWith('.jsx') || file.endsWith('.mdx'))) {
+      // Read the file content
+      let fileContent = fs.readFileSync(filePath, 'utf8');
+
+      // Regular expression pattern to match image URLs
+      const imageUrlPattern = /https?:\/\/\S+\.(png|jpg)/g;
+
+      // Replace occurrences of .png and .jpg with .webp, excluding web links
+      fileContent = fileContent.replace(/(?<!https?:\/\/\S+)\.(png|jpg)/g, (match, extension) => {
+        return extension === 'png' || extension === 'jpg' ? `.webp` : match;
+      });
+
+      // Replace image URLs back to original format
+      fileContent = fileContent.replace(imageUrlPattern, (match) => {
+        return match.replace('.webp', (extension) => (extension === 'webp' ? '.png' : extension));
+      });
+
+      // Write the updated content back to the file
+      fs.writeFileSync(filePath, fileContent, 'utf8');
+    }
+  });
+}
+
+// Run the script
+processFiles(projectDirectory);
+console.log('File content update completed.');
