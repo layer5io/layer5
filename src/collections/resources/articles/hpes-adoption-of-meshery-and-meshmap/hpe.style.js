4088e1a2ba55a8f55c0d1ba51b836bb9132a2231
Lee Calcote <lee.calcote@layer5.io>
2023-06-11 17:47:37 -0500

    secondaryLightColorTwo
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- src/collections/resources/articles/hpes-adoption-of-meshery-and-meshmap/hpe.style.js
+++ src/collections/resources/articles/hpes-adoption-of-meshery-and-meshmap/hpe.style.js
@@ -1,13 +1,18 @@
 import styled from "styled-components";
 
-export const HPEfacts = styled("table")(() => ({
+export const HPEfacts = styled("table")(({ theme }) => ({
   backgroundColor: "#212121",
   borderRadius: "0.5rem",
   marginBottom: "1rem",
+  color: `${theme.secondaryLightColorTwo}`,
+
   " > tr:first-child": {
+    color: `${theme.secondaryLightColorTwo}`,
     padding: "0rem",
   },
+
   " > tr:first-child td": {
+    color: `${theme.secondaryLightColorTwo}`,
     backgroundColor: "#414141",
     padding: ".5rem",
     textAlign: "center",
@@ -23,3 +28,19 @@ export const HPEfacts = styled("table")(() => ({
   }
 }));
 
+
+export const HPEintro = styled("div")(({ theme }) => ({
+  display: "flex",
+  paddingLeft: "3rem",
+  paddingRight: "3rem",
+  paddingBottom: "1.5rem",
+  paddingTop: "1.5rem",
+  fontStyle: "italic",
+  fontSize: "1.5rem",
+
+  borderTop: `1px dashed ${theme.primaryLightColor}`,
+  borderBottom: `1px dashed ${theme.primaryLightColor}`,
+  marginBottom: "1rem",
+
+  backgroundColor: `${theme.secondaryLightColorTwo}`,
+}));
