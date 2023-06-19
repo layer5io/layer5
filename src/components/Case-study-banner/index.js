6bb2d3c04a529317a05f67cab9bd2f68777056ad
Shivam Sood <shivamsood634@gmail.com>
2023-06-19 17:13:54 +0530

    added responsiveness
    Signed-off-by: Shivam Sood <shivamsood634@gmail.com>

--- src/components/Case-study-banner/index.js
+++ src/components/Case-study-banner/index.js
@@ -2,32 +2,37 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import MesheryLogo from "../../assets/images/meshery/icon-only/meshery-logo-light.svg";
 import SpireLogo from "../../collections/integrations/spire/icon/color/spire-color.svg";
-import { Link } from "gatsby";
+import MesheryAndSpire from "../../collections/resources/articles/hpes-adoption-of-meshery-and-meshmap/meshery-and-hpe.svg";
 
 
 const BannerWrapper = styled.div`
-display: flex;
-max-width: 100%;
-overflow: hidden;
-margin: 5% 0%;
+
+.large-screen {
+  display: flex;
+  max-width: 100%;
+  overflow: hidden;
+  margin: 5% 0%;
+
+  @media screen and (max-width: 600px) {
+    display: none;
+  }
+}
 
 .left-img {
   display: flex;
-  background: linear-gradient(90.01deg, rgba(0, 179, 159, 0.54) 0.53%, rgba(71, 126, 150, 0.26) 51.1%, rgba(18, 18, 18, 0.16) 99.99%);
+  background: ${props => props.theme.bannerGradientBackgroundToWhite};
     flex: 0 0 20%;
     text-align: center;
     justify-content: center;
-    /* padding: 4%; */
-    border: 2px solid #eee;
-    border-bottom-width: 2px;
-    border-right-width: 10px;
+    border: 1px solid;
+    border-color: ${props => props.theme.whiteToBlack};
     transform-origin: top right;
     transform: skewX(-25deg);
     padding: 3%;
 }
 
 .left-img > * {
-  transition: transform 0.3s ease;
+  transition: transform 0.4s ease;
   transform: skewX(25deg); /* Counteract the parent's skew */
   width: 40%;
 
@@ -35,21 +40,28 @@ margin: 5% 0%;
 
 .desc {
   display: flex;
+  background:${props => props.theme.bannerDescriptionGradient};
     flex: 0 0 60%;
-    /* width: 60vw; */
     text-align: center;
-    border: 1px solid #eee;
-    border-bottom-width: 2px;
+    border: 2px solid transparent;
+    border-image: ${props => props.theme.borderGradientTransitionBefore} ;
+    border-image-slice: 1 1 1 1;
+    transition: border 0.4s ease-in-out;
     justify-content: center;
     align-items: center;
-    /* border-left-width: 10px;
-    border-right-width: 10px; */
-    /* padding: 4%; */
     transform: skewX(-25deg);
 
     h2 {
       text-align: center;
       width: 80%;
+      color: #fff;
+
+      @media screen and (max-width: 800px) {
+        font-size: 1.25rem;
+      }
+      @media screen and (max-width: 600px) {
+        font-size: 1rem;
+      }
     }
 }
 
@@ -59,30 +71,55 @@ margin: 5% 0%;
 
 .right-img {
   display: flex;
-  background: linear-gradient(90.01deg, rgba(18, 18, 18, 0.16) 0.01%, rgba(71, 126, 150, 0.26) 49.9%, rgba(0, 179, 159, 0.54) 99.47%);
+  background: ${props => props.theme.bannerGradientBackgroundToWhite};
     flex: 0 0 20%;
     text-align: center;
     justify-content: center;
-    /* padding: 4%; */
-    border: 1px solid #eee;
-    border-left-width: 10px;
-    border-bottom-width: 2px;
+    border: 1px solid;
+    border-color: ${props => props.theme.whiteToBlack};
     transform-origin: bottom left;
     transform: skewX(-25deg);
     padding: 3%;
 }
 
 .right-img > * {
-  transition: transform 0.3s ease;
+  transition: transform 0.4s ease;
   transform: skewX(25deg); /* Counteract the parent's skew */
   width: 40%;
 }
 
 .scale-on-hover {
-
   > * {
-    transition: transform 0.5s ease;
+    transition: transform 0.4s ease;
     transform: skewX(25deg) scale(1.4);
+  }
+}
+
+.desc-hover {
+  border-image: ${props => props.theme.borderGradientTransitionAfter} ;
+    border-image-slice: 1 1 1 1;
+    transition: 0.4s ease-in-out;
+}
+
+.small-screen {
+  display: none;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  @media screen and (max-width: 600px) {
+    display: flex;
+  }
+
+  .small-screen-desc {
+    width: 80%;
+  }
+
+  .meshery-and-spire {
+    margin-top: 5%;
+  width: 80%;
+  border: 1px solid #fff;
   }
 }
 `;
@@ -100,19 +137,29 @@ const CaseStudyBanner = () => {
   };
 
   return (
-    <BannerWrapper>
-      <div className={`left-img ${hover ? "scale-on-hover" : ""}`}>
-        <img src={MesheryLogo} alt="meshery-logo" />
-      </div>
-      {/* <Link to= "../../collections/resources/articles/"> */}
-      <div className="desc" onMouseEnter={handleHover} onMouseLeave={handleLeave}>
-        <h2>Discover how HPE uses Meshery to manage SPIRE</h2>
-      </div>
-      {/* </Link> */}
-      <div className={`right-img ${hover ? "scale-on-hover" : ""}`}>
-        <img src={SpireLogo} alt="spire-logo" />
-      </div>
-    </BannerWrapper>
+    <a href="/resources/cloud-native/hpes-adoption-of-meshery-and-meshmap">
+      <BannerWrapper>
+        <div className="small-screen">
+          <div className="small-screen-desc">
+            <h2>Discover how HPE uses Meshery to manage SPIRE</h2>
+          </div>
+          <div className="meshery-and-spire">
+            <img src={MesheryAndSpire} alt="meshery-and-spire" />
+          </div>
+        </div>
+        <div className="large-screen">
+          <div className={`left-img ${hover ? "scale-on-hover" : ""}`} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
+            <img src={MesheryLogo} alt="meshery-logo" />
+          </div>
+          <div className={`desc ${hover ? "desc-hover" : ""}`} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
+            <h2>Discover how HPE uses Meshery to manage SPIRE</h2>
+          </div>
+          <div className={`right-img ${hover ? "scale-on-hover" : ""}`} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
+            <img src={SpireLogo} alt="spire-logo" />
+          </div>
+        </div>
+      </BannerWrapper>
+    </a>
   );
 };
 
