46bba6c9fd7f31905e4d7390b99a4fafe02e6dd7
Randy Lau <randychilau@gmail.com>
2023-06-21 11:09:56 -0700

    added more shortcodes for mdx files to dry up code
    Signed-off-by: Randy Lau <randychilau@gmail.com>

--- root-wrapper.js
+++ root-wrapper.js
@@ -6,20 +6,43 @@ import CTA_FullWidth from "./src/components/Call-To-Actions/CTA_FullWidth";
 import CTA_Bottom from "./src/components/Call-To-Actions/CTA_Bottom";
 import { ContextWrapper } from "./context-wrapper";
 
+import { Link } from "gatsby";
+import { BlogWrapper } from "./src/collections/blog/Blog.style.js";
+import { CareerWrapper } from "./src/collections/careers/Career.style.js";
+import { BookWrapper } from "./src/collections/service-mesh-books/Book.style.js";
+import { NewsWrapper } from "./src/collections/news/News.style.js";
+import { ResourcesWrapper } from "./src/collections/resources/Resources.style.js";
+import { ProgramsWrapper } from "./src/collections/programs/Programs.style.js";
+import Button from "./src/reusecore/Button";
+import Blockquote from "./src/reusecore/Blockquote";
+import BlockquoteAlt from "./src/reusecore/Blockquote/Blockquote-alt-style";
+
+const layer5Pre = ({ children: { props, type } }) => {
+  if (type === "code") {
+    return (
+      <Code
+        codeString={props.children.trim()}
+        language={
+          props.className && props.className.replace("language-", "")
+        }
+        {...props}
+      />
+    );
+  }
+};
+
 const components = {
-  pre: ({ children: { props } }) => {
-    if (props.mdxType === "code") {
-      return (
-        <Code
-          codeString={props.children.trim()}
-          language={
-            props.className && props.className.replace("language-", "")
-          }
-          {...props}
-        />
-      );
-    }
-  },
+  pre: layer5Pre,
+  Link,
+  Button,
+  Blockquote,
+  BlockquoteAlt,
+  BlogWrapper,
+  BookWrapper,
+  CareerWrapper,
+  NewsWrapper,
+  ProgramsWrapper,
+  ResourcesWrapper,
   CTA_ImageOnly,
   CTA_FullWidth,
   CTA_Bottom
