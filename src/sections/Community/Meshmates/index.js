d96f140b283688e1863260cbfb1826ae9eeda29d
Randy Lau <randychilau@gmail.com>
2023-06-26 14:45:40 -0700

    eslint corrections to files from merge conflicts
    Signed-off-by: Randy Lau <randychilau@gmail.com>

--- src/sections/Community/Meshmates/index.js
+++ src/sections/Community/Meshmates/index.js
@@ -82,9 +82,9 @@ const Meshmates = () => {
           <h5> What to Expect </h5>
           <h2> Engaging with a MeshMate</h2>
           <div className="bookcomponent"><BookComponent/></div>
-          <p> The program pairs experienced Layer5 community members with community newcomers to ensure a smooth onboarding experience. There is a lot going in the Layer5 community. Projects and working groups move fast. MeshMates are committed to helping their mentees in identifying an area of the projects to engage within, working groups to join, growing their Cloud Native knowledge, and network of relationships. By connecting one-on-one, MeshMates will share tips on how to have the best community experience possible. </p>       
-          <p> Meshtees are encouraged to reach out to any MeshMate directly in order to pair up. Introduce yourself either on in the <a href="https://discuss.layer5.io/c/community/12">Layer5 discussion forum</a>. Help your MeshMate understand your current skills, ideal topics of learning, and areas of passion. Doing so will help them to point out various aspects of projects that you might find your first foothold. </p>          
-        </div>       
+          <p> The program pairs experienced Layer5 community members with community newcomers to ensure a smooth onboarding experience. There is a lot going in the Layer5 community. Projects and working groups move fast. MeshMates are committed to helping their mentees in identifying an area of the projects to engage within, working groups to join, growing their Cloud Native knowledge, and network of relationships. By connecting one-on-one, MeshMates will share tips on how to have the best community experience possible. </p>
+          <p> Meshtees are encouraged to reach out to any MeshMate directly in order to pair up. Introduce yourself either on in the <a href="https://discuss.layer5.io/c/community/12">Layer5 discussion forum</a>. Help your MeshMate understand your current skills, ideal topics of learning, and areas of passion. Doing so will help them to point out various aspects of projects that you might find your first foothold. </p>
+        </div>
         <div className="meshmate-meet">
           <Row Vcenter>
             <Col sm={12} lg={6}>
