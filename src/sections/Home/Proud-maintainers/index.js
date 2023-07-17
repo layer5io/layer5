31db87ef4f2351473a20926944d16d924626da64
Rakshit Sharma <kumarrakshit0402@gmail.com>
2023-07-17 15:34:59 +0530

    Enabled mode swith & improved the slider
    Signed-off-by: Rakshit Sharma <kumarrakshit0402@gmail.com>
    
    Resolved the warning
    
    Signed-off-by: Rakshit Sharma <kumarrakshit0402@gmail.com>

--- src/sections/Home/Proud-maintainers/index.js
+++ src/sections/Home/Proud-maintainers/index.js
@@ -2,36 +2,43 @@ import React from "react";
 import { Container, Row } from "../../../reusecore/Layout";
 import SectionTitle from "../../../reusecore/SectionTitle";
 import ProjectItemWrapper from "./proudMaintainers.style";
-import { Link } from "gatsby";
-import { maintainers } from "./proud-maintainers-data";
 import Slider from "react-slick";
+import Traefik from "../../../assets/images/service-mesh-icons/traefik.svg";
+import Istio from "../../../assets/images/service-mesh-icons/istio.svg";
+import ImageHub from "../../../assets/images/image-hub/layer5-image-hub.svg";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const settings = {
-  arrows: false,
-  dots: false,
-  infinite: true,
-  speed: 500,
-  centerMode: true,
-  variableWidth: true,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  className: "maintainer-slider",
-  responsive: [
-    {
-      breakpoint: 1400,
-      settings: "unslick",
-    },
-  ],
-};
+import { ReactComponent as CNCF } from "../../../assets/images/partners/cncf/horizontal/color/cncf-colorMode.svg";
+import { ReactComponent as OAM } from "../../../assets/images/partners/oam-colorMode.svg";
+import { ReactComponent as Linkerd } from "../../../assets/images/service-mesh-icons/linkerd/horizontal/color/linkerd-horizontal-colorMode.svg";
+import { ReactComponent as Meshery } from "../../../assets/images/meshery/meshery-logo-colorMode.svg";
+import { ReactComponent as Envoy } from "../../../assets/images/service-mesh-icons/envoy/horizontal/color/envoy-horizontal-colorMode.svg";
+import { ReactComponent as OSM } from "../../../assets/images/service-mesh-icons/open-service-mesh/stacked/color/openservicemesh-stacked-colorMode.svg";
+import { ReactComponent as SMP } from "../../../assets/images/service-mesh-performance/horizontal/smp-colorMode.svg";
+import { ReactComponent as SMI } from "../../../assets/images/service-mesh-icons/service-mesh-interface/horizontal-stackedtext/color/servicemeshinterface-horizontal-stackedtext-colorMode.svg";
 
 const ProudMaintainers = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: "unslick",
+      },
+    ],
+  };
+
   return (
     <ProjectItemWrapper>
       <Container>
         <Row className="project-title">
-
           <SectionTitle UniWidth="100%">
             <h4>Proudly maintaining and contributing</h4>
             <h2>
@@ -43,25 +50,67 @@ const ProudMaintainers = () => {
             We are the world’s largest collection of service mesh practitioners
             and maintainers of leading open source projects.
           </p>
-
         </Row>
       </Container>
       <Slider {...settings}>
-        {maintainers.map((maintainer, index) => (
-          <Link className="maintainer-card" to={maintainer.imageRoute} key={index}>
-            <div className={maintainer.innerDivStyle}>
-              <img
-                className="maintainer-image"
-                id={maintainer.name}
-                loading="lazy"
-                src={maintainer.imageLink}
-                alt={maintainer.name}
-                width={maintainer.imageWidth}
-                height={maintainer.imageHeight}
-              />
-            </div>
-          </Link>
-        ))}
+        <div>
+          <div className="proj_icon_1">
+            <CNCF alt="CNCF" />
+          </div>
+        </div>
+        <div>
+          <div className="proj_icon_1">
+            <Envoy alt="Envoy" />
+          </div>
+        </div>
+        <div>
+          <div className="proj_icon_1">
+            <Linkerd alt="Linkerd" />
+          </div>
+        </div>
+        <div>
+          <div className="proj_icon_1">
+            <Meshery alt="Meshery" />
+          </div>
+        </div>
+        <div>
+          <div className="proj_icon_1">
+            <OAM alt="OAM" />
+          </div>
+        </div>
+        <div>
+          <div className="proj_icon_2">
+            <img src={Istio} alt="Istio" />
+            <h4>Istio</h4>
+          </div>
+        </div>
+        <div>
+          <div className="proj_icon_1">
+            <OSM alt="OSM" />
+          </div>
+        </div>
+        <div>
+          <div className="proj_icon_1">
+            <SMP alt="SMP" />
+          </div>
+        </div>
+        <div>
+          <div className="proj_icon_2">
+            <img src={ImageHub} alt="ImageHub" />
+            <h4>Image Hub</h4>
+          </div>
+        </div>
+        <div>
+          <div className="proj_icon_1">
+            <SMI alt="SMI" />
+          </div>
+        </div>
+        <div>
+          <div className="proj_icon_2">
+            <img src={Traefik} alt="Traefik" />
+            <h4>Traefik Mesh</h4>
+          </div>
+        </div>
       </Slider>
     </ProjectItemWrapper>
   );
--- src/sections/Home/Proud-maintainers/proudMaintainers.style.js
+++ src/sections/Home/Proud-maintainers/proudMaintainers.style.js
@@ -1,189 +1,174 @@
 import styled from "styled-components";
 
-const ProjectItemWrapper = styled.section`
-	.left-col {
-		text-align: left;
-		align-self: flex-start;
-		margin: -5.5rem auto auto 0;
-		max-width: 60rem;
-		padding: 0rem;
-		@media screen and (max-width: 1600px) {
-			margin: -4rem auto auto 0;
-			max-width: 45rem
-		}
-		@media screen and (max-width: 1200px) {
-			margin: 2rem auto auto 0;
-			max-width: none;
-		}
-	}
-	//margin: 5rem 0rem;
+const ProudMaintainersStyle = styled.section`
+  .left-col {
+    text-align: left;
+    align-self: flex-start;
+    margin: -5.5rem auto auto 0;
+    max-width: 60rem;
+    padding: 0rem;
+    @media screen and (max-width: 1600px) {
+      margin: -4rem auto auto 0;
+      max-width: 45rem;
+    }
+    @media screen and (max-width: 1200px) {
+      margin: 2rem auto auto 0;
+      max-width: none;
+    }
+  }
+  margin: 1rem 0rem;
 
-	.project-title {
-		text-align: center;
-		align-self: flex-start;
-		margin: auto;
+  .project-title {
+    text-align: center;
+    align-self: flex-start;
+    margin: auto;
 
-		h4 {
-			margin-bottom: 0.5rem;
-			color: ${props => props.theme.grey737373ToGreyB3B3B3};
-			text-transform: uppercase;
-			transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-		}
-		h2{
-			 span {
-				color: ${props => props.theme.text};
-				transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-			 }
-			margin-bottom: 1rem;
-		}
-		.project-text {
-			margin:0 auto 4rem auto;
-			color: ${props => props.theme.whiteSixToBlackSix};
-			max-width: 60%;
-			transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-		}
-	}
-
-	.row, .row_2 {
-		justify-content: center;
-		margin: 1rem 0;
-		flex-wrap: nowrap;
-		overflow: hidden;
-	}
-
-	.proj_row_1_col, .proj_row_2_col {
-		flex: 0 0 25%;
-		max-width: 20%;
-
-		@media screen and (max-width: 1600px) {
-			max-width: 22%;
-		}
-		@media screen and (max-width: 992px) {
-			max-width: 24%;
-		}
-		@media screen and (max-width: 768px) {
-			flex: 0 0 25%;
-			max-width: 25%;
-
-		}
-		@media screen and (max-width: 550px) {
-			flex: 0 0 35%;
-			max-width: 35%;
-		}
-	}
-
-	}
-	.proj_icon_2,
-	.proj_icon_1 {
-		max-width: 18rem;
-		height: 7rem;
-		background: ${props => props.theme.grey212121ToGreyF3F3F3};
-		display: flex;
-		border-radius: 1.5rem;
-		justify-content: center;
-		margin: auto;
-		transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-
-		svg {
-			.cncf-colorMode_svg__colorMode1,
-			.linkerd-horizontal-colorMode_svg__colorMode1,
-			.envoy-horizontal-colorMode_svg__colorMode1,
-			.oam-colorMode_svg__colorMode1,
-			.meshery-logo-colorMode_svg__colorMode1,
-			.smp-colorMode_svg__colorMode1
-			 {
-				fill: ${props => props.theme.whiteToBlack};
-				transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-			}
-	
-			.openservicemesh-stacked-colorMode_svg__colorMode1 {
-				fill: ${props => props.theme.whiteToBlue1229A9};
-				transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-			}
-			
-			.servicemeshinterface-horizontal-stackedtext-colorMode_svg__colorMode1
-			{
-				fill: ${props => props.theme.whiteToBlue2A2DDE};
-				transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-			}
-
-			.oam-colorMode_svg__colorMode2 {
-				fill: ${props => props.theme.blackToWhite};
-				transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-			}
-		
-		}
-
-		h4 {
-			font-weight: 500;
-			margin: auto 0;
-			text-align: center;
-			transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-		}
-
-		// @media only screen and (max-width: 912px) {
-		// 	height: 5.5rem;
-		//  }
-
-		//  @media screen and (max-width: 992px) {
-		// 	height: 5.5rem;
-		// }
-
-		@media screen and (max-width: 900px) {
-			height: 4.5rem;
-		}
-		@media screen and (max-width: 768px) {
-			height: 4.5rem;
-
-		}
-		@media screen and (max-width: 480px) {
-			height: 4.5rem;
-		}
-	}
-
-	.proj_icon_1 {
-		padding: 1rem;
-	}
-	.proj_icon_2 {
-		img {
-			padding: 1.5rem 1rem;
-			max-height: 125px;
-		}
-      @media screen and (max-width: 660px){
-           h4{
-	    padding-right:1.2rem;
-	    font-size:0.95rem;
-	   }
+    h4 {
+      margin-bottom: 0.5rem;
+      color: ${(props) => props.theme.grey737373ToGreyB3B3B3};
+      text-transform: uppercase;
+      transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+    }
+    h2 {
+      span {
+        color: ${(props) => props.theme.text};
+        transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
       }
-	  @media screen and (max-width: 550px) {
-		img {
-		  padding: 1.65rem 0.5rem 1.65rem 0.8rem;
-		}
-		h4 {
-		  font-size: 0.7rem;
-		}
-	}
-	  @media screen and (max-width: 768px) {
+      margin-bottom: 1rem;
+    }
+    .project-text {
+      margin: 0 auto 4rem auto;
+      color: ${(props) => props.theme.whiteSixToBlackSix};
+      max-width: 60%;
+      transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+    }
+  }
 
-		.proj_icon_1 {
-			padding: 1rem;
-		}
+  .row,
+  .row_2 {
+    justify-content: center;
+    margin: 1rem 0;
+    flex-wrap: nowrap;
+    overflow: hidden;
+  }
 
-		.proj_icon_2 {
-			img {
-				padding: 1rem 1rem;
-				max-height: 125px;
-			}
-	}
+  .proj_row_1_col,
+  .proj_row_2_col {
+    flex: 0 0 25%;
+    max-width: 20%;
 
-      @media screen and (max-width: 460px){
-        h4 {
-          margin-left: 0px;
+    @media screen and (max-width: 1600px) {
+      max-width: 22%;
+    }
+    @media screen and (max-width: 992px) {
+      max-width: 24%;
+    }
+    @media screen and (max-width: 768px) {
+      flex: 0 0 25%;
+      max-width: 25%;
+    }
+    @media screen and (max-width: 550px) {
+      flex: 0 0 35%;
+      max-width: 35%;
+    }
+  }
+
+  .proj_icon_2,
+  .proj_icon_1 {
+    max-width: 18rem;
+    height: 7rem;
+    background: ${(props) => props.theme.grey212121ToGreyF3F3F3};
+    display: flex;
+    border-radius: 1.5rem;
+    justify-content: center;
+    margin: auto;
+    transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+
+    svg {
+      .cncf-colorMode_svg__colorMode1,
+      .linkerd-horizontal-colorMode_svg__colorMode1,
+      .envoy-horizontal-colorMode_svg__colorMode1,
+      .oam-colorMode_svg__colorMode1,
+      .meshery-logo-colorMode_svg__colorMode1,
+      .smp-colorMode_svg__colorMode1 {
+        fill: ${(props) => props.theme.whiteToBlack};
+        transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+      }
+
+      .openservicemesh-stacked-colorMode_svg__colorMode1 {
+        fill: ${(props) => props.theme.whiteToBlue1229A9};
+        transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+      }
+
+      .servicemeshinterface-horizontal-stackedtext-colorMode_svg__colorMode1 {
+        fill: ${(props) => props.theme.whiteToBlue2A2DDE};
+        transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+      }
+
+      .oam-colorMode_svg__colorMode2 {
+        fill: ${(props) => props.theme.blackToWhite};
+        transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+      }
+    }
+
+    h4 {
+      font-weight: 500;
+      margin: auto 0;
+      text-align: center;
+      transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+    }
+
+    @media screen and (max-width: 900px) {
+      height: 4.5rem;
+    }
+    @media screen and (max-width: 768px) {
+      height: 4.5rem;
+    }
+    @media screen and (max-width: 480px) {
+      height: 4.5rem;
+    }
+  }
+
+  .proj_icon_1 {
+    padding: 1rem;
+  }
+  .proj_icon_2 {
+    img {
+      padding: 1.5rem 1rem;
+      max-height: 125px;
+    }
+    @media screen and (max-width: 660px) {
+      h4 {
+        padding-right: 1.2rem;
+        font-size: 0.95rem;
+      }
+    }
+    @media screen and (max-width: 550px) {
+      img {
+        padding: 1.65rem 0.5rem 1.65rem 0.8rem;
+      }
+      h4 {
+        font-size: 0.7rem;
+      }
+    }
+    @media screen and (max-width: 768px) {
+      .proj_icon_1 {
+        padding: 1rem;
+      }
+      .proj_icon_2 {
+        img {
+          padding: 1rem 1rem;
+          max-height: 125px;
         }
       }
-	}
+    }
 
-
+    @media screen and (max-width: 460px) {
+      h4 {
+        margin-left: 0px;
+      }
+    }
+  }
 `;
 
-export default ProjectItemWrapper;
+export default ProudMaintainersStyle;
