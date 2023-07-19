018e40f0f02d2446da461bb2293a971fc43e2b85
Lee Calcote <lee.calcote@layer5.io>
2023-07-19 00:49:44 -0500

    video swap
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- src/sections/Home/Banner-4/banner4.style.js
+++ src/sections/Home/Banner-4/banner4.style.js
@@ -31,6 +31,39 @@ const Banner1SectionWrapper = styled.section`
             }
         }
     }
+    .embedVideo {
+        position: relative;
+        min-width:25%;
+        max-width:100%;
+        object-fit: cover;
+        /* height: 44vw !important;
+        border-radius: 2.5%;
+        transition: 0.2s ease-in-out;
+        box-shadow: 0px 3px 20px 4px rgba(0, 179, 159, 0.5); */
+
+        .react-player__preview {
+        border-radius: 1.5%;
+        }
+
+        .react-player__play-icon {
+        transform: scale(3, 3);
+        }
+
+        iframe {
+        border-radius: 2.5%;
+        }
+
+        @media (max-width: 768px) {
+        height: 54vw !important;
+        }
+
+        &:hover {
+        box-shadow: 0px 3px 20px 4px rgba(0, 179, 159, 0.75);
+        .react-player__play-icon {
+            border-color: transparent transparent transparent #EBC017 !important;
+        }
+        }
+    }
     .meshmapVideo {
         position: relative;
         min-width:25%;
