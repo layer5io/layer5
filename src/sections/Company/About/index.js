1c0eb5768ad173e718149927df7f8520388c8948
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2023-07-20 12:10:12 +0530

    Replace cncf image
    Signed-off-by: Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>

--- src/sections/Company/About/index.js
+++ src/sections/Company/About/index.js
@@ -10,10 +10,10 @@ import collabMap from "./images/layer5-collaboration-map.svg";
 import location from "./images/location.svg";
 import WhoWeAre from "../WhoWeAre";
 import FiveIcon from "../../../assets/images/layer5/5 icon/svg/light/5-light-bg.svg";
-import { ReactComponent as CNCFstackedlogo } from "./images/cncf-stacked-color.svg";
 import { Link } from "gatsby";
-import Layer5Projects from "./images/layer5-projects.svg";
+import Layer5Projects from "./images/layer5-projects.webp";
 import communityStat from "./images/layer5-community-stat.svg";
+
 
 const About = () => {
   const Community_meetup = "./images/Community_meetup.webp";
@@ -42,8 +42,7 @@ const About = () => {
                 </div>
               </Col>
             </Row>
-            <img loading="lazy" src={Layer5Projects} alt="About Layer5 Projects" />
-            <img src={communityStat} className="community_stat" alt="Layer5 Community Stat" loading="lazy"/>
+            <img src={Layer5Projects} alt="About Layer5 Projects" loading="lazy"/>
             <Row Vcenter={true} className="row-img-cont-2">
               <Col xs={12} sm={6}>
                 <div className="about-text text-two">
@@ -58,7 +57,7 @@ const About = () => {
               </Col>
               <Col xs={12} sm={6}>
                 <div className="head-images">
-                  <CNCFstackedlogo alt="About Layer5 Projects" />
+                  <img src={communityStat} alt="Layer5 Community Stat" loading="lazy"/>
                 </div>
               </Col>
             </Row>
