664556e4810ef68f90d8e30e7b2ca209f29c418e
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2023-07-20 12:14:39 +0530

    Update about.style.js
    Signed-off-by: Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>

--- src/sections/Company/About/about.style.js
+++ src/sections/Company/About/about.style.js
@@ -19,10 +19,6 @@ const AboutSectionWrapper = styled.section`
       left: -22px;
     }
   }
-  .community_stat{
-    margin-top:4rem;
-    padding-left:5rem;
-  }
   .busCard {
     display: block;
     margin-bottom: 2rem;
