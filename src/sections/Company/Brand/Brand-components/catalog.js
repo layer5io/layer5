52bee340f739be0f8b9fe518abfcf87f1f8e6e31
Xiao Ran <xiaor2@illinois.edu>
2023-08-07 02:23:06 +0000

    Fixed the catalog logos download link.
    Signed-off-by: Xiao Ran <xiaor2@illinois.edu>

--- src/sections/Company/Brand/Brand-components/catalog.js
+++ src/sections/Company/Brand/Brand-components/catalog.js
@@ -28,7 +28,7 @@ const Catalog = () => {
             <h1 className="layerH3">Catalog</h1>
           </Col>
           <Col xs={12} sm={6} className="download-button">
-            <a href="/brand/layer5-brand-kit.zip">
+            <a href="/brand/catalog-brand-kit.zip">
               <Button primary title="Download Logo Assets" external={false}>
                 <FiDownloadCloud size={21} className="icon-left" />
               </Button>
