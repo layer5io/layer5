622a68aeeb8cdcf364319d6c0ca62289fb469811
Gautam-666 <2022.gautam.rai@ves.ac.in>
2023-08-10 20:09:47 +0530

    Added the GitOps section
    Signed-off-by: Gautam-666 <2022.gautam.rai@ves.ac.in>

--- src/sections/gitops/index.js
+++ src/sections/gitops/index.js
@@ -7,6 +7,7 @@ import { GitOpsWrapper, SquarePointWrapper } from "./gitops.style.js";
 import SmpLogo_dark from "../../assets/images/app/projects/smp.svg";
 import SmpLogo_light from "../../assets/images/service-mesh-performance/icon/smp-light.svg";
 import Reviews from "../Pricing/review-slider";
+import Github_Dark from "../../assets/images/socialIcons/github-dark.svg";
 import { ReactComponent as MeshMapStack } from "../../assets/images/meshmap/meshmap-light-stacked.svg";
 
 
@@ -28,7 +29,7 @@ const GitOpsPage = () => {
           <Col className="desc-text-container" lg={6} md={6} sm={10} xs={10}>
             <h1 className="heading-1"> GitOps with Cloud <br/> Native Insights</h1>
             <p className="desc-p">
-                Give your pipelines super powers with Meshery's GitHub Actions
+              Give your pipelines super powers with Meshery's GitHub Actions
             </p>
           </Col>
           <Col lg={6} md={6} className="hero-images">
@@ -39,6 +40,38 @@ const GitOpsPage = () => {
             <div className="hero-image-container">
               <img src={isDark ? SmpLogo_light : SmpLogo_dark} style={{ position: "absolute", height: "235px", width: "235px" }} />
               <img src={Cone} />
+            </div>
+          </Col>
+        </Row>
+      </div>
+
+      <div className="content-container content-container-more-width">
+        <Row className="content-row">
+          <Col lg={6} md={6} className="desc-text-container">
+            <h1 className="heading-1">What is GitOps?</h1>
+            <SquarePoint>
+              GitOps uses Git for defining and tracking infrastructure and app
+              changes.
+            </SquarePoint>
+            <SquarePoint>
+              It automates syncing actual state with Git's desired state.
+            </SquarePoint>
+            <SquarePoint>
+              GitOps ensures consistent, auditable changes using code-defined
+              configurations.
+            </SquarePoint>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <Button
+                primary
+                className="learn-more-btn"
+                title="Read More"
+                url="https://layer5.io/resources/cloud-native/what-is-gitops"
+              />
+            </div>
+          </Col>
+          <Col className="hero-image" lg={6} md={6} xs={12} sm={10}>
+            <div className="image-container">
+              <img src={Github_Dark} className="logo" />
             </div>
           </Col>
         </Row>
