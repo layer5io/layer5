bb4e655f2ece335aeeb3f7f112027e09c9c0a60b
Parth <82119640+malaviya-parth@users.noreply.github.com>
2023-08-21 23:04:47 +0530

    Alignment for mobile device changed to center
    Signed-off-by: Parth <82119640+malaviya-parth@users.noreply.github.com>

--- src/sections/Partners/partner.style.js
+++ src/sections/Partners/partner.style.js
@@ -477,6 +477,17 @@ const PartnerWrapper = styled.section`
         }
     }
     @media only screen and (max-width:476px){
+        .container{
+            padding: 1rem;
+            text-align: center;
+        }        
+        .partner-container{
+            .partner-container-body{
+                .container{
+                    text-align: center;
+                }
+            }
+        }
         .custom-col{
            width: 100%;
         }   
@@ -517,6 +528,17 @@ const PartnerWrapper = styled.section`
         }
     }
     @media only screen and (max-width:376px){
+        .container{
+            padding: 1rem;
+            text-align: center;
+        }        
+        .partner-container{
+            .partner-container-body{
+                .container{
+                    text-align: center;
+                }
+            }
+        }
         .mob-col{
             flex: 0 0 50%;
             max-width: 50%;
