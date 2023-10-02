a66a4370b7b1dd04001383e5f6de0a731c4228cf
creativeBuilder007 <3pm2022adityaprakash@gmail.com>
2023-10-02 20:28:31 +0530

    https://github.com/layer5io/layer5/issues/4987

--- src/sections/Blog/Blog-sidebar/index.js
+++ src/sections/Blog/Blog-sidebar/index.js
@@ -125,32 +125,6 @@ const Sidebar = ({ pageContext }) => {
           </a>
         </div>
       </div>
-      <div className="subscribe">
-        <form
-          name="contactform"
-          method="post"
-          action="https://calcotestudios.us15.list-manage.com/subscribe/post?u=6b50be5aea3dfe1fd4c041d80&amp;id=6bb65defeb"
-        >
-          <div>
-            <span>
-              <h4>Subscribe to our Newsletter</h4>
-            </span>
-            <input
-              className="inputrow subscribe-email"
-              type="email"
-              placeholder="Email Address"
-              name="EMAIL"
-              id="mce-EMAIL"
-              required
-              onInvalid={(e) =>
-                e.target.setCustomValidity("Please fill-in this field")
-              }
-              onInput={(e) => e.target.setCustomValidity("")}
-            />
-            <Button secondary title="Subscribe" aria-label="subscribe-us" id="mc-embedded-subscribe" />
-          </div>
-        </form>
-      </div>
     </BlogSideBarWrapper>
   );
 };
