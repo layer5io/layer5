8a905875ded7213146a7861f075a46942ab2f374
Ankush Bhardwaj <ankushbhardwaj408@gmail.com>
2023-10-04 22:12:32 +0530

    Added tooltip hover effect on the badges in individual member profile page
    Signed-off-by: Ankush Bhardwaj <ankushbhardwaj408@gmail.com>

--- src/sections/Community/Member-single/index.js
+++ src/sections/Community/Member-single/index.js
@@ -103,7 +103,7 @@ const MemberSingle = ({ frontmatter }) => {
                         </div>
                       )}
                       {badges.includes("smp") && (
-                        <div class="tooltip">
+                        <div className="tooltip">
                           <li>
                             <Link to="/projects/service-mesh-performance">
                               <img
