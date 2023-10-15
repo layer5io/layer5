c0a478ee0a33932b9f6ae345d8d5b417f6ae7e90
Akshun-01 <akshunkuthiala2002@gmail.com>
2023-10-15 19:00:52 +0530

    adjust the hover effect for card shape
    Signed-off-by: Akshun-01 <akshunkuthiala2002@gmail.com>

--- src/components/Learn-Components/Card-Component/learn-card.style.js
+++ src/components/Learn-Components/Card-Component/learn-card.style.js
@@ -6,11 +6,11 @@ const CardWrapper = styled.div`
     margin: auto;
     border-radius: 1rem;
     cursor: pointer;
-    box-shadow: rgba(0, 179, 158, 0.9) 0px 2px 10px 1px;
+    box-shadow: rgba(0, 179, 158, 0.4) 0px 0px 15px 8px;
     transition: all 0.8s cubic-bezier(0.2, 0.8, 0.2, 1) 0.1s;
 
     &:hover{
-        box-shadow: rgba(0, 179, 158, 0.7) 0px 2px 18px 5px;
+        box-shadow: rgba(0, 179, 158, 0.9) 0px 0px 19px 6px;
     }
 
     .card-link{
