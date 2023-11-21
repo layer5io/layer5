ffe7aed589ca6893420183905aa24faf5df8cb9e
Lee Calcote <lee.calcote@layer5.io>
2023-11-20 19:37:46 -0600

    quickfix
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- src/pages/cloud-native-management/meshmap/index.js
+++ src/pages/cloud-native-management/meshmap/index.js
@@ -3,15 +3,22 @@ import React from "react";
 import SEO from "../../../components/seo";
 
 import Meshmap from "../../../sections/Meshmap/index";
-const MeshmapPage = () => {
-  let teaser = "";
-  if (window !== undefined) {
-    const params = new URLSearchParams(window.location.search);
-    teaser = params.get("teaser");
-  }
+const MeshmapPage = ({ location }) => {
+  const params = new URLSearchParams(location.search);
+  const parameter1 = params.get("teaser");
+  const parameter2 = params.get("parameter2");
+
+  console.log(parameter1); // -> "firstParam"
+  console.log(parameter2); // -> "secondParam"
+  // ...
+  // let teaser = "";
+  // if (window !== undefined) {
+  //   const params = new URLSearchParams(window.location.search);
+  //   teaser = params.get("teaser");
+  // }
   return (
     <>
-      <Meshmap teaser={teaser === "play"} />
+      <Meshmap teaser={parameter1 === "play"} />
     </>
   );
 };
