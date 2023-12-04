717564129c76b3519dd92c98639897661efe5320
Jay <jaykumar20march@gmail.com>
2023-12-04 20:13:52 +0530

    feat: add /comments
    - fix light theme issues
    - add comments page
    
    Signed-off-by: Jay <jaykumar20march@gmail.com>

---
+++ src/components/FeatureUseCard/featureUseCard.style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const FeatureUseCardWrapper = styled.div`
+  margin-bottom: 40px;
+  height: 100%;
+  
+  .image-container {
+    height: 15rem;
+    width: 100%;
+    border-radius: 0.5rem;
+    border: 1px solid var(--grey-light, #A0AAAA);
+    margin: 0 auto 10px auto;
+  }
+
+  .image-heading {
+    font-weight: bold;
+    font-size: 1.2rem;
+  }
+
+
+`;
+
+export default FeatureUseCardWrapper;
---
+++ src/sections/FeatureHero/featureHero.style.js
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+
+const FeatureHeroWrapper = styled.div`
+  .heading-container {
+    z-index: 3;
+    width: 100%;
+    overflow: hidden;
+    position: relative;
+    padding: 100px 100px 0 100px ;
+
+    .section-title {
+      text-align: left;
+    }
+
+    .whiteboard-image{
+      perspective: 1000px;
+    }
+
+    #whiteboard-svg {
+      transition: transform 0.3s ease-out;
+      transform-origin: top center;
+      min-width: 400px;
+    }
+
+    h2 {
+      margin: 0 0 10px 0;
+    }
+
+    .whiteboard-heading {
+      color: ${props => props.theme.whiteToBlack} !important; !important;
+    }
+
+    .heading-image {
+      display: flex;
+      flex-direction: row;
+      align-items: flex-start;
+      justify-content: center;
+      margin: 0 auto;
+    }
+
+  }
+
+  .heading-container::before {
+    content: '';
+    width: 15rem;
+    height: 15rem;
+    border-radius: 50%;
+    background: #00B39F;
+    filter: blur(150px);
+    position: absolute;
+    left: 50%;
+    top: 40%;
+    transform: translate(-50%, -50%);
+    z-index: -3;
+  }
+
+  @media (max-width: 500px) {
+    .heading-image {
+      align-items: center;
+      justify-content: center;
+      flex-wrap: wrap;
+    }
+
+    .section-title {
+      margin-bottom: 15px;
+      text-align: center !important;
+    }
+  
+`;
+
+export default FeatureHeroWrapper;
