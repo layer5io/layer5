4f744f68637c6bb65c6cd40db93b241281c059ac
Ritik Saxena <ritiksaxena124@gmail.com>
2023-12-11 13:53:18 +0530

    initial commit
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

---
+++ src/components/TopPromotionalBanner/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { TopBannercontainer } from "./style";
+
+const TopPromotionalBanner = () => {
+  return (
+    <>
+      <TopBannercontainer>
+        <p>Soar to New Heights with Cloud Native Playground v0.7.0 🚀 <a href="https://play.meshery.io/" rel="noreferrer" target="_blank">Try it now &rarr;</a></p>
+      </TopBannercontainer>
+    </>
+  );
+};
+export default TopPromotionalBanner;
