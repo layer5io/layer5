e95e44b4ada8c5a039e96b8e20e8d98e0e91dbd5
Ritik Saxena <ritiksaxena124@gmail.com>
2023-12-12 19:30:39 +0530

    emoji rocket replaced with five's rocket
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- src/components/TopPromotionalBanner/index.js
+++ src/components/TopPromotionalBanner/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { TopBannercontainer } from "./style";
+import Rocket from "../../assets/images/top-promotional-banner/rocket.svg";
 
 const TopPromotionalBanner = () => {
   return (
     <>
       <TopBannercontainer>
-        <p>Soar to New Heights with Cloud Native Playground v0.7.0 🚀 <a href="https://play.meshery.io/" rel="noreferrer" target="_blank">Try it now &rarr;</a></p>
+        <p><img src={Rocket} alt="Five on rocket"/>Soar to New Heights with Cloud Native Playground v0.7.0 <a href="https://play.meshery.io/" rel="noreferrer" target="_blank">Try it now &rarr;</a></p>
       </TopBannercontainer>
     </>
   );
