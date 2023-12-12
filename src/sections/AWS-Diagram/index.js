1dc184da024951cb3cff80a39f4220d54b77242a
Archit Sharma <archit8679@gmail.com>
2023-12-12 19:14:23 +0530

    AWS Diagram page created
    Signed-off-by: Archit Sharma <archit8679@gmail.com>

---
+++ src/sections/AWS-Diagram/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./header";
+import Partners from "../Home/Partners-home";
+import Reviews from "../Pricing/review-slider";
+import Diagram from "./diagram";
+
+const AwsDiagramPage = () => {
+  return (
+    <>
+      <Header />
+      <Partners />
+      <Diagram />
+      <Reviews />
+    </>
+  );
+};
+
+export default AwsDiagramPage;
