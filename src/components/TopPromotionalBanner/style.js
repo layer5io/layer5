0a1368cef96eb5e8b2dd14851f17a10271c58ba3
Ritik Saxena <ritiksaxena124@gmail.com>
2023-12-12 19:30:39 +0530

    emoji rocket replaced with five's rocket
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- src/components/TopPromotionalBanner/style.js
+++ src/components/TopPromotionalBanner/style.js
@@ -5,14 +5,21 @@ export const TopBannercontainer = styled.div`
     color: ${props => props.theme.body};
     position: sticky;
     & > p {
-       padding: 0.75rem 1rem;
-       text-align: center;
-       font-weight: 500;
-       line-height: 1.1875rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0.75rem 1rem;
+        font-weight: 500;
+        line-height: 1.1875rem;
+
+       & > img {
+        width: 3rem;
+        margin-right: 0.5rem;
+       }
 
        & > a {
         font-weight: 700;
-        margin-left: 1rem;
+        margin-left: 0.5rem;
         color: ${props => props.theme.body};
        }
     }
