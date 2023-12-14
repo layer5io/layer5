4619e01c9a79857e965bb1f178808cdd2cc0a5b8
Lee Calcote <lee.calcote@layer5.io>
2023-12-14 15:43:37 -0600

    enhance
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- src/sections/Home/FeaturesContainer/index.js
+++ src/sections/Home/FeaturesContainer/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 // Default imports
 import Features from "../../../components/Features/index.js";
-import WhiteboardingImage from "../../../assets/images/features-section/whiteboarding.svg";
-import WhiteboardingImageLight from "../../../assets/images/features-section/whiteboarding-light.svg";
-import CommentingImage from "../../../assets/images/features-section/commenting.svg";
+import WhiteboardingImage from "./images/whiteboarding.svg";
+import WhiteboardingImageLight from "./images/whiteboarding-light.svg";
+import CommentingImage from "./images/commenting.svg";
 // Named imports
 import { Container } from "./style.js";
 import { useStyledDarkMode } from "../../../theme/app/useStyledDarkMode.js";
@@ -16,11 +16,14 @@ const FeaturesContainer = () => {
   return (
     <>
       <Container>
-        <h1 className="title">A platform designed for <span>growth</span></h1>
-        <p className="subtitle">Tools to help you scale your cloud native infrastructure</p>
-        <Features title="Collaborate with Precision" show_custom_cursor={true} redirectLink="/comments" desc="Real-time collaboration for cloud-native designs with live editing, instant feedback, and secure access controls. Elevate teamwork with integrated communication channels, ensuring efficient and secure project success." imgLink={CommentingImage}/>
-        <Features title="Get rid of boring designs, Customize it!"  redirectLink="/whiteboard" desc="This expansion augments the tool’s functionality with a suite of annotation and visualization tools, allowing users to draw, annotate, and collaborate in real-time on their cloud native designs." imgLink={isDark ? WhiteboardingImage : WhiteboardingImageLight}/>
+        <h1 className="title">Deploy faster together with complete <span>Confidence</span></h1>
+        <p className="subtitle">Give your team full application visibility at every layer so the team can ship, refactor and onboard faster.</p>
+        {/* Tools to help you scale your cloud native infrastructure */}
+        <Features title="Collaborate with Precision" show_custom_cursor={true} redirectLink="/comments" desc="Real-time collaboration for cloud and cloud native designs with live-editing, instant feedback, deploy dry runs, and secure access controls." imgLink={CommentingImage}/>
+        {/* Elevate teamwork with integrated communication channels, ensuring efficient and secure project success. */}
+        <Features title="Generate stunning diagrams"  redirectLink="/whiteboard" desc="Crafting cloud-native symphonies: Our engineering diagramming tool is your conductor's baton, turning Kubernetes infrastructure into a canvas for freestyle orchestration." imgLink={isDark ? WhiteboardingImage : WhiteboardingImageLight}/>
       </Container>
+      {/* a suite of annotation and visualization tools, allowing users to draw, annotate, and collaborate in real-time on their cloud native designs. */}
     </>
   );
 };
