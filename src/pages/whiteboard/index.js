e8411bdba6b5ff311629c575883dee30cea75f0f
Lee Calcote <lee.calcote@layer5.io>
2023-12-15 08:48:08 -0600

    move book
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- src/pages/whiteboard/index.js
+++ src/pages/whiteboard/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import loadable from "@loadable/component";
 
 import Whiteboard from "../../sections/Whiteboard";
-const ServiceMeshFocussed = loadable(() => import("../sections/Home/Service-mesh-focussed"));
+// const ServiceMeshFocussed = loadable(() => import("../sections/Home/Service-mesh-focussed"));
+import ServiceMeshFocused from "../../sections/Home/Service-mesh-focussed";
 
 const WhiteboardPage = () => {
   return (
     <>
       <Whiteboard/>
-      <ServiceMeshFocussed bookName={"enterprise-path"} />
+      <ServiceMeshFocused bookName={"enterprise-path"} />
     </>
   );
 };
