c4b351d1b6184f1ed97155e4e1a32cda4bd06606
Jatin <jatinsood1024@gmail.com>
2023-12-15 22:59:11 +0530

    update
    Signed-off-by: Jatin <jatinsood1024@gmail.com>

--- src/sections/Community/Handbook/repository.js
+++ src/sections/Community/Handbook/repository.js
@@ -179,7 +179,7 @@ const Repository = () => {
                               >
                                 <img className="github-icon" src={github} alt="github-icon" />
                               </a>
-                              <div className="accessRequired"><Link to="/community/handbook/faq" style = {{cursor : 'pointer'}}> { accessRequired != "" ? accessRequired : ""}</Link></div>
+                              <div className="accessRequired"><Link to="/community/handbook/faq/#program-permission"> { accessRequired != "" ? accessRequired : ""}</Link></div>
                             </td>
                           </tr>
                         </tbody>
