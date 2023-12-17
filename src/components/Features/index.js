29f29b128f3787d02dd367bebad8dff415a99153
captain-Akshay <akshay.sharma0503@gmail.com>
2023-12-17 15:50:34 +0530

    added cursor animation
    Signed-off-by: captain-Akshay <akshay.sharma0503@gmail.com>

--- src/components/Features/index.js
+++ src/components/Features/index.js
@@ -1,37 +1,129 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Link } from "gatsby";
-import { Container, ContentWrapper, ImageWrapper, ImageWrapperTwo } from "./style.js";
+import {
+  Container,
+  ContentWrapper,
+  ImageWrapper,
+  ImageWrapperTwo,
+  SvgRandomWrapper,
+} from "./style.js";
 import cursor from "./images/cursor.svg";
+import person1 from "./images/cursor-1.svg";
+import person2 from "./images/cursor-2.svg";
+import person3 from "./images/cursor-3.svg";
+import person4 from "./images/cursor-4.svg";
+import wave from "./images/wave.svg";
 
 const Features = (props) => {
+  const containerRef = useRef(null);
+  const [waveStart, setWaveStart] = useState(false);
+  function getPerson(index) {
+    const persons = [person1, person2, person3, person4];
+    return persons[index];
+  }
+  useEffect(() => {
+    const container = containerRef.current;
 
+    const moveRandomly = (element) => {
+      const rect = container.getBoundingClientRect();
+      const maxX = rect.width;
+      const maxY = rect.height;
+      console.log(maxX, maxY, "here");
+      const randomX = Math.floor(Math.random() * maxX);
+      const halfMaxY = maxY / 2;
+      const randomY = Math.floor(Math.random() * (maxY + 1)) - halfMaxY;
+
+      element.style.transition = "transform 3s ease";
+      element.style.transform = `translate(${randomX}px, ${randomY}px)`;
+
+      setTimeout(() => {
+        element.style.transition = "";
+      }, 3000);
+    };
+
+    const setInitialRandomPosition = (element) => {
+      const rect = container.getBoundingClientRect();
+      const maxX = rect.width - element.clientWidth;
+      const maxY = rect.height - element.clientHeight;
+      const randomX = Math.floor(Math.random() * maxX);
+      const randomY = Math.floor(Math.random() * maxY);
+
+      element.style.transform = `translate(${randomX}px, ${randomY}px)`;
+    };
+
+    const person1Element = container.querySelector(".person1");
+    const person2Element = container.querySelector(".person2");
+
+    setInitialRandomPosition(person1Element);
+    setInitialRandomPosition(person2Element);
+
+    const movePersonsRandomly = () => {
+      moveRandomly(person1Element);
+      setTimeout(() => {
+        moveRandomly(person2Element);
+      }, 3000);
+    };
+
+    movePersonsRandomly();
+
+    const intervalId = setInterval(movePersonsRandomly, 6000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+  function mouseEnterEvent() {
+    setWaveStart(true);
+    setTimeout(() => {
+      setWaveStart(false);
+    }, 1000);
+  }
+  function mouseLeaveEvent() {
+    setWaveStart(false);
+  }
   const style = {
     cursor: props.show_custom_cursor ? `url(${cursor}), auto` : "",
   };
 
   return (
-    <>
-      <Container style={style}>
-        <ContentWrapper>
-          <h2>{props.title}</h2>
-          <p className="text">{props.desc}</p>
-          <Link to={props.redirectLink}>Learn more &rarr;</Link>
-        </ContentWrapper>
-        {/* <Link to={props.redirectLink}> */}
-        {props.animationOne ? (<ImageWrapper>
+    <Container
+      ref={containerRef}
+      style={style}
+      onMouseEnter={mouseEnterEvent}
+      onMouseLeave={mouseLeaveEvent}
+    >
+      <SvgRandomWrapper className="person1">
+        {waveStart && (
+          <img
+            src={wave}
+            alt=""
+            style={{ position: "absolute" }}
+            className="waveAnimation"
+          />
+        )}
+        <img src={getPerson(props.index * 2)} alt="" />
+      </SvgRandomWrapper>
+      <SvgRandomWrapper className="person2">
+        <img src={getPerson(props.index * 2 + 1)} alt="" />
+      </SvgRandomWrapper>
+      <ContentWrapper>
+        <h2>{props.title}</h2>
+        <p className="text">{props.desc}</p>
+        <Link to={props.redirectLink}>Learn more &rarr;</Link>
+      </ContentWrapper>
+      {props.animationOne ? (
+        <ImageWrapper>
           <Link to={props.redirectLink}>
             <img src={props.imgLink} alt="image" />
           </Link>
-        </ImageWrapper>) : (<ImageWrapperTwo>
+        </ImageWrapper>
+      ) : (
+        <ImageWrapperTwo>
           <Link to={props.redirectLink}>
             <img src={props.imgLink} alt="image" />
           </Link>
-        </ImageWrapperTwo>)}
-        {/* </Link> */}
-      </Container>
-    </>
+        </ImageWrapperTwo>
+      )}
+    </Container>
   );
 };
-
 
 export default Features;
--- src/sections/Home/FeaturesContainer/index.js
+++ src/sections/Home/FeaturesContainer/index.js
@@ -17,17 +17,36 @@ const FeaturesContainer = () => {
   return (
     <>
       <Container>
-        <h1 className="title">Deploy <i>faster </i>together with <span>Confidence</span></h1>
-        <p className="subtitle">Give your team full application visibility at every layer so the team can ship, refactor and onboard faster.</p>
+        <h1 className="title">
+          Deploy <i>faster </i>together with <span>Confidence</span>
+        </h1>
+        <p className="subtitle">
+          Give your team full application visibility at every layer so the team
+          can ship, refactor and onboard faster.
+        </p>
         {/* Tools to help you scale your cloud native infrastructure */}
-        <Features title="Collaborate with Precision" show_custom_cursor={true} animationOne = {true} redirectLink="/comments" desc="Real-time collaboration for cloud and cloud native designs with live-editing, instant feedback, deploy dry runs, and secure access controls." imgLink={isDark ? CommentingImageDark : CommentingImageLight}/>
+        <Features
+          title="Collaborate with Precision"
+          show_custom_cursor={true}
+          animationOne={true}
+          redirectLink="/comments"
+          desc="Real-time collaboration for cloud and cloud native designs with live-editing, instant feedback, deploy dry runs, and secure access controls."
+          imgLink={isDark ? CommentingImageDark : CommentingImageLight}
+          index={0}
+        />
         {/* Elevate teamwork with integrated communication channels, ensuring efficient and secure project success. */}
-        <Features title="Generate stunning diagrams" show_custom_cursor={true} redirectLink="/whiteboard" desc="Crafting cloud-native symphonies: Our engineering diagramming tool is your conductor's baton, turning Kubernetes infrastructure into a canvas for freestyle orchestration." imgLink={isDark ? WhiteboardingImage : WhiteboardingImageLight}/>
+        <Features
+          title="Generate stunning diagrams"
+          show_custom_cursor={true}
+          redirectLink="/whiteboard"
+          desc="Crafting cloud-native symphonies: Our engineering diagramming tool is your conductor's baton, turning Kubernetes infrastructure into a canvas for freestyle orchestration."
+          imgLink={isDark ? WhiteboardingImage : WhiteboardingImageLight}
+          index={1}
+        />
       </Container>
       {/* a suite of annotation and visualization tools, allowing users to draw, annotate, and collaborate in real-time on their cloud native designs. */}
     </>
   );
 };
-
 
 export default FeaturesContainer;
