254eedbc068518c46c6d7033fb8c1a0b1a3d8e48
Archit Sharma <archit8679@gmail.com>
2023-12-23 23:05:59 +0530

    new hero section
    Signed-off-by: Archit Sharma <archit8679@gmail.com>

--- src/sections/AWS-Diagram/index.js
+++ src/sections/AWS-Diagram/index.js
@@ -1,12 +1,20 @@
 import React from "react";
-import Header from "./header";
+import FeatureHero from "../FeatureHero";
+// import Header from "./header";
 import Reviews from "../Pricing/review-slider";
 import Diagram from "./diagram";
+import AWSCardDark from "../../assets/images/AWS-Diagram/aws.svg";
 
 const AwsDiagramPage = () => {
+  let data = {
+    heading: "AWS Architecture Diagram Tool",
+    sub_heading: "Create AWS diagrams easily with predefined templates and symbols designed for professionals.",
+    image: AWSCardDark
+  };
   return (
     <>
-      <Header />
+      <FeatureHero data={data} />
+      {/* <Header /> */}
       <Diagram />
       <Reviews />
     </>
