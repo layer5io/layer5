6c4f5da5bfe87637a2eb81689be7b0062c72cbf0
Archit Sharma <archit8679@gmail.com>
2023-12-26 10:59:13 +0530

    GCP page hero section update
    Signed-off-by: Archit Sharma <archit8679@gmail.com>

--- src/sections/GCP-Diagram/index.js
+++ src/sections/GCP-Diagram/index.js
@@ -1,12 +1,20 @@
 import React from "react";
-import Header from "./header";
+//import Header from "./header";
+import FeatureHero from "../FeatureHero";
+import GCPDiagramDark from "./images/gcp-diagram-dark.svg";
 import Reviews from "../Pricing/review-slider";
 import Diagram from "./diagram";
 
 const GcpDiagramPage = () => {
+  let data = {
+    heading: "Google Cloud Platform Diagram Tool",
+    sub_heading: "Create GCP diagrams easily with predefined templates and symbols designed for professionals.",
+    image: GCPDiagramDark
+  };
   return (
     <>
-      <Header />
+      <FeatureHero data={data} />
+      {/* <Header /> */}
       <Diagram />
       <Reviews />
     </>
