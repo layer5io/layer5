eff2a03634baf4c077ad95ab99a39790f756f880
Akshun-01 <akshunkuthiala2002@gmail.com>
2023-12-26 19:57:54 +0530

    linted
    Signed-off-by: Akshun-01 <akshunkuthiala2002@gmail.com>

--- src/sections/UseCases/index.js
+++ src/sections/UseCases/index.js
@@ -1,77 +1,104 @@
 import React from "react";
 import UseCasesWrapper from "./usecases.style";
-// import { graphql, useStaticQuery, Link } from "gatsby";
-// import { MDXRenderer } from "gatsby-plugin-mdx";
-// import { Container, Row, Col } from "../../../reusecore/Layout";
-// import WorkshopCard from "../../../components/Workshop-Card";
+import { Container, Row, Col } from "../../reusecore/Layout";
+import WorkshopCard from "../../components/Workshop-Card";
 import Partners from "../Home/Partners-home";
 import SeeYou from "../SeeYou";
-import use_case_1 from "../../assets/images/comments/use-case-1.png";
-import use_case_2 from "../../assets/images/comments/use-case-2.png";
-// import { FaRegWindowMaximize } from "@react-icons/all-files/fa/FaRegWindowMaximize";
+import { FaRegWindowMaximize } from "@react-icons/all-files/fa/FaRegWindowMaximize";
+// import use_case_1 from "../../assets/images/comments/use-case-1.png";
+// import use_case_2 from "../../assets/images/comments/use-case-2.png";
 
-let CARD_DATA = [
-    {
-        heading: "Whiteboarding",
-        subtitle: "The Whiteboarding and Freestyle Drawing feature introduces versatile drawing capabilities within MeshMap.",
-        image: use_case_1,
-        url:"#"
+const CARD_DATA = [
+  {
+    id: 1,
+    frontmatter: {
+      title: "Whiteboarding",
+      abstract: "The Whiteboarding and Freestyle Drawing feature introduces versatile drawing capabilities within MeshMap.",
+      thumbnail: {
+        childImageSharp: false,
+        extension: "png",
+        publicURL: "https://layer5.io/static/whiteboard-1e9f33293030bc98a01945af9740863f.png",
+      },
+      status: "delivered",
     },
-    {
-        heading: "Brainstorm with commenting",
-        subtitle: "MeshMap's Designer Mode offers a powerful collaboration feature through comments.",
-        image: use_case_2,
-        url:"#"
+    url: "https://layer5.io/whiteboard",
+  },
+  {
+    id: 2,
+    frontmatter: {
+      title: "Brainstorm with commenting",
+      abstract: "MeshMap's Designer Mode offers a powerful collaboration feature through comments.",
+      thumbnail: {
+        childImageSharp: false,
+        extension: "png",
+        publicURL: "https://layer5.io/static/comment.b77548b0.svg",
+      },
+      status: "delivered",
     },
-    {
-        heading: "Whiteboarding",
-        subtitle: "The Whiteboarding and Freestyle Drawing feature introduces versatile drawing capabilities within MeshMap.",
-        image: use_case_1,
-        url:"#"
+    url: "https://layer5.io/comments",
+  },
+  {
+    id: 3,
+    frontmatter: {
+      title: "Whiteboarding",
+      abstract: "The Whiteboarding and Freestyle Drawing feature introduces versatile drawing capabilities within MeshMap.",
+      thumbnail: {
+        childImageSharp: false,
+        extension: "png",
+        publicURL: "https://layer5.io/static/whiteboard-1e9f33293030bc98a01945af9740863f.png",
+      },
+      status: "delivered",
     },
-    {
-        heading: "Brainstorm with commenting",
-        subtitle: "MeshMap's Designer Mode offers a powerful collaboration feature through comments.",
-        image: use_case_2,
-        url:"#"
+    url: "https://layer5.io/whiteboard",
+  },
+  {
+    id: 4,
+    frontmatter: {
+      title: "Brainstorm with commenting",
+      abstract: "MeshMap's Designer Mode offers a powerful collaboration feature through comments.",
+      thumbnail: {
+        childImageSharp: false,
+        extension: "png",
+        publicURL: "https://layer5.io/static/comment.b77548b0.svg",
+      },
+      status: "delivered",
     },
+    url: "https://layer5.io/comments",
+  },
 ];
 
 const UseCases = () => {
-    return (
-        <UseCasesWrapper>
-            <div className="page-title">
-                <h1 className="heading">Platform Engineering done right</h1>
-                <p className="description">Infrastructure diagraming is a breeze with Meshery. Configuration and operational design reviews between SRE, Platform Engineer, DevOps pro, or Developer</p>
-            </div>
-            {/* Card */}
-            {/* <Container>
-                <div className="workshop-grid-wrapper">
-                    <Row>
-                        {data.allMdx.nodes.map(({ id, frontmatter, fields, body }) => (
-                            <Col {...content && ID === id ? { xs: 12, sm: 12, lg: 12 } : { xs: 12, sm: 6, lg: 4 }} key={id} className="workshop-grid-col">
-                                <div className="workshop-grid-card">
-                                    <WorkshopCard frontmatter={frontmatter} content={content} ID={ID} id={id} />
-                                    <div className={content && ID === id ? "active" : "text-contents"}>
-                                        <div className="content">
-                                            <MDXRenderer>{body}</MDXRenderer>
-                                        </div>
-                                    </div>
+  const content = false;
+  const ID = "";
 
-                                    <div className="externalLink">
-                                        <Link to={fields.slug} className="siteLink"><FaRegWindowMaximize style={{ height: "25px", width: "auto" }} /></Link>
-                                    </div>
+  return (
+    <UseCasesWrapper>
+      <div className="use-cases-title" >
+        <h2 className="heading"> Platform Engineering done right </h2>
+        <p className="description"> Infrastructure diagraming is a breeze with Meshery. Configuration and operational design reviews between SRE, Platform Engineer, DevOps pro, or Developer </p>
+      </div>
 
-                                </div>
-                            </Col>
-                        ))}
-                    </Row>
+      <Container>
+        <div className="workshop-grid-wrapper">
+          <Row>
+            {CARD_DATA.map(({ id, frontmatter, url }) => (
+              <Col {...content && ID === id ? { xs: 12, sm: 12, lg: 12 } : { xs: 12, sm: 6, lg: 4 } } key={id} className="workshop-grid-col">
+                <div className="workshop-grid-card">
+                  <WorkshopCard frontmatter={frontmatter} content={content} ID={ID} id={id} />
+                  <div className="externalLink">
+                    <a href={url} className="siteLink"><FaRegWindowMaximize style={{ height: "25px", width: "auto" }} /></a>
+                  </div>
                 </div>
-            </Container> */}
-            <Partners />
-            <SeeYou />
-        </UseCasesWrapper>
-    );
+              </Col>
+            ))}
+          </Row>
+        </div>
+      </Container>
+
+      <Partners />
+      <SeeYou />
+    </UseCasesWrapper>
+  );
 };
 
 export default UseCases;
--- src/sections/UseCases/usecases.style.js
+++ src/sections/UseCases/usecases.style.js
@@ -1,67 +1,74 @@
 import styled from "styled-components";
 
-export const UseCasesWrapper = styled.div`
-    .page-title{
+const UseCasesWrapper = styled.div`
+	.use-cases-title{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		margin: 1rem 0;
 
-        .heading{
+		.heading{
+			color: ${props => props.theme.whiteToBlack} !important;
+      		font-size: 4rem;
+      		font-weight: 700;
+      		max-width: 80%;
+      		margin: 0 auto;
+      		padding: 0 1rem;
+		}
+		.description{
+			color: ${props => props.theme.greyA0AAAAToGrey666666};
+    		font-size: 1.5rem;
+    		line-height: 2rem;
+    		max-width: 60%;
+    		margin: 1rem auto;
+    		padding: 0 1rem
+		}
 
-        }
-        .description{
-
-        }
-    }
-
-    // card
-    .workshop-grid-wrapper {
-		padding-bottom: 3.75rem;
+		@media (max-width: 768px) {
+			.heading{
+			  font-size: 2.75rem !important;
+			}
+			.description {
+			  font-size: 1.25rem;
+			  line-height: 1.5rem;
+			}
+		  }
+		
+		  @media (max-width: 500px) {
+			.heading{
+			  font-size: 2rem !important;
+			}
+		
+			.whiteboard-text {
+			  font-size: 1rem;
+			  line-height: 1.2rem;
+			}
 	}
 
-    .workshop-grid-card {
-		background-color: ${props => props.theme.grey212121ToWhite};
-		width: 100%;
-		display: block;
-		height: auto;
-		border-radius: 0.3125rem;
-		margin-bottom: 1.25rem;
-		box-shadow: 0rem 0.0625rem 0.3125rem rgba(0, 0, 0, 0.2);
-		transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+	.usecases-grid-wrapper{
 
-		&:hover {
-			.main {
-				box-shadow: 0rem 0.25rem 1rem rgba(0, 0, 0, 0.15);
+		.usecases-grid-card{
+
+		}
+
+		.externalLink {
+			position: relative;
+			left: 1rem;
+			margin-top: 0.25rem;
+		}
+		a.siteLink {
+			padding: 0.5rem 0.25rem 0rem;
+			filter: grayscale(100%) brightness(${props => props.theme.siteLinkBrightness});
+			transition: 0.2s ease-in-out all;
+	
+			&:hover {
+				color: ${props => props.theme.keppelColor};
+				filter: none;
 			}
 		}
 	}
+`;
 
-    .text-contents {
-		display: none;
-		width: 100%;
-		height: 100%;
-		font-family: "Qanelas Soft", "Open Sans", sans-serif;
-	}
-
-    .active {
-		display: block;
-		width: 100%;
-		height: 100%;
-		padding: 1.5rem;
-	}
-
-    .externalLink {
-		position: relative;
-		left: 1rem;
-		margin-top: 0.25rem;
-	}
-
-    a.siteLink {
-		padding: 0.5rem 0.25rem 0rem;
-		filter: grayscale(100%) brightness(${props => props.theme.siteLinkBrightness});
-		transition: 0.2s ease-in-out all;
-
-		&:hover {
-			color: ${props => props.theme.keppelColor};
-			filter: none;
-		}
-	}
-
-`
+export default UseCasesWrapper;
