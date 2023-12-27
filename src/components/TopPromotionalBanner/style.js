378f6a1387ddc989a0340b4ac4c70fc6b2b04b50
Lee Calcote <lee.calcote@layer5.io>
2023-12-27 15:13:51 -0600

    [Home] Change blog link
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- src/components/TopPromotionalBanner/style.js
+++ src/components/TopPromotionalBanner/style.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const TopBannercontainer = styled.div`
     background: ${props => props.theme.saffronColor};
     position: sticky;
+    a {
+        color: #000 !important,
+    }
     .content {
         display: flex;
         align-items: center;
@@ -10,16 +13,19 @@ export const TopBannercontainer = styled.div`
         padding: 0 1rem;
         font-weight: 500;
         line-height: 1.1875rem;
-        color: ${props => props.theme.body};
+        color: #000 !important;
+        /* color: ${props => props.theme.body}; */
         padding: 0.75rem 1rem;
-
+        &:hover {
+            text-decoration: underline;
+        }
        & > img {
         width: 3rem;
         margin-right: 0.5rem;
        }
 
        & > span {
-        font-weight: 700;
+        font-weight: normal;
         margin-left: 0.5rem;
         &:hover {
             text-decoration: underline;
