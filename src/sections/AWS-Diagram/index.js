0a76a5442694b68fc6e3a3ae89b34af6ae286e8d
Ritik Saxena <ritiksaxena124@gmail.com>
2023-12-28 19:02:58 +0530

    hero image theme compatibility fixed
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- src/sections/AWS-Diagram/index.js
+++ src/sections/AWS-Diagram/index.js
@@ -3,13 +3,17 @@ import FeatureHero from "../FeatureHero";
 // import Header from "./header";
 import Reviews from "../Pricing/review-slider";
 import Diagram from "./diagram";
-import AWSCardDark from "../../assets/images/AWS-Diagram/aws.svg";
+import AWSDark from "../../assets/images/AWS-Diagram/aws.svg";
+import AWSLight from "../../assets/images/AWS-Diagram/aws-light.svg";
+import { useStyledDarkMode } from "../../theme/app/useStyledDarkMode";
 
 const AwsDiagramPage = () => {
+  const { isDark } = useStyledDarkMode();
+
   let data = {
     heading: "AWS Architecture Diagram Tool",
     sub_heading: "Create AWS diagrams easily with predefined templates and symbols designed for professionals.",
-    image: AWSCardDark
+    image: isDark ? AWSDark : AWSLight
   };
   return (
     <>
--- src/sections/FeatureHero/index.js
+++ src/sections/FeatureHero/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import FeatureHeroWrapper from "./featureHero.style";
 import { Container } from "../../reusecore/Layout";
-
+import Button from "../../reusecore/Button";
 const FeatureHero = (props) => {
   useEffect(() => {
     const scrollingImage = document.getElementById("whiteboard-svg");
@@ -25,8 +25,14 @@ const FeatureHero = (props) => {
     <FeatureHeroWrapper>
       <Container className={"heading-container"}>
         <div className={"section-title"}>
-          <h2 className={"whiteboard-heading"}>{props.data.heading}</h2>
+          <h1 className={"whiteboard-heading"}>{props.data.heading}</h1>
           <p className={"whiteboard-text"}>{props.data.sub_heading}</p>
+          <Button
+            primary
+            title="Try Now!"
+            url="https://play.meshery.io"
+            external={true}
+          />
         </div>
         <div className={"whiteboard-image"}>
           <img id={"whiteboard-svg"} src={props.data.image} alt={""} />
