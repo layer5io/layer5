6f90b8a6d3451fb01edf9ce2b678a652395f47c4
Ritik Saxena <ritiksaxena124@gmail.com>
2023-12-28 19:02:58 +0530

    hero image theme compatibility fixed
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- src/sections/FeatureHero/featureHero.style.js
+++ src/sections/FeatureHero/featureHero.style.js
@@ -8,7 +8,10 @@ const FeatureHeroWrapper = styled.div`
     padding: 100px 0 70px;
 
     .section-title {
-      text-align: left;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
     }
 
     .whiteboard-image{
@@ -31,7 +34,7 @@ const FeatureHeroWrapper = styled.div`
       text-align: center;
       font-size: 4rem;
       font-weight: 600;
-      max-width: 768px;
+      max-width: 965px;
       margin: 0 auto;
       padding: 0 1rem;
     }
@@ -61,7 +64,7 @@ const FeatureHeroWrapper = styled.div`
     text-align: center;
     font-size: 1.75rem;
     line-height: 2.4rem;
-    max-width: 600px;
+    max-width: 965px;
     margin: 1rem auto;
     padding: 0 1rem
   }
--- src/sections/GCP-Diagram/index.js
+++ src/sections/GCP-Diagram/index.js
@@ -4,12 +4,15 @@ import FeatureHero from "../FeatureHero";
 import GCPDiagramDark from "./images/gcp-diagram-dark.svg";
 import Reviews from "../Pricing/review-slider";
 import Diagram from "./diagram";
+import GCPDiagramLight from "./images/gcp-diagram-light.svg";
+import { useStyledDarkMode } from "../../theme/app/useStyledDarkMode";
 
 const GcpDiagramPage = () => {
+  const { isDark } = useStyledDarkMode();
   let data = {
     heading: "GCP Architecture Diagram Tool",
     sub_heading: "Create GCP diagrams easily with predefined templates and symbols designed for professionals.",
-    image: GCPDiagramDark
+    image: isDark ? GCPDiagramDark : GCPDiagramLight
   };
   return (
     <>
