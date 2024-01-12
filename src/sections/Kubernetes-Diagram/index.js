a735651b336f3cb12554b8ca369884f0fa585255
Ritik Saxena <ritiksaxena124@gmail.com>
2024-01-13 00:34:29 +0530

    Kubernerets page content updated
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- src/sections/Kubernetes-Diagram/index.js
+++ src/sections/Kubernetes-Diagram/index.js
@@ -3,16 +3,16 @@ import FeatureHero from "../FeatureHero";
 import Reviews from "../Pricing/review-slider";
 import Diagram from "./diagram";
 import KubernetesDark from "./images/kubernetes.svg";
-import KubernetesLight from "./images/kubernetes-light.svg";
+// import KubernetesLight from "./images/kubernetes-light.svg";
 import { useStyledDarkMode } from "../../theme/app/useStyledDarkMode";
 
 const KubernetesDiagramPage = () => {
   const { isDark } = useStyledDarkMode();
 
   let data = {
-    heading: "Kubernetes Architecture Diagram Tool",
-    sub_heading: "Create Kubernetes diagrams easily with predefined templates and symbols designed for professionals.",
-    image: isDark ? KubernetesDark : KubernetesLight
+    heading: "Kubernetes Orchestration and diagramming software",
+    sub_heading: "Orchestrate Kubernetes and Create diagrams easily with predefined templates and symbols designed for professionals.",
+    image: isDark ? KubernetesDark : KubernetesDark
   };
   return (
     <>
