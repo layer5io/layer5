6aa5971dbbf5bec12fddb75adc132c5d3e181b26
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2024-01-28 17:54:31 +0530

    fetch-data-using-query
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

--- src/sections/UseCases/index.js
+++ src/sections/UseCases/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 import UseCasesWrapper from "./usecases.style";
 import { Container, Row, Col } from "../../reusecore/Layout";
 import WorkshopCard from "../../components/Workshop-Card";
@@ -8,68 +9,38 @@ import { FaRegWindowMaximize } from "@react-icons/all-files/fa/FaRegWindowMaximi
 // import use_case_1 from "../../assets/images/comments/use-case-1.png";
 // import use_case_2 from "../../assets/images/comments/use-case-2.png";
 
-const CARD_DATA = [
-  {
-    id: 1,
-    frontmatter: {
-      title: "Whiteboarding",
-      abstract: "The Whiteboarding and Freestyle Drawing feature introduces versatile drawing capabilities within MeshMap.",
-      thumbnail: {
-        childImageSharp: false,
-        extension: "png",
-        publicURL: "https://layer5.io/static/whiteboard-1e9f33293030bc98a01945af9740863f.png",
-      },
-      status: "delivered",
-    },
-    url: "https://layer5.io/whiteboard",
-  },
-  {
-    id: 2,
-    frontmatter: {
-      title: "Brainstorm with commenting",
-      abstract: "MeshMap's Designer Mode offers a powerful collaboration feature through comments.",
-      thumbnail: {
-        childImageSharp: false,
-        extension: "png",
-        publicURL: "https://layer5.io/static/comment.b77548b0.svg",
-      },
-      status: "delivered",
-    },
-    url: "https://layer5.io/comments",
-  },
-  {
-    id: 3,
-    frontmatter: {
-      title: "Whiteboarding",
-      abstract: "The Whiteboarding and Freestyle Drawing feature introduces versatile drawing capabilities within MeshMap.",
-      thumbnail: {
-        childImageSharp: false,
-        extension: "png",
-        publicURL: "https://layer5.io/static/whiteboard-1e9f33293030bc98a01945af9740863f.png",
-      },
-      status: "delivered",
-    },
-    url: "https://layer5.io/whiteboard",
-  },
-  {
-    id: 4,
-    frontmatter: {
-      title: "Brainstorm with commenting",
-      abstract: "MeshMap's Designer Mode offers a powerful collaboration feature through comments.",
-      thumbnail: {
-        childImageSharp: false,
-        extension: "png",
-        publicURL: "https://layer5.io/static/comment.b77548b0.svg",
-      },
-      status: "delivered",
-    },
-    url: "https://layer5.io/comments",
-  },
-];
 
 const UseCases = () => {
   const content = false;
   const ID = "";
+
+  const data = useStaticQuery(
+    graphql `query allUseCases {
+        allMdx(
+          filter: {fields: {collection: {eq: "use-cases"}}, frontmatter: {category: {eq: "usecase"}}}
+        ) {
+          nodes {
+            id
+            frontmatter {
+              title
+              abstract
+              thumbnail {
+                childImageSharp {
+                  gatsbyImageData(layout: FULL_WIDTH)
+                }
+                extension
+                publicURL
+              }
+              eurl
+              category
+              status
+            }
+          }
+        }
+      }
+    `
+  );
+
 
   return (
     <UseCasesWrapper>
@@ -81,12 +52,12 @@ const UseCases = () => {
       <Container>
         <div className="workshop-grid-wrapper">
           <Row>
-            {CARD_DATA.map(({ id, frontmatter, url }) => (
+            {data.allMdx.nodes.map(({ id, frontmatter }) => (
               <Col {...content && ID === id ? { xs: 12, sm: 12, lg: 12 } : { xs: 12, sm: 6, lg: 4 } } key={id} className="workshop-grid-col">
                 <div className="workshop-grid-card">
                   <WorkshopCard frontmatter={frontmatter} content={content} ID={ID} id={id} />
                   <div className="externalLink">
-                    <a href={url} className="siteLink"><FaRegWindowMaximize style={{ height: "25px", width: "auto" }} /></a>
+                    <a href={frontmatter.eurl} className="siteLink"><FaRegWindowMaximize style={{ height: "25px", width: "auto" }} /></a>
                   </div>
                 </div>
               </Col>
@@ -100,5 +71,6 @@ const UseCases = () => {
     </UseCasesWrapper>
   );
 };
+
 
 export default UseCases;
