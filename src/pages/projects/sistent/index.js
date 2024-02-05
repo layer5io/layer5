d22a86ccf36104677708c06bfdf52426f58eadaf
Lakshya Satpal <lakshyasatpal372@gmail.com>
2024-02-06 01:06:05 +0530

    feat: Sistent coming soon
    Signed-off-by: Lakshya Satpal <lakshyasatpal372@gmail.com>

---
+++ src/pages/projects/sistent/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SistentHome from "../../../sections/Projects/Sistent/";
+
+const SistentIndexPage = () => {
+  return <SistentHome />;
+};
+
+export default SistentIndexPage;
