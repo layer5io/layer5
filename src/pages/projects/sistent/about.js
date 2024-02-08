b92771acba21aa68a3db2e8128c5e568993f67af
Lakshya Satpal <lakshyasatpal372@gmail.com>
2024-02-08 06:03:43 +0530

    using handbook components with sistent
    Signed-off-by: Lakshya Satpal <lakshyasatpal372@gmail.com>

---
+++ src/pages/projects/sistent/about.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SistentAbout = () => {
+  return <div>SistentAbout</div>;
+};
+
+export default SistentAbout;
---
+++ src/pages/projects/sistent/components.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SistentComponents = () => {
+  return <div>SistentComponents</div>;
+};
+
+export default SistentComponents;
