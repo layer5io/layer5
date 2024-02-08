29121ec198a5154390437ffb4b8381bc5022c841
Akshun-01 <akshunkuthiala2002@gmail.com>
2024-02-08 13:33:00 +0530

    add use cases page
    Signed-off-by: Akshun-01 <akshunkuthiala2002@gmail.com>

---
+++ src/pages/use-cases/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UseCases from "../../sections/UseCases";
+
+const UseCasesPage = () => {
+  return (
+    <>
+      <UseCases/>
+    </>
+  );
+};
+
+export default UseCasesPage;
---
+++ src/sections/useCases/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import UseCasesWrapper from "./usecases.style";
+import { Container, Row, Col } from "../../reusecore/Layout";
+import WorkshopCard from "../../components/Workshop-Card";
+import Partners from "../Home/Partners-home";
+import SeeYou from "../SeeYou";
+import { FaRegWindowMaximize } from "@react-icons/all-files/fa/FaRegWindowMaximize";
+
+const UseCases = () => {
+  const content = false;
+  const ID = "";
+
+  const data = useStaticQuery(
+    graphql `query allUseCases {
+        allMdx(
+          filter: {fields: {collection: {eq: "use-cases"}}, frontmatter: {category: {eq: "usecase"}}}
+        ) {
+          nodes {
+            id
+            frontmatter {
+              title
+              abstract
+              thumbnail {
+                childImageSharp {
+                  gatsbyImageData(layout: FULL_WIDTH)
+                }
+                extension
+                publicURL
+              }
+              eurl
+              category
+              status
+            }
+          }
+        }
+      }
+    `
+  );
+
+  return (
+    <UseCasesWrapper>
+      <div className="use-cases-title" >
+        <h2 className="heading"> Platform Engineering done right </h2>
+        <p className="description"> Infrastructure diagraming is a breeze with Meshery. Configuration and operational design reviews between SRE, Platform Engineer, DevOps pro, or Developer </p>
+      </div>
+
+      <Container>
+        <div className="workshop-grid-wrapper">
+          <Row>
+            {data.allMdx.nodes.map(({ id, frontmatter }) => (
+              <Col {...content && ID === id ? { xs: 12, sm: 12, lg: 12 } : { xs: 12, sm: 6, lg: 4 } } key={id} className="workshop-grid-col">
+                <div className="workshop-grid-card">
+                  <WorkshopCard frontmatter={frontmatter} content={content} ID={ID} id={id} />
+                  <div className="externalLink">
+                    <a href={frontmatter.eurl} className="siteLink"><FaRegWindowMaximize style={{ height: "25px", width: "auto" }} /></a>
+                  </div>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </div>
+      </Container>
+
+      <Partners />
+      <SeeYou />
+    </UseCasesWrapper>
+  );
+};
+
+export default UseCases;
