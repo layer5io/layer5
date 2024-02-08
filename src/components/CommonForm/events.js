eb97a6f1ac7ba9c37be138c118c45c24e7695a3e
Vivek Vishal <vishalvivek488@gmail.com>
2024-02-08 15:49:43 +0530

    [chore] Update webhook signup forms

--- src/components/CommonForm/events.js
+++ src/components/CommonForm/events.js
@@ -23,7 +23,7 @@ const EventForm = ({ form, title, submit_title, submit_body }) => {
 
   useEffect(() => {
     if (submit) {
-      axios.post("https://hook.us1.make.com/x4crqi16j8wfhctosk8y47fj6gknyvvh", {
+      axios.post("https://hook.us1.make.com/7c1op88rysnmeitovt35fxzcv2spspp0", {
         memberFormOne,
       });
     }
