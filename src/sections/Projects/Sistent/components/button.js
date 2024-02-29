96ecf7280f24ece85a19abdac2d38994248c8e1d
lakshya <lakshyasatpal372@gmail.com>
2024-02-29 17:57:21 +0530

    add components page
    Signed-off-by: lakshya <lakshyasatpal372@gmail.com>

---
+++ src/sections/Projects/Sistent/components/button.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SistentButton = () => {
+  return <div>SistentComponentsButton</div>;
+};
+
+export default SistentButton;
---
+++ src/sections/Projects/Sistent/components/text-input.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SistentTextInput = () => {
+  return <div>SistentTextInput</div>;
+};
+
+export default SistentTextInput;
