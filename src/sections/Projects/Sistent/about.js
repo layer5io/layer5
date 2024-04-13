d8e48f31c63bd2c6e6085cb659c3435d746ab8a3
lakshya <lakshyasatpal372@gmail.com>
2024-04-14 05:09:22 +0530

    add about section for sistent, fix misspelling
    Signed-off-by: lakshya <lakshyasatpal372@gmail.com>

--- src/sections/Projects/Sistent/about.js
+++ src/sections/Projects/Sistent/about.js
@@ -20,7 +20,28 @@ const SistentAbout = () => {
             <a id="About Sistent">
               <h2>About Sistent</h2>
             </a>
-            <p>Sorry, this page is still under work</p>
+            <p>
+              We have several different React apps that heavily uses the
+              Material UI v4 and v5 libraries, and this discrepancies led up to
+              setting up the Sistent Design System.
+            </p>
+            <p>
+              <b>
+                We want to be able to have the source of truth of what Layer5.io
+                and Meshery looks like.
+              </b>
+            </p>
+            <p>
+              'Sistent' is a play on word to ensure that we have a consistent
+              theme, components, design tokens, etc across all of the apps that
+              will be using this library. Sistent is a design system that uses
+              the <b>MUI v5 components</b> and a <b>custom theme provider </b>
+              instead of using the default theme from MUI v5.
+            </p>
+            <p>
+              If you’re interested in joining, please let us know, and we can
+              help you get started on contributing to the Sistent Design System.
+            </p>
           </div>
           <SistentPagination />
         </Container>
