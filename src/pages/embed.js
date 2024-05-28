02fa1ab5bea5b4523791f66fee28ee4ba820c0a6
captain-Akshay <akshay.sharma0503@gmail.com>
2024-05-29 03:38:09 +0530

    border
    Signed-off-by: captain-Akshay <akshay.sharma0503@gmail.com>

--- src/pages/embed.js
+++ src/pages/embed.js
@@ -4,6 +4,7 @@ const Testing = () => {
   return (
     <main>
       <MesheryDesignEmbed
+        style={{ border: "1px solid #eee" }}
         embedId="embedded-design-6f2255ba-54bf-4cb8-9033-262b8464d5d2"
         embedScriptSrc="embed-test/embedded-design-embed1.js"
       />
