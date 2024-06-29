2fa165a189419067b11ac1d068375a9a9a672b5f
rishabhsharma1997 <rishabhsharma19997@gmail.com>
2024-06-29 05:48:48 +0530

    image validity step
    Signed-off-by: rishabhsharma1997 <rishabhsharma19997@gmail.com>

--- src/sections/Meshery/Meshery-integrations/Individual-Integrations/ComponentsGrid.js
+++ src/sections/Meshery/Meshery-integrations/Individual-Integrations/ComponentsGrid.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ComponentsWrapper } from "./Component.style";
 
+export const checkImageUrlValidity = async (url) => {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.src = url;
+
+    img.onload = () => {
+      // Check if the image loaded successfully
+      resolve(true);
+    };
+
+    img.onerror = () => {
+      // Handle the case where the image could not be loaded
+      resolve(false);
+    };
+  });
+};
 
 const ComponentsGrid = ({ frontmatter }) => {
-
-
   const components = frontmatter.components.map((component) => {
     const { name, colorIcon } = component; // currently we don't have description for components
     return { name, colorIcon };
   });
+  const [validComponents, setValidComponents] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const promises = components.map(async (item) => {
+        const isSvgPathValid = await checkImageUrlValidity(
+          item.colorIcon.publicURL
+        );
+        if (isSvgPathValid) {
+          return item;
+        }
+        return null; // Return null for items that are not valid
+      });
 
+      const validItems = await Promise.all(promises);
+      // Filter out null values (items that are not valid)
+      const filteredValidComponents = validItems.filter(
+        (item) => item !== null
+      );
 
+      setValidComponents(filteredValidComponents);
+    };
+
+    fetchData();
+  }, []);
+  console.log("here", validComponents);
   return (
     <ComponentsWrapper>
       <section className="heading">
@@ -20,7 +57,7 @@ const ComponentsGrid = ({ frontmatter }) => {
       </section>
 
       <section className="componentsSection">
-        {components.map((item) => (
+        {validComponents.map((item) => (
           <div key={item.id} className="maincontainer">
             <div className="componentimg">
               <img src={item.colorIcon.publicURL} alt={item.name} />
