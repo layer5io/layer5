c6e3e6d75cc15ea8b0db74e91c620540f0e5b551
Tharun T <tharun242424@gmail.com>
2024-07-16 00:14:32 +0530

    revert: trigger errors
    Signed-off-by: Tharun T <tharun242424@gmail.com>

--- src/components/image.js
+++ src/components/image.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
 
-const Image = ({ childImageSharp, extension, publicURL, alt, test_var, ...rest }) => {
+const Image = ({ childImageSharp, extension, publicURL, alt, ...rest }) => {
 
   if (!childImageSharp && extension === "svg") {
-    const b=1+2;
     return (
       <div className="old-gatsby-image-wrapper">
         <img src={publicURL} alt={alt} />
       </div>
     );
   }
-
-  console.log(b);
 
   return <GatsbyImage
     key={publicURL} image={childImageSharp?.gatsbyImageData} {...rest} alt={alt}/>;
