15846524e7477209c6f75d8cf158c1af0c8d49e0
Tharun T <tharun242424@gmail.com>
2024-07-15 23:39:27 +0530

    trigger: more errors
    Signed-off-by: Tharun T <tharun242424@gmail.com>

--- src/components/image.js
+++ src/components/image.js
@@ -4,12 +4,15 @@ import { GatsbyImage } from "gatsby-plugin-image";
 const Image = ({ childImageSharp, extension, publicURL, alt, test_var, ...rest }) => {
 
   if (!childImageSharp && extension === "svg") {
+    const b=1+2;
     return (
       <div className="old-gatsby-image-wrapper">
         <img src={publicURL} alt={alt} />
       </div>
     );
   }
+
+  console.log(b);
 
   return <GatsbyImage
     key={publicURL} image={childImageSharp?.gatsbyImageData} {...rest} alt={alt}/>;
