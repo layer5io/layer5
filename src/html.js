87fc03becb9c152ba6f5f9cd5683e228280db8c3
Tharun T <tharun242424@gmail.com>
2024-07-20 01:25:19 +0530

    fix: remove unused eslint disable comment
    Signed-off-by: Tharun T <tharun242424@gmail.com>

--- src/html.js
+++ src/html.js
@@ -12,7 +12,6 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        {/* eslint-disable-next-line react/no-unknown-property*/}
         <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="anonymous" />
         <link
           rel="preload"
