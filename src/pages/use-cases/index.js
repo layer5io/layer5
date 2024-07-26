261f1434cbbe5cd9b94dbc0eb3e99378af053486
Hargun Kaur <hargunkaur286@gmail.com>
2024-07-27 02:01:57 +0530

    added use cases page
    Signed-off-by: Hargun Kaur <hargunkaur286@gmail.com>

---
+++ src/pages/use-cases/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UseCases from "../../sections/useCases";
+
+const UseCasesPage = () => {
+  return (
+    <>
+      <UseCases/>
+    </>
+  );
+};
+
+export default UseCasesPage;
