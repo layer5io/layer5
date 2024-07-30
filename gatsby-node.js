2071361e82e0f83200875b807aba764b1b42ef9a
Lee Calcote <lee.calcote@layer5.io>
2024-07-30 17:28:37 -0500

    Chore: Redirect to new blog location
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- gatsby-node.js
+++ gatsby-node.js
@@ -272,7 +272,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     redirectInBrowser: true,
     isPermanent: true,
   });
-  
+
   //****
   // External Resource Redirects
   //****
