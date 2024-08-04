39373b5d3c5b135aa9a11c051d2571cabcef0735
Naresh Biradar <naresh.23bcs10081@ms.sst.scaler.com>
2024-08-04 18:22:15 +0000

    addded the table
    Signed-off-by: Naresh Biradar <naresh.23bcs10081@ms.sst.scaler.com>

--- src/pages/projects/sistent/components/index.js
+++ src/pages/projects/sistent/components/index.js
@@ -1,23 +1,23 @@
 import React from "react";
 import SistentComponents from "../../../../sections/Projects/Sistent/components";
-import { ResponsiveDataTable  }  from '@layer5/sistent';
+import { ResponsiveDataTable  }  from "@layer5/sistent";
 
 // Define some sample columns
 const columns = [
-  { id: 'name', label: 'Name', minWidth: 100 },
-  { id: 'code', label: 'ISO Code', minWidth: 70 },
-  { id: 'population', label: 'Population', minWidth: 100, align: 'right' },
-  { id: 'size', label: 'Size (km²)', minWidth: 100, align: 'right' },
-  { id: 'density', label: 'Density', minWidth: 100, align: 'right' },
+  { id: "name", label: "Name", minWidth: 100 },
+  { id: "code", label: "ISO Code", minWidth: 70 },
+  { id: "population", label: "Population", minWidth: 100, align: "right" },
+  { id: "size", label: "Size (km²)", minWidth: 100, align: "right" },
+  { id: "density", label: "Density", minWidth: 100, align: "right" },
 ];
 
 // Sample data to display
 const data = [
-  ['India', 'IN', '1,366,417,754', '3,287,263', '464'],
-  ['China', 'CN', '1,398,715,000', '9,596,961', '146'],
-  ['United States', 'US', '331,883,986', '9,525,067', '35'],
-  ['Indonesia', 'ID', '267,026,366', '1,904,569', '140'],
-  ['Pakistan', 'PK', '216,565,318', '881,913', '245'],
+  ["India", "IN", "1,366,417,754", "3,287,263", "464"],
+  ["China", "CN", "1,398,715,000", "9,596,961", "146"],
+  ["United States", "US", "331,883,986", "9,525,067", "35"],
+  ["Indonesia", "ID", "267,026,366", "1,904,569", "140"],
+  ["Pakistan", "PK", "216,565,318", "881,913", "245"],
 ];
 
 const columnVisibility = {
@@ -29,16 +29,19 @@ const columnVisibility = {
 };
 
 const SistentComponentsPage = () => {
-  
-  return(
+
+  const updateColumns = (updatedColumns) => {
+    console.log("Updated Columns:", updatedColumns);
+  };
+  return (
     <>
-        <SistentComponents />
-        <ResponsiveDataTable
-          data={data}
-          columns={columns}
-          columnVisibility={columnVisibility}
-          updateCols={updateColumns}
-        />
+      <SistentComponents />
+      {/* <ResponsiveDataTable
+        data={data}
+        columns={columns}
+        columnVisibility={columnVisibility}
+        updateCols={updateColumns}
+      /> */}
     </>
   );
 };
