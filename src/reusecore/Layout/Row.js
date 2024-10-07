fc6831ecdb989d62648be791571377717d9fdc6a
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2024-10-07 13:47:48 +0530

    test
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>

--- src/reusecore/Layout/Row.js
+++ src/reusecore/Layout/Row.js
@@ -3,6 +3,7 @@ import styled, { css } from "styled-components";
 
 const RowWrapper = styled.div`
   display: flex;
+  flex-wrap: wrap;
   margin-left: -15px;
   margin-right: -15px;
   @media (max-width: 760px) {
