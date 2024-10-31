2f9d805c077537a222a90e237a0dc7b919a82dfb
Tharanishwaran <tharanishwaran.ta@gmail.com>
2024-10-31 23:48:28 +0530

    Reverted viewBox and applied inline styling
    Signed-off-by: Tharanishwaran <tharanishwaran.ta@gmail.com>

--- src/sections/General/Navigation/utility/KanvasIcon.js
+++ src/sections/General/Navigation/utility/KanvasIcon.js
@@ -6,44 +6,32 @@ function KanvasIcon() {
       width="24px"
       height="24px"
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 500 500"
+      viewBox="0 0 218.18 350.43"
     >
-      <defs>
-        <style>
-          {`
-            .cls-1 {
-              fill: #0bd0a7;
-            }
-            .cls-2 {
-              fill: #04b19d;
-            }
-          `}
-        </style>
-      </defs>
       <g id="Layer_2" data-name="Layer 2">
-        <g id="Layer_1-2" data-name="Layer 1">
+        <g id="Layer_1-2" data-name="Layer 1" transform="scale(0.7) translate(-100, 30)">
           <polygon
-            className="cls-1"
+            fill="#0bd0a7"
             points="261.82 30.1 261.82 228.75 433.99 129.07 261.82 30.1"
           />
           <polygon
-            className="cls-1"
+            fill="#0bd0a7"
             points="261.82 270.3 261.82 469.9 435.56 370.56 261.82 270.3"
           />
           <polygon
-            className="cls-2"
+            fill="#04b19d"
             points="237.03 227.38 237.03 31.77 66.97 129.25 237.03 227.38"
           />
           <polygon
-            className="cls-2"
+            fill="#04b19d"
             points="237.03 468.98 237.03 271.74 66.56 370.43 237.03 468.98"
           />
           <polygon
-            className="cls-2"
+            fill="#04b19d"
             points="447.37 348.75 447.37 149.97 275.4 249.52 447.37 348.75"
           />
           <polygon
-            className="cls-1"
+            fill="#0bd0a7"
             points="52.63 149.59 52.63 349.85 225.87 249.56 52.63 149.59"
           />
         </g>
