42d786faefdff0fd19dc4f8c9fe43be4260ba734
Vivek Vishal <vishalvivek488@gmail.com>
2024-11-05 08:45:15 +0530

    Update kanvas-visualize-banner.js
    Signed-off-by: Vivek Vishal <vishalvivek488@gmail.com>

--- src/sections/Kanvas/Kanvas-visualize/kanvas-visualize-banner.js
+++ src/sections/Kanvas/Kanvas-visualize/kanvas-visualize-banner.js
@@ -93,7 +93,7 @@ const KanvasisualizeBanner = () => {
         <div className="banner-text">
           <h1>Visualize your infrastructure </h1>
           <h4>See your designs <span>in action.</span> Operate with <span>best practices.</span></h4>
-          <KanvasBtn title="Explore Kanvasisualizer" />
+          <KanvasBtn title="Explore Kanvasoperator" />
         </div>
         {/* <div className="banner-image">
           <img src={BannerImage} alt="" />
