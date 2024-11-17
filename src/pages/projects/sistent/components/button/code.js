2f3e0ee6f2520fc0324e9cdbdb7b82200e72b724
Vidit Kushwaha <120644015+Vidit-Kushwaha@users.noreply.github.com>
2024-11-17 12:01:08 +0000

    Removed sistent componenets pages
    Signed-off-by: Vidit Kushwaha <120644015+Vidit-Kushwaha@users.noreply.github.com>

--- src/pages/projects/sistent/components/button/code.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ButtonCode } from "../../../../../sections/Projects/Sistent/components/button/code";
-
-const ButtonCodePage = () => {
-  return <ButtonCode />;
-};
-
-export default ButtonCodePage;
--- src/pages/projects/sistent/components/button/guidance.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ButtonGuidance } from "../../../../../sections/Projects/Sistent/components/button/guidance";
-
-const ButtonGuidancePage = () => {
-  return <ButtonGuidance />;
-};
-
-export default ButtonGuidancePage;
