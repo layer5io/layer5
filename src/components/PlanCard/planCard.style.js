27eef36904c9c5d29bb8a7b6ccbf8ec0e85f31a7
Lee Calcote <lee.calcote@layer5.io>
2024-12-04 20:39:21 -0600

    [UI]: Update PlanCard layout and styles for better responsiveness and visual clarity
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- src/components/PlanCard/planCard.style.js
+++ src/components/PlanCard/planCard.style.js
@@ -5,49 +5,58 @@ const PlanCardWrapper = styled.section`
   .pricing-container {
     display: flex;
     flex-wrap: wrap;
-    gap: 20px;
+    gap: 22rem;
     justify-content: space-between;
     align-items: flex-start;
   }
   .pricing-card {
-    /* display: flex;
-    flex: 1 1 calc(25% - 20px);  */
+    display: flex;
     text-align: left; /* Align all content to the left */
     padding: 20px;
     width: 100%;
     border: 1px solid #ccc;
     border-radius: 10px;
     background-color: #f9f9f9;
+    gap: 22rem;
   }
   .price-container {  
+    transform: scale(1.15);
+    align-items: baseline;
     .price {
       display: flex;
       align-items: baseline;
-      gap: 4px; /* Adjust space between elements */
       font-weight: bold;
-      color: #000;
+      color: ${(props) => props.theme.whiteToBlack}; 
       white-space: nowrap; /* Prevent line breaks in price text */
       justify-content: center;
     }
     .currency-symbol {
-      font-size: 1.2em;
+      font-size: 1rem;
       align-self: flex-start;
-      font-weight: 400;
+      font-weight: thin;
       color: ${(props) => props.theme.primaryColor}; 
     }
-    .price-value {
-      font-size: 2.5rem; 
+    .price-amount {
+      font-size: 3rem; 
       color: ${(props) => props.theme.primaryColor};
-    }
-    .price-text {
-      font-size: 1em;
-      font-weight: 400;
-      color: ${(props) => props.theme.primaryColor}; 
-    .usd-sup {
-        font-size: 0.6em; 
-        vertical-align: super; 
-        margin-bottom: .5rem;
+      > sup {
+        font-weight: 200;
+        font-size: 1rem;
       }
+    }
+    .currency {
+      font-size: .65rem;
+      font-weight: normal;
+      color: ${(props) => props.theme.primaryColor}; 
+      align-content: baseline;
+    }
+    .price-per {
+      margin-left: .25rem;
+      align-self: center;
+      font-weight: 200;
+      font-size: .75rem;
+      letter-spacing: -.025rem;
+      color: ${(props) => props.theme.primaryColor}; 
     }
   }
   .pricing-button {
@@ -60,35 +69,30 @@ const PlanCardWrapper = styled.section`
     cursor: pointer;
     &:hover {background-color: #0056b3;}
   }
-  .featured {
-    transform: scale(1.05);
-    background-color: ${(props) => props.theme.grey212121ToWhite};
-    box-shadow: 0px 2px 12px 0px rgb(0, 179, 159, 0.7);
-    transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-    &:hover { box-shadow: 0px 2px 16px rgb(235, 192, 23, 0.9); }
-  }
   .pricing-table {
     width: 100%;
+    min-width: 250px;
     background-color: ${(props) => props.theme.grey212121ToWhite};
     box-shadow: 0px 2px 16px rgba(0, 0, 0, 0.1);
     padding: 2rem;
     border-radius: 4px;
     transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
     margin-bottom: 2rem;
+    flex: 1 1 calc(25% - 20px); /* 4 columns */
     &:hover {
       box-shadow: 0px 1px 10px -4px rgba(0, 0, 0, 0.15);
-    }  
+    }
     .pricing-label {
       border-radius: 0px 4px;
-      background: ${(props) => props.theme.grey1D1817ToGreyE6E6E6};
+      background: ${(props) => props.theme.grey050505ToGreyF1F2F1};
       padding: 0.125rem 0.5rem;
       position: absolute;
       width: fit-content;
       font-size: 0.75rem;
       font-weight: 500;
       top: 0rem;
-      right: .95rem;
-      color: ${(props) => props.theme.whiteEighty};
+      right: .9rem;
+      color: ${(props) => props.theme.greyDEE3DEToGrey363636};
       transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
     }
     h6 {
@@ -105,22 +109,32 @@ const PlanCardWrapper = styled.section`
       transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
     }
   }
-    .byline {
-      color: ${(props) => props.theme.greyC8C8C8ToGreen3C494F};
-      margin-top: -.5rem;
-      margin-bottom: 1.5rem;
-      font-size: .9rem;
-      font-weight: 400;
-      transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-      justify-content: center;
-      text-align: center;
-    }
-    .pricing-features {
-      margin-top: 2rem;
-      line-height: 1rem;
+  .featured {
+    /* transform: scale(1.05); */
+    /* border: .5rem solid ${(props) => props.theme.whiteToBlack}; */
+    background-color: ${(props) => props.theme.grey212121ToWhite};
+    box-shadow: 0px 2px 12px 0px rgb(0, 179, 159, 0.7);
+    transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+    &:hover { box-shadow: 0px 2px 16px rgb(235, 192, 23, 0.9); }
+  }
+  .byline {
+    /* color: ${(props) => props.theme.greyC8C8C8ToGreen3C494F}; */
+    color: ${(props) => props.theme.blueE0FFFCToBlue477E96};
+    margin-top: -.5rem;
+    margin-bottom: 2.5rem;
+    font-size: .9rem;
+    font-weight: 400;
+    transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+    justify-content: center;
+    text-align: center;
+  }
+  .pricing-features {
+    margin-top: 2rem;
+    line-height: 1rem;
     .feature {
       margin: 0rem 0rem;
       line-height: 1rem;
+      font-weight: bold;
       .details {
         display: block;
         height: auto;
@@ -138,54 +152,54 @@ const PlanCardWrapper = styled.section`
         color: ${(props) => props.theme.greyD3D7DBToGreen1E2117};
         display: block;
         font-size: 1rem;
-        font-weight: 400;
+        font-weight: bold;
         transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
       }
-      .check {
-        padding-right: 0.2rem;
-        vertical-align: middle;
-        color: ${(props) => props.theme.secondaryColor};
+        .check {
+          padding-right: 0.2rem;
+          vertical-align: middle;
+          color: ${(props) => props.theme.secondaryColor};
+        }
       }
+  }
+  .price-tag {
+    margin-top: 2rem;
+    text-align: center;
+    font-weight: 500;
+    color: ${(props) => props.theme.secondaryColor};
+    &.symbol {
+      font-size: 24px;
+    }
+    &.amount {
+      letter-spacing: -2px;
+      font-size: 3rem;
+    }
+    &.free {
+      font-size: 40px;
+    }
+    &.after {
+    color: #3b3b3b;
+    font-weight: 500;
     }
   }
-    .price-tag {
-      margin-top: 2rem;
-      text-align: center;
-      font-weight: 500;
-      color: ${(props) => props.theme.secondaryColor};
-      &.symbol {
-        font-size: 24px;
-      }
-      &.amount {
-        letter-spacing: -2px;
-        font-size: 3rem;
-      }
-      &.free {
-        font-size: 40px;
-      }
-      &.after {
-      color: #3b3b3b;
-      font-weight: 500;
-     }
+.price-button-disabled {
+    background: rgb(0, 179, 158, 0.6);
+    &:hover {
+      cursor: default;
+      box-shadow: none;
     }
-    .price-button-disabled {
-      background: rgb(0, 179, 158, 0.6);
-      &:hover {
-        cursor: default;
-        box-shadow: none;
-      }
-    }
-    .price-button-link {
-      color: #fff;
-      display: block;
-      margin: 2rem auto 0;
-      padding: 1rem 2rem;
-      width: 100%;
-      text-align: center;
-      font-weight: 500;
-      transition: 0.3s;
-      background: ${(props) => props.theme.secondaryColor};
-    }
+  }
+.price-button-link {
+  color: #fff;
+  display: block;
+  margin: 2rem auto 0;
+  padding: 1rem 2rem;
+  width: 100%;
+  text-align: center;
+  font-weight: 500;
+  transition: 0.3s;
+  background: ${(props) => props.theme.secondaryColor};
+}
 `;
 
 export default PlanCardWrapper;
