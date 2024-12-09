0787dd5253a0764b217c51c2869aeca3e34a8d69
Shivam Gupta <sudhanshudasgupta9@gmail.com>
2024-12-09 11:28:54 +0530

    Update servicemeshpatterns.js
    Signed-off-by: Shivam Gupta <sudhanshudasgupta9@gmail.com>

--- src/sections/Company/Brand/Brand-components/servicemeshpatterns.js
+++ src/sections/Company/Brand/Brand-components/servicemeshpatterns.js
@@ -79,8 +79,8 @@ const ServiceMeshPatterns = () => {
           </p>
         </Col>
         <Row style={{
-            flexWrap: "wrap"
-          }} className="color-code-wrapper">
+          flexWrap: "wrap"
+        }} className="color-code-wrapper">
           <ColorBox name="Charcoal" R="60" G="73" B="79" colorCode="#3C494F" />
           <ColorBox name="Keppel" R="0" G="179" B="159" colorCode="#00B39F" />
           <ColorBox name="Caribbean Green" R="0" G="211" B="169" colorCode="#00D3A9" />
