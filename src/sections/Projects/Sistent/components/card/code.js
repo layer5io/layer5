0a39a84ee570ba736b6137c11b907d684455227d
Anand-Theertha <anandtheerthav3@gmail.com>
2025-01-05 16:33:26 +0000

    Review Updates
    Signed-off-by: Anand-Theertha <anandtheerthav3@gmail.com>

--- src/sections/Projects/Sistent/components/card/code.js
+++ src/sections/Projects/Sistent/components/card/code.js
@@ -2,7 +2,15 @@ import React from "react";
 import { navigate } from "gatsby";
 import { useLocation } from "@reach/router";
 
-import { SistentThemeProvider, Card, CardActions, CardContent, CardMedia , Typography, Button } from "@layer5/sistent";
+import {
+  SistentThemeProvider,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+  Button,
+} from "@layer5/sistent";
 import { CodeBlock } from "../button/code-block";
 import { SistentLayout } from "../../sistent-layout";
 
@@ -13,154 +21,82 @@ import TabButton from "../../../../../reusecore/Button";
 import { useStyledDarkMode } from "../../../../../theme/app/useStyledDarkMode";
 
 const codes = [
-  `const cardOutlined = (
-    <React.Fragment>
+  `<Card variant="outlined">
+    <CardContent>
+      <Typography
+        gutterBottom
+        sx={{ color: "#9d9d9d", fontSize: 15 }}
+      >
+        Company Spotlight
+      </Typography>
+      <Typography variant="h5" component="div">
+        Layer5
+      </Typography>
+      <Typography sx={{ color: "#9d9d9d", fontSize: 15, mb: 1.5 }}>
+        Platform
+      </Typography>
+      <Typography variant="body">
+        Empowering engineers with cloud-native management solutions.
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small" variant="outlined">
+        Learn More
+      </Button>
+    </CardActions>
+  </Card>`,
+
+  `<Card sx={{ maxWidth: 345 }} variant="outlined">
+    <CardActions>
+      <CardMedia
+        component="img"
+        height="80"
+        image={MesheryLogo}
+        alt="Meshery"
+      />
       <CardContent>
-        <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
-          Company Spotlight
+        <Typography gutterBottom variant="h5" component="div">
+          Meshery
         </Typography>
-        <Typography variant="h5" component="div">
-          Layer5
-        </Typography>
-        <Typography sx={{ color: "text.secondary", fontSize: 14 , mb: 1.5 }}>Platform</Typography>
-        <Typography variant="body2">
-          Empowering engineers with cloud-native management solutions.
+        <Typography variant="body" sx={{ color: "#9d9d9d" }}>
+          Meshery is an open-source cloud-native management platform
+          designed to help users manage, monitor, and optimize the
+          performance of service meshes and their workloads.
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </React.Fragment>
-  );
-  
-  <Card variant="outlined">{cardOutlined}</Card>
-  `,
+    </CardActions>
+  </Card>`,
 
-  `const cardMainAction1 = (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActions>
-        <CardMedia
-          component="img"
-          height="90"
-          image={MesheryLogo}
-          alt="Meshery"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Meshery
-          </Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-          Meshery is an open-source cloud-native management platform. 
-          </Typography>
-        </CardContent>
-      </CardActions>
-    </Card>
-  );
-  
-  <Card variant="outlined">{cardMainAction1}</Card>
-  `,
-
-  `
-  const cardMainAction2 = (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActions>
-        <CardMedia
-          component="img"
-          height="80"
-          image={KanvasLogo}
-          alt="Kanvas"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Kanvas
-          </Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            Kanvas is a collaborative design tool.
-          </Typography>
-        </CardContent>
-      </CardActions>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-      </CardActions>
-    </Card>
-  );
-
-  <Card variant="outlined">{cardMainAction2}</Card>
-  `,
+  `<Card sx={{ maxWidth: 345 }} variant="outlined">
+    <CardActions>
+      <CardMedia
+        component="img"
+        height="90"
+        image={KanvasLogo}
+        alt="Kanvas"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Kanvas
+        </Typography>
+        <Typography variant="body" sx={{ color: "#9d9d9d" }}>
+          Kanvas is a collaborative design tool that allows users to
+          create, edit, and manage visual assets seamlessly within a
+          shared workspace.
+        </Typography>
+      </CardContent>
+    </CardActions>
+    <CardActions>
+      <Button size="small" color="primary" variant="outlined">
+        Share
+      </Button>
+    </CardActions>
+  </Card>`,
 ];
 
 const CardCode = () => {
   const location = useLocation();
   const { isDark } = useStyledDarkMode();
-
-  const cardOutlined = (
-    <React.Fragment>
-      <CardContent>
-        <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
-          Company Spotlight
-        </Typography>
-        <Typography variant="h5" component="div">
-          Layer5
-        </Typography>
-        <Typography sx={{ color: "text.secondary", fontSize: 14 , mb: 1.5 }}>Platform</Typography>
-        <Typography variant="body2">
-          Empowering engineers with cloud-native management solutions.
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </React.Fragment>
-  );
-
-  const cardMainAction1 = (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActions>
-        <CardMedia
-          component="img"
-          height="80"
-          image={MesheryLogo}
-          alt="Meshery"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Meshery
-          </Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-          Meshery is an open-source cloud-native management platform designed to help users manage, monitor, and optimize the performance of service meshes and their workloads.
-          </Typography>
-        </CardContent>
-      </CardActions>
-    </Card>
-  );
-
-  const cardMainAction2 = (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActions>
-        <CardMedia
-          component="img"
-          height="90"
-          image={KanvasLogo}
-          alt="Kanvas"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Kanvas
-          </Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            Kanvas is a collaborative design tool that allows users to create, edit, and manage visual assets seamlessly within a shared workspace.
-          </Typography>
-        </CardContent>
-      </CardActions>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-      </CardActions>
-    </Card>
-  );
 
   return (
     <SistentLayout title="Card">
@@ -168,7 +104,13 @@ const CardCode = () => {
         <a id="Identity">
           <h2>Card</h2>
         </a>
-        <p>The Card component is a UI element used to encapsulate information or actions related to a single topic, typically in a visually separated, easily digestible format. It often includes elements like images, text, and buttons to present content and enable quick interactions without navigating away from the card.</p>
+        <p>
+          The Card component is a UI element used to encapsulate information or
+          actions related to a single topic, typically in a visually separated,
+          easily digestible format. It often includes elements like images,
+          text, and buttons to present content and enable quick interactions
+          without navigating away from the card.
+        </p>
         <div className="filterBtns">
           <TabButton
             className={
@@ -181,8 +123,7 @@ const CardCode = () => {
           />
           <TabButton
             className={
-              location.pathname ===
-              "/projects/sistent/components/card/guidance"
+              location.pathname === "/projects/sistent/components/card/guidance"
                 ? "active"
                 : ""
             }
@@ -193,31 +134,60 @@ const CardCode = () => {
           />
           <TabButton
             className={
-              location.pathname ===
-              "/projects/sistent/components/card/code"
+              location.pathname === "/projects/sistent/components/card/code"
                 ? "active"
                 : ""
             }
-            onClick={() =>
-              navigate("/projects/sistent/components/card/code")
-            }
+            onClick={() => navigate("/projects/sistent/components/card/code")}
             title="Code"
           />
         </div>
         <div className="main-content">
           <p>
-          A card often lets users interact with its entire surface to trigger its main function, whether it’s expanding, navigating to another screen, or activating some other behavior.
+            A card often lets users interact with its entire surface to trigger
+            its main function, whether it’s expanding, navigating to another
+            screen, or activating some other behavior.
           </p>
           <a id="Outlined Card">
             <h2>Oultined Card Example</h2>
           </a>
           <p>
-          An outlined card is a variation of the standard card component, often used to provide a lighter, more minimalist design. Instead of a solid background or shadow, an outlined card uses a border to define its boundaries, giving it a cleaner and more refined appearance.
+            An outlined card is a variation of the standard card component,
+            often used to provide a lighter, more minimalist design. Instead of
+            a solid background or shadow, an outlined card uses a border to
+            define its boundaries, giving it a cleaner and more refined
+            appearance.
           </p>
           <div className="showcase">
             <div className="items">
               <SistentThemeProvider initialMode={isDark ? "dark" : "light"}>
-                <Card variant="outlined">{cardOutlined}</Card>
+                <Card variant="outlined">
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      sx={{ color: "#9d9d9d", fontSize: 15 }}
+                    >
+                      Company Spotlight
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                      Layer5
+                    </Typography>
+                    <Typography
+                      sx={{ color: "#9d9d9d", fontSize: 15, mb: 1.5 }}
+                    >
+                      Platform
+                    </Typography>
+                    <Typography variant="body">
+                      Empowering engineers with cloud-native management
+                      solutions.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" variant="outlined">
+                      Learn More
+                    </Button>
+                  </CardActions>
+                </Card>
               </SistentThemeProvider>
             </div>
             <CodeBlock name="container-fluid-sm" code={codes[0]} />
@@ -228,24 +198,74 @@ const CardCode = () => {
               <h2>Main Action Cards</h2>
             </a>
             <p>
-          This type of interactive card is well-suited for use in applications where quick access to more information or actions is important, such as dashboards, or product catalogs. For example, ont the website, users can tap anywhere on the card to open the full product page.
+              This type of interactive card is well-suited for use in
+              applications where quick access to more information or actions is
+              important, such as dashboards, or product catalogs. For example,
+              ont the website, users can tap anywhere on the card to open the
+              full product page.
             </p>
             <div className="showcase">
               <div className="items">
                 <SistentThemeProvider initialMode={isDark ? "dark" : "light"}>
-                  <Card variant="outlined">{cardMainAction1}</Card>
+                  <Card sx={{ maxWidth: 345 }} variant="outlined">
+                    <CardActions>
+                      <CardMedia
+                        component="img"
+                        height="80"
+                        image={MesheryLogo}
+                        alt="Meshery"
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          Meshery
+                        </Typography>
+                        <Typography variant="body" sx={{ color: "#9d9d9d" }}>
+                          Meshery is an open-source cloud-native management
+                          platform designed to help users manage, monitor, and
+                          optimize the performance of service meshes and their
+                          workloads.
+                        </Typography>
+                      </CardContent>
+                    </CardActions>
+                  </Card>
                 </SistentThemeProvider>
               </div>
               <CodeBlock name="Container-fluid-md" code={codes[1]} />
             </div>
           </div>
           <p>
-          Cards can also include secondary actions, which should be positioned away from the primary action area to avoid unintentional interaction conflicts.
+            Cards can also include secondary actions, which should be positioned
+            away from the primary action area to avoid unintentional interaction
+            conflicts.
           </p>
           <div className="showcase">
             <div className="items">
               <SistentThemeProvider initialMode={isDark ? "dark" : "light"}>
-                <Card variant="outlined">{cardMainAction2}</Card>
+                <Card sx={{ maxWidth: 345 }} variant="outlined">
+                  <CardActions>
+                    <CardMedia
+                      component="img"
+                      height="90"
+                      image={KanvasLogo}
+                      alt="Kanvas"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Kanvas
+                      </Typography>
+                      <Typography variant="body" sx={{ color: "#9d9d9d" }}>
+                        Kanvas is a collaborative design tool that allows users
+                        to create, edit, and manage visual assets seamlessly
+                        within a shared workspace.
+                      </Typography>
+                    </CardContent>
+                  </CardActions>
+                  <CardActions>
+                    <Button size="small" color="primary" variant="outlined">
+                      Share
+                    </Button>
+                  </CardActions>
+                </Card>
               </SistentThemeProvider>
             </div>
             <CodeBlock name="Container-fixed" code={codes[2]} />
--- src/sections/Projects/Sistent/components/card/index.js
+++ src/sections/Projects/Sistent/components/card/index.js
@@ -4,7 +4,15 @@ import { useLocation } from "@reach/router";
 import MesheryLogo from "../../../../../assets/images/meshery/meshery-logo-light-text-side.webp";
 import KanvasLogo from "../../../../../assets/images/kanvas/icon-only/kanvas-icon-color.png";
 
-import { SistentThemeProvider, Card, CardActions, CardContent, CardMedia , Typography, Button } from "@layer5/sistent";
+import {
+  SistentThemeProvider,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+  Button,
+} from "@layer5/sistent";
 import TabButton from "../../../../../reusecore/Button";
 import { SistentLayout } from "../../sistent-layout";
 import { Row } from "../../../../../reusecore/Layout";
@@ -14,80 +22,18 @@ const SistentCard = () => {
   const location = useLocation();
   const { isDark } = useStyledDarkMode();
 
-  const cardOutlined = (
-    <React.Fragment>
-      <CardContent>
-        <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
-          Company Spotlight
-        </Typography>
-        <Typography variant="h5" component="div">
-          Layer5
-        </Typography>
-        <Typography sx={{ color: "text.secondary", fontSize: 14 , mb: 1.5 }}>Platform</Typography>
-        <Typography variant="body2">
-          Empowering engineers with cloud-native management solutions.
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </React.Fragment>
-  );
-
-  const cardMainAction1 = (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActions>
-        <CardMedia
-          component="img"
-          height="80"
-          image={MesheryLogo}
-          alt="Meshery"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Meshery
-          </Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-          Meshery is an open-source cloud-native management platform designed to help users manage, monitor, and optimize the performance of service meshes and their workloads.
-          </Typography>
-        </CardContent>
-      </CardActions>
-    </Card>
-  );
-
-  const cardMainAction2 = (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActions>
-        <CardMedia
-          component="img"
-          height="90"
-          image={KanvasLogo}
-          alt="Kanvas"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Kanvas
-          </Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            Kanvas is a collaborative design tool that allows users to create, edit, and manage visual assets seamlessly within a shared workspace.
-          </Typography>
-        </CardContent>
-      </CardActions>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-      </CardActions>
-    </Card>
-  );
   return (
     <SistentLayout title="Card">
       <div className="content">
         <a id="Identity">
           <h2>Card</h2>
         </a>
-        <p>The Card component is a UI element used to encapsulate information or actions related to a single topic, typically in a visually separated, easily digestible format. It often includes elements like images, text, and buttons to present content and enable quick interactions without navigating away from the card.
-
+        <p>
+          The Card component is a UI element used to encapsulate information or
+          actions related to a single topic, typically in a visually separated,
+          easily digestible format. It often includes elements like images,
+          text, and buttons to present content and enable quick interactions
+          without navigating away from the card.
         </p>
         <div className="filterBtns">
           <TabButton
@@ -101,8 +47,7 @@ const SistentCard = () => {
           />
           <TabButton
             className={
-              location.pathname ===
-              "/projects/sistent/components/card/guidance"
+              location.pathname === "/projects/sistent/components/card/guidance"
                 ? "active"
                 : ""
             }
@@ -113,55 +58,136 @@ const SistentCard = () => {
           />
           <TabButton
             className={
-              location.pathname ===
-              "/projects/sistent/components/card/code"
+              location.pathname === "/projects/sistent/components/card/code"
                 ? "active"
                 : ""
             }
-            onClick={() =>
-              navigate("/projects/sistent/components/card/code")
-            }
+            onClick={() => navigate("/projects/sistent/components/card/code")}
             title="Code"
           />
         </div>
         <div className="main-content">
           <p>
-            The card component is a versatile UI element commonly used in web and mobile applications. It's designed to showcase content in a clean, concise, and visually organized manner. Typically, a card groups related information, making it easy for users to digest and interact with. It often includes an image or icon, title, description, and actionable buttons or links, all contained within a defined boundary that makes it visually distinct.
+            The card component is a versatile UI element commonly used in web
+            and mobile applications. It's designed to showcase content in a
+            clean, concise, and visually organized manner. Typically, a card
+            groups related information, making it easy for users to digest and
+            interact with. It often includes an image or icon, title,
+            description, and actionable buttons or links, all contained within a
+            defined boundary that makes it visually distinct.
           </p>
           <a id="Types of Cards">
             <h2>Types of Cards</h2>
           </a>
-          <p>
-          </p>
+          <p></p>
           <h3>Outlined</h3>
           <p>
-          An outlined card is a variation of the standard card component, often used to provide a lighter, more minimalist design. Instead of a solid background or shadow, an outlined card uses a border to define its boundaries, giving it a cleaner and more refined appearance.
+            An outlined card is a variation of the standard card component,
+            often used to provide a lighter, more minimalist design. Instead of
+            a solid background or shadow, an outlined card uses a border to
+            define its boundaries, giving it a cleaner and more refined
+            appearance.
           </p>
           <Row $Hcenter className="image-container">
             <SistentThemeProvider initialMode={isDark ? "dark" : "light"}>
-              <Card variant="outlined">{cardOutlined}</Card>
+              <Card variant="outlined">
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    sx={{ color: "#9d9d9d", fontSize: 15 }}
+                  >
+                    Company Spotlight
+                  </Typography>
+                  <Typography variant="h5" component="div">
+                    Layer5
+                  </Typography>
+                  <Typography sx={{ color: "#9d9d9d", fontSize: 15, mb: 1.5 }}>
+                    Platform
+                  </Typography>
+                  <Typography variant="body">
+                    Empowering engineers with cloud-native management solutions.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" variant="outlined">
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
             </SistentThemeProvider>
           </Row>
 
           <h3>Main Action</h3>
-          <p>1. A card often lets users interact with its entire surface to trigger its main function, whether it’s expanding, navigating to another screen, or activating some other behavior.</p>
-          <Row $Hcenter className="image-container">
-            <SistentThemeProvider initialMode={isDark ? "dark" : "light"}>
-              <Card variant="outlined">{cardMainAction1}</Card>
-            </SistentThemeProvider>
-          </Row>
-
           <p>
-          2. Cards can also include secondary actions, which should be positioned away from the primary action area to avoid unintentional interaction conflicts.
+            1. A card often lets users interact with its entire surface to
+            trigger its main function, whether it’s expanding, navigating to
+            another screen, or activating some other behavior.
           </p>
           <Row $Hcenter className="image-container">
             <SistentThemeProvider initialMode={isDark ? "dark" : "light"}>
-              <Card variant="outlined">{cardMainAction2}</Card>
+              <Card sx={{ maxWidth: 345 }} variant="outlined">
+                <CardActions>
+                  <CardMedia
+                    component="img"
+                    height="80"
+                    image={MesheryLogo}
+                    alt="Meshery"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Meshery
+                    </Typography>
+                    <Typography variant="body" sx={{ color: "#9d9d9d" }}>
+                      Meshery is an open-source cloud-native management platform
+                      designed to help users manage, monitor, and optimize the
+                      performance of service meshes and their workloads.
+                    </Typography>
+                  </CardContent>
+                </CardActions>
+              </Card>
             </SistentThemeProvider>
           </Row>
 
           <p>
-          NOTE: Cards may offer multiple actions, UI elements, and an overflow menu, but it’s important to exercise restraint, as they are primarily designed to serve as entry points to deeper and more comprehensive content.
+            2. Cards can also include secondary actions, which should be
+            positioned away from the primary action area to avoid unintentional
+            interaction conflicts.
+          </p>
+          <Row $Hcenter className="image-container">
+            <SistentThemeProvider initialMode={isDark ? "dark" : "light"}>
+              <Card sx={{ maxWidth: 345 }} variant="outlined">
+                <CardActions>
+                  <CardMedia
+                    component="img"
+                    height="90"
+                    image={KanvasLogo}
+                    alt="Kanvas"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Kanvas
+                    </Typography>
+                    <Typography variant="body" sx={{ color: "#9d9d9d" }}>
+                      Kanvas is a collaborative design tool that allows users to
+                      create, edit, and manage visual assets seamlessly within a
+                      shared workspace.
+                    </Typography>
+                  </CardContent>
+                </CardActions>
+                <CardActions>
+                  <Button size="small" color="primary" variant="outlined">
+                    Share
+                  </Button>
+                </CardActions>
+              </Card>
+            </SistentThemeProvider>
+          </Row>
+
+          <p>
+            NOTE: Cards may offer multiple actions, UI elements, and an overflow
+            menu, but it’s important to exercise restraint, as they are
+            primarily designed to serve as entry points to deeper and more
+            comprehensive content.
           </p>
         </div>
       </div>
