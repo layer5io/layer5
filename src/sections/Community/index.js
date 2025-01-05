9aefa6bdf1ee32a43574c255dc0c1c6b7f4575d4
Shivam Gupta <sudhanshudasgupta9@gmail.com>
2025-01-05 05:55:45 +0000

    re add slider
    Signed-off-by: Shivam Gupta <sudhanshudasgupta9@gmail.com>
    Signed-off-by: Jaishree2310 <jaishrees23102001@gmail.com>

--- src/sections/Community/index.js
+++ src/sections/Community/index.js
@@ -18,7 +18,7 @@ import { BgImage } from "gbimage-bridge";
 import useHasMounted from "../../utils/useHasMounted";
 import InlineQuotes from "../../components/Inline-quotes";
 import AdventuresCallout from "../Adventures-Callout";
-// import PictureSlider from "./slider";
+import PictureSlider from "./slider";
 
 const CommunityMember = "./Community-pictures/five.svg";
 const Picture = "./Community-pictures/join-the-community.png";
@@ -101,7 +101,7 @@ const CommunityPage = () => {
             </Col>
             <Col className="slider" $sm={12} $lg={6}>
               <StaticImage src={Picture} alt="Five_with_banner" />
-              {/* <PictureSlider /> */}
+              <PictureSlider />
             </Col>
           </Row>
         </div>
