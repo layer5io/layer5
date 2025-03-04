608ace2b8ddaec7a53f764718a24b6c74fe51ba0
Touriist <126756092+Touriist@users.noreply.github.com>
2025-03-04 23:32:57 +0530

    Update JourneySection.js
    Signed-off-by: Touriist <126756092+Touriist@users.noreply.github.com>

--- src/sections/Devrel-platform/JourneySection.js
+++ src/sections/Devrel-platform/JourneySection.js
@@ -187,7 +187,7 @@ const JourneySection = () => {
       <Container>
         <div className="journey-header">
           <h2>Expand Your Network and Visibility in the Cloud Native Community</h2>
-          <p>Connect with ambassadors, maintainers, and experts while collaborating on CNCF projects.</p>
+          <p>Connect with ambassadors, developer evangelists, maintainers, and experts while collaborating on CNCF projects.</p>
         </div>
         <div className="timeline-container">
           <Row className="timeline-row">
