10d650e71917766216549d445ae0cb01ea637122
Vivek Vishal <vishalvivek488@gmail.com>
2025-03-04 22:24:39 +0530

    update
    Signed-off-by: Vivek Vishal <vishalvivek488@gmail.com>

--- src/sections/Devrel-platform/content.js
+++ src/sections/Devrel-platform/content.js
@@ -284,7 +284,7 @@ const DevrelContent = () => {
               <h2 className="heading">Build Your Professional Brand</h2>
               <ul className="caption">
                 <li><b>Public User Profile:</b>  All your published designs and reference architectures are linked to your profile, with real-time stats to track their reach and impact.</li>
-                <li><b>Showcase Your Metrics and Leaderboard Position: </b>Track your content's performance with analytics, leaderboard rankings, and engagement metrics. Assess what's resonating with your audience and refine your advocacy strategy accordingly.</li>
+                <li><b>Showcase Your Metrics and Leaderboard Position: </b>Track your content's performance with analytics, leaderboard rankings, and engagement metrics. Assess what's resonating with your audience and refine your developer advocacy strategy accordingly.</li>
                 <li><b>Themed Organization:</b> Create a personalized dashboard with your organization's colors and logo, and invite members.</li>
                 <li><b>Dedicated Workspace:</b> Maintain a centralized environment for all your content and resources.</li>
               </ul>
@@ -330,7 +330,7 @@ const DevrelContent = () => {
           <Row className="devrel">
             <InlineQuotes
               person="Yash Sharma"
-              title="Engineering at DigitalOcean"
+              title="Layer5 MeshMate & Developer Advocate at DigitalOcean"
               quote="One of my published designs is a top performer in the catalog, opening up new opportunities for me."
               image={YashSharmaIMg}/>
           </Row>
