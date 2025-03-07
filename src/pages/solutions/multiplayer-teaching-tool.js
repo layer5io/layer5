cc323a7fcc5b4f94e4bdd14e60d96485991a860d
Vivek Vishal <vishalvivek488@gmail.com>
2025-03-07 21:07:18 +0530

    Create multiplayer-teaching-tool.js
    Signed-off-by: Vivek Vishal <vishalvivek488@gmail.com>

---
+++ src/pages/solutions/multiplayer-teaching-tool.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import SEO from "../../components/seo";
+import MultiplayerTeachingPage from "../../sections/muliplayer-teaching-tool";
+
+const MultiplayerTeaching = () => {
+  return (
+    <>
+      <MultiplayerTeachingPage />
+
+    </>
+  );
+};
+export default MultiplayerTeaching;
+export const Head = () => {
+  return <SEO title="Multi-Player Cloud Native Teaching and Collaboration Platform"
+    description="Empowering content creators, instructors, and trainers with a real-time, interactive environment for cloud-native education."
+    image="/images/meshery-logo-dark-text.webp" />;
+};
