2c7e691f8728959354252f02ce9dfa3b16550939
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2025-05-04 17:09:52 +0530

    fix: rename Service Mesh Workshops to Cloud Native workshops
    Signed-off-by: M-DEV-1 <mahadevankizhakkedathu@gmail.com>

--- src/pages/learn/cloud-native-workshops.js
+++ src/pages/learn/cloud-native-workshops.js
@@ -13,5 +13,5 @@ const WorkshopsGridPage = () => {
 };
 export default WorkshopsGridPage;
 export const Head = () => {
-  return <SEO title="Service Mesh Workshops" description="Service mesh workshops and tutorials for Istio, Envoy, Linkerd, Consul, App Mesh, Open Service Mesh, Cilium, Kuma, NGINX" />;
+  return <SEO title="Cloud Native Workshops" description="Cloud Native workshops and tutorials for Istio, Envoy, Linkerd, Consul, App Mesh, Open Service Mesh, Cilium, Kuma, NGINX" />;
 };
