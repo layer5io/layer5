f3ceb6531566817d6876cb06f572e464f2d2151f
Lee Calcote <lee.calcote@layer5.io>
2025-05-14 17:29:18 -0500

    Performance: Reduce layout shifts
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- src/components/image.js
+++ src/components/image.js
@@ -6,26 +6,26 @@ const Image = ({ childImageSharp, extension, publicURL, alt, imgStyle, ...rest }
   if (!childImageSharp && extension === "svg") {
     return (
       <div className="old-gatsby-image-wrapper" style={{ width: "100%", height: "auto", aspectRatio: "16/9" }}>
-        <img 
-          src={publicURL} 
-          alt={alt || "Blog image"} 
-          width="100%" 
+        <img
+          src={publicURL}
+          alt={alt || "Blog image"}
+          width="100%"
           height="auto"
-          style={{ 
-            aspectRatio: "16/9", 
+          style={{
+            aspectRatio: "16/9",
             objectFit: imgStyle?.objectFit || "cover",
-            ...imgStyle 
-          }} 
+            ...imgStyle
+          }}
         />
       </div>
     );
   }
 
   return <GatsbyImage
-    key={publicURL} 
-    image={childImageSharp?.gatsbyImageData} 
+    key={publicURL}
+    image={childImageSharp?.gatsbyImageData}
     alt={alt || "Blog image"}
-    {...rest} 
+    {...rest}
   />;
 };
 
