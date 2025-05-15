5d049b840712ed2884dd12d8354464e1c17dc1fe
Lee Calcote <lee.calcote@layer5.io>
2025-05-15 11:40:58 -0500

    enable webpack bundle analyzer when not in production
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
       options: {
-        disable: true
+        disable: process.env.NODE_ENV !== "production",
       }
     },
     {
