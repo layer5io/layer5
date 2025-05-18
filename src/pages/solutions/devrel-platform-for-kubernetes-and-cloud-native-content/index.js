45b4dcfa2a5f9ca8b71050fe8c7217211e04a802
Lee Calcote <lee.calcote@layer5.io>
2025-05-18 17:40:33 -0500

    Refactor code structure for improved readability and maintainability
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- src/pages/solutions/devrel-platform-for-kubernetes-and-cloud-native-content/index.js
+++ src/pages/solutions/devrel-platform-for-kubernetes-and-cloud-native-content/index.js
@@ -2,6 +2,9 @@ import React from "react";
 
 import SEO from "../../../components/seo";
 import DevrelSolutionsPage from "../../../sections/Devrel-platform";
+// import {openGraphImage} from "../../../sections/Devrel-platform/Devrel-platform.style";
+import { openGraphImage } from "./solutions-devrel.png";
+
 
 const DevrelSolutions = () => {
   return (
@@ -14,5 +17,5 @@ export default DevrelSolutions;
 export const Head = () => {
   return <SEO title="Devrel Platform for Cloud Native Content"
     description="Amplify Your Voice in the Cloud Native Community. Build, share, innovate with CNCF project-curated best practices."
-    image="/images/meshery-logo-dark-text.webp" />;
+    image={openGraphImage} />;
 };
