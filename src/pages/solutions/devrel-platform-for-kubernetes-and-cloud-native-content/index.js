32b1f22c246eaa2b634a5be3072bff3477ae394f
Vivek Vishal <vishalvivek488@gmail.com>
2025-05-19 07:49:58 +0530

    properly-pass-expected-static-asset-to-seo-component

--- src/pages/solutions/devrel-platform-for-kubernetes-and-cloud-native-content/index.js
+++ src/pages/solutions/devrel-platform-for-kubernetes-and-cloud-native-content/index.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import SEO from "../../../components/seo";
 import DevrelSolutionsPage from "../../../sections/Devrel-platform";
-// import {openGraphImage} from "../../../sections/Devrel-platform/Devrel-platform.style";
-import { openGraphImage } from "./solutions-devrel.png";
 
 
 const DevrelSolutions = () => {
@@ -17,5 +15,5 @@ export default DevrelSolutions;
 export const Head = () => {
   return <SEO title="Devrel Platform for Cloud Native Content"
     description="Amplify Your Voice in the Cloud Native Community. Build, share, innovate with CNCF project-curated best practices."
-    image={openGraphImage} />;
+    image="/images/solutions-devrel.png" />;
 };
