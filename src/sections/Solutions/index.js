fa246651fd29bb266984ec3d81c785963e6913df
Madina <madinaamankeldinova5@gmail.com>
2025-05-31 02:26:05 +0500

    solutions page created
    Signed-off-by: Madina <madinaamankeldinova5@gmail.com>

---
+++ src/sections/Solutions/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import SolutionsWrapper from "./solutions.style";
+import { Container } from "../../reusecore/Layout";
+
+
+
+const Solutions = (props) => {
+
+  return (
+    <SolutionsWrapper>
+      <div >
+      </div>
+      <>
+      </>
+    </SolutionsWrapper>
+  );
+};
+
+export default Solutions;
