61950b77497ac37f5abb9bea85922c188b450d80
Vivek Vishal <vishalvivek488@gmail.com>
2025-06-17 03:36:06 +0530

    hyperlink button to corresponding plan subscription
    Signed-off-by: Vivek Vishal <vishalvivek488@gmail.com>

--- src/components/PlanCard/index.js
+++ src/components/PlanCard/index.js
@@ -16,6 +16,12 @@ const PlanCard = ({ planData , isYearly }) => {
   if (!planData || !Array.isArray(planData) || planData.length === 0) {
     return <div>No plan data available</div>;
   }
+  const planLink = {
+    "Personal": "https://kanvas.new",
+    "Team Designer": "https://cloud.layer5.io/account/plans/upgrade?plan=49b77ce8-eeea-4ec4-adf3-48979a14b140",
+    "Team Operator": "https://cloud.layer5.io/account/plans/upgrade?plan=f7a5b2b5-b36b-421e-9211-f4dfb204e5b3",
+    "Enterprise": "https://cloud.layer5.io/account/plans/upgrade?plan=ad68ce59-8c5a-42b0-955c-9b2b2f7c98e3"
+  }
 
   return (
     <PlanCardWrapper>
@@ -30,11 +36,11 @@ const PlanCard = ({ planData , isYearly }) => {
 
                 {x.tier === "Team Designer" ? <div className="featured-label">Most Popular</div> : null}
 
-                {x.pricing_coming_soon && (
+                {/* {x.pricing_coming_soon && (
                   <div className="pricing_coming_soon">
                     {x.pricing_coming_soon}
                   </div>
-                )}
+                )} */}
 
                 <h2>{x.tier}</h2>
                 <h5 className="byline">{x.byline}</h5>
@@ -85,8 +91,9 @@ const PlanCard = ({ planData , isYearly }) => {
                       : "price-button-link"
                   }
                   onClick={x.tier === "Enterprise" ? openModal : undefined}
+                  $url={planLink[x.tier] ?? "https://cloud.layer5.io/account/plans"}
                 >
-                  {x.tier === "Enterprise" ? "Contact Sales" : x.button[0]}
+                  {x.tier === "Personal" ? "Join for Free" : "Subscribe"}
                 </Button>
 
                 <h6>{x.byline2}</h6>
