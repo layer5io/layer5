3b0aa034cd0ea3911c67bacccf6dd9bca7f3b24a
Vivek Vishal <vishalvivek488@gmail.com>
2025-07-04 00:34:02 +0530

    complete platform section

---
+++ src/sections/Platform-engineering/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./header";
+import Platform from "./platform";
+
+const PlatformEngineeringSolutionPage = () => {
+  return (
+    <>
+      <Header />
+      <Platform />
+    </>
+  );
+};
+
+export default PlatformEngineeringSolutionPage;
