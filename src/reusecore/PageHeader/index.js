10ff5b9d716aa5050d68c716e822ef66317ad809
Varad Gupta <114755221+vr-varad@users.noreply.github.com>
2025-07-16 20:20:06 +0530

    Fix: Update
    Signed-off-by: Varad Gupta <114755221+vr-varad@users.noreply.github.com>

--- src/reusecore/PageHeader/index.js
+++ src/reusecore/PageHeader/index.js
@@ -20,7 +20,7 @@ const authorField = (author, isSlugAvailable) => {
   );
 };
 
-const PageHeader = ({ category, title, img, feedlink, subtitle, author, thumbnail, superscript, date, noAspectRatio = false }) => {
+const PageHeader = ({ category, title, img, feedlink, subtitle, author, thumbnail, superscript, date }) => {
   let isSlugAvailable = false;
   if (author){
     const validMembers = useStaticQuery(
