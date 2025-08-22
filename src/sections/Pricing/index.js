d3d90d329c78d537e8bb7d5f9139db84b80a988a
Lee Calcote <lee.calcote@layer5.io>
2025-08-22 17:34:25 +0000

    Enhance PricingAddons component by adding setIsYearly prop for better state management
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- src/sections/Pricing/index.js
+++ src/sections/Pricing/index.js
@@ -29,8 +29,8 @@ const Pricing = () => {
       <div className="wrapper">
         <PlanCard planData={options} isYearly={isYearly}/>
       </div>
-      <div style={{ marginTop: "7rem" }}>
-        <PricingAddons isYearly={isYearly}/>
+      <div style={{ marginTop: "7rem", marginBottom: "3rem" }}>
+        <PricingAddons isYearly={isYearly} setIsYearly={setIsYearly}/>
       </div>
       <Comparison />
       <Reviews />
