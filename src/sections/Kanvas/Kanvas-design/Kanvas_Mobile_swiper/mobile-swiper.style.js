68070c6a216f3cac3125ffcd941649af3d814a9e
Lakshay Pareek <hopesalive0291@gmail.com>
2025-09-01 19:03:08 +0530

    Update mobile-swiper.style.js
    Signed-off-by: Lakshay Pareek <hopesalive0291@gmail.com>

--- src/sections/Kanvas/Kanvas-design/Kanvas_Mobile_swiper/mobile-swiper.style.js
+++ src/sections/Kanvas/Kanvas-design/Kanvas_Mobile_swiper/mobile-swiper.style.js
@@ -1,14 +1,11 @@
 import styled from "styled-components";
-
 const SwiperContainer = styled.div`
   @media (min-width: 768px) {
     display: none;
   }
-
   .swiper-slide {
     border-radius: 20px;
   }
-
   .card.one {
     background: linear-gradient(222deg, #077165 2.76%, #1D2827 117.91%);
   }
@@ -24,19 +21,15 @@ const SwiperContainer = styled.div`
   .card.four {
     background: linear-gradient(180deg, #202020 -32.18%, #18433F 52.35%, #0B7E71 106.65%, #00B39F 142.23%);
   }
-
   .card {
     /* box-shadow: rgb(38, 57, 77, 0.5) 0px 20px 30px -10px; */
     /* box-shadow: none; */
-
     /* background: linear-gradient(
     266.65deg,
     #00b39f 40.19%,
     #477e96 74.31%,
     #3c494f 92.89%
-
   ); */
-
     height: 70vh;
     padding: 1rem;
     border-radius: 20px;
@@ -46,7 +39,7 @@ const SwiperContainer = styled.div`
     justify-content: center;
   }
   .card img {
-    height: 45%;
+    height: 50%;
     width: 100%;
     /* margin-left: 1.5rem; */
   }
@@ -69,7 +62,7 @@ const SwiperContainer = styled.div`
       height: 60vh;
     }
     .card img {
-      height: 35%;
+      height: 40%;
       width: auto;
     }
   }
@@ -112,7 +105,7 @@ const SwiperContainer = styled.div`
       padding: 0.5rem;
     }
     .card img {
-      height: 35%;
+      height: 40%;
     }
     p {
       line-height: 19px;
@@ -124,7 +117,7 @@ const SwiperContainer = styled.div`
       padding: 0.7rem;
     }
     .card img {
-      height: 30%;
+      height: 35%;
     }
   }
   @media (max-width: 380px) {
@@ -158,25 +151,20 @@ const SwiperContainer = styled.div`
       line-height: 16px;
     }
   }
-
   margin-top: 10%;
   border-radius: 20px;
   width: 57vw;
   text-align: center;
-
   .swiper {
     width: 100%;
     height: 100%;
   }
-
   h1 {
     text-align: center;
   }
-
   h2 {
     margin: 0 0 1rem 0;
   }
-
   h6 {
     margin: 10px 0 2.5rem 0;
   }
