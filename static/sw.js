edf88467bd46bbbddc7689f4151581127f3af634
SplinterSword <sparshj2003@gmail.com>
2025-09-18 20:34:58 +0530

    Fixed Security Header Problem
    Signed-off-by: SplinterSword <sparshj2003@gmail.com>

--- static/sw.js
+++ static/sw.js
@@ -12,14 +12,14 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     (async () => {
       try {
+        console.log("SW fetch:", event.request.url);
+
         const response = await fetch(event.request);
 
-        // Clone response because body can only be used once
         const responseClone = response.clone();
 
         const newHeaders = new Headers(responseClone.headers);
 
-        // Add security headers
         newHeaders.set("X-Frame-Options", "DENY");
         newHeaders.set("X-Content-Type-Options", "nosniff");
         newHeaders.set("Referrer-Policy", "strict-origin-when-cross-origin");
@@ -35,7 +35,7 @@ self.addEventListener("fetch", (event) => {
         });
       } catch (err) {
         console.error("SW fetch failed:", err);
-        return fetch(event.request); // fallback
+        return fetch(event.request);
       }
     })()
   );
