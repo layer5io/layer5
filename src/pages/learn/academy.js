0575609f35939005432af53738d8053b70ee25e3
Aryan Shah <149894557+ARYANSHAH1567@users.noreply.github.com>
2025-09-19 21:21:28 +0530

    Update academy.js
    Signed-off-by: Aryan Shah <149894557+ARYANSHAH1567@users.noreply.github.com>

--- src/pages/learn/academy.js
+++ src/pages/learn/academy.js
@@ -11,10 +11,10 @@ const AcademySection = () => {
   return (
     <div className="academy-page">
       <HeroSection />
-      <LearnerSection />
-      <EnterpriseSection />
       <AcademyMobileSwiper />
       <AcademyFeatureCarousel />
+      <LearnerSection />
+      <EnterpriseSection />
     </div>
   );
 };
