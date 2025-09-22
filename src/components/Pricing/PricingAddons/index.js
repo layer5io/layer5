7e700d8cda13cdd0f654d4c5958c6097d64d70f7
Chetan 👾 <128224987+ChetanFTW@users.noreply.github.com>
2025-09-22 23:23:43 +0100

    Update index.js
    Signed-off-by: Chetan 👾 <128224987+ChetanFTW@users.noreply.github.com>

--- src/components/Pricing/PricingAddons/index.js
+++ src/components/Pricing/PricingAddons/index.js
@@ -43,7 +43,7 @@ import {
 import { formatAndConvertPrice } from "../../../utils/currencies";
 
 export const PricingAddons = ({ isYearly = false, setIsYearly ,currency,enterprisePlan }) => {
-  
+
   const [selectedAddon, setSelectedAddon] = useState(null);
   const [addonMenuOpen, setAddonMenuOpen] = useState(false);
   const quantity = 1;
