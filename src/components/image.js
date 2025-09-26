933716ce1bad6aa7994502f116b00549ce97502d
Lee Calcote <lee.calcote@layer5.io>
2025-09-26 06:11:40 -0500

    Refactor SVG handling in Image component for clarity
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- src/components/image.js
+++ src/components/image.js
@@ -2,22 +2,23 @@ import React from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
 
 const Image = ({ childImageSharp, extension, publicURL, alt, imgStyle, ...rest }) => {
-
-  if (!childImageSharp && extension === "svg") {
-    return (
-      <div className="old-gatsby-image-wrapper" style={{ width: "100%", height: "auto" }}>
-        <img
-          src={publicURL}
-          alt={alt || "Blog image"}
-          width="100%"
-          height="auto"
-          style={{
-            objectFit: imgStyle?.objectFit || "cover",
-            ...imgStyle
-          }}
-        />
-      </div>
-    );
+  if (extension === "svg") {
+    if (!childImageSharp && extension === "svg") {
+      return (
+        <div className="old-gatsby-image-wrapper" style={{ width: "100%", height: "auto" }}>
+          <img
+            src={publicURL}
+            alt={alt || "Blog image"}
+            width="100%"
+            height="auto"
+            style={{
+              objectFit: imgStyle?.objectFit || "cover",
+              ...imgStyle
+            }}
+          />
+        </div>
+      );
+    }
   }
 
   return <GatsbyImage
