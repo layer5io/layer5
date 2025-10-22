af7a1c5bd4affd3064ed2fd5dcd15f3597485dc0
nandansameerreddy <2400030343@kluniversity.in>
2025-10-22 13:53:53 +0530

    Fix: Add spacing above the heading (#5648)

--- src/components/Features-carousel/index.js
+++ src/components/Features-carousel/index.js
@@ -1,56 +1,10 @@
-import React, { useState } from "react";
-import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox";
-import { Link } from "gatsby";
-import { IoIosArrowRoundForward } from "@react-icons/all-files/io/IoIosArrowRoundForward";
-import { FeaturesWrapper } from "./FeaturesCarousel.style";
-import Slider from "react-slick";
-
-
-const Features = ({ features, heading }) => (
-  <>
-    {/* carousel rendered at smaller breakpoints */}
-    <FeaturesCarousel features={features} heading={heading} />
-    <FeaturesList features={features} />
-  </>
-);
-
-
-const FeaturesList = ({ features }) => {
-  const [activeFeature, setActiveFeature] = useState(0);
-  return (
-    <FeaturesWrapper>
-      <div className="features">
-        <ul className="options">
-          {features.map((feature, stableIdx) => (
-            <Feature
-              id={stableIdx}
-              key={stableIdx}
-              title={feature.title}
-              active={stableIdx === activeFeature}
-              onClick={setActiveFeature}
-              learnMoreLink={feature.learnMoreLink}
-            >
-              {feature.description}
-            </Feature>
-          ))}
-        </ul>
-        <div className="terminal-wrapper">
-          <SimpleReactLightbox>
-            <SRLWrapper>
-              {features[activeFeature].content}
-            </SRLWrapper>
-          </SimpleReactLightbox>
-        </div>
-      </div>
-    </FeaturesWrapper>
-  );
-};
-
 const FeaturesCarousel = ({ features, heading }) => {
   return (
     <FeaturesWrapper>
       <div className="features-carousel">
-        <h2 className="main-heading">{heading ? heading : "Features"}</h2>
+        <h2 className="main-heading" style={{ marginTop: '20px' }}>
+          {heading ? heading : "Features"}
+        </h2>
         <Slider
           autoplay={true}
           autoplaySpeed={3500}
@@ -76,35 +30,3 @@ const FeaturesCarousel = ({ features, heading }) => {
     </FeaturesWrapper>
   );
 };
-
-const Feature = ({ children, title, active, onClick, learnMoreLink, id, Element = "li" }) => {
-  return (
-    <Element className={active ? "feature active-feature" : "feature"}>
-      {onClick ? (
-        <button
-          className="heading"
-          onClick={() => onClick(id)}
-          aria-expanded={active}
-          aria-controls={`feature-${id}`}
-        >
-          {title}
-        </button>
-      ) : (
-<span className="heading mt-6">{title}</span>
-      )}
-      <div className="body" id={`feature-${id}`} >
-        <p>{children}</p>
-        {learnMoreLink && learnMoreLink.startsWith("/")
-          ? <Link className="learn-more-link" to={learnMoreLink}>
-            Explore <IoIosArrowRoundForward />
-          </Link>
-          : <a href={learnMoreLink} className="learn-more-link">
-           Explore <IoIosArrowRoundForward />
-          </a>
-        }
-      </div>
-    </Element>
-  );
-};
-
-export default Features;
