dde4a70cbe7873b3ab7b0f5160b76424553d2869
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-11-30 21:10:29 +0530

    test2
    Signed-off-by: saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>

--- src/components/image.js
+++ src/components/image.js
@@ -2,8 +2,15 @@ import React from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
 
 const Image = ({ childImageSharp, extension, publicURL, alt, imgStyle, ...rest }) => {
+  // Handle null/undefined cases
+  if (!publicURL && !childImageSharp) {
+    return null;
+  }
 
-  if (!childImageSharp && extension === "svg") {
+  // Handle SVG files - check extension or if publicURL ends with .svg
+  const isSvg = extension === "svg" || (publicURL && publicURL.endsWith(".svg"));
+
+  if (!childImageSharp && isSvg && publicURL) {
     return (
       <div className="old-gatsby-image-wrapper" style={{ width: "100%", height: "auto" }}>
         <img
@@ -18,6 +25,11 @@ const Image = ({ childImageSharp, extension, publicURL, alt, imgStyle, ...rest }
         />
       </div>
     );
+  }
+
+  // If no childImageSharp and not SVG, return null or fallback
+  if (!childImageSharp) {
+    return null;
   }
 
   return <GatsbyImage
