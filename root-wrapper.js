79a2c12232828a987d880986b26b90d965095fea
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-12-01 11:40:50 +0530

    update root-wrapper.js for v5
    Signed-off-by: saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { MDXProvider } from "@mdx-js/react";
+import { Link } from "gatsby";
 import Code from "./src/components/CodeBlock";
 import CTA_ImageOnly from "./src/components/Call-To-Actions/CTA_ImageOnly";
 import CTA_FullWidth from "./src/components/Call-To-Actions/CTA_FullWidth";
 import CTA_Bottom from "./src/components/Call-To-Actions/CTA_Bottom";
+import Blockquote from "./src/reusecore/Blockquote";
+import Button from "./src/reusecore/Button";
 import { ContextWrapper } from "./context-wrapper";
 
-// Custom image component for better CLS scores
 const OptimizedImage = props => {
+  const imgSrc = typeof props.src === "object" ? props.src?.publicURL || props.src : props.src;
+
   return (
     <div style={{ width: "100%", height: "auto" }}>
       <img
         {...props}
+        src={imgSrc}
         width={props.width || "100%"}
         height={props.height || "auto"}
         style={{
@@ -26,21 +31,32 @@ const OptimizedImage = props => {
   );
 };
 
+const PreComponent = (props) => {
+  const { children } = props;
+
+  if (children && children.props) {
+    const { className, children: codeString } = children.props;
+    const language = className ? className.replace("language-", "") : "";
+
+    return (
+      <Code
+        codeString={typeof codeString === "string" ? codeString.trim() : ""}
+        language={language}
+        {...children.props}
+      />
+    );
+  }
+
+  return <pre {...props} />;
+};
+
 const components = {
-  pre: ({ children: { props } }) => {
-    if (props.mdxType === "code") {
-      return (
-        <Code
-          codeString={props.children.trim()}
-          language={
-            props.className && props.className.replace("language-", "")
-          }
-          {...props}
-        />
-      );
-    }
-  },
+  pre: PreComponent,
   img: OptimizedImage,
+  Link,
+  Blockquote,
+  BlockQuote: Blockquote,
+  Button,
   CTA_ImageOnly,
   CTA_FullWidth,
   CTA_Bottom
