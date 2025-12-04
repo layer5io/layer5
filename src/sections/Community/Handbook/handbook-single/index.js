51d96c673a867bec497a66d0646bbede49064892
Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>
2025-12-04 13:59:08 +0530

    fix intra page
    Signed-off-by: Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>

--- src/sections/Community/Handbook/handbook-single/index.js
+++ src/sections/Community/Handbook/handbook-single/index.js
@@ -8,6 +8,22 @@ const HandbookSingle = ({ data, children }) => {
   const { mdx, allHandbookPages } = data;
   const currentSlug = mdx.fields.slug;
 
+  // Extract IntraPage component from children if present
+  let contentChildren = children;
+  let intraPageComponent = null;
+
+  if (React.Children.count(children) > 0) {
+    const childArray = React.Children.toArray(children);
+    const intraPageIndex = childArray.findIndex(
+      (child) => child?.type?.name === "IntraPage"
+    );
+
+    if (intraPageIndex !== -1) {
+      intraPageComponent = childArray[intraPageIndex];
+      contentChildren = childArray.filter((_, index) => index !== intraPageIndex);
+    }
+  }
+
   return (
     <HandbookWrapper>
       <div className="page-header-section">
@@ -17,13 +33,14 @@ const HandbookSingle = ({ data, children }) => {
       <div className="page-section">
         <Container>
           <div className="content">
-            {children}
+            {contentChildren}
           </div>
           <HandbookPagination
             pages={allHandbookPages.nodes}
             currentSlug={currentSlug}
           />
         </Container>
+        {intraPageComponent}
       </div>
     </HandbookWrapper>
   );
