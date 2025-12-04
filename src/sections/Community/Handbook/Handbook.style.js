3f2b62eccc116c114f78b2524c582bcb042624ab
Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>
2025-12-04 21:47:46 +0530

    handbook margin fix
    Signed-off-by: Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>

--- src/sections/Community/Handbook/Handbook.style.js
+++ src/sections/Community/Handbook/Handbook.style.js
@@ -41,13 +41,13 @@ export const HandbookWrapper = styled.div`
       flex-direction: row;    
       align-items: flex-start;
       @media (min-width: 751px){
-        margin-top: -54rem;
+        margin-top: -58rem;
         position: relative;
         margin-left: 20rem;
         padding: 2rem 3rem;
       }
       @media (max-width: 750px) {
-        margin-top: -4rem;
+        margin-top: -5rem;
         margin-left: 2rem;
       }
     }
