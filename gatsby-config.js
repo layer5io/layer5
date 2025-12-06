65740278849e5ca5c8f3be05adf467227c156ee7
Lee Calcote <leecalcote@gmail.com>
2025-12-05 19:56:50 -0600

    Revert "fix workflows and improve logic"

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 
 const isProduction = process.env.NODE_ENV === "production";
-const isFullSiteBuild = process.env.BUILD_FULL_SITE === "true";
+const isFullSiteBuild = process.env.BUILD_FULL_SITE !== "false";
 const HEAVY_COLLECTIONS = ["members", "integrations"];
 const collectionIgnoreGlobs = isFullSiteBuild
   ? []
