1ce9f4b9426b720860c7d177717734323b68952c
kanishksingh23 <kanishksingh763@gmail.com>
2026-05-02 10:00:00 +0000

    perf: improve home page performance
    Signed-off-by: kanishksingh23 <kanishksingh763@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,6 +64,9 @@ module.exports = {
       resolve: "gatsby-plugin-netlify",
       options: {
         headers: {
+          "/static/*": ["Cache-Control: public, max-age=31536000, immutable"],
+          "/*.js": ["Cache-Control: public, max-age=31536000, immutable"],
+          "/*.css": ["Cache-Control: public, max-age=31536000, immutable"],
           "/*": [
             "X-Frame-Options: SAMEORIGIN",
             "Content-Security-Policy: frame-ancestors 'self'",
@@ -480,7 +483,9 @@ module.exports = {
           {
             resolve: "gatsby-plugin-purgecss",
             options: {
-              printRejected: true,
+              printRejected: false,
+              develop: false,
+              purgeOnly: ["src/"],
             },
           },
         ]
