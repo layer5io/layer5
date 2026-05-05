1fb496124d319d6690e85a1fb530126f685a3111
kanishksingh23 <kanishksingh763@gmail.com>
2026-05-05 18:59:10 +0000

    fix: use public URL paths for static images
    Signed-off-by: kanishksingh23 <kanishksingh763@gmail.com>

--- src/sections/Kanvas/Kanvas-collaborate/collaboration-feature-team.js
+++ src/sections/Kanvas/Kanvas-collaborate/collaboration-feature-team.js
@@ -4,10 +4,10 @@ import { ReactComponent as CollaborationImg } from "./../FeaturesSection/Collabo
 import { useInView } from "react-intersection-observer";
 import { useState } from "react";
 
-import ConversationImg from "../../../../static/images/kanvas-collaborate/conversation.png";
-import ComponentsGif from "../../../../static/images/kanvas-collaborate/components.gif";
-import ActivityLogsImg from "../../../../static/images/kanvas-collaborate/activity-logs.png";
-import UndoRedoImg from "../../../../static/images/kanvas-collaborate/undo-redo.png";
+const ConversationImg = "/images/kanvas-collaborate/conversation.png";
+const ComponentsGif = "/images/kanvas-collaborate/components.gif";
+const ActivityLogsImg = "/images/kanvas-collaborate/activity-logs.png";
+const UndoRedoImg = "/images/kanvas-collaborate/undo-redo.png";
 
 const CollaborationFeatureWrapper = styled.div`
     display: flex;
