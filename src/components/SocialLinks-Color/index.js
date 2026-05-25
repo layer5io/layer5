c5a51c42141385212644685097e7d384e74ef35e
Maanvi Chetwani <maanvichetwani21@gmail.com>
2026-05-25 21:57:57 +0100

    reduce tooltip size
    Signed-off-by: Maanvi Chetwani <maanvichetwani21@gmail.com>

--- src/components/SocialLinks-Color/index.js
+++ src/components/SocialLinks-Color/index.js
@@ -12,26 +12,17 @@ import bluesky_icon from "../../assets/images/socialIcons/bluesky.svg";
 import { CustomTooltip } from "@sistent/sistent";
 import SocialLinksWrapper from "./sociallinkscolor.style";
 
-const tooltipStyles = {
-  tooltip: {
-    sx: {
-      fontSize: "0.65rem",
-      padding: "3px 6px",
-    },
-  },
-};
-
 const SocialLinksColor = () => {
   return (
     <SocialLinksWrapper>
       <Col $xs={12}>
         <Row className="social_icons">
-          <CustomTooltip title="Layer5 Discussion Forum" placement="top" componentsProps={tooltipStyles}>
+          <CustomTooltip title="Layer5 Discussion Forum" placement="top" variant="small">
             <a href="https://discuss.layer5.io" target="_blank" rel="noreferrer">
               <img height="30px" src={forum_icon} alt="forum" />
             </a>
           </CustomTooltip>
-          <CustomTooltip title="Email Layer5 Community" placement="top" componentsProps={tooltipStyles}>
+          <CustomTooltip title="Email Layer5 Community" placement="top" variant="small">
             <a className="mail_icon"
               href="mailto:community@layer5.io"
               target="_blank"
@@ -40,12 +31,12 @@ const SocialLinksColor = () => {
               <img height="30px" src={mail_icon} alt="mail" />
             </a>
           </CustomTooltip>
-          <CustomTooltip title="Join Layer5 on Slack" placement="top" componentsProps={tooltipStyles}>
+          <CustomTooltip title="Join Layer5 on Slack" placement="top" variant="small">
             <a href="https://slack.layer5.io/" target="_blank" rel="noreferrer">
               <img className="slack" height="30px" src={slack_icon} alt="slack" />
             </a>
           </CustomTooltip>
-          <CustomTooltip title="Follow Layer5 on X (Twitter)" placement="top" componentsProps={tooltipStyles}>
+          <CustomTooltip title="Follow Layer5 on X (Twitter)" placement="top" variant="small">
             <a
               href="https://x.com/layer5"
               target="_blank"
@@ -55,7 +46,7 @@ const SocialLinksColor = () => {
               <TwitterIcon />
             </a>
           </CustomTooltip>
-          <CustomTooltip title="Follow Layer5 on Bluesky" placement="top" componentsProps={tooltipStyles}>
+          <CustomTooltip title="Follow Layer5 on Bluesky" placement="top" variant="small">
             <a
               className="bluesky_icon"
               href="https://bsky.app/profile/layer5.bsky.social"
@@ -70,7 +61,7 @@ const SocialLinksColor = () => {
               />
             </a>
           </CustomTooltip>
-          <CustomTooltip title="Layer5 on GitHub" placement="top" componentsProps={tooltipStyles}>
+          <CustomTooltip title="Layer5 on GitHub" placement="top" variant="small">
             <a
               href="https://github.com/layer5io"
               target="_blank"
@@ -80,7 +71,7 @@ const SocialLinksColor = () => {
               <img height="30px" src={github_icon} alt="github" />
             </a>
           </CustomTooltip>
-          <CustomTooltip title="Layer5 on LinkedIn" placement="top" componentsProps={tooltipStyles}>
+          <CustomTooltip title="Layer5 on LinkedIn" placement="top" variant="small">
             <a
               href="https://www.linkedin.com/company/layer5"
               target="_blank"
@@ -94,7 +85,7 @@ const SocialLinksColor = () => {
               />
             </a>
           </CustomTooltip>
-          <CustomTooltip title="Layer5 on YouTube" placement="top" componentsProps={tooltipStyles}>
+          <CustomTooltip title="Layer5 on YouTube" placement="top" variant="small">
             <a
               className="youtube_icon"
               href="https://youtube.com/Layer5io?sub_confirmation=1"
@@ -109,7 +100,7 @@ const SocialLinksColor = () => {
               />
             </a>
           </CustomTooltip>
-          <CustomTooltip title="Layer5 on Docker Hub" placement="top" componentsProps={tooltipStyles}>
+          <CustomTooltip title="Layer5 on Docker Hub" placement="top" variant="small">
             <a
               className="docker_icon"
               href="https://hub.docker.com/u/layer5/"
