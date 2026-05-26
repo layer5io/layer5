a3565b3a175b63bd16869d63f97a3b8aeea9dea9
kannan-ms <kannanmayilsamy04@gmail.com>
2026-05-26 11:11:23 +0530

    fix: resolve layout issues on careers page
    Signed-off-by: kannan-ms <kannanmayilsamy04@gmail.com>

--- src/sections/Careers/careers.style.js
+++ src/sections/Careers/careers.style.js
@@ -1,79 +1,77 @@
 import styled from "styled-components";
 
 const CareersSectionWrapper = styled.div`
-p {
-    color: ${props => props.theme.whiteEightToBlack};
+  p {
+    color: ${(props) => props.theme.whiteEightToBlack};
     transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-}
-    h1{
-        color: ${props => props.theme.text};
-        transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-    }
-    .page-header{
-        margin:4rem auto;
-    }
-    .centerTexts{
-        margin-top: -3rem;
-        text-align: center;
-    }
-    .center{
-        text-align: center;
-    }
+  }
+  h1 {
+    color: ${(props) => props.theme.text};
+    transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+  }
+  .page-header {
+    margin: 4rem auto;
+  }
+  .centerTexts {
+    margin-top: -3rem;
+    text-align: center;
+  }
+  .center {
+    text-align: center;
+  }
 
+  .videoText {
+    flex-wrap: nowrap;
+    margin: auto;
+    margin-top: 5rem;
+    display: flex;
+    align-items: flex-start;
+    gap: 2rem;
+  }
+  .introText {
+    margin-top: 0;
+  }
+  .introText h1 {
+    margin-bottom: 1rem;
+  }
+  .introText p {
+    text-align: justify;
+    color: ${(props) => props.theme.whiteEightToBlack};
+    transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
+  }
+  .introVideo {
+    width: 50%;
+    margin: auto;
+    text-align: center;
+
+    iframe {
+      width: 26rem;
+    }
+  }
+  @media only screen and (max-width: 996px) {
     .videoText {
-        flex-wrap: nowrap;
-        margin: auto;
-        margin-top: 1rem;
-        display: flex;
-        align-items: center;
-        gap: 2rem;
+      flex-wrap: wrap;
     }
-    .introText{
-        margin-top: 10rem;
+    .introText {
+      width: 100%;
     }
-    .introText h1{
-        margin-bottom: 1rem;
+    .introVideo {
+      width: 100%;
+      margin: auto;
+      text-align: center;
     }
-    .introText p {
-        text-align: justify;
-        color: ${props => props.theme.whiteEightToBlack};
-        transition: 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-    }
-    .introVideo{
-        width: 50%;
-        margin: auto;
-        text-align: center;
+  }
 
-        iframe{
-            width: 26rem;
-        }
+  @media only screen and (max-width: 420px) {
+    .introVideo {
+      iframe {
+        width: 20rem;
+      }
     }
-    @media only screen and (max-width:996px){
-        .videoText{
-            flex-wrap: wrap;
-        }
-        .introText{
-            width: 100%;
-        }
-        .introVideo{
-            width: 100%;
-            margin: auto;
-            text-align: center;
-        }
-    }
+  }
 
-    @media only screen and (max-width:420px){
-        .introVideo{
-            iframe{
-                width: 20rem;
-            }
-        }
-    }
-
-    .opportunity-section {
- 
-    }
-    
+  .opportunity-section {
+  }
 `;
 
 export default CareersSectionWrapper;
