d398335d9340dad2cf3e5de2f6c8d5bb9b277607
11yashiagrawal <11yashiagrawal@gmail.com>
2026-06-09 21:54:28 +0530

    Remove LearnInfo component and its styles from LearnPathsPage
    Signed-off-by: 11yashiagrawal <11yashiagrawal@gmail.com>

--- src/sections/Learn-Layer5/index.js
+++ src/sections/Learn-Layer5/index.js
@@ -1,54 +1,13 @@
 import React from "react";
 import { LearnLayer5GridWrapper } from "./learnlayer5.style";
-import { graphql, useStaticQuery } from "gatsby";
 import PageHeader from "../../reusecore/PageHeader";
 import { Container } from "../../reusecore/Layout";
-import LearnInfo from "../../components/Learn-Components/LearnInfo";
 import WhatAwaitsSection from "../../components/Learn-Components/what-await-section";
 import Book_Giveaway from "../../assets/images/learn/istio-book-giveaway.webp";
 import JoinCommunity from "../Community/Join-community";
 import CanvasIllustration from "../../components/Learn-Components/Canvas-Illustration";
 
 const LearnPathsPage = () => {
-  const data = useStaticQuery(graphql`
-    query allLearnPath {
-      coursesCount: allMdx(
-        filter: { fields: { collection: { eq: "content-learn" } } }
-      ) {
-        edges {
-          node {
-            fields {
-              course
-            }
-          }
-        }
-      }
-      chaptersCount: allMdx(
-        filter: { fields: { collection: { eq: "content-learn" } } }
-      ) {
-        edges {
-          node {
-            fields {
-              chapter
-            }
-          }
-        }
-      }
-      meshesCount: allMdx(
-        filter: { fields: { collection: { eq: "content-learn" } } }
-      ) {
-        edges {
-          node {
-            fields {
-              section
-              pageType
-            }
-          }
-        }
-      }
-    }
-  `);
-
   return (
     <LearnLayer5GridWrapper>
       <Container>
@@ -60,11 +19,6 @@ const LearnPathsPage = () => {
         <CanvasIllustration />
       </Container>
       <Container>
-        <LearnInfo
-          courses={data.coursesCount.edges}
-          chapters={data.chaptersCount.edges}
-          meshes={data.meshesCount.edges}
-        />
         <WhatAwaitsSection />
         <JoinCommunity
           image={Book_Giveaway}
