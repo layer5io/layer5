f0c1cc05d5d869533c67d63073ff2d2e619ed9e2
𝙰𝚁𝚈𝙰 𝚁𝙲𝙱 <248160976+superezzdev@users.noreply.github.com>
2026-07-29 19:19:43 +0000

    fix(styles): revert lint formatting and preserve original tab structure
    Signed-off-by: GitHub <noreply@github.com>

--- src/sections/Meshery/Features-section/features-section.style.js
+++ src/sections/Meshery/Features-section/features-section.style.js
@@ -2,14 +2,13 @@ import styled from "styled-components";
 
 const FeaturesSectionWrapper = styled.section`
   background-color: black;
-  padding: 4rem 0 5rem 0;
+  padding-bottom: 5rem;
   margin-bottom: 6rem;
   position: relative;
   width: 100%;
   max-width: 100%;
   z-index: 1;
   height: auto;
-
   .skw {
     display: grid;
     position: relative;
@@ -21,37 +20,38 @@ const FeaturesSectionWrapper = styled.section`
     grid-column: 1 / span 2;
     height: 4rem;
     grid-row: 1;
+    position: relative;
+    top: 0.5px;
   }
   .rect-2-left {
     background: ${(props) => props.theme.secondaryColor};
     border-radius: 0 0 3.5rem 0;
-    grid-column: 1 / 2;
+    grid-column: 1/2;
     height: 4rem;
     grid-row: 2;
   }
   .rect-3-right {
     background: ${(props) => props.theme.secondaryColor};
     border-radius: 3.5rem 0 0 0;
-    grid-column: 10 / 11;
+    grid-column: 10/11;
     grid-row: 1;
     height: 4rem;
   }
   .rect-4-right {
     background: #c9fcf6;
     border-radius: 0 0 0 2.125rem;
-    grid-column: 9 / 11;
+    grid-column: 9/11;
     grid-row: 2;
     height: 4rem;
+    position: relative;
+    top: -0.75px;
   }
 
   .section-data {
-    padding: 0 2rem 3rem;
+    padding: 0 1rem 3rem;
     text-align: center;
     position: relative;
     z-index: 1;
-    width: 100%;
-    margin: 0 auto;
-
     h1,
     h2,
     p {
@@ -64,9 +64,8 @@ const FeaturesSectionWrapper = styled.section`
       max-height: 8rem;
     }
   }
-
   .section-header {
-    margin-top: 1rem;
+    margin-top: 3rem;
     h1 {
       line-height: 3.125rem;
       span {
@@ -78,29 +77,23 @@ const FeaturesSectionWrapper = styled.section`
     }
     p {
       font-size: 1.25rem;
-      line-height: 1.6rem;
-      margin: 1rem auto 0;
-      max-width: 50rem;
+      line-height: 1.25rem;
+      margin: 1rem 0 0;
     }
   }
-
   .smp-section {
     margin: 3rem auto;
-    width: 100%;
-    max-width: 1200px;
+    max-width: 85%;
+    overflow: hidden;
 
     .smp-section-row {
       margin: 4rem 0;
       align-items: center;
-      display: flex;
-      flex-wrap: wrap;
     }
-
     h2,
     h4 {
       color: white;
     }
-
     .smp-section-data {
       h1,
       h2,
@@ -115,50 +108,66 @@ const FeaturesSectionWrapper = styled.section`
     }
 
     .smp-section-carousel {
-      width: 100%;
-
       .slick-slider {
         position: relative;
-        display: block;
-        box-sizing: border-box;
+        max-height: 520px;
 
         .slick-list {
-          width: 100%;
-          margin: 0 auto;
+          max-width: calc(100% - 6rem);
+          max-height: 520px;
+          height: auto;
+
+          .slick-track {
+            height: auto;
+          }
 
           .slick-slide {
+            height: auto;
             img {
-              max-height: 22rem;
-              width: auto;
+              max-height: 20rem;
+              width: 100%;
+              object-fit: contain;
+              aspect-ratio: 16 / 9;
               margin: auto;
-              border-radius: 0.5rem;
             }
           }
         }
 
         .slick-arrow {
-          display: none !important;
+          display: none;
         }
 
         .slick-dots {
           position: absolute;
-          right: -2rem;
+          right: 0;
           top: 50%;
           transform: translateY(-50%);
           width: 5rem;
           list-style: none;
+          margin: 0;
+          padding: 0;
 
           li {
             display: block;
             width: 100%;
-            margin: 0.75rem 0;
-            opacity: 0.5;
-            transition: opacity 0.3s ease;
+            height: auto;
+            margin: 1rem auto;
+            opacity: 0.6;
 
-            p img {
-              height: 3.5rem;
-              width: auto;
-              border-radius: 0.25rem;
+            &:focus-visible,
+            button:focus-visible {
+              outline: 2px solid ${(props) => props.theme.secondaryColor};
+              outline-offset: 2px;
+              border-radius: 4px;
+            }
+
+            p {
+              display: block;
+
+              img {
+                height: 5rem;
+                vertical-align: middle;
+              }
             }
           }
 
@@ -170,52 +179,156 @@ const FeaturesSectionWrapper = styled.section`
     }
   }
 
+  @media screen and (max-width: 1600px) {
+    .smp-section .smp-section-carousel .slick-slider {
+      .slick-list {
+        max-width: 38rem;
+      }
+
+      .slick-dots {
+        left: 40rem;
+        width: 6rem;
+
+        li p img {
+          height: 4rem;
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 1200px) {
+    padding-bottom: 2rem;
+    margin-bottom: 6rem;
+
+    .smp-section {
+      .smp-section-row {
+        margin: 2.5rem 0;
+      }
+
+      .smp-section-data {
+        h3,
+        p {
+          text-align: center;
+        }
+      }
+
+      .smp-section-carousel .slick-slider {
+        .slick-list {
+          max-width: 80%;
+          margin: 2rem auto;
+
+          .slick-slide img {
+            max-height: 18rem;
+          }
+        }
+
+        .slick-dots {
+          position: relative;
+          left: 0;
+          right: auto;
+          top: auto;
+          transform: none;
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          gap: 1rem;
+
+          li {
+            width: auto;
+            margin: 0;
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 960px) {
+    padding-bottom: 0rem;
+    margin-bottom: 4rem;
+    .smp-section .smp-section-carousel .slick-slider .slick-list {
+      .slick-slide {
+        img {
+          max-height: 16rem;
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 700px) {
+    .smp-section {
+      max-width: 95%;
+
+      .smp-section-carousel .slick-slider {
+        .slick-list {
+          max-width: 85%;
+          margin: 2rem 0;
+        }
+        .slick-dots {
+          left: 90%;
+          width: 4rem;
+
+          li p img {
+            height: 2.5rem;
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 500px) {
+    .smp-section {
+      max-width: 95%;
+
+      .smp-section-carousel .slick-slider {
+        .slick-list {
+          max-width: 80%;
+          .slick-slide {
+            img {
+              max-height: 10rem;
+            }
+          }
+        }
+        .slick-dots {
+          left: 85%;
+          top: -1rem;
+        }
+      }
+    }
+  }
+
   .mesh-mngmnt {
-    margin-top: 4rem;
     h3 {
       margin: 1rem 0;
-      color: rgba(255, 255, 255, 0.7);
+      color: rgba(255, 255, 255, 0.6);
       font-size: 1.5rem;
     }
     p {
       margin: 0.5rem 0;
-      color: rgba(255, 255, 255, 0.7);
+      color: rgba(255, 255, 255, 0.6);
     }
 
     .mesh-mngmnt-btn {
       margin: 2rem auto;
       display: flex;
-      justify-content: center;
       flex-wrap: wrap;
-      gap: 1rem;
-      max-width: 100%;
+      max-width: 75%;
 
       .mgmt_button {
-        min-width: 12rem;
+        margin: 0.5rem 0;
+        min-width: 15rem;
         border-radius: 1rem;
       }
     }
   }
 
   @media screen and (max-width: 992px) {
-    .smp-section {
-      .smp-section-carousel {
-        .slick-slider {
-          .slick-dots {
-            position: relative;
-            right: auto;
-            top: auto;
-            transform: none;
-            width: 100%;
-            display: flex !important;
-            justify-content: center;
-            gap: 1rem;
-            margin-top: 1rem;
+    .mesh-mngmnt {
+      .mesh-mngmnt-btn {
+        justify-content: center;
 
-            li p img {
-              height: 2.5rem;
-            }
-          }
+        .mgmt_button {
+          margin: 0.5rem auto;
+          min-width: 11rem;
         }
       }
     }
