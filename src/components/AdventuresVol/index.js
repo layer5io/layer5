3328c04f3b4a8cae757b786d74f8b741a18a9d08
Piyush Tripathi <piyushtripathi9424@gmail.com>
2026-09-19 01:41:15 +0530

    fix: correct layout shift on Meet Five and Handbook cards
    Signed-off-by: Piyush Tripathi <piyushtripathi9424@gmail.com>

--- src/components/AdventuresVol/index.js
+++ src/components/AdventuresVol/index.js
@@ -1,27 +1,50 @@
 import React from "react";
+import { Link } from "gatsby";
 import { HiOutlineChevronRight } from "@react-icons/all-files/hi/HiOutlineChevronRight";
 import { AdventuresVolWrapper } from "./adventures-vol.style";
 
 const AdventuresVol = ({ title, description, to, image }) => {
+  const isExternalOrStatic =
+    to &&
+    (to.startsWith("http://") ||
+      to.startsWith("https://") ||
+      to.endsWith(".pdf") ||
+      to.startsWith("mailto:"));
+
+  const cardContent = (
+    <div className="handbook__card">
+      <h1 className="handbook__card--head">{title}</h1>
+      <div className="handbook__card--line"></div>
+      <img src={image} alt={title} />
+      <p className="handbook__card--para">{description}</p>
+      {to && (
+        <div className="handbook__card--lm__container">
+          <span className="handbook__card--lm">
+            Read More <HiOutlineChevronRight />
+          </span>
+        </div>
+      )}
+    </div>
+  );
+
   return (
     <AdventuresVolWrapper>
-      <a href={to} className="handbook__card--main">
-        <div className="handbook__card">
-          <h1 className="handbook__card--head">{title}</h1>
-          <div className="handbook__card--line"></div>
-          <img src={image} />
-          <p className="handbook__card--para">
-            {description}
-          </p>
-          {to && (
-            <div className="handbook__card--lm__container">
-              <a href={to} className="handbook__card--lm">
-                Read More <HiOutlineChevronRight />
-              </a>
-            </div>
-          )}
-        </div>
-      </a>
+      {!to ? (
+        <div className="handbook__card--main">{cardContent}</div>
+      ) : isExternalOrStatic ? (
+        <a
+          href={to}
+          className="handbook__card--main"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {cardContent}
+        </a>
+      ) : (
+        <Link to={to} className="handbook__card--main">
+          {cardContent}
+        </Link>
+      )}
     </AdventuresVolWrapper>
   );
 };
