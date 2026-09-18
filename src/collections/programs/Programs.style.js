90d5b9378c5f927020b47e70a5e884edee3aeb0b
Amarachi Ezeonyekwere <203523295+Amarachi-Ezeonyekwere@users.noreply.github.com>
2026-09-18 11:13:20 +0100

    fix: center SCA logo on mobile
    Signed-off-by: Amarachi Ezeonyekwere <203523295+Amarachi-Ezeonyekwere@users.noreply.github.com>

--- src/collections/programs/Programs.style.js
+++ src/collections/programs/Programs.style.js
@@ -117,7 +117,7 @@ export const ProgramsWrapper = styled.div`
       display: block;
       float: none;
       width: 35vw;
-      margin: 0 auto 30px auto;
+      margin: 0 auto 40px auto !important;
     }
   }
   .lfx_logo {
