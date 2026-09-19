f2cbdc5ea1718572db4ad1219c3b4b44dd5f8119
Tanav Poswal <tanavposwal939@gmail.com>
2026-09-19 17:34:46 +0530

    fix: make integerations logos responsive on mobile

--- src/sections/Meshery/Meshery-integrations/Honeycomb/Honeycomb.style.js
+++ src/sections/Meshery/Meshery-integrations/Honeycomb/Honeycomb.style.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const HoneycombGrid = styled.div`
+  width: 100%;
+
   display: flex;
   text-align: left;
   ul {
@@ -28,5 +30,31 @@ export const HoneycombGrid = styled.div`
       margin-bottom: calc(3px - 150px * 0.2885);
       filter: drop-shadow(rgba(0, 0, 0, 0.1) 0px 4px 20px);
     }
-   
+
+  /* Use a normal wrapping layout on small screens. The float/shape-outside
+     layout needs fixed desktop dimensions and can otherwise overlap the next
+     section when the viewport becomes narrow. */
+  @media (max-width: 500px) {
+    height: auto !important;
+
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      height: auto;
+      margin: 2.5rem 0 0;
+      padding: 0;
+
+      &:before {
+        display: none;
+      }
+    }
+
+    li {
+      width: 100px;
+      height: 115.47px;
+      margin: 3px;
+      margin-bottom: 3px;
+    }
+  }
 `;
