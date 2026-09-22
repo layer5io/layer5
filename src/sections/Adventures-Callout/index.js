9b793db38a2f26a5e7e0b7f6b502081e51cb5a16
Parth Gartan <parthgartan26feb@gmail.com>
2026-09-22 09:10:43 +0000

    fix(adventures): use transient props on Col in AdventuresCallout
    Signed-off-by: Parth Gartan <parthgartan26feb@gmail.com>

--- src/sections/Adventures-Callout/index.js
+++ src/sections/Adventures-Callout/index.js
@@ -11,7 +11,7 @@ const AdventuresCallout = () => {
     <AdventuresWrapper>
       <div className="explain">
         <div className="cards">
-          <Col lg={12} md={12} sm={12}>
+          <Col $lg={12} $md={12} $sm={12}>
             <a
               target="_blank"
               href="/community/adventures-of-five-and-friends"
