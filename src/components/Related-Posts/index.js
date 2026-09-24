966519dab48206aa3140dba3b613031d7e5e3519
Piyush Tripathi <piyushtripathi9424@gmail.com>
2026-09-24 23:16:31 +0530

    fix: disable carousel arrows on mobile for Related Posts
    Signed-off-by: Piyush Tripathi <piyushtripathi9424@gmail.com>

--- src/components/Related-Posts/index.js
+++ src/components/Related-Posts/index.js
@@ -53,7 +53,7 @@ const RelatedPosts = props => {
 
   const settings = {
     dots: slidesToShowState < 2,
-    arrows: true,
+    arrows: slidesToShowState >= 2,
     infinite: false,
     speed: 500,
     slidesToShow: slidesToShowState,
